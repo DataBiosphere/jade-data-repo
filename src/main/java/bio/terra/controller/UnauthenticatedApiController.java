1e7ba6b0ef9e1a75c2dee0d2355ca7e67036d96e
ddietterich <ddietterich@verily.com>
2018-11-21 16:22:49 -0500

    Implement an always-OK status

--- src/main/java/bio/terra/controller/UnauthenticatedApiController.java
+++ src/main/java/bio/terra/controller/UnauthenticatedApiController.java
@@ -1,7 +1,13 @@
 package bio.terra.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
 @javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2018-11-21T14:51:14.798-05:00")
@@ -13,7 +19,7 @@ public class UnauthenticatedApiController implements UnauthenticatedApi {
 
     private final HttpServletRequest request;
 
-    @org.springframework.beans.factory.annotation.Autowired
+    @Autowired
     public UnauthenticatedApiController(ObjectMapper objectMapper, HttpServletRequest request) {
         this.objectMapper = objectMapper;
         this.request = request;
@@ -27,6 +33,13 @@ public class UnauthenticatedApiController implements UnauthenticatedApi {
     @Override
     public Optional<HttpServletRequest> getRequest() {
         return Optional.ofNullable(request);
+    }
+
+    @Override
+    @RequestMapping(value = "/status",
+            method = RequestMethod.GET)
+    public ResponseEntity<Void> serviceStatus() {
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }

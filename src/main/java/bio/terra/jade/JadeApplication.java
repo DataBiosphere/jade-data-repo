117aceba2cee0f55cb324f6cceb2189fb766700b
Andrea Haessly <ahaessly@broadinstitute.org>
2018-11-26 18:39:38 -0500

    initial liquibase

---
+++ src/main/java/bio/terra/jade/JadeApplication.java
@@ -0,0 +1,12 @@
+package bio.terra.jade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JadeApplication.class, args);
+	}
+}

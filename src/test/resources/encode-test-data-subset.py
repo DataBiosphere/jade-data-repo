3fbef1902d9eedd618a166db3ab074f6c9ddde1e
rori <aurora@wm41b-664.broadinstitute.org>
2018-11-29 09:51:55 -0500

    split the python code into two runnable scripts

--- src/test/resources/encode-test-data-subset.py
+++ src/test/resources/encode-test-data-subset.py
@@ -1,33 +1,14 @@
 #!/usr/bin/env python
 
-# This script grabs the full ~ 300 donors and gets up to 100 files for each
-# it outputs one file with the truncated file list
-dataDonors = open('encode-data-donors.json')
-donorRows = dataDonors.readlines()
-testDataFiles = open('encode-test-data-files.json', 'w+')
+# ASSUMPTIONS:
+# encode data has been pulled down and is stored locally
 
-for donorRow in donorRows:
-    donorId = donorRow[20: 31]
-    dataFiles = open('encode-data-files.json')
-    total = 0
-    for line in dataFiles:
-        if donorId in line:
-            total += 1
-            testDataFiles.write(line)
-            if total > 99:
-                break
-    print donorId, total
-    dataFiles.close()
-
-dataDonors.close()
-testDataFiles.close()
-
-
-# This script grabs only the ~100 donors that have less than 500 files
+maxFiles = 500;
+# This script grabs only the subset of donors that have less than maxFiles files
 # it outputs two files. The first with the subset of donors and the second with all the files for the subset of donors
 dataDonors = open('encode-data-donors.json')
 donorRows = dataDonors.readlines()
-testDataDonors = open('encode-test-data-donors.json', 'w+')
+testDataDonors = open('encode-test-subset-data-donors.json', 'w+')
 
 for donorRow in donorRows: # first we will collect the subset of donors
     donorId = donorRow[20: 31]
@@ -37,7 +18,7 @@ for donorRow in donorRows: # first we will collect the subset of donors
     for line in dataFiles:
         if donorId in line:
             total += 1
-            if total > 500:
+            if total > maxFiles:
                 testDonor = ''
                 break
     testDataDonors.write(testDonor)
@@ -47,9 +28,9 @@ for donorRow in donorRows: # first we will collect the subset of donors
 dataDonors.close()
 testDataDonors.close()
 
-testDataDonors = open('encode-test-data-donors.json') # then using the subset of donors, we can get the full set of files for each
+testDataDonors = open('encode-test-subset-data-donors.json') # then using the subset of donors, we can get the full set of files for each
 testDonorRows = testDataDonors.readlines()
-testDataFiles = open('encode-test-data-files.json', 'w+')
+testDataFiles = open('encode-test-subset-data-files.json', 'w+')
 
 for testDonorRow in testDonorRows:
     donorId = testDonorRow[20: 31]

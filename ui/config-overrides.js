37b227f3234a9bb425871847e020f04dd189f0ee
rori <aurora@wm41b-664.broadinstitute.org>
2019-01-22 12:55:47 -0500

    add proxy to config

--- ui/config-overrides.js
+++ ui/config-overrides.js
@@ -7,7 +7,7 @@ module.exports = {
   // The function to use to create a webpack dev server configuration when running the development
   // server with 'npm run start' or 'yarn start'.
   // Example: set the dev server to use a specific certificate in https.
-  devServer: function(configFunction) {
+    devServer: function(configFunction) {
     // Return the replacement function for create-react-app to use to generate the Webpack
     // Development Server config. "configFunction" is the function that would normally have
     // been used to generate the Webpack Development server config - you can use it to create
@@ -15,19 +15,10 @@ module.exports = {
     return function(proxy, allowedHost) {
       // Create the default config by calling configFunction with the proxy/allowedHost parameters
       const config = configFunction(proxy, allowedHost);
-
-      // Change the https certificate options to match your certificate, using the .env file to
-      // set the file paths & passphrase.
-      const fs = require('fs');
-      // config.https = {
-        // key: fs.readFileSync(process.env.REACT_HTTPS_KEY, 'utf8'),
-        // cert: fs.readFileSync(process.env.REACT_HTTPS_CERT, 'utf8'),
-        // ca: fs.readFileSync(process.env.REACT_HTTPS_CA, 'utf8'),
-        // passphrase: process.env.REACT_HTTPS_PASS
-      // };
-
-      // Return your customised Webpack Development Server config.
-      return config;
+      config.proxy = {
+        '/api': 'http://localhost:8080'
+      };
+     return config;
     }
   }
 }

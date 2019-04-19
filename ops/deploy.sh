096454dcc20840e41ea1661c4f04688f2ebe0cf5
Jeremy Hert <jhert@broadinstitute.org>
2019-04-19 06:27:58 -0400

    cleaning up, adding ui deploy

--- ops/deploy.sh
+++ ops/deploy.sh
@@ -40,47 +40,52 @@ command -v consul-template >/dev/null 2>&1 || {
 # make a temporary directory for rendering, we'll delete it later
 mkdir -p $SCRATCH
 
-kubectl get namespace data-repo && kubectl delete namespace data-repo
+kubectl get namespace data-repo 2>/dev/null && kubectl delete namespace data-repo
 
 # create a data-repo namespace to put everything in
 kubectl apply -f "${WD}/k8s/namespace.yaml"
 
-# create the pod security policies and service accounts
-kubectl apply --namespace data-repo -f "${WD}/k8s/psp"
+# create the pod security policies and service account
+kubectl apply --namespace data-repo -f "${WD}/k8s/psp/service-account.yaml"
+
+# TODO: I have a hunch that this only works on terraformed CIS k8s clusters, leaving commented out for now
+#kubectl apply --namespace data-repo -f "${WD}/k8s/psp"
 
 # render secrets, create or update on kubernetes
 consul-template -template "${WD}/k8s/secrets/api-secrets.yaml.ctmpl:${SCRATCH}/api-secrets.yaml" -once
 kubectl apply -f "${SCRATCH}/api-secrets.yaml"
 
-sleep 10
+# TODO: use kubectl waits instead of sleeps
+echo 'waiting 5 sec for secrets to be ready'
+sleep 5
 
 # update the service account key
 vault read "secret/dsde/firecloud/${ENVIRONMENT}/datarepo/sa-key${GOOGLE_CLOUD_PROJECT}.json" -format=json | jq .data  > "${SCRATCH}/sa-key.json"
-kubectl --namespace data-repo get secret sa-key && kubectl --namespace data-repo delete secret sa-key
+kubectl --namespace data-repo get secret sa-key 2>/dev/null && kubectl --namespace data-repo delete secret sa-key
 kubectl --namespace data-repo create secret generic sa-key --from-file="sa-key.json=${SCRATCH}/sa-key.json"
 
 # set the tsl certificate
 vault read -field=value secret/dsde/datarepo/dev/common/server.crt > "${SCRATCH}/server.crt"
 vault read -field=value secret/dsde/datarepo/dev/common/server.key > "${SCRATCH}/server.key"
-kubectl get secret tls-cert && kubectl delete secret tls-cert
+kubectl get secret server-key 2>/dev/null && kubectl delete secret server-key
+kubectl get secret server-cert 2>/dev/null && kubectl delete secret server-cert
 kubectl --namespace=data-repo create secret generic server-key --from-file=${SCRATCH}/server.key
 kubectl --namespace=data-repo create secret generic server-cert --from-file=${SCRATCH}/server.crt
-
-sleep 5
 
 # create or update postgres pod + service
 kubectl apply -f "${WD}/k8s/pods/psql-pod.yaml"
 kubectl apply -f "${WD}/k8s/services/psql-service.yaml"
 
 # wait for the db to be ready so that we can run commands against it
+echo 'waiting 5 sec for database to be up'
+sleep 5
+# TODO: add readiness probe to postgres pod def to check for port 5432 to be available
 kubectl wait --for=condition=Ready -f "${WD}/k8s/pods/psql-pod.yaml"
-
-echo 'waiting for 10 sec'
-sleep 10
 
 # create the right databases/user/extensions (TODO: moving this to be the APIs responsibility soon)
 cat "${WD}/../db/create-data-repo-db" | \
-    kubectl --namespace data-repo run psql -i --serviceaccount=jade-sa --restart=Never --rm --image=postgres:9.6 -- psql -h postgres-service.data-repo -U postgres
+    kubectl --namespace data-repo run psql -i --serviceaccount=jade-sa --restart=Never --rm --image=postgres:9.6 -- \
+    psql -h postgres-service.data-repo -U postgres
 
 # build a docker container and push it to gcr
 ${WD}/../gradlew dockerPush
@@ -89,10 +94,17 @@ ${WD}/../gradlew dockerPush
 kubectl apply -f "${WD}/k8s/deployments/api-deployment.yaml"
 kubectl apply -f "${WD}/k8s/services/api-service.yaml"
 
-kubectl --namespace data-repo set image deployments/api-deployment "data-repo-api-container=gcr.io/broad-jade-dev/jade-data-repo:latest${GOOGLE_CLOUD_PROJECT}"
+kubectl --namespace data-repo set image deployments/api-deployment \
+    "data-repo-api-container=gcr.io/broad-jade-dev/jade-data-repo:latest${GOOGLE_CLOUD_PROJECT}"
 
 # create or update oidc proxy pod + service, probably need to render secrets
 kubectl apply -f "${WD}/k8s/deployments/oidc-proxy-deployment.yaml"
 kubectl apply -f "${WD}/k8s/services/oidc-proxy-service.yaml"
 
 rm -r $SCRATCH
+
+# try to deploy the ui, assuming that the jade-data-repo-ui directory is a sibling of this jade-data-repo directory
+UI_DEPLOY="${WD}/../../jade-data-repo-ui/ops/deploy.sh"
+if test -f "$UI_DEPLOY"; then
+    bash $UI_DEPLOY
+fi

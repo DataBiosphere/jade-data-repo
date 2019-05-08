f7118ed10b456d63bbcbcf5a04ebfc9333fe5121
ddietterich <ddietterich@verily.com>
2019-05-08 18:18:40 -0400

    Plumb in the project id

--- src/main/java/bio/terra/pdao/bigquery/BigQueryConfiguration.java
+++ src/main/java/bio/terra/pdao/bigquery/BigQueryConfiguration.java
@@ -3,6 +3,7 @@ package bio.terra.pdao.bigquery;
 
 import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.BigQueryOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -14,13 +15,28 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile("google")
 public class BigQueryConfiguration {
+    // TODO: This is temporary. When we do the resource manager, the project id will via there
+    // for a given study. This project id is used here and in the file system (FireStore) to
+    // allocate the accessing instance.
+
+    @Value("${google.projectid}")
+    private String projectId;
+
+    @Bean("googleProjectId")
+    public String googleProjectId() {
+        return projectId;
+    }
+
     @Bean("bigQueryProjectId")
     public String bigQueryProjectId() {
-        return BigQueryOptions.getDefaultProjectId();
+        return projectId;
     }
 
     @Bean("bigQuery")
     public BigQuery bigQuery() {
-        return BigQueryOptions.getDefaultInstance().getService();
+        return BigQueryOptions.newBuilder()
+            .setProjectId(projectId)
+            .build()
+            .getService();
     }
 }

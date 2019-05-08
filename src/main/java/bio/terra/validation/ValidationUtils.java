d5ce8f63b9b101c0248edb2f5d99d1937bb4a8bb
Andrea Haessly <ahaessly@broadinstitute.org>
2019-05-08 15:24:17 -0400

    add/fix validations

--- src/main/java/bio/terra/validation/ValidationUtils.java
+++ src/main/java/bio/terra/validation/ValidationUtils.java
@@ -8,6 +8,10 @@ public final class ValidationUtils {
 
     private static final String VALID_NAME_REGEX = "[a-zA-Z0-9_]{1,63}";
 
+    private static final Pattern VALID_EMAIL_REGEX = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+" +
+        "(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+        Pattern.CASE_INSENSITIVE);
+
     private ValidationUtils() {}
 
     public static <T> boolean hasDuplicates(List<T> list) {
@@ -21,4 +25,7 @@ public final class ValidationUtils {
     public static boolean isValidDescription(String name) {
         return name.length() < 2048;
     }
+
+    public static boolean isValidEmail(String email) { return VALID_EMAIL_REGEX.matcher(email).matches(); }
+
 }

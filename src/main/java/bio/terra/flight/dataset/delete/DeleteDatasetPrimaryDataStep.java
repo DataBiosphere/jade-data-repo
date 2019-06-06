b8b2e50e439dd9546fff6f6b07e8f4efce39d7be
ddietterich <ddietterich@verily.com>
2019-06-06 16:49:43 -0400

    Move dependency cleanup from metadata step to primary data step

--- src/main/java/bio/terra/flight/dataset/delete/DeleteDatasetPrimaryDataStep.java
+++ src/main/java/bio/terra/flight/dataset/delete/DeleteDatasetPrimaryDataStep.java
@@ -2,7 +2,9 @@ package bio.terra.flight.dataset.delete;
 
 import bio.terra.dao.DatasetDao;
 import bio.terra.exception.NotFoundException;
+import bio.terra.filesystem.FireStoreDependencyDao;
 import bio.terra.metadata.Dataset;
+import bio.terra.metadata.DatasetSource;
 import bio.terra.pdao.bigquery.BigQueryPdao;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.Step;
@@ -16,13 +18,16 @@ public class DeleteDatasetPrimaryDataStep implements Step {
 
     private BigQueryPdao bigQueryPdao;
     private DatasetDao datasetDao;
+    private FireStoreDependencyDao dependencyDao;
     private UUID datasetId;
 
     public DeleteDatasetPrimaryDataStep(BigQueryPdao bigQueryPdao,
                                         DatasetDao datasetDao,
+                                        FireStoreDependencyDao dependencyDao,
                                         UUID datasetId) {
         this.bigQueryPdao = bigQueryPdao;
         this.datasetDao = datasetDao;
+        this.dependencyDao = dependencyDao;
         this.datasetId = datasetId;
     }
 
@@ -31,6 +36,14 @@ public class DeleteDatasetPrimaryDataStep implements Step {
         try {
             Dataset dataset = datasetDao.retrieveDataset(datasetId);
             bigQueryPdao.deleteDataset(dataset);
+
+            // Remove dataset file references from the underlying studies
+            for (DatasetSource datasetSource : dataset.getDatasetSources()) {
+                dependencyDao.deleteDatasetFileDependencies(
+                    datasetSource.getStudy().getId().toString(),
+                    datasetId.toString());
+            }
+
         } catch (NotFoundException nfe) {
             // If we do not find the study, we assume things are already clean
         }

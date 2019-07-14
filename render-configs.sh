7ab9a1e2c5bb4d53e7a247f0170df3534d9970d6
Jeremy Hert <jhert@broadinstitute.org>
2019-07-14 13:17:24 -0400

    switching to json for sa creds

--- render-configs.sh
+++ render-configs.sh
@@ -3,11 +3,8 @@
 LOCAL_TOKEN=$(cat ~/.vault-token)
 VAULT_TOKEN=${1:-$LOCAL_TOKEN}
 
-render () {
-    file=$1
-    destfile=$2
-    docker run --rm -v ${PWD}/src/main/resources:/working -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox:latest consul-template -config=/etc/consul-template/config/config.json -template=/working/${file}:/working/${destfile} -once
-}
+docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox:latest \
+    vault read -format=json secret/dsde/datarepo/dev/sa-key.json | \
+    jq .data | tee /tmp/jade-dev-account.json | \
+    jq -r .private_key > /tmp/jade-dev-account.pem
 
-render jade-dev-account.pem.ctmpl jade-dev-account.pem
-mv ${PWD}/src/main/resources/jade-dev-account.pem /tmp/jade-dev-account.pem

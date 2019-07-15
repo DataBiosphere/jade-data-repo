1ea437c50abf792eb9ba01e346c379e4067ce020
tshahria <tanvir_shahriar@brown.edu>
2019-07-15 10:25:37 -0400

    Finished test

--- src/test/java/bio/terra/integration/AccessTest.java
+++ src/test/java/bio/terra/integration/AccessTest.java
@@ -28,8 +28,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;

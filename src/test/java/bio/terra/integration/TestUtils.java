a5598d74555fbd29b27cf82f10adddd82da0b2e3
tshahria <tanvir_shahriar@brown.edu>
2019-07-22 10:28:46 -0400

    added logging to testUtils

--- src/test/java/bio/terra/integration/TestUtils.java
+++ src/test/java/bio/terra/integration/TestUtils.java
@@ -1,21 +1,29 @@
 package bio.terra.integration;
 
+import org.slf4j.LoggerFactory;
+
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.concurrent.Callable;
 import java.util.concurrent.TimeUnit;
 
 public final class TestUtils {
+    private static org.slf4j.Logger logger = LoggerFactory.getLogger(TestUtils.class);
+
     private TestUtils() {}
 
     public static <T> boolean flappyExpect(
         int secInterval, int secTimeout, T expected, Callable<T> callable) throws Exception {
         LocalDateTime end = LocalDateTime.now().plus(Duration.ofSeconds(secTimeout));
+        int tries = 0;
         while (LocalDateTime.now().isBefore(end)) {
+            String logging = String.format("Time elapsed: %03d seconds, Tried: %03d times", secInterval * tries, tries);
+            logger.info(logging);
             if (callable.call().equals(expected)) {
                 return true;
             }
             TimeUnit.SECONDS.sleep(secInterval);
+            tries++;
         }
         return false;
     }

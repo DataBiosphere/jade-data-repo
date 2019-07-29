07f16314513d69f490b271b47c232d944a0b84cc
Jeremy Hert <jhert@broadinstitute.org>
2019-07-29 08:46:03 -0400

    some progress

--- tools/data-populate.sh
+++ tools/data-populate.sh
@@ -1,48 +1,47 @@
 #!/bin/bash
+set -e
 
-set -ex
+ACCESS_TOKEN=$(gcloud auth print-access-token)
+HOST=https://jade-jh.datarepo-dev.broadinstitute.org
+#HOST=http://localhost:8080
 
-# first argument must be a server address with a protocol
-: ${1?"server address required"}
+# use the first profile id if it is there
+PROFILE_ID=$(curl --header 'Accept: application/json' --header "Authorization: Bearer ${ACCESS_TOKEN}" \
+    "${HOST}/api/resources/v1/profiles" \
+    | jq .items[0].id)
 
-ACCESS_TOKEN=$( gcloud beta auth application-default print-access-token )
+# if not, make a new one
+if [ "$PROFILE_ID" == "null" ]; then
+    PROFILE_ID=$(curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' \
+        --header "Authorization: Bearer ${ACCESS_TOKEN}" \
+        -d '{ "biller": "direct", "billingAccountId": "00708C-45D19D-27AAFA", "profileName": "core" }' \
+        "${HOST}/api/resources/v1/profiles" \
+        | jq .id)
+fi
 
-WD=$( dirname "${BASH_SOURCE[0]}" )
-BASE="${1}/api/repository/v1"
+# create the encode study
+STAMP=$(date +"%Y_%m_%d_%H_%M_%S")
+STUDY_ID=$(cat ../src/test/resources/encodefiletest-study.json \
+    | jq ".defaultProfileId = ${PROFILE_ID} | .name = \"encode_file_test_${STAMP}\"" \
+    | curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' \
+        --header "Authorization: Bearer ${ACCESS_TOKEN}" \
+        -d @- "${HOST}/api/repository/v1/studies" \
+    | jq -r .id)
 
-# create a few studies
-curl "${BASE}/studies" -H "Authorization: Bearer ${ACCESS_TOKEN}" \
-    -H 'Content-Type: application/json' \
-    --data-binary "@${WD}/../src/test/resources/study-minimal.json"
+# ingest file data into it
+INGEST_PAYLOAD=$(cat <<EOF
+{
+  "format": "json",
+  "ignore_unknown_values": false,
+  "load_tag": "data-populate.sh",
+  "max_bad_records": 0,
+  "path": "gs://jade-testdata/encodetest/file.json",
+  "table": "file"
+}
+EOF
+)
 
-curl "${BASE}/studies" -H "Authorization: Bearer ${ACCESS_TOKEN}" \
-    -H 'Content-Type: application/json' \
-    --data-binary "@${WD}/../src/test/resources/encode-study.json"
-
-curl "${BASE}/studies" -H "Authorization: Bearer ${ACCESS_TOKEN}" \
-    -H 'Content-Type: application/json' \
-    --data-binary "@${WD}/../src/test/resources/it-study-omop.json"
-
-curl "${BASE}/studies" -H "Authorization: Bearer ${ACCESS_TOKEN}" \
-    -H 'Content-Type: application/json' \
-    --data-binary "@${WD}/../src/test/resources/dataset-test-study.json"
-
-curl "${BASE}/studies" -H "Authorization: Bearer ${ACCESS_TOKEN}" \
-    -H 'Content-Type: application/json' \
-    --data-binary "@${WD}/../src/test/resources/study-create-test.json"
-
-curl "${BASE}/studies" -H "Authorization: Bearer ${ACCESS_TOKEN}" \
-    -H 'Content-Type: application/json' \
-    --data-binary "@${WD}/../src/test/resources/ingest-test-study.json"
-
-# populate tables
-bq load --source_format=CSV --skip_leading_rows=1 datarepo_Minimal.participant \
-    "${WD}/../src/test/resources/study-minimal-participant.csv"
-
-bq load --source_format=CSV --skip_leading_rows=1 datarepo_Minimal.sample \
-    "${WD}/../src/test/resources/study-minimal-sample.csv"
-
-# create a dataset
-curl "${BASE}/datasets" -H "Authorization: Bearer ${ACCESS_TOKEN}" \
-    -H 'Content-Type: application/json' \
-    --data-binary "@${WD}/../src/test/resources/study-minimal-dataset.json"
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' \
+    --header "Authorization: Bearer ${ACCESS_TOKEN}" \
+    -d "$INGEST_PAYLOAD" \
+    "${HOST}/api/repository/v1/studies/${STUDY_ID}/ingest"

38e3e7cb8728cf3e8804c08ef6401546a2af60ea
Jeremy Hert <jhert@broadinstitute.org>
2019-08-08 11:21:12 -0400

    intermediate

---
+++ src/main/java/bio/terra/metadata/FileDataBucket.java
@@ -0,0 +1,71 @@
+package bio.terra.metadata;
+
+import bio.terra.resourcemanagement.metadata.google.GoogleBucketResource;
+
+import java.util.UUID;
+
+public class FileDataBucket {
+
+    private FileDataBucketSummary fileDataBucketSummary = new FileDataBucketSummary();
+    private GoogleBucketResource googleBucketResource = new GoogleBucketResource();
+
+    public FileDataBucket() {}
+
+    public FileDataBucket(FileDataBucketSummary fileDataBucketSummary) {
+        this.fileDataBucketSummary = fileDataBucketSummary;
+    }
+
+    public FileDataBucketSummary getFileDataBucketSummary() {
+        return fileDataBucketSummary;
+    }
+
+    public FileDataBucket setFileDataBucketSummary(FileDataBucketSummary fileDataBucketSummary) {
+        this.fileDataBucketSummary = fileDataBucketSummary;
+        return this;
+    }
+
+    public GoogleBucketResource getGoogleBucketResource() {
+        return googleBucketResource;
+    }
+
+    public FileDataBucket googleBucketResource(GoogleBucketResource projectResource) {
+        this.googleBucketResource = projectResource;
+        return this;
+    }
+
+    public UUID getProfileId() {
+        return googleBucketResource.getProfileId();
+    }
+
+    public FileDataBucket profileId(UUID profileId) {
+        googleBucketResource.profileId(profileId);
+        return this;
+    }
+
+    public UUID getProjectResourceId() {
+        return googleBucketResource.getRepositoryId();
+    }
+
+    public FileDataBucket projectResourceId(UUID projectResourceId) {
+        googleBucketResource.repositoryId(projectResourceId);
+        return this;
+    }
+
+    public UUID getId() {
+        return fileDataBucketSummary.getId();
+    }
+
+    public FileDataBucket id(UUID id) {
+        fileDataBucketSummary.id(id);
+        return this;
+    }
+
+    public UUID getFileObjectId() {
+        return fileDataBucketSummary.getFileObjectId();
+    }
+
+    public FileDataBucket fileObjectId(UUID fileObjectId) {
+        fileDataBucketSummary.fileObjectId(fileObjectId);
+        return this;
+    }
+}

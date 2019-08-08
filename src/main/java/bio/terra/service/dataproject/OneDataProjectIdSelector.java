982a354e517fdc92d68578430999b6a2fdda91fa
Michael Yessaillian <myessail@ccs.neu.edu>
2019-08-08 11:14:16 -0400

    DR-496 create 1-project-per-profile selector (#205)
    * add selector and small gitignore change
    
    * add test and refactor SnapshotService to return snapshot instead of model
    
    * line length
    
    * more line lengths
    
    * findbugs
    
    * beans

--- src/main/java/bio/terra/service/dataproject/OneDataProjectIdSelector.java
+++ src/main/java/bio/terra/service/dataproject/OneDataProjectIdSelector.java
@@ -1,11 +1,15 @@
 package bio.terra.service.dataproject;
 
-import bio.terra.metadata.Snapshot;
 import bio.terra.metadata.Dataset;
+import bio.terra.metadata.Snapshot;
 import bio.terra.resourcemanagement.service.google.GoogleResourceConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
+@Primary
+@Profile("!terra")
 @Component
 public class OneDataProjectIdSelector implements DataProjectIdSelector {
 

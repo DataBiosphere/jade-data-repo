0fd25b4c93a3ebfd0c5fdd2b84a61452644ff7f2
rori <6863459+RoriCremer@users.noreply.github.com>
2019-11-20 16:10:17 -0500

    remove asset addition during undo if theres been a failure in creation

--- src/main/java/bio/terra/service/dataset/flight/create/CreateDatasetAssetStep.java
+++ src/main/java/bio/terra/service/dataset/flight/create/CreateDatasetAssetStep.java
@@ -1,9 +1,7 @@
 package bio.terra.service.dataset.flight.create;
 
-import bio.terra.model.AssetModel;
 import bio.terra.service.dataset.AssetDao;
 import bio.terra.service.dataset.AssetSpecification;
-import bio.terra.service.dataset.DatasetJsonConversion;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.Step;
 import bio.terra.stairway.StepResult;
@@ -14,25 +12,23 @@ public class CreateDatasetAssetStep implements Step {
 
     private UUID datasetId;
     private AssetDao assetDao;
-    private AssetModel assetModel;
+    private AssetSpecification assetSpecification;
 
-    public CreateDatasetAssetStep(UUID datasetId, AssetDao assetDao, AssetModel assetModel) {
+    public CreateDatasetAssetStep(UUID datasetId, AssetDao assetDao, AssetSpecification assetSpecification) {
         this.datasetId = datasetId;
         this.assetDao = assetDao;
-        this.assetModel = assetModel;
+        this.assetSpecification = assetSpecification;
     }
 
     @Override
     public StepResult doStep(FlightContext context) {
-        // TODO this needs the dataset tables and relationships added in
-        AssetSpecification assetSpecification = DatasetJsonConversion.assetModelToAssetSpecification(assetModel);
         assetDao.create(assetSpecification, datasetId);
         return StepResult.getStepResultSuccess();
     }
 
     @Override
     public StepResult undoStep(FlightContext context) {
-        // TODO is there a asset deletion method, or is that going to need to get added as well?
+        assetDao.delete(assetSpecification.getId());
         return StepResult.getStepResultSuccess();
     }
 }

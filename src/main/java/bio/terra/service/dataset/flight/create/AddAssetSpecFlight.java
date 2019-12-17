55c0a90e09fb0fa6ca23993fa86cc14d9db94ed6
rori <6863459+RoriCremer@users.noreply.github.com>
2019-12-17 14:39:44 -0500

    move work out of flight constructor

--- src/main/java/bio/terra/service/dataset/flight/create/AddAssetSpecFlight.java
+++ src/main/java/bio/terra/service/dataset/flight/create/AddAssetSpecFlight.java
@@ -1,18 +1,11 @@
 package bio.terra.service.dataset.flight.create;
 
-import bio.terra.app.controller.exception.ValidationException;
 import bio.terra.service.dataset.AssetDao;
-import bio.terra.service.dataset.AssetSpecification;
-import bio.terra.service.dataset.Dataset;
 import bio.terra.service.dataset.DatasetService;
-import bio.terra.service.job.JobMapKeys;
 import bio.terra.stairway.Flight;
 import bio.terra.stairway.FlightMap;
 import bio.terra.stairway.UserRequestInfo;
 import org.springframework.context.ApplicationContext;
-
-import java.util.List;
-import java.util.UUID;
 
 public class AddAssetSpecFlight extends Flight {
 
@@ -24,34 +17,8 @@ public class AddAssetSpecFlight extends Flight {
         AssetDao assetDao = (AssetDao) appContext.getBean("assetDao");
         DatasetService datasetService = (DatasetService) appContext.getBean("datasetService");
 
-        // get our dataset Id and dataset
-        UUID datasetId = UUID.fromString(
-            inputParameters.get(JobMapKeys.DATASET_ID.getKeyName(), String.class)
-        );
-        Dataset dataset = datasetService.retrieve(datasetId);
-
-
-        // validate the assetspec
-        AssetSpecification assetSpecification = inputParameters.get(
-            JobMapKeys.REQUEST.getKeyName(), AssetSpecification.class);
-        String assetName = assetSpecification.getName();
-
-        // get the dataset assets that already exist --asset name needs to be unique
-        List<AssetSpecification> datasetAssetSpecificationList = dataset.getAssetSpecifications();
-        // could have used AssetDao.retrieve(dataset)
-        if(datasetAssetSpecificationList.stream()
-            .anyMatch(asset -> asset.getName().equalsIgnoreCase(assetName))) {
-            throw new ValidationException("Can not add an asset to a dataset with a duplicate name");
-        }
-
-        //Asset columns and tables need to match things in the dataset schema
-        // get the dataset schema
-        // can I use DatasetRequestValidator.validateAsset ?
-
-
-
         // create job to add the assetspec to the dataset
-        addStep(new CreateDatasetAssetStep(datasetId, assetDao, assetSpecification));
+        addStep(new CreateDatasetAssetStep(datasetService, assetDao));
     }
 
 }
--- src/main/java/bio/terra/service/dataset/flight/create/CreateDatasetAssetStep.java
+++ src/main/java/bio/terra/service/dataset/flight/create/CreateDatasetAssetStep.java
@@ -1,34 +1,74 @@
 package bio.terra.service.dataset.flight.create;
 
+import bio.terra.app.controller.exception.ValidationException;
 import bio.terra.service.dataset.AssetDao;
 import bio.terra.service.dataset.AssetSpecification;
+import bio.terra.service.dataset.Dataset;
+import bio.terra.service.dataset.DatasetService;
+import bio.terra.service.job.JobMapKeys;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.Step;
 import bio.terra.stairway.StepResult;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public class CreateDatasetAssetStep implements Step {
 
-    private UUID datasetId;
+    private DatasetService datasetService;
     private AssetDao assetDao;
-    private AssetSpecification assetSpecification;
 
-    public CreateDatasetAssetStep(UUID datasetId, AssetDao assetDao, AssetSpecification assetSpecification) {
-        this.datasetId = datasetId;
+    public CreateDatasetAssetStep(DatasetService datasetService, AssetDao assetDao) {
+        this.datasetService = datasetService;
         this.assetDao = assetDao;
-        this.assetSpecification = assetSpecification;
+    }
+
+    private Dataset getDataset(FlightContext context) {
+        // Use the dataset id to fetch the Dataset object
+        UUID datasetId = UUID.fromString(
+            context.getInputParameters().get(JobMapKeys.DATASET_ID.getKeyName(), String.class)
+        );
+        return datasetService.retrieve(datasetId);
+    }
+
+    private AssetSpecification getNewAssetSpec(FlightContext context) {
+        // validate the assetspec
+        AssetSpecification assetSpecification = context.getInputParameters().get(
+            JobMapKeys.REQUEST.getKeyName(), AssetSpecification.class);
+        return assetSpecification;
     }
 
     @Override
     public StepResult doStep(FlightContext context) {
-        assetDao.create(assetSpecification, datasetId);
+        // TODO: Asset columns and tables need to match things in the dataset schema
+        Dataset dataset = getDataset(context);
+        // get the dataset assets that already exist --asset name needs to be unique
+        AssetSpecification newAssetSpecification = getNewAssetSpec(context);
+        List<AssetSpecification> datasetAssetSpecificationList = dataset.getAssetSpecifications();
+        if (datasetAssetSpecificationList.stream()
+            .anyMatch(asset -> asset.getName().equalsIgnoreCase(newAssetSpecification.getName()))) {
+            throw new ValidationException("Can not add an asset to a dataset with a duplicate name");
+        }
+        assetDao.create(newAssetSpecification, getDataset(context).getId());
         return StepResult.getStepResultSuccess();
     }
 
     @Override
     public StepResult undoStep(FlightContext context) {
-        assetDao.delete(assetSpecification.getId());
+        Dataset dataset = getDataset(context);
+        // Search the Asset list in the dataset object to see if the asset you were trying to create got created.
+        AssetSpecification newAssetSpecification = getNewAssetSpec(context);
+        Optional<AssetSpecification> assetSpecificationToDelete =
+            dataset.getAssetSpecificationByName(newAssetSpecification.getName());
+        // This only works if we are sure asset names are unique.
+        // You cannot assume that the flight object created when the doStep was run is the same flight object
+        // when the undoStep is run.
+        if (assetSpecificationToDelete.isPresent()) {
+            // If the asset is found, then you get its id and call delete.
+            assetDao.delete(assetSpecificationToDelete.get().getId());
+        }
+        // Else, if the asset is not found, then you are done. It never got created.
         return StepResult.getStepResultSuccess();
     }
 }

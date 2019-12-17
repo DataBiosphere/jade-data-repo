8f5ba3d3a8679b5b03d04b85cf54c9d9154e2e71
rori <6863459+RoriCremer@users.noreply.github.com>
2019-12-17 14:51:04 -0500

    move work out of constructor

--- src/main/java/bio/terra/service/dataset/flight/create/DeleteDatasetAssetStep.java
+++ src/main/java/bio/terra/service/dataset/flight/create/DeleteDatasetAssetStep.java
@@ -1,6 +1,7 @@
 package bio.terra.service.dataset.flight.create;
 
 import bio.terra.service.dataset.AssetDao;
+import bio.terra.service.job.JobMapKeys;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.Step;
 import bio.terra.stairway.StepResult;
@@ -10,22 +11,28 @@ import java.util.UUID;
 public class DeleteDatasetAssetStep implements Step {
 
     private AssetDao assetDao;
-    private UUID assetId;
 
-    public DeleteDatasetAssetStep(AssetDao assetDao, UUID assetId) {
+    public DeleteDatasetAssetStep(AssetDao assetDao) {
         this.assetDao = assetDao;
-        this.assetId = assetId;
+    }
+
+    private UUID getAssetId(FlightContext context) {
+        // get asset Id
+        UUID assetId =  UUID.fromString(
+            context.getInputParameters().get(JobMapKeys.ASSET_ID.getKeyName(), String.class)
+        );
+        return assetId;
     }
 
     @Override
     public StepResult doStep(FlightContext context) {
-        assetDao.delete(assetId);
+        assetDao.delete(getAssetId(context));
         return StepResult.getStepResultSuccess();
     }
 
     @Override
     public StepResult undoStep(FlightContext context) {
-        // can't undo delete ?!?!??!
+        // can't undo delete
         return StepResult.getStepResultSuccess();
     }
 }

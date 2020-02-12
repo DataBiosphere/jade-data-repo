42ba09d58d3a8cedeb195a70572cab401cf4d31e
Florian Boulnois <fboulnoi@broadinstitute.org>
2020-02-12 16:10:03 -0500

    Update db-connect script for latest devops env

--- ops/db-connect.sh
+++ ops/db-connect.sh
@@ -4,10 +4,10 @@
 : ${ENVIRONMENT:?}
 SUFFIX=${SUFFIX:-$ENVIRONMENT}
 
-VAULT_PATH="secret/dsde/datarepo/${ENVIRONMENT}/api-secrets-${SUFFIX}.json"
+VAULT_PATH="secret/dsde/datarepo/${ENVIRONMENT}/helm-datarepodb-${ENVIRONMENT}"
 echo $VAULT_PATH
 PW=$( vault read -format=json $VAULT_PATH | \
-      jq -r .data.datarepoPassword )
+      jq -r .data.datarepopassword )
 
-kubectl --namespace data-repo run psql -it --serviceaccount=jade-sa --restart=Never --rm --image postgres:9.6 -- \
-    psql "postgresql://drmanager:${PW}@cloudsql-proxy-service.data-repo/${DB}"
+kubectl --namespace ${SUFFIX} run psql -it --serviceaccount=${SUFFIX}-jade-datarepo-api --restart=Never --rm --image postgres:9.6 -- \
+    psql "postgresql://drmanager:${PW}@${SUFFIX}-jade-gcloud-sqlproxy.${SUFFIX}/${DB}"

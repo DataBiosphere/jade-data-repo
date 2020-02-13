1b82713c3538bdb7fc2eb4893b9e13e8ec5c1586
Florian Boulnois <fboulnoi@broadinstitute.org>
2020-02-13 14:00:37 -0500

    remove unused import

--- src/main/java/bio/terra/service/iam/LocalAuthenticatedUserRequestFactory.java
+++ src/main/java/bio/terra/service/iam/LocalAuthenticatedUserRequestFactory.java
@@ -4,7 +4,6 @@ import bio.terra.app.configuration.ApplicationConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;

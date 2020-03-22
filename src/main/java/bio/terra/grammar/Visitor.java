80dcdb093a837d9ce6ee5099be5486480a032a3f
Jeremy Hert <jhert@broadinstitute.org>
2020-03-22 16:28:53 -0400

    progress

--- src/main/java/bio/terra/grammar/Visitor.java
+++ src/main/java/bio/terra/grammar/Visitor.java
@@ -1,10 +1,13 @@
 package bio.terra.grammar;
 
 import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.tree.RuleNode;
+import org.antlr.v4.runtime.tree.TerminalNode;
 
 public class Visitor extends SQLBaseVisitor<String> {
 
-    String getNameFromContext(ParserRuleContext ctx) {
+    private String getNameFromContext(ParserRuleContext ctx) {
         return ctx == null ? null : ctx.getText();
     }
 
@@ -12,7 +15,46 @@ public class Visitor extends SQLBaseVisitor<String> {
     public String visitTable_expr(SQLParser.Table_exprContext ctx) {
         String datasetName = getNameFromContext(ctx.dataset_name());
         String tableName = getNameFromContext(ctx.table_name());
-        System.out.println(String.format("%s.%s", datasetName, tableName));
-        return visitChildren(ctx);
+        return String.format("%s.%s", datasetName, tableName);
+    }
+
+    @Override
+    public String visitColumn_expr(SQLParser.Column_exprContext ctx) {
+        String datasetName = getNameFromContext(ctx.dataset_name());
+        String tableName = getNameFromContext(ctx.table_name());
+        String columnName = getNameFromContext(ctx.column_name());
+        return String.format("%s.%s.%s", datasetName, tableName, columnName);
+    }
+
+    @Override
+    public String visitTerminal(TerminalNode node) {
+        return node.getText();
+    }
+
+    @Override
+    public String visitChildren(RuleNode node) {
+        String result = defaultResult();
+        int n = node.getChildCount();
+        for (int i = 0; i < n; i++) {
+            if (!shouldVisitNextChild(node, result)) {
+                break;
+            }
+
+            ParseTree c = node.getChild(i);
+            String childResult = c.accept(this);
+            result = aggregateResult(result, childResult);
+        }
+
+        return result;
+    }
+
+    protected String aggregateResult(String aggregate, String nextResult) {
+        if (aggregate == null) {
+            return nextResult;
+        }
+        if (nextResult == null) {
+            return aggregate;
+        }
+        return String.format("%s %s", aggregate, nextResult);
     }
 }
--- src/test/java/bio/terra/grammar/GrammarTest.java
+++ src/test/java/bio/terra/grammar/GrammarTest.java
@@ -2,25 +2,39 @@ package bio.terra.grammar;
 
 
 import bio.terra.common.category.Unit;
+import org.antlr.v4.runtime.BailErrorStrategy;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 @Category(Unit.class)
 public class GrammarTest {
 
-    @Test
-    public void testValidate() {
-        //String sql = "mr. michael";
-        String sql = "SELECT * FROM foo";
+    public SQLParser.Query_statementContext parseQuery(String sql) {
         CharStream charStream = CharStreams.fromString(sql);
         SQLLexer lexer = new SQLLexer(charStream);
         SQLParser parser = new SQLParser(new CommonTokenStream(lexer));
-        SQLParser.Query_statementContext queryStatement = parser.query_statement();
+        parser.setErrorHandler(new BailErrorStrategy());
+        return parser.query_statement();
+    }
+
+    @Test
+    public void testValidate() {
+        String sql = "SELECT foo.bar.baz FROM foo.bar";
         Visitor visitor = new Visitor();
-        visitor.visit(queryStatement);
+        String visitResult = visitor.visit(parseQuery(sql));
+        assertThat("visitResult matches", visitResult, equalTo(sql));
+    }
+
+    @Test
+    public void testBad() {
+        String sql = "mr. michael";
+        String visitResult = new Visitor().visit(parseQuery(sql));
     }
 
 }

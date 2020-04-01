6a31d57cc1045a0054795a4923515d97cb39d403
Mariko Medlock <mariko.medlock@gmail.com>
2020-04-01 18:04:59 -0400

    Add comments to db-connect about how to set env vars.

--- ops/db-connect.sh
+++ ops/db-connect.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+## Call this to connect to PostGres for a specific Data Repo instance.
+## e.g. DB=datarepo SUFFIX=mm ENVIRONMENT=dev ./db-connect.sh
+## If the connection times out, check that you are on the Broad VPN and are connected to correct Kubernetes cluster.
+
 : ${DB:?}
 : ${ENVIRONMENT:?}
 SUFFIX=${SUFFIX:-$ENVIRONMENT}

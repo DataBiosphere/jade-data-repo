358cbfa353cc7f9665d9c4bcde20235ff75569ee
Jeremy Hert <jhert@broadinstitute.org>
2020-04-10 08:36:14 -0400

    remove file checking from step

--- src/main/java/bio/terra/service/dataset/flight/datadelete/CreateExternalTablesStep.java
+++ src/main/java/bio/terra/service/dataset/flight/datadelete/CreateExternalTablesStep.java
@@ -1,21 +1,14 @@
 package bio.terra.service.dataset.flight.datadelete;
 
-import bio.terra.common.exception.PdaoInvalidUriException;
-import bio.terra.common.exception.PdaoSourceFileNotFoundException;
 import bio.terra.model.DataDeletionRequest;
 import bio.terra.model.DataDeletionTableModel;
 import bio.terra.service.dataset.Dataset;
 import bio.terra.service.dataset.DatasetService;
-import bio.terra.service.dataset.exception.InvalidFileRefException;
 import bio.terra.service.dataset.exception.TableNotFoundException;
-import bio.terra.service.dataset.flight.ingest.IngestUtils;
-import bio.terra.service.filedata.google.gcs.GcsPdao;
 import bio.terra.service.tabulardata.google.BigQueryPdao;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.Step;
 import bio.terra.stairway.StepResult;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -31,43 +24,12 @@ public class CreateExternalTablesStep implements Step {
 
     private final BigQueryPdao bigQueryPdao;
     private final DatasetService datasetService;
-    private final Storage storage;
 
     private static Logger logger = LoggerFactory.getLogger(CreateExternalTablesStep.class);
 
     public CreateExternalTablesStep(BigQueryPdao bigQueryPdao, DatasetService datasetService) {
         this.bigQueryPdao = bigQueryPdao;
         this.datasetService = datasetService;
-        this.storage = StorageOptions.getDefaultInstance().getService();
-    }
-
-    private boolean fileExistsForTable(DataDeletionTableModel table) {
-        // TODO: support other file types (assuming header-less csv here)
-        String path  = table.getGcsFileSpec().getPath();
-        IngestUtils.GsUrlParts gsUrlParts = IngestUtils.parseBlobUri(path);
-        try {
-            if (gsUrlParts.getIsWildcard()) {
-                // There's not an easy way to check this with the SDK. Let BQ handle catching this on its side.
-                return true;
-            } else {
-                return GcsPdao.getBlobFromGsPath(storage, path).exists();
-            }
-        } catch (PdaoInvalidUriException | PdaoSourceFileNotFoundException ex) {
-            logger.warn("Could not fetch details about: " + path, ex);
-            throw ex;
-        }
-    }
-
-    private void validateFilesExistForTables(DataDeletionRequest request) {
-        List<String> missingFiles = request.getTables()
-            .stream()
-            .filter(t -> !fileExistsForTable(t))
-            .map(t -> t.getGcsFileSpec().getPath())
-            .collect(Collectors.toList());
-
-        if (missingFiles.size() > 0) {
-            throw new InvalidFileRefException("Not all files referenced in request exist.", missingFiles);
-        }
     }
 
     private void validateTablesExistInDataset(DataDeletionRequest request, Dataset dataset) {
@@ -90,7 +52,6 @@ public class CreateExternalTablesStep implements Step {
         DataDeletionRequest dataDeletionRequest = getRequest(context);
 
         validateTablesExistInDataset(dataDeletionRequest, dataset);
-        validateFilesExistForTables(dataDeletionRequest);
 
         dataDeletionRequest.getTables().forEach(table -> {
             String path = table.getGcsFileSpec().getPath();

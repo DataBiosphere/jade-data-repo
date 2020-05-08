82e669ef114dc9a12f2b16cef0993dc1ef1ebefb
Shelby <sfholden3@gmail.com>
2020-05-08 11:26:28 -0400

    Update staging table name for more consistency

--- src/main/java/bio/terra/common/PdaoConstant.java
+++ src/main/java/bio/terra/common/PdaoConstant.java
@@ -12,5 +12,5 @@ public final class PdaoConstant {
     public static final String PDAO_INGEST_DATE_COLUMN_ALIAS = PDAO_PREFIX + "ingest_date";
     public static final String PDAO_EXTERNAL_TABLE_PREFIX = PDAO_PREFIX + "ext_";
     public static final String PDAO_LOAD_HISTORY_TABLE = PDAO_PREFIX + "load_history";
-    public static final String PDAO_LOAD_HISTORY_STAGING_TABLE_PREIX = "bulk_results_staging";
+    public static final String PDAO_LOAD_HISTORY_STAGING_TABLE_PREIX = PDAO_LOAD_HISTORY_TABLE + "_staging_";
 }

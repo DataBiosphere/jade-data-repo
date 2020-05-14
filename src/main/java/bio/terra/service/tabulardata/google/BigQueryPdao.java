dae8cb2801a2817b81b4e43e78b7f0a752c6bc3c
rori <6863459+RoriCremer@users.noreply.github.com>
2020-05-14 11:42:52 -0400

    DRY up snapshot creation w helper methods

--- src/main/java/bio/terra/service/tabulardata/google/BigQueryPdao.java
+++ src/main/java/bio/terra/service/tabulardata/google/BigQueryPdao.java
@@ -433,6 +433,7 @@ public class BigQueryPdao implements PrimaryDataAccess {
         walkRelationships(datasetBqDatasetName, snapshotName, walkRelationships, rootTableId, projectId, bigQuery);
 
         snapshotViewCreation(datasetBqDatasetName, snapshotName, snapshot, projectId, bigQuery, bigQueryProject);
+<<<<<<< HEAD
     }
 
     private static final String getAllLiveViewDataTemplate =

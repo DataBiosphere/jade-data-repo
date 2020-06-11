15ad7b9976c8dde2cd3ed4cfd9ddb89e6bdbee96
Shelby Holden <sholden@broadinstitute.org>
2020-06-11 06:53:23 -0400

    switch back to InternalServerErrorException

--- src/main/java/bio/terra/common/exception/RetryQueryException.java
+++ src/main/java/bio/terra/common/exception/RetryQueryException.java
@@ -1,15 +1,23 @@
 package bio.terra.common.exception;
 
-import org.springframework.dao.DataAccessException;
+import java.util.List;
 
 
-public class RetryQueryException extends DataAccessException {
+public class RetryQueryException extends InternalServerErrorException {
     public RetryQueryException(String message) {
         super(message);
     }
 
     public RetryQueryException(String message, Throwable cause) {
         super(message, cause);
+    }
+
+    public RetryQueryException(Throwable cause) {
+        super(cause);
+    }
+
+    public RetryQueryException(String message, Throwable cause, List<String> errorDetails) {
+        super(message, cause, errorDetails);
     }
 
 }

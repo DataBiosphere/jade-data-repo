aa3b8cc59f0f178b853d0a3d090b1a7423be30f5
Shelby Holden <sholden@broadinstitute.org>
2020-06-18 10:48:46 -0400

    Rework logs

--- src/main/java/bio/terra/service/iam/sam/SamIam.java
+++ src/main/java/bio/terra/service/iam/sam/SamIam.java
@@ -180,7 +180,7 @@ public class SamIam implements IamProviderInterface {
                 role.toString());
             String policyEmail = getPolicyGroupEmailFromResponse(results);
             rolePolicies.add(policyEmail);
-            logger.info("Dataset SAMIAM for role: {}, Policy: {}", role.toString(), policyEmail);
+            logger.info("Dataset Policy Group Email: {}; Role: {}", role.toString(), policyEmail);
         }
 
         return rolePolicies;
@@ -231,7 +231,7 @@ public class SamIam implements IamProviderInterface {
             snapshotId.toString(),
             IamRole.READER.toString());
         String policyEmail = getPolicyGroupEmailFromResponse(results);
-        logger.info("Snapshot SAMIAM for role: {}, Policy: {} ", IamRole.READER.toString(), policyEmail);
+        logger.info("Snapshot Reader Policy Group Email:  {} ", policyEmail);
         return policyEmail;
     }
 

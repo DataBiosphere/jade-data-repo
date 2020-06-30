9e5c09d83242d756b5e55e1455ab906e310c6539
Mariko Medlock <mariko.medlock@gmail.com>
2020-06-30 16:52:05 -0400

    Update KubernetesClientUtils to use AuthenticationUtils methods for access token.

--- datarepo-clienttests/src/main/java/utils/KubernetesClientUtils.java
+++ datarepo-clienttests/src/main/java/utils/KubernetesClientUtils.java
@@ -2,7 +2,6 @@ package utils;
 
 import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.auth.oauth2.ServiceAccountCredentials;
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.Configuration;
@@ -20,55 +19,35 @@ import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
+import runner.config.ServerSpecification;
 
 public final class KubernetesClientUtils {
 
-  private static GoogleCredentials applicationDefaultCredentials;
-  private static CoreV1Api kubernetesClientObject;
+  private static volatile CoreV1Api kubernetesClientObject;
+
+  private static final Object lockKubernetesClientObject = new Object();
 
   private KubernetesClientUtils() {}
 
-  public static AccessToken getApplicationDefaultAccessToken() throws IOException {
-    if (applicationDefaultCredentials == null) {
-      // get the application default credentials
-      applicationDefaultCredentials = ServiceAccountCredentials.getApplicationDefault();
-
-      // not sure when this case happens
-      if (applicationDefaultCredentials.createScopedRequired()) {
-        applicationDefaultCredentials =
-            applicationDefaultCredentials.createScoped(
-                Collections.singletonList("https://www.googleapis.com/auth/cloud-platform"));
-      }
-    }
-
-    // refresh the token if needed before returning
-    applicationDefaultCredentials.refreshIfExpired();
-    return applicationDefaultCredentials.getAccessToken();
-  }
-
-  public static CoreV1Api getKubernetesClientObject() throws Exception {
-    // TODO: move these to config (these values are for dev)
-    String clusterName = "gke_broad-jade-dev_us-central1_dev-master";
-    String region = "us-central1";
-    String project = "broad-jade-dev";
-
+  public static CoreV1Api getKubernetesClientObject(ServerSpecification server) throws Exception {
     if (kubernetesClientObject == null) {
-      kubernetesClientObject = buildKubernetesClientObject(clusterName, region, project);
+      synchronized (lockKubernetesClientObject) {
+        kubernetesClientObject = buildKubernetesClientObject(server);
+      }
     }
     return kubernetesClientObject;
   }
 
-  private static CoreV1Api buildKubernetesClientObject(
-      String clusterName, String region, String project) throws Exception {
+  private static CoreV1Api buildKubernetesClientObject(ServerSpecification server)
+      throws Exception {
     // call the fetchGKECrednetials script that uses gcloud to generate the kubeconfig file
     List<String> scriptArgs = new ArrayList<>();
     scriptArgs.add("tools/fetchGKECredentials.sh");
-    scriptArgs.add(clusterName);
-    scriptArgs.add(region);
-    scriptArgs.add(project);
+    scriptArgs.add(server.clusterName);
+    scriptArgs.add(server.region);
+    scriptArgs.add(server.project);
     executeCommand("sh", scriptArgs);
 
     // path to kubeconfig file, that was just created/updated by gcloud get-credentials above
@@ -80,7 +59,9 @@ public final class KubernetesClientUtils {
     KubeConfig kubeConfig = KubeConfig.loadKubeConfig(filereader);
 
     // get a refreshed SA access token and its expiration time
-    AccessToken accessToken = getApplicationDefaultAccessToken();
+    GoogleCredentials applicationDefaultCredentials =
+        AuthenticationUtils.getApplicationDefaultCredential();
+    AccessToken accessToken = AuthenticationUtils.getAccessToken(applicationDefaultCredentials);
     Instant tokenExpiration = accessToken.getExpirationTime().toInstant();
     String expiryUTC = tokenExpiration.atZone(ZoneOffset.UTC).format(DateTimeFormatter.ISO_INSTANT);
 
@@ -91,13 +72,13 @@ public final class KubernetesClientUtils {
 
     LinkedHashMap<String, Object> authProviderSA = new LinkedHashMap<>();
     authProviderSA.put("name", "gcp");
-    authProviderSA.put("runner", authConfigSA);
+    authProviderSA.put("config", authConfigSA);
 
     LinkedHashMap<String, Object> userSA = new LinkedHashMap<>();
     userSA.put("auth-provider", authProviderSA);
 
     LinkedHashMap<String, Object> userWrapperSA = new LinkedHashMap<>();
-    userWrapperSA.put("name", clusterName);
+    userWrapperSA.put("name", server.clusterName);
     userWrapperSA.put("user", userSA);
 
     ArrayList<Object> usersList = new ArrayList<>();
@@ -105,34 +86,32 @@ public final class KubernetesClientUtils {
 
     // CONTEXTS: build list of one context, the specified cluster
     LinkedHashMap<String, Object> context = new LinkedHashMap<>();
-    context.put("cluster", clusterName);
-    context.put("user", clusterName); // when is the user ever different from the cluster name?
+    context.put("cluster", server.clusterName);
+    context.put(
+        "user", server.clusterName); // when is the user ever different from the cluster name?
 
     LinkedHashMap<String, Object> contextWrapper = new LinkedHashMap<>();
-    contextWrapper.put("name", clusterName);
+    contextWrapper.put("name", server.clusterName);
     contextWrapper.put("context", context);
 
     ArrayList<Object> contextsList = new ArrayList<>();
     contextsList.add(contextWrapper);
 
     // CLUSTERS: use the cluster list read in from the kubeconfig file, because I can't figure out
-    // how to get the
-    // certificate-authority-data and server address for the cluster via the Java client library,
-    // only with gcloud
+    // how to get the certificate-authority-data and server address for the cluster via the Java
+    // client library, only with gcloud
     ArrayList<Object> clusters = kubeConfig.getClusters();
 
     // build the config object, replacing the contexts and users lists from the kubeconfig file with
-    // the ones
-    // constructed programmatically above
+    // the ones constructed programmatically above
     kubeConfig = new KubeConfig(contextsList, clusters, usersList);
-    kubeConfig.setContext(clusterName);
+    kubeConfig.setContext(server.clusterName);
 
     // build the client object from the config
     ApiClient client = ClientBuilder.kubeconfig(kubeConfig).build();
 
     // set the global default client to the one created above because the CoreV1Api constructor gets
-    // the client
-    // object from the global configuration
+    // the client object from the global configuration
     Configuration.setDefaultApiClient(client);
 
     return new CoreV1Api();
@@ -169,13 +148,5 @@ public final class KubernetesClientUtils {
     V1PodList list =
         k8sclient.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
     return list.getItems();
-  }
-
-  // example usage. need to be on the Broad VPN to talk to the dev cluster because of IP whitelist
-  public static void main(String[] args) throws Exception {
-    CoreV1Api k8sclient = KubernetesClientUtils.getKubernetesClientObject();
-    for (V1Pod item : KubernetesClientUtils.listKubernetesPods(k8sclient)) {
-      System.out.println(item.getMetadata().getName());
-    }
   }
 }

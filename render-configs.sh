d4057bc905c0ded37c7da8a62a853dd7795a26e9
Dan Moran <danmoran@broadinstitute.org>
2020-07-07 14:27:46 -0400

    Revert "Fix?"
    This reverts commit f33cf2feaa403b7f3e9886d4eda56b1937284d5d.

--- render-configs.sh
+++ render-configs.sh
@@ -5,5 +5,6 @@ VAULT_TOKEN=${1:-$LOCAL_TOKEN}
 
 docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox:latest \
     vault read -format=json secret/dsde/datarepo/dev/sa-key.json | \
-    jq .data > /tmp/jade-dev-account.pem
+    jq .data | tee /tmp/jade-dev-account.json | \
+    jq -r .private_key > /tmp/jade-dev-account.pem
 

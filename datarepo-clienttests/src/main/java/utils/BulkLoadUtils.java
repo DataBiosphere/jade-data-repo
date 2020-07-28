9f555e15b5ac0c4621c84376cd5c9ce9cd52e007
Shelby Holden <sholden@broadinstitute.org>
2020-07-28 10:56:47 -0400

    DR-1166: Scale pods to 0 during file ingest and recover (#545)
    * first pass at making the bulk load more sharable
    
    * Add second configuration
    
    * wip
    
    * retry on failure
    
    * logging for debugging
    
    * wip
    
    * Working test
    
    * clean up
    
    * cleanup
    
    * add assert utility
    
    * auto formatting
    
    * Small change to prompt test rerun without overwriting old test results
    
    * switch to using hamcrest matcher
    
    * move the scale call to the individual tests

--- datarepo-clienttests/src/main/java/utils/BulkLoadUtils.java
+++ datarepo-clienttests/src/main/java/utils/BulkLoadUtils.java
@@ -1,4 +1,7 @@
-package testscripts;
+package utils;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 import bio.terra.datarepo.api.RepositoryApi;
 import bio.terra.datarepo.api.ResourcesApi;
@@ -9,33 +12,24 @@ import java.util.List;
 import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import utils.DataRepoUtils;
-import utils.FileUtils;
-import utils.KubernetesClientUtils;
 
-public class KubeConfig extends runner.TestScript {
-  private static final Logger logger = LoggerFactory.getLogger(KubeConfig.class);
+public class BulkLoadUtils {
+  private static final Logger logger = LoggerFactory.getLogger(BulkLoadUtils.class);
 
-  /** Public constructor so that this class can be instantiated via reflection. */
-  public KubeConfig() {
-    super();
-  }
-
-  private int filesToLoad;
   private String datasetCreator;
   private BillingProfileModel billingProfileModel;
   private DatasetSummaryModel datasetSummaryModel;
+  private String loadTag;
 
-  public void setParameters(List<String> parameters) throws Exception {
-    if (parameters == null || parameters.size() == 0) {
-      throw new IllegalArgumentException(
-          "Must provide a number of files to load in the parameters list");
-    } else {
-      filesToLoad = Integer.parseInt(parameters.get(0));
+  public String getDatasetId() {
+    if (datasetSummaryModel == null) {
+      throw new UnsupportedOperationException("datasetSummaryModel has not been defined.");
     }
+    return datasetSummaryModel.getId();
   }
 
-  public void setup(Map<String, ApiClient> apiClients) throws Exception {
+  public void bulkLoadSetup(Map<String, ApiClient> apiClients, String billingAccount)
+      throws Exception {
     // pick the first user to be the dataset creator
     List<String> apiClientList = new ArrayList<>(apiClients.keySet());
     datasetCreator = apiClientList.get(0);
@@ -48,7 +42,8 @@ public class KubeConfig extends runner.TestScript {
     // create a new profile
     billingProfileModel =
         DataRepoUtils.createProfile(resourcesApi, billingAccount, "profile-simple", true);
-    logger.info("Successfully created profile: {}", billingProfileModel.getProfileName());
+
+    logger.info("successfully created profile: " + billingProfileModel.getProfileName());
 
     // make the create dataset request and wait for the job to finish
     JobModel createDatasetJobResponse =
@@ -59,52 +54,16 @@ public class KubeConfig extends runner.TestScript {
     datasetSummaryModel =
         DataRepoUtils.expectJobSuccess(
             repositoryApi, createDatasetJobResponse, DatasetSummaryModel.class);
-    logger.info("Successfully created dataset: {}", datasetSummaryModel.getName());
-  }
 
-  // The purpose of this test is to have a long-running workload that completes successfully
-  // while we delete pods and have them recover.
-  public void userJourney(ApiClient apiClient) throws Exception {
-    RepositoryApi repositoryApi = new RepositoryApi(apiClient);
-
-    // set up and start bulk load job
-    BulkLoadArrayRequestModel arrayLoad = buildBulkLoadFileRequest();
-    JobModel bulkLoadArrayJobResponse =
-        repositoryApi.bulkFileLoadArray(datasetSummaryModel.getId(), arrayLoad);
-
-    // =========================================================================
-    /* Manipulating kubernetes pods during file ingest */
-
-    // initial poll as file ingest begins
-    bulkLoadArrayJobResponse =
-        DataRepoUtils.pollForRunningJob(repositoryApi, bulkLoadArrayJobResponse, 30);
-
-    if (bulkLoadArrayJobResponse.getJobStatus().equals(JobModel.JobStatusEnum.RUNNING)) {
-      logger.debug("Scaling pods down to 1");
-      KubernetesClientUtils.changeReplicaSetSizeAndWait(1);
-
-      // allow job to run on scaled down pods for interval
-      bulkLoadArrayJobResponse =
-          DataRepoUtils.pollForRunningJob(repositoryApi, bulkLoadArrayJobResponse, 30);
-
-      // if job still running, scale back up
-      if (bulkLoadArrayJobResponse.getJobStatus().equals(JobModel.JobStatusEnum.RUNNING)) {
-        logger.debug("Scaling pods back up to 4");
-        KubernetesClientUtils.changeReplicaSetSizeAndWait(4);
-      }
-    }
-    // =========================================================================
-
-    // wait for the job to complete and print out results
-    getAndDisplayResults(repositoryApi, bulkLoadArrayJobResponse);
+    logger.info("successfully created dataset: " + datasetSummaryModel.getName());
   }
 
   // This should run about 5 minutes on 2 DRmanager instances. The speed of loads is when
   // they are not local is about 2.5GB/minutes. With a fixed size of 1GB, each instance should do
   // 2.5 files per minute, so two instances should do 5 files per minute. To run 5 minutes we should
   // run 25 files.
-  private BulkLoadArrayRequestModel buildBulkLoadFileRequest() {
-    String loadTag = FileUtils.randomizeName("longtest");
+  public BulkLoadArrayRequestModel buildBulkLoadFileRequest(int filesToLoad) {
+    loadTag = FileUtils.randomizeName("longtest");
 
     BulkLoadArrayRequestModel arrayLoad =
         new BulkLoadArrayRequestModel()
@@ -135,15 +94,25 @@ public class KubeConfig extends runner.TestScript {
     return arrayLoad;
   }
 
-  private void getAndDisplayResults(RepositoryApi repositoryApi, JobModel bulkLoadArrayJobResponse)
+  public BulkLoadArrayRequestModel buildSecondArrayLoadTry() {
+    return new BulkLoadArrayRequestModel().profileId(billingProfileModel.getId()).loadTag(loadTag);
+  }
+
+  public void getAndDisplayResults(RepositoryApi repositoryApi, JobModel bulkLoadArrayJobResponse)
       throws Exception {
     bulkLoadArrayJobResponse =
         DataRepoUtils.waitForJobToFinish(repositoryApi, bulkLoadArrayJobResponse);
+
     BulkLoadArrayResultModel result =
         DataRepoUtils.expectJobSuccess(
             repositoryApi, bulkLoadArrayJobResponse, BulkLoadArrayResultModel.class);
 
     BulkLoadResultModel loadSummary = result.getLoadSummary();
+
+    assertThat(
+        "Number of successful files loaded should equal total files.",
+        loadSummary.getTotalFiles(),
+        equalTo(loadSummary.getSucceededFiles()));
     logger.debug("Total files    : {}", loadSummary.getTotalFiles());
     logger.debug("Succeeded files: {}", loadSummary.getSucceededFiles());
     logger.debug("Failed files   : {}", loadSummary.getFailedFiles());
@@ -162,10 +131,12 @@ public class KubeConfig extends runner.TestScript {
         DataRepoUtils.waitForJobToFinish(repositoryApi, deleteDatasetJobResponse);
     DataRepoUtils.expectJobSuccess(
         repositoryApi, deleteDatasetJobResponse, DeleteResponseModel.class);
+
     logger.info("Successfully deleted dataset: {}", datasetSummaryModel.getName());
 
     // delete the profile
     resourcesApi.deleteProfile(billingProfileModel.getId());
+
     logger.info("Successfully deleted profile: {}", billingProfileModel.getProfileName());
   }
 }

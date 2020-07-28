6dc0eae32a5133bffb8ab8ada98a47a1b0aa2728
rori <6863459+RoriCremer@users.noreply.github.com>
2020-07-28 11:56:09 -0400

    will need to add data loading

--- datarepo-clienttests/src/main/java/testscripts/SoftDeleteDataset.java
+++ datarepo-clienttests/src/main/java/testscripts/SoftDeleteDataset.java
@@ -18,11 +18,13 @@ import utils.DataRepoUtils;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import com.google.cloud.bigquery.TableResult;
+import utils.FileUtils;
 
 public class SoftDeleteDataset extends runner.TestScript {
   private static final Logger logger = LoggerFactory.getLogger(SoftDeleteDataset.class);
@@ -75,6 +77,63 @@ public class SoftDeleteDataset extends runner.TestScript {
     JobModel createDatasetJobResponse =
         DataRepoUtils.createDataset(
             repositoryApi, billingProfileModel.getId(), "dataset-simple.json", true);
+
+/*
+      // load data into the new dataset
+      // note that there's a fileref in the dataset
+      // ingest a file -- TODO CannedTestData.getMeA1KBFile
+      URI sourceUri = new URI("gs://jade-testdata/fileloadprofiletest/1KBfile.txt");
+
+      String targetPath = "/testrunner/IngestFile/" + FileUtils.randomizeName("") + ".txt";
+
+      BulkLoadFileModel fileLoadModel =
+          new BulkLoadFileModel()
+              .sourcePath(sourceUri.toString())
+              .description("IngestFile")
+              .mimeType("text/plain")
+              .targetPath(targetPath);
+      String loadTag = FileUtils.randomizeName("lookupTest");
+      BulkLoadArrayRequestModel fileLoadModelArray =
+          new BulkLoadArrayRequestModel()
+              .profileId(datasetSummaryModel.getDefaultProfileId())
+              .loadTag(loadTag)
+              .maxFailedFileLoads(0);
+      fileLoadModelArray.addLoadArrayItem(fileLoadModel);
+
+      JobModel ingestFileJobResponse =
+          repositoryApi.bulkFileLoadArray(datasetSummaryModel.getId(), fileLoadModelArray);
+      ingestFileJobResponse = DataRepoUtils.waitForJobToFinish(repositoryApi, ingestFileJobResponse);
+      BulkLoadArrayResultModel bulkLoadArrayResultModel =
+          DataRepoUtils.expectJobSuccess(
+              repositoryApi, ingestFileJobResponse, BulkLoadArrayResultModel.class);
+      String fileId = bulkLoadArrayResultModel.getLoadFileResults().get(0).getFileId();
+
+      // ingest the tabular data from the JSON file we just generated
+      // generate a JSON file with the fileref
+      String jsonLine =
+          "{\"VCF_File_Name\":\"name1\", \"Description\":\"description1\", \"VCF_File_Ref\":\""
+              + fileId
+              + "\"}\n";
+      byte[] fileRefBytes = jsonLine.getBytes(StandardCharsets.UTF_8);
+      // load a JSON file that contains the table rows to load into the test bucket
+      String jsonFileName = FileUtils.randomizeName("this-better-pass") + ".json";
+      String fileRefName = "scratch/testRetrieveSnapshot/" + jsonFileName;
+      String gsPath = FileUtils.createGsPath(fileRefBytes, fileRefName, testConfigGetIngestbucket);
+
+      IngestRequestModel ingestRequest =
+          new IngestRequestModel()
+              .format(IngestRequestModel.FormatEnum.JSON)
+              .table("vcf_file")
+              .path(gsPath);
+      JobModel ingestTabularDataJobResponse =
+          repositoryApi.ingestDataset(datasetSummaryModel.getId(), ingestRequest);
+
+      ingestTabularDataJobResponse =
+          DataRepoUtils.waitForJobToFinish(repositoryApi, ingestTabularDataJobResponse);
+      IngestResponseModel ingestResponse =
+          DataRepoUtils.expectJobSuccess(
+              repositoryApi, ingestTabularDataJobResponse, IngestResponseModel.class);
+      System.out.println("successfully loaded data into dataset: " + ingestResponse.getDataset()); */
 
     // save a reference to the dataset summary model so we can delete it in cleanup()
     datasetSummaryModel =

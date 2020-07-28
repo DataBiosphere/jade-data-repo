f1f975103d6b3adfcdf32810fe0788bb9e0911f0
Shelby Holden <sholden@broadinstitute.org>
2020-07-28 13:15:00 -0400

    change delete pod to use new test setup

--- datarepo-clienttests/src/main/java/testscripts/DeletePod.java
+++ datarepo-clienttests/src/main/java/testscripts/DeletePod.java
@@ -1,48 +1,105 @@
 package testscripts;
 
+import bio.terra.datarepo.api.RepositoryApi;
 import bio.terra.datarepo.client.ApiClient;
+import bio.terra.datarepo.client.ApiException;
 import bio.terra.datarepo.model.*;
-import io.kubernetes.client.openapi.models.*;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import utils.BulkLoadUtils;
+import utils.DataRepoUtils;
 import utils.KubernetesClientUtils;
 
 public class DeletePod extends runner.TestScript {
-  private static final Logger logger = LoggerFactory.getLogger(KubeConfig.class);
+    private static final Logger logger = LoggerFactory.getLogger(ScalePodsToZero.class);
 
-  /** Public constructor so that this class can be instantiated via reflection. */
-  public DeletePod() {
-    super();
-  }
-
-  private int filesToLoad;
-  private String datasetCreator;
-  private BillingProfileModel billingProfileModel;
-  private DatasetSummaryModel datasetSummaryModel;
-
-  public void setParameters(List<String> parameters) throws Exception {
-    if (parameters == null || parameters.size() == 0) {
-      throw new IllegalArgumentException(
-          "Must provide a number of files to load in the parameters list");
-    } else {
-      filesToLoad = Integer.parseInt(parameters.get(0));
+    /** Public constructor so that this class can be instantiated via reflection. */
+    public DeletePod() {
+        super();
     }
-  }
 
-  public void setup(Map<String, ApiClient> apiClients) throws Exception {}
+    private int filesToLoad;
+    private BulkLoadUtils bulkLoadUtils;
 
-  // The purpose of this test is to have a long-running workload that completes successfully
-  // while we delete pods and have them recover.
-  public void userJourney(ApiClient apiClient) throws Exception {
-    logger.debug("Deleteing pod");
-    V1Deployment apiDeployment = KubernetesClientUtils.getApiDeployment();
-    if (apiDeployment == null) {
-      throw new RuntimeException("API deployment not found.");
+    public void setParameters(List<String> parameters) throws Exception {
+        if (parameters == null || parameters.size() == 0) {
+            throw new IllegalArgumentException(
+                "Must provide a number of files to load in the parameters list");
+        } else {
+            filesToLoad = Integer.parseInt(parameters.get(0));
+        }
     }
-    KubernetesClientUtils.deleteRandomPod(apiDeployment);
-  }
 
-  public void cleanup(Map<String, ApiClient> apiClients) throws Exception {}
+    public void setup(Map<String, ApiClient> apiClients) throws Exception {
+        bulkLoadUtils = new BulkLoadUtils();
+        bulkLoadUtils.bulkLoadSetup(apiClients, billingAccount);
+    }
+
+    // The purpose of this test is to have a long-running workload that completes successfully
+    // while we scale pods to zero and then scale them back up.
+    public void userJourney(ApiClient apiClient) throws Exception {
+        RepositoryApi repositoryApi = new RepositoryApi(apiClient);
+
+        // set up and start bulk load job
+        BulkLoadArrayRequestModel arrayLoad = bulkLoadUtils.buildBulkLoadFileRequest(filesToLoad);
+        JobModel bulkLoadArrayJobResponse =
+            repositoryApi.bulkFileLoadArray(bulkLoadUtils.getDatasetId(), arrayLoad);
+
+        // =========================================================================
+        /* Manipulating kubernetes pods during file ingest */
+
+        // initial poll as file ingest begins
+        bulkLoadArrayJobResponse =
+            DataRepoUtils.pollForRunningJob(repositoryApi, bulkLoadArrayJobResponse, 30);
+
+        if (bulkLoadArrayJobResponse.getJobStatus().equals(JobModel.JobStatusEnum.RUNNING)) {
+            logger.debug("Scaling pods down to 0");
+            KubernetesClientUtils.changeReplicaSetSizeAndWait(0);
+
+            try {
+                bulkLoadArrayJobResponse =
+                    DataRepoUtils.pollForRunningJob(repositoryApi, bulkLoadArrayJobResponse, 30);
+            } catch (ApiException ex) {
+                logger.debug(
+                    "Catching expected exception while pod size = 0, Job Status: {}",
+                    bulkLoadArrayJobResponse.getJobStatus());
+            }
+
+            logger.debug("Scaling pods back up to 3.");
+            KubernetesClientUtils.changeReplicaSetSizeAndWait(3);
+            int retryCounter = 0;
+            // give the job a few chances to get a non-failing results while the pods are scaled back up.
+            ApiException lastException = null;
+            while (bulkLoadArrayJobResponse.getJobStatus().equals(JobModel.JobStatusEnum.RUNNING)
+                && retryCounter < 10) {
+                retryCounter++;
+                try {
+                    bulkLoadArrayJobResponse =
+                        DataRepoUtils.pollForRunningJob(repositoryApi, bulkLoadArrayJobResponse, 30);
+                    lastException = null;
+                } catch (ApiException ex) {
+                    logger.debug(
+                        "Catching expected error while we wait for pods to scale back up. Retry # {}",
+                        retryCounter);
+                    lastException = ex;
+                    TimeUnit.SECONDS.sleep(30);
+                }
+            }
+            if (lastException != null) {
+                throw lastException;
+            }
+        }
+        // =========================================================================
+
+        // wait for the job to complete and print out results
+        bulkLoadUtils.getAndDisplayResults(repositoryApi, bulkLoadArrayJobResponse);
+    }
+
+    public void cleanup(Map<String, ApiClient> apiClients) throws Exception {
+        KubernetesClientUtils.changeReplicaSetSizeAndWait(1);
+        bulkLoadUtils.cleanup(apiClients);
+    }
 }

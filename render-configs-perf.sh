7b212af7fa65a2509840954b32345d61ce392be5
Shelby Holden <sholden@broadinstitute.org>
2020-08-06 11:02:29 -0400

    closer to working

--- render-configs-perf.sh
+++ render-configs-perf.sh
@@ -4,6 +4,6 @@ LOCAL_TOKEN=$(cat ~/.vault-token)
 VAULT_TOKEN=${1:-$LOCAL_TOKEN}
 
 docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox:latest \
-    vault read -format=json secret/dsde/datarepo/perf/sa-key.json | \
-    jq .data | tee /tmp/jade-perf-account.json | \
+    vault read -format=json secret/dsde/datarepo/perf/datarepo-api-sa  | jq -r .data.key | base64 --decode \
+    | tee /tmp/jade-perf-account.json | \
     jq -r .private_key > /tmp/jade-perf-account.pem

23f425b6dabd151a81a79e1ecbb971dd01d1bacd
rori <6863459+RoriCremer@users.noreply.github.com>
2020-08-11 14:29:18 -0400

    update test

--- datarepo-clienttests/src/main/java/testscripts/SoftDeleteDatasetManyRows.java
+++ datarepo-clienttests/src/main/java/testscripts/SoftDeleteDatasetManyRows.java
@@ -18,9 +18,12 @@ import bio.terra.datarepo.model.IngestRequestModel;
 import bio.terra.datarepo.model.IngestResponseModel;
 import bio.terra.datarepo.model.JobModel;
 import com.google.api.client.util.Charsets;
+import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.TableResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import runner.config.TestUserSpecification;
+import testscripts.baseclasses.SimpleDataset;
 import utils.BigQueryUtils;
 import utils.DataRepoUtils;
 import utils.FileUtils;
@@ -38,9 +41,10 @@ import java.util.stream.StreamSupport;
 
 import static java.lang.Integer.max;
 import static java.lang.Integer.min;
+import static java.lang.Math.random;
 import static java.lang.Math.round;
 
-public class SoftDeleteDatasetManyRows extends runner.TestScript {
+public class SoftDeleteDatasetManyRows extends SimpleDataset {
   private static final Logger logger = LoggerFactory.getLogger(SoftDeleteDatasetManyRows.class);
 
   /** Public constructor so that this class can be instantiated via reflection. */
@@ -50,53 +54,41 @@ public class SoftDeleteDatasetManyRows extends runner.TestScript {
 
   private Integer datasetRowsCount;
   private Integer deleteRowsCount;
-  private String datasetCreator;
-  private BillingProfileModel billingProfileModel;
-  private DatasetSummaryModel datasetSummaryModel;
-  private List<DataDeletionRequest> dataDeletionRequestList;
+  private String dataProject;
+  private String tableName;
+  private DatasetModel datasetModel;
 
   private String testConfigGetIngestbucket;
+  String dirInCloud = "scratch/softDel";
 
     public void setParameters(List<String> parameters) {
-        if (parameters == null || parameters.size() == 0) {
-            throw new IllegalArgumentException( // or should there be a default number set?
-                "Must provide a number of how many rows for the dataset to soft delete from");
-        } else {
-            // TODO, check for the larger number and make that the one that the is the dataset? Is max / min too lazy?
-            datasetRowsCount = max(Integer.parseInt(parameters.get(0)), Integer.parseInt(parameters.get(1)));
-            deleteRowsCount = min(Integer.parseInt(parameters.get(0)), Integer.parseInt(parameters.get(1)));
-            logger.debug("Source dataset row count: {}, and count to delete: {}", datasetRowsCount, deleteRowsCount);
-        }
-    }
+      if (parameters == null || parameters.size() == 0) {
+          throw new IllegalArgumentException( // or should there be a default number set?
+              "Must provide the number of rows in each soft delete request");
+      } else {
+          // Hard-code the size of the dataset (say 10k rows or 1 million if we can ingest that in a reasonable amount of time).
+          // Accept as a parameter to this test script the number of rows in each soft delete request.
+          datasetRowsCount = 10000;
+          deleteRowsCount = Integer.parseInt(parameters.get(0));
+          // Validate that the number of rows in each soft delete request <= hard-coded dataset size.
+          if (deleteRowsCount > datasetRowsCount) {
+              throw new IllegalArgumentException(
+                  "Number of rows to soft delete per request must be less than number of rows in the dataset");
+          }
+          logger.debug("Source dataset row count: {}, and count to delete per request: {}",
+              datasetRowsCount, deleteRowsCount);
+      }
+  }
 
+  public void setup(List<TestUserSpecification> testUsers) throws Exception {
+    // create the profile and dataset
+    super.setup(testUsers);
 
-  public void setup(Map<String, ApiClient> apiClients) throws Exception {
-    // pick the first user to be the dataset creator
-    List<String> apiClientList = new ArrayList<>(apiClients.keySet());
-    datasetCreator = apiClientList.get(0);
-
-    // get the ApiClient for the dataset creator --- and the snapshot creator?
-    ApiClient datasetCreatorClient = apiClients.get(datasetCreator);
-    ResourcesApi resourcesApi = new ResourcesApi(datasetCreatorClient);
+    // get the ApiClient for the dataset creator
+    ApiClient datasetCreatorClient = DataRepoUtils.getClientForTestUser(datasetCreator, server);
     RepositoryApi repositoryApi = new RepositoryApi(datasetCreatorClient);
 
-    testConfigGetIngestbucket = "jade-testdata";
-
-    // create a new profile
-    billingProfileModel =
-        DataRepoUtils.createProfile(resourcesApi, billingAccount, "profile-simple", true);
-    logger.info("Successfully created profile: {}", billingProfileModel.getProfileName());
-
-    // make the create dataset request and wait for the job to finish
-    JobModel createDatasetJobResponse =
-        DataRepoUtils.createDataset(
-            repositoryApi, billingProfileModel.getId(), "dataset-simpler.json", true);
-
-    // save a reference to the dataset summary model so we can delete it in cleanup()
-    datasetSummaryModel =
-        DataRepoUtils.expectJobSuccess(
-            repositoryApi, createDatasetJobResponse, DatasetSummaryModel.class);
-    logger.info("Successfully created dataset: {}", datasetSummaryModel.getName());
+    testConfigGetIngestbucket = "jade-testdata"; // this could be put in DRUtils
 
     // load data into the new dataset-- no fileref necessary
     // TODO I need to load in a lot of rows---but no file refs necessary
@@ -107,11 +99,10 @@ public class SoftDeleteDatasetManyRows extends runner.TestScript {
             + i
             + "\",  \"Description\":\"description1\"}\n";
         newRows.write((jsonLine).getBytes(Charsets.UTF_8));
-    }
+    } // TODO just make this file once like today tho --- and make sure it has the index as it's value for pulling out of postges?
 
     byte[] newRowBytes = newRows.toByteArray();
     String jsonFileName = FileUtils.randomizeName("this-better-pass") + ".json";
-    String dirInCloud = "scratch/softDel";
     String fileRefName = dirInCloud + "/" + jsonFileName;
     String gsPath = FileUtils.createGsPath(newRowBytes, fileRefName, testConfigGetIngestbucket);
 
@@ -131,110 +122,83 @@ public class SoftDeleteDatasetManyRows extends runner.TestScript {
     logger.info("Successfully loaded data into dataset: {}", ingestResponse.getDataset());
 
     String datasetId = datasetSummaryModel.getId();
-    DatasetModel datasetModel = repositoryApi.retrieveDataset(datasetId);
-    String dataProject = datasetModel.getDataProject();
-    String tableName = datasetModel.getSchema().getTables().get(0).getName();
-
-    // get row ids for table to determine what to delete
-      // TODO create multiple soft delete files that can all be used to separately delete rows
-      // TODO how do we want to mod out the # we delete?
-      // we need many files and those files can be picked randomly during the actual test
-
-    Integer deletionFilesCount = round(datasetRowsCount / deleteRowsCount);
-    Long limit = deleteRowsCount.longValue();
-
-      for (int j = 0; j < deletionFilesCount; j++) { // TODO this will get the same limit / group each query, yeah?
-          String sqlQuery =
-              BigQueryUtils.constructQuery(
-                  dataProject,
-                  BigQueryUtils.getDatasetName(datasetModel.getName()),
-                  tableName,
-                  "datarepo_row_id",
-                  limit);
-
-          TableResult result = BigQueryUtils.queryBigQuery(dataProject, sqlQuery);
-          List<String> rowIds =
-              StreamSupport.stream(result.getValues().spliterator(), false)
-                  .map(fieldValues -> fieldValues.get(0).getStringValue())
-                  .collect(Collectors.toList());
-
-          logger.info("Successfully retrieved row ids during round {} for table: {}", j, tableName);
-          // write to GCS
-          String csvFileName = FileUtils.randomizeName("this-too-better-pass")+ j + ".csv";
-          String csvFileRefName = dirInCloud + "/" + csvFileName;
-
-          ByteArrayOutputStream output = new ByteArrayOutputStream();
-          for (String line : rowIds) {
-              output.write((line + "\n").getBytes(Charsets.UTF_8));
-          }
-          byte[] bytes = output.toByteArray();
-          String gcsPath = FileUtils.createGsPath(bytes, csvFileRefName, testConfigGetIngestbucket);
-
-          // build the deletion request with pointers to the file with row ids to soft delete
-          DataDeletionGcsFileModel deletionGcsFileModel =
-              new DataDeletionGcsFileModel()
-                  .fileType(DataDeletionGcsFileModel.FileTypeEnum.CSV)
-                  .path(gcsPath);
-          DataDeletionTableModel deletionTableFile =
-              new DataDeletionTableModel().tableName(tableName).gcsFileSpec(deletionGcsFileModel);
-          List<DataDeletionTableModel> dataDeletionTableModels =
-              Collections.singletonList(deletionTableFile);
-          DataDeletionRequest dataDeletionRequest =
-              new DataDeletionRequest()
-                  .deleteType(DataDeletionRequest.DeleteTypeEnum.SOFT)
-                  .specType(DataDeletionRequest.SpecTypeEnum.GCSFILE)
-                  .tables(dataDeletionTableModels);
-
-          dataDeletionRequestList.add(dataDeletionRequest);
-      }
+    datasetModel = repositoryApi.retrieveDataset(datasetId);
+    dataProject = datasetModel.getDataProject();
+    tableName = datasetModel.getSchema().getTables().get(0).getName();
   }
 
-  public void userJourney(ApiClient apiClient) throws Exception {
+  public void userJourney(TestUserSpecification testUser) throws Exception {
+    ApiClient apiClient = DataRepoUtils.getClientForTestUser(testUser, server);
     RepositoryApi repositoryApi = new RepositoryApi(apiClient);
 
-    // send off the soft delete requests
-    dataDeletionRequestList.forEach( dataDeletionRequest -> {
-        JobModel softDeleteJobResponse;
-        DeleteResponseModel deleteResponseModel;
-        try {
-        softDeleteJobResponse = repositoryApi.applyDatasetDataDeletion(datasetSummaryModel.getId(), dataDeletionRequest);
-        softDeleteJobResponse = DataRepoUtils.waitForJobToFinish(repositoryApi, softDeleteJobResponse);
-        deleteResponseModel =
-            DataRepoUtils.expectJobSuccess(
-                repositoryApi, softDeleteJobResponse, DeleteResponseModel.class);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        logger.debug(
-            "Successfully soft deleted rows from dataset: {} with state {}",
-            datasetSummaryModel.getName(),
-            deleteResponseModel.getObjectState()); // TODO is this line needed?
-        }
-    );
+    // create dataDeletionRequest: query BQ, write the row ids into a file, load into GCS, build request
 
+    // For the BQ query to get the rowids to delete, pick a random chunk of rows
+    // So if the parameter is 5 (rows per soft delete request), then we select 5 rows from BQ at a random offset.
+    long limit = Double.valueOf(random()).longValue(); // (rows per soft delete request)
+    long offset = limit + deleteRowsCount.longValue(); // (rows per soft delete request)
 
+    String sqlQuery =
+        BigQueryUtils.buildSelectQuery(
+            dataProject,
+            BigQueryUtils.getDatasetName(datasetModel.getName()),
+            tableName,
+            "datarepo_row_id",
+            limit,
+            offset);
 
+    BigQuery bigQueryClient =
+        BigQueryUtils.getClientForTestUser(datasetCreator, datasetModel.getDataProject());
+    TableResult result = BigQueryUtils.queryBigQuery(bigQueryClient, sqlQuery);
+    List<String> rowIds =
+        StreamSupport.stream(result.getValues().spliterator(), false)
+            .map(fieldValues -> fieldValues.get(0).getStringValue())
+            .collect(Collectors.toList());
+
+    logger.info("Successfully retrieved row ids for table: {}", tableName);
+    // write to GCS
+    String csvFileName = FileUtils.randomizeName("this-too-better-pass") + ".csv";
+    String csvFileRefName = dirInCloud + "/" + csvFileName;
+
+    ByteArrayOutputStream output = new ByteArrayOutputStream();
+    for (String line : rowIds) {
+        output.write((line + "\n").getBytes(Charsets.UTF_8));
+    }
+    byte[] bytes = output.toByteArray();
+    String gcsPath = FileUtils.createGsPath(bytes, csvFileRefName, testConfigGetIngestbucket);
+
+    // build the deletion request with pointers to the file with row ids to soft delete
+    DataDeletionGcsFileModel deletionGcsFileModel =
+        new DataDeletionGcsFileModel()
+            .fileType(DataDeletionGcsFileModel.FileTypeEnum.CSV)
+            .path(gcsPath);
+    DataDeletionTableModel deletionTableFile =
+        new DataDeletionTableModel().tableName(tableName).gcsFileSpec(deletionGcsFileModel);
+    List<DataDeletionTableModel> dataDeletionTableModels =
+        Collections.singletonList(deletionTableFile);
+    DataDeletionRequest dataDeletionRequest =
+        new DataDeletionRequest()
+            .deleteType(DataDeletionRequest.DeleteTypeEnum.SOFT)
+            .specType(DataDeletionRequest.SpecTypeEnum.GCSFILE)
+            .tables(dataDeletionTableModels);
+
+    // send off the soft delete request
+    JobModel softDeleteJobResponse =
+        repositoryApi.applyDatasetDataDeletion(datasetSummaryModel.getId(), dataDeletionRequest);
+    softDeleteJobResponse = DataRepoUtils.waitForJobToFinish(repositoryApi, softDeleteJobResponse);
+    DeleteResponseModel deleteResponseModel =
+        DataRepoUtils.expectJobSuccess(repositoryApi, softDeleteJobResponse, DeleteResponseModel.class);
+    logger.debug(
+        "Successfully soft deleted rows from dataset: {} with state {}",
+        datasetSummaryModel.getName(),
+        deleteResponseModel.getObjectState());
   }
 
-  public void cleanup(Map<String, ApiClient> apiClients) throws Exception {
-    // get the ApiClient for the dataset creator
-    ApiClient datasetCreatorClient = apiClients.get(datasetCreator);
-    ResourcesApi resourcesApi = new ResourcesApi(datasetCreatorClient);
-    RepositoryApi repositoryApi = new RepositoryApi(datasetCreatorClient);
-
-    // make the delete request and wait for the job to finish
-    JobModel deleteDatasetJobResponse = repositoryApi.deleteDataset(datasetSummaryModel.getId());
-    deleteDatasetJobResponse =
-        DataRepoUtils.waitForJobToFinish(repositoryApi, deleteDatasetJobResponse);
-    DataRepoUtils.expectJobSuccess(
-        repositoryApi, deleteDatasetJobResponse, DeleteResponseModel.class);
-    logger.info("Successfully deleted dataset: {}", datasetSummaryModel.getName());
+  public void cleanup(List<TestUserSpecification> testUsers) throws Exception {
+    // delete the profile and dataset
+    super.cleanup(testUsers);
 
     // delete scratch files
     FileUtils.cleanupScratchFiles(testConfigGetIngestbucket);
-
-    // delete the profile
-    resourcesApi.deleteProfile(billingProfileModel.getId());
-    logger.info("Successfully deleted profile: {}", billingProfileModel.getProfileName());
   }
 }

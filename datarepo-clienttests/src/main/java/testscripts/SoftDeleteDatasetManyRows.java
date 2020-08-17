f70a7c2cd73025ebce5bae5e1b72554ed6eb365a
rori <6863459+RoriCremer@users.noreply.github.com>
2020-08-17 13:56:17 -0400

    move testing file to cloud bucket

--- datarepo-clienttests/src/main/java/testscripts/SoftDeleteDatasetManyRows.java
+++ datarepo-clienttests/src/main/java/testscripts/SoftDeleteDatasetManyRows.java
@@ -1,18 +1,11 @@
 package testscripts;
 
 import bio.terra.datarepo.api.RepositoryApi;
-import bio.terra.datarepo.api.ResourcesApi;
 import bio.terra.datarepo.client.ApiClient;
-import bio.terra.datarepo.client.ApiException;
-import bio.terra.datarepo.model.BillingProfileModel;
-import bio.terra.datarepo.model.BulkLoadArrayRequestModel;
-import bio.terra.datarepo.model.BulkLoadArrayResultModel;
-import bio.terra.datarepo.model.BulkLoadFileModel;
 import bio.terra.datarepo.model.DataDeletionGcsFileModel;
 import bio.terra.datarepo.model.DataDeletionRequest;
 import bio.terra.datarepo.model.DataDeletionTableModel;
 import bio.terra.datarepo.model.DatasetModel;
-import bio.terra.datarepo.model.DatasetSummaryModel;
 import bio.terra.datarepo.model.DeleteResponseModel;
 import bio.terra.datarepo.model.IngestRequestModel;
 import bio.terra.datarepo.model.IngestResponseModel;
@@ -20,6 +13,11 @@ import bio.terra.datarepo.model.JobModel;
 import com.google.api.client.util.Charsets;
 import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.TableResult;
+import java.io.ByteArrayOutputStream;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import runner.config.TestUserSpecification;
@@ -27,22 +25,6 @@ import testscripts.baseclasses.SimpleDataset;
 import utils.BigQueryUtils;
 import utils.DataRepoUtils;
 import utils.FileUtils;
-
-import java.io.ByteArrayOutputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.StreamSupport;
-
-import static java.lang.Integer.max;
-import static java.lang.Integer.min;
-import static java.lang.Math.random;
-import static java.lang.Math.round;
 
 public class SoftDeleteDatasetManyRows extends SimpleDataset {
   private static final Logger logger = LoggerFactory.getLogger(SoftDeleteDatasetManyRows.class);
@@ -61,23 +43,26 @@ public class SoftDeleteDatasetManyRows extends SimpleDataset {
   private String testConfigGetIngestbucket;
   String dirInCloud = "scratch/softDel";
 
-    public void setParameters(List<String> parameters) {
-      if (parameters == null || parameters.size() == 0) {
-          throw new IllegalArgumentException( // or should there be a default number set?
-              "Must provide the number of rows in each soft delete request");
-      } else {
-          // Hard-code the size of the dataset (say 10k rows or 1 million if we can ingest that in a reasonable amount of time).
-          // Accept as a parameter to this test script the number of rows in each soft delete request.
-          datasetRowsCount = 10000;
-          deleteRowsCount = Integer.parseInt(parameters.get(0));
-          // Validate that the number of rows in each soft delete request <= hard-coded dataset size.
-          if (deleteRowsCount > datasetRowsCount) {
-              throw new IllegalArgumentException(
-                  "Number of rows to soft delete per request must be less than number of rows in the dataset");
-          }
-          logger.debug("Source dataset row count: {}, and count to delete per request: {}",
-              datasetRowsCount, deleteRowsCount);
+  public void setParameters(List<String> parameters) {
+    if (parameters == null || parameters.size() == 0) {
+      throw new IllegalArgumentException( // or should there be a default number set?
+          "Must provide the number of rows in each soft delete request");
+    } else {
+      // Hard-code the size of the dataset (say 10k rows or 1 million if we can ingest that in a
+      // reasonable amount of time).
+      // Accept as a parameter to this test script the number of rows in each soft delete request.
+      datasetRowsCount = 10000;
+      deleteRowsCount = Integer.parseInt(parameters.get(0));
+      // Validate that the number of rows in each soft delete request <= hard-coded dataset size.
+      if (deleteRowsCount > datasetRowsCount) {
+        throw new IllegalArgumentException(
+            "Number of rows to soft delete per request must be less than number of rows in the dataset");
       }
+      logger.debug(
+          "Source dataset row count: {}, and count to delete per request: {}",
+          datasetRowsCount,
+          deleteRowsCount);
+    }
   }
 
   public void setup(List<TestUserSpecification> testUsers) throws Exception {
@@ -91,25 +76,17 @@ public class SoftDeleteDatasetManyRows extends SimpleDataset {
     testConfigGetIngestbucket = "jade-testdata"; // this could be put in DRUtils
 
     // load data into the new dataset-- no fileref necessary
-    // TODO I need to load in a lot of rows---but no file refs necessary
-    ByteArrayOutputStream newRows = new ByteArrayOutputStream();
-    for (int i = 0; i < datasetRowsCount; i++) {
-        String jsonLine =
-            "{\"Name\":\""
-            + i
-            + "\",  \"Description\":\"description1\"}\n";
-        newRows.write((jsonLine).getBytes(Charsets.UTF_8));
-    } // TODO just make this file once like today tho --- and make sure it has the index as it's value for pulling out of postges?
+    // load 10,000,000 rows into the dataset table -- 10 mil rows at once is successful---but we
+    // time out w 100,000,000. To speed up this test, use bulk_test_delete_10000.json
 
-    byte[] newRowBytes = newRows.toByteArray();
-    String jsonFileName = FileUtils.randomizeName("this-better-pass") + ".json";
-    String fileRefName = dirInCloud + "/" + jsonFileName;
-    String gsPath = FileUtils.createGsPath(newRowBytes, fileRefName, testConfigGetIngestbucket);
+    String fileRefName = "scratch/bulk_test_delete/bulk_test_delete_10000000.json";
+    String gsPath = String.format("gs://%s/%s", testConfigGetIngestbucket, fileRefName);
+    logger.info("Loading data from: {}", gsPath);
 
     IngestRequestModel ingestRequest =
         new IngestRequestModel()
             .format(IngestRequestModel.FormatEnum.JSON)
-            .table("table")
+            .table("vcf_file")
             .path(gsPath);
     JobModel ingestTabularDataJobResponse =
         repositoryApi.ingestDataset(datasetSummaryModel.getId(), ingestRequest);
@@ -130,13 +107,19 @@ public class SoftDeleteDatasetManyRows extends SimpleDataset {
   public void userJourney(TestUserSpecification testUser) throws Exception {
     ApiClient apiClient = DataRepoUtils.getClientForTestUser(testUser, server);
     RepositoryApi repositoryApi = new RepositoryApi(apiClient);
-
-    // create dataDeletionRequest: query BQ, write the row ids into a file, load into GCS, build request
+    // create dataDeletionRequest: query BQ, write row ids into file, load into GCS, build request
 
     // For the BQ query to get the rowids to delete, pick a random chunk of rows
-    // So if the parameter is 5 (rows per soft delete request), then we select 5 rows from BQ at a random offset.
-    long limit = Double.valueOf(random()).longValue(); // (rows per soft delete request)
-    long offset = limit + deleteRowsCount.longValue(); // (rows per soft delete request)
+    // So if the parameter is 5 (rows per soft delete request), then we select 5 rows from BQ at a
+    // random offset.
+    long leftLimit = 0L;
+    long rightLimit = datasetRowsCount;
+    long offset =
+        leftLimit
+            + (long)
+                (Math.random()
+                    * (rightLimit - leftLimit)); // (starting point for soft delete request)
+    long limit = deleteRowsCount.longValue(); // (rows per soft delete request)
 
     String sqlQuery =
         BigQueryUtils.buildSelectQuery(
@@ -155,17 +138,18 @@ public class SoftDeleteDatasetManyRows extends SimpleDataset {
             .map(fieldValues -> fieldValues.get(0).getStringValue())
             .collect(Collectors.toList());
 
-    logger.info("Successfully retrieved row ids for table: {}", tableName);
+    logger.info("Successfully retrieved {} row ids for table: {}", rowIds.size(), tableName);
     // write to GCS
     String csvFileName = FileUtils.randomizeName("this-too-better-pass") + ".csv";
     String csvFileRefName = dirInCloud + "/" + csvFileName;
 
     ByteArrayOutputStream output = new ByteArrayOutputStream();
     for (String line : rowIds) {
-        output.write((line + "\n").getBytes(Charsets.UTF_8));
+      output.write((line + "\n").getBytes(Charsets.UTF_8));
     }
     byte[] bytes = output.toByteArray();
     String gcsPath = FileUtils.createGsPath(bytes, csvFileRefName, testConfigGetIngestbucket);
+    logger.info("Successfully created file for row ids to soft delete: {}", gcsPath);
 
     // build the deletion request with pointers to the file with row ids to soft delete
     DataDeletionGcsFileModel deletionGcsFileModel =
@@ -187,7 +171,9 @@ public class SoftDeleteDatasetManyRows extends SimpleDataset {
         repositoryApi.applyDatasetDataDeletion(datasetSummaryModel.getId(), dataDeletionRequest);
     softDeleteJobResponse = DataRepoUtils.waitForJobToFinish(repositoryApi, softDeleteJobResponse);
     DeleteResponseModel deleteResponseModel =
-        DataRepoUtils.expectJobSuccess(repositoryApi, softDeleteJobResponse, DeleteResponseModel.class);
+        DataRepoUtils.expectJobSuccess(
+            repositoryApi, softDeleteJobResponse, DeleteResponseModel.class);
+
     logger.debug(
         "Successfully soft deleted rows from dataset: {} with state {}",
         datasetSummaryModel.getName(),

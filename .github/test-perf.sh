e016b1d6305075c0ed8bfe5f9f38c96270a5c5ce
Shelby Holden <sholden@broadinstitute.org>
2020-09-14 13:53:46 -0400

    update shell script

--- .github/test-perf.sh
+++ .github/test-perf.sh
@@ -2,7 +2,7 @@
 BRANCH="sh-DR-1116-perf-action"
 TOKEN=$(cat $HOME/.gh_token)
 REPO="DataBiosphere/jade-data-repo"
-WORKFLOW="perf-tests.yaml"
+WORKFLOW="test-runner-on-perf.yml"
 
 curl -H "Accept: application/vnd.github.everest-preview+json" \
     -H "Authorization: token ${TOKEN}" \

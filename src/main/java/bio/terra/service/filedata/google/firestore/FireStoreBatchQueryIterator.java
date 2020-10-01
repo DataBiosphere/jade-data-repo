a413b8d9be487eef47203b818663c379f499fadf
ddietterich <ddietterich@verily.com>
2020-10-01 13:55:19 -0400

    Add a sleep

--- src/main/java/bio/terra/service/filedata/google/firestore/FireStoreBatchQueryIterator.java
+++ src/main/java/bio/terra/service/filedata/google/firestore/FireStoreBatchQueryIterator.java
@@ -14,6 +14,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class FireStoreBatchQueryIterator {
     private static final Logger logger = LoggerFactory.getLogger(FireStoreBatchQueryIterator.class);
@@ -85,6 +86,8 @@ public class FireStoreBatchQueryIterator {
                     throw new FileSystemExecutionException("get batch execution exception", ex);
                 }
             }
+
+            TimeUnit.MILLISECONDS.sleep(SLEEP_MILLISECONDS);
         }
         throw new FileSystemExecutionException("get batch failed - no more retries");
     }

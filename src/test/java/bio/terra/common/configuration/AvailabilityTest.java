6038749007c0e8ee021341be9dd19fe327876a47
Shelby Holden <sholden@broadinstitute.org>
2020-10-20 23:16:55 -0400

    working livness and readiness endpoints

--- src/test/java/bio/terra/common/configuration/AvailabilityTest.java
+++ src/test/java/bio/terra/common/configuration/AvailabilityTest.java
@@ -1,6 +1,5 @@
 package bio.terra.common.configuration;
 
-import bio.terra.common.*;
 import bio.terra.common.category.*;
 import org.junit.*;
 import org.junit.experimental.categories.*;
@@ -9,12 +8,9 @@ import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.availability.*;
 import org.springframework.boot.test.autoconfigure.web.servlet.*;
 import org.springframework.boot.test.context.*;
-import org.springframework.http.*;
-import org.springframework.mock.web.*;
 import org.springframework.test.context.junit4.*;
 import org.springframework.test.web.servlet.*;
 
-import static org.junit.Assert.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -25,27 +21,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class AvailabilityTest {
 
     @Autowired
-    ApplicationAvailability applicationAvailability;
-
-    @Autowired
     private MockMvc mvc;
 
-   @Test
-   public void test() throws Exception {
-       /*ReadinessState state = applicationAvailability.getReadinessState();
-       assertThat(state)
+    @Test
+    public void test() throws Exception {
+        /*ReadinessState state = applicationAvailability.getReadinessState();
+            assertThat(state)
            .isEqualTo(ReadinessState.ACCEPTING_TRAFFIC);*/
-       ResultActions result = mvc.perform(get("/actuator/health"));
-       result.andExpect(status().isOk())
-           .andExpect(jsonPath("$.status").value("UP"));
+        ResultActions result = mvc.perform(get("/actuator/health/liveness"));
+        result.andExpect(status().isOk())
+            .andExpect(jsonPath("$.status").value("UP"));
+        ResultActions readinessResult = mvc.perform(get("/actuator/health/readiness"));
+        readinessResult.andExpect(status().isOk())
+            .andExpect(jsonPath("$.status").value("UP"));
 
-
-       /*AvailabilityChangeEvent.publish(context, ReadinessState.REFUSING_TRAFFIC);
-
-       assertThat(applicationAvailability.getReadinessState())
+        /*AvailabilityChangeEvent.publish(context, ReadinessState.REFUSING_TRAFFIC);
+            assertThat(applicationAvailability.getReadinessState())
            .isEqualTo(ReadinessState.REFUSING_TRAFFIC);
-       mockMvc.perform(get("/actuator/health/readiness"))
+            mockMvc.perform(get("/actuator/health/readiness"))
            .andExpect(status().isServiceUnavailable())
            .andExpect(jsonPath("$.status").value("OUT_OF_SERVICE"));*/
-   }
+    }
 }

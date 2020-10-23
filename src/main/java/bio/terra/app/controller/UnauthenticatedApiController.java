155e71f3b80530bfb3fc19d11b36a0a9a5083ce8
Michael Yessaillian <myessail@broadinstitute.org>
2020-10-23 13:54:49 -0400

    cleanup

--- src/main/java/bio/terra/app/controller/UnauthenticatedApiController.java
+++ src/main/java/bio/terra/app/controller/UnauthenticatedApiController.java
@@ -114,14 +114,4 @@ public class UnauthenticatedApiController implements UnauthenticatedApi {
         }
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
-
-//    /**
-//     * Home redirection to swagger api documentation
-//     */
-//    @RequestMapping(value = "/")
-//    public String index() {
-//        System.out.println("swagger-ui.html");
-//        return "redirect:swagger-ui.html";
-//    }
-
 }

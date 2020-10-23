1d78aca17d7722391854bd549199f923a0cda700
Shelby Holden <sholden@broadinstitute.org>
2020-10-23 13:34:10 -0400

    cleanup logging

--- src/main/java/bio/terra/app/logging/AvailabilityListener.java
+++ src/main/java/bio/terra/app/logging/AvailabilityListener.java
@@ -1,6 +1,8 @@
 package bio.terra.app.logging;
 
-import org.springframework.boot.availability.*;
+import org.springframework.boot.availability.LivenessState;
+import org.springframework.boot.availability.ReadinessState;
+import org.springframework.boot.availability.AvailabilityChangeEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import org.slf4j.Logger;
@@ -12,18 +14,29 @@ public class AvailabilityListener {
 
     @EventListener
     public void onLivenessEvent(AvailabilityChangeEvent<LivenessState> event) {
-        logger.info("AvailabilityChangeEvent - LivenessState: {}", event.getState());
         switch (event.getState()) {
             case BROKEN:
-                // notify others
+                logger.error("LivenessState: {}", event.getState());
                 break;
             case CORRECT:
-                // we're back
+                logger.debug("LivenessState: {}", event.getState());
+                break;
+            default:
+                logger.error("LivenessState {} is not a known state", event.getState());
         }
     }
 
     @EventListener
     public void onReadinessEvent(AvailabilityChangeEvent<ReadinessState> event) {
-        logger.info("AvailabilityChangeEvent - ReadinessState: {}", event.getState());
+        switch (event.getState()) {
+            case REFUSING_TRAFFIC:
+                logger.error("ReadinessState: {}", event.getState());
+                break;
+            case ACCEPTING_TRAFFIC:
+                logger.debug("ReadinessState: {}", event.getState());
+                break;
+            default:
+                logger.error("ReadinessState {} is not a known state", event.getState());
+        }
     }
 }

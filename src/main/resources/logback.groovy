0c391d53c30dccf7a2b615fd731d604fa6de9c9d
nmalfroy <nmalfroy@broadinstitute.org>
2020-10-30 15:20:30 -0400

    Make default logging Stackdriver JSON based
    There is still the ability to run the api service using standard console logging by setting the environment variable:
    TDR_LOG_APPENDER=Console-Standard
    
    But by default this will log with JSON and include:
    - Severity in its own field
    - Multiline messages consolidated
    - Span and Trace IDs are also logged

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,3 +1,5 @@
+import org.springframework.cloud.gcp.logging.StackdriverJsonLayout
+
 // LogBack Configuration File
 // This file controls how, where, and what gets logged.
 // For more information see: https://logback.qos.ch/manual/groovy.html
@@ -8,9 +10,18 @@ scan("30 seconds")
 def LOG_PATH = "logs"
 
 // Appender that sends to the console
-appender("Console-Appender", ConsoleAppender) {
+appender("Console-Standard", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "%date %-5level [%thread] %logger{36}: %message%n"
+    }
+}
+
+appender("Console-Stackdriver", ConsoleAppender) {
+    encoder(LayoutWrappingEncoder) {
+        layout(StackdriverJsonLayout) {
+            includeTraceId = true
+            includeSpanId = true
+        }
     }
 }
 
@@ -29,4 +40,4 @@ logger("bio.terra.service.tabulardata.google.BigQueryProject", DEBUG);
 */
 
 // root sets the default logging level and appenders
-root(INFO, ["Console-Appender"])
+root(INFO, [System.getenv().getOrDefault("TDR_LOG_APPENDER", "Console-Stackdriver")])

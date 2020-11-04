9ffc046290cec1be97972abd39ded1f35ed95626
Shelby Holden <sholden@broadinstitute.org>
2020-11-04 09:58:51 -0500

    Code Review: move to collable function structure

--- src/main/java/bio/terra/service/configuration/StatusService.java
+++ src/main/java/bio/terra/service/configuration/StatusService.java
@@ -4,12 +4,15 @@ import bio.terra.model.RepositoryStatusModel;
 import bio.terra.model.RepositoryStatusModelSystems;
 import bio.terra.service.dataset.DatasetDao;
 import bio.terra.service.iam.IamProviderInterface;
+import org.broadinstitute.dsde.workbench.client.sam.ApiException;
 import org.broadinstitute.dsde.workbench.client.sam.model.SystemStatus;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.Callable;
 
 @Service
 public class StatusService {
@@ -32,15 +35,19 @@ public class StatusService {
     public RepositoryStatusModel getStatus() {
         RepositoryStatusModel statusModel = new RepositoryStatusModel();
 
-        // Used by unit test: StatusTest
+        // Used by Unit test: StatusTest
         if (configurationService.testInsertFault(ConfigEnum.LIVENESS_FAULT)) {
             logger.info("LIVENESS_FAULT insertion - failing status response");
             statusModel.setOk(false);
             return statusModel;
         }
 
-        statusModel.putSystemsItem("Postgres", postgresStatus());
-        statusModel.putSystemsItem("Sam", samStatus());
+        RepositoryStatusModelSystems postgresStatus =
+            checkStatus("postgresStatus", this::postgresStatusInner, true);
+        statusModel.putSystemsItem("Postgres", postgresStatus);
+        RepositoryStatusModelSystems samStatus =
+            checkStatus("samStatus", this::samStatusInner, true);
+        statusModel.putSystemsItem("Sam", samStatus);
 
         // if all critical systems are ok, then isOk = true
         // if any one critical system is down, then isOk = false
@@ -50,8 +57,24 @@ public class StatusService {
         return statusModel;
     }
 
-    private RepositoryStatusModelSystems postgresStatus() {
-        // Used by unit test: StatusTest
+    private RepositoryStatusModelSystems checkStatus(String statusFnName, Callable<RepositoryStatusModelSystems> statusFn, boolean isCritical) {
+        RepositoryStatusModelSystems statusModel;
+        try {
+            statusModel = statusFn.call();
+            statusModel.setCritical(isCritical);
+        } catch (Exception ex) {
+            logger.error("Error during {}, exception was: {}", statusFnName, ex);
+            statusModel = new RepositoryStatusModelSystems()
+                .ok(false)
+                .critical(isCritical)
+                .message(ex.getMessage());
+        }
+        return statusModel;
+    }
+
+    // Use in conjunction with checkStatus() to get postgres status
+    private RepositoryStatusModelSystems postgresStatusInner() throws DataAccessException {
+        // Used by Unit test: StatusTest
         if (configurationService.testInsertFault(ConfigEnum.CRITICAL_SYSTEM_FAULT)) {
             logger.info("CRITICAL_SYSTEM_FAULT inserted for test - setting postgres system status to failing");
             return new RepositoryStatusModelSystems()
@@ -60,38 +83,20 @@ public class StatusService {
                 .message("CRITICAL_SYSTEM_FAULT inserted for test");
         }
 
-        Boolean dbStatus = false;
-        String msg;
-        try {
-            datasetDao.probeDatabase();
-            dbStatus = true;
-            msg = "Successfully queried database.";
-        } catch (DataAccessException ex) {
-            msg = "Failed to query database: " + ex.getMessage();
-            logger.error("Failed to query database. Ex: {}", ex);
-        }
+        // if can successfully complete call without throwing exception, then system is up
+        datasetDao.probeDatabase();
         RepositoryStatusModelSystems databaseSystem = new RepositoryStatusModelSystems()
-            .ok(dbStatus)
-            .critical(true)
-            .message(msg);
+            .ok(true)
+            .message("Successfully queried database.");
         return databaseSystem;
     }
+    
+    private RepositoryStatusModelSystems samStatusInner() throws ApiException {
+        SystemStatus samStatusModel = iamProviderInterface.samStatus();
 
-    private RepositoryStatusModelSystems samStatus() {
-        Boolean samStatus = false;
-        String msg;
-        try {
-            SystemStatus samStatusModel = iamProviderInterface.samStatus();
-            samStatus = samStatusModel.getOk();
-            msg = samStatusModel.getSystems().toString();
-        } catch (Exception ex) {
-            logger.error("Failed to complete sam status check. Ex: {}", ex);
-            msg = ex.getMessage();
-        }
         RepositoryStatusModelSystems samSystem = new RepositoryStatusModelSystems()
-            .ok(samStatus)
-            .critical(true)
-            .message(msg);
+            .ok(samStatusModel.getOk())
+            .message(samStatusModel.getSystems().toString());
         return samSystem;
     }
 }

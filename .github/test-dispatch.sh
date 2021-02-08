db2c6ab3cf4d8d9fe755b4cfc2abc9f204c2d431
Shelby Holden <sholden@broadinstitute.org>
2021-02-08 14:57:09 -0500

    switch back test-dispatch

--- .github/test-dispatch.sh
+++ .github/test-dispatch.sh
@@ -2,7 +2,7 @@
 BRANCH=$(git branch --show-current)
 TOKEN=$(cat $HOME/.gh_token)
 REPO="DataBiosphere/jade-data-repo"
-WORKFLOW="alpha-integration-tests.yaml"
+WORKFLOW="test-runner-on-perf.yaml"
 
 curl -H "Accept: application/vnd.github.everest-preview+json" \
     -H "Authorization: token ${TOKEN}" \

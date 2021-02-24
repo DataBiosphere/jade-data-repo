606dbe0a822dd727e100341cfca62165c38a0964
Shelby Holden <sholden@broadinstitute.org>
2021-02-24 10:36:31 -0500

    reference new library in dev-image-update

--- .github/test-dispatch.sh
+++ .github/test-dispatch.sh
@@ -2,7 +2,7 @@
 BRANCH=$(git branch --show-current)
 TOKEN=$(cat $HOME/.gh_token)
 REPO="DataBiosphere/jade-data-repo"
-WORKFLOW="int-and-connected-test-run.yaml"
+WORKFLOW="int-and-connected-test-run.yml"
 
 curl -H "Accept: application/vnd.github.everest-preview+json" \
     -H "Authorization: token ${TOKEN}" \

808f905ff3c0abe42d53eba23da27a7a4fe04a5d
Shelby Holden <sholden@broadinstitute.org>
2021-02-24 12:58:19 -0500

    define merge branch

--- .github/test-dispatch.sh
+++ .github/test-dispatch.sh
@@ -2,7 +2,7 @@
 BRANCH=$(git branch --show-current)
 TOKEN=$(cat $HOME/.gh_token)
 REPO="DataBiosphere/jade-data-repo"
-WORKFLOW="int-and-connected-test-run.yml"
+WORKFLOW="dev-image-update.yaml"
 
 curl -H "Accept: application/vnd.github.everest-preview+json" \
     -H "Authorization: token ${TOKEN}" \

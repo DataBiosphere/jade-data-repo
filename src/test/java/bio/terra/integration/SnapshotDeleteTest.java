841f026a48a145b7f908f83c30421c2a72555938
Shelby Holden <sholden@broadinstitute.org>
2021-03-18 09:00:10 -0400

    add test

---
+++ src/test/java/bio/terra/integration/SnapshotDeleteTest.java
@@ -0,0 +1,80 @@
+package bio.terra.integration;
+
+import bio.terra.common.category.Integration;
+import bio.terra.service.dataset.Dataset;
+import bio.terra.service.dataset.DatasetService;
+import bio.terra.service.filedata.google.firestore.FireStoreDao;
+import bio.terra.service.filedata.google.firestore.FireStoreDependencyDao;
+import bio.terra.service.snapshot.Snapshot;
+import bio.terra.service.snapshot.SnapshotService;
+import bio.terra.service.snapshot.SnapshotSource;
+import bio.terra.service.tabulardata.google.BigQueryPdao;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.UUID;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles({"google", "integrationtest"})
+@AutoConfigureMockMvc
+@Category(Integration.class)
+public class SnapshotDeleteTest extends UsersBase {
+
+    private static Logger logger = LoggerFactory.getLogger(SnapshotDeleteTest.class);
+
+    @Autowired
+    private SnapshotService snapshotService;
+
+    @Autowired
+    private BigQueryPdao bigQueryPdao;
+
+    @Autowired
+    private DatasetService datasetService;
+
+    @Autowired
+    private FireStoreDao fileDao;
+
+    @Autowired
+    private FireStoreDependencyDao dependencyDao;
+
+    private String datasetId;
+    private String snapshotId;
+    private String profileId;
+
+    @Before
+    public void setup() throws Exception {
+        super.setup();
+        profileId = "390e7a85-d47f-4531-b612-165fc977d3bd";
+        datasetId = "ecb5601e-9026-428c-b49d-3c5f1807ecb7";
+        snapshotId = "4122d403-8d70-4922-ab6e-7db13e44a9a5";
+        /*dataRepoFixtures.addDatasetPolicyMember(
+            steward(), datasetSummaryModel.getId(), IamRole.CUSTODIAN, custodian().getEmail());*/
+    }
+
+    @Test
+    public void PrimaryDataStepSnapshotDelete() throws InterruptedException {
+        Snapshot snapshot = snapshotService.retrieve(UUID.fromString(snapshotId));
+        bigQueryPdao.deleteSnapshot(snapshot);
+
+        // Remove snapshot file references from the underlying datasets
+        for (SnapshotSource snapshotSource : snapshot.getSnapshotSources()) {
+            Dataset dataset = datasetService.retrieve(snapshotSource.getDataset().getId());
+            dependencyDao.deleteSnapshotFileDependencies(
+                dataset,
+                snapshotId);
+        }
+        fileDao.deleteFilesFromSnapshot(snapshot);
+    }
+
+}

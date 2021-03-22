c16705ca28c535ee82b534c4f2b8c9caf71c1f9c
Shelby Holden <sholden@broadinstitute.org>
2021-03-22 10:20:27 -0400

    add delete code

--- tools/manual-snapshot-cleanup/cleanup-snapshot-firestore.py
+++ tools/manual-snapshot-cleanup/cleanup-snapshot-firestore.py
@@ -7,7 +7,7 @@
 #Example Usage: python3 cleanup-snapshot-firestore.py
 
 #-------------
-#TODO: Add in code that actually deletes. Right now, just lists files from firestore
+#TODO: Right now, just lists files from firestore. Uncomment "TODO" lines to actually delete.
 from google.cloud import firestore
 
 #----USER DEFINE SOURCE DATASET AND SNAPSHOTID HERE----
@@ -18,17 +18,50 @@ snapshotId='4122d403-8d70-4922-ab6e-7db13e44a9a5'
 sourceDatasetFormatted = sourceDatasetId + '-dependencies'
 db = firestore.Client()
 query = db.collection(sourceDatasetFormatted).where(u'snapshotId', u'==', snapshotId)
-results = query.stream()#.delete()
+
+def write_results_to_file(file_name):
+    f = open(file_name, "a")
+    f.write("==================File Dependency Firestore Delete===================\n")
+    f.write(snapshotId)
+    f.write("\n")
+
+def delete_collection(coll_ref, batch_size):
+    docs = coll_ref.limit(batch_size).stream()
+    deleted = 0
+
+    for doc in docs:
+        f.write(f'Deleting doc {doc.id} => {doc.to_dict()}\n')
+        #TODO: When ready to delete - uncomment following line
+        #doc.reference.delete()
+        deleted = deleted + 1
+
+    if deleted >= batch_size:
+        return delete_collection(coll_ref, batch_size)
+
+
+#TODO: When ready to delete, uncomment the following line
+#write_results_to_file("dependencies_delete.txt")
+#delete_collection(query, 10)
+#f.close()
+
+# Print number of files that would be deleted
+results = query.stream()
 print("dependencies: ")
 print(sum(1 for k in results))
-#result: 17585
+
+
 
 #-----Step 3, part 4 - files-----
 file_query = db.collection(snapshotId)
-file_results = file_query.stream()#.delete()
+
+#TODO: When ready to delete, uncomment the following line
+#write_results_to_file("snapshot_file_delete.txt")
+#delete_collection(file_query, 10)
+#f.close()
+
+file_results = file_query.stream()
 print("files: ")
 print(sum(1 for k in file_results))
-#result: 35195
 
 
 

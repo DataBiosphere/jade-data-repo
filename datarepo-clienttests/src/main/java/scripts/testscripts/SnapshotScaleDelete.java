48f62abf60d4cc74f10faf8f963d28e4c1c4f48d
rori <6863459+RoriCremer@users.noreply.github.com>
2021-03-24 14:42:37 -0400

    create user journey for snapshot scaling

---
+++ datarepo-clienttests/src/main/java/scripts/testscripts/SnapshotScaleDelete.java
@@ -0,0 +1,142 @@
+package scripts.testscripts;
+
+import bio.terra.datarepo.api.DataRepositoryServiceApi;
+import bio.terra.datarepo.api.RepositoryApi;
+import bio.terra.datarepo.client.ApiClient;
+import bio.terra.datarepo.model.BulkLoadArrayRequestModel;
+import bio.terra.datarepo.model.BulkLoadArrayResultModel;
+import bio.terra.datarepo.model.BulkLoadFileModel;
+import bio.terra.datarepo.model.DRSObject;
+import bio.terra.datarepo.model.DeleteResponseModel;
+import bio.terra.datarepo.model.IngestRequestModel;
+import bio.terra.datarepo.model.IngestResponseModel;
+import bio.terra.datarepo.model.JobModel;
+import bio.terra.datarepo.model.PolicyMemberRequest;
+import bio.terra.datarepo.model.SnapshotModel;
+import bio.terra.datarepo.model.SnapshotSummaryModel;
+import bio.terra.datarepo.model.TableModel;
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.TableResult;
+import com.google.cloud.storage.BlobId;
+import common.utils.BigQueryUtils;
+import common.utils.FileUtils;
+import common.utils.StorageUtils;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import runner.config.TestUserSpecification;
+import scripts.testscripts.baseclasses.SimpleDataset;
+import scripts.utils.DataRepoUtils;
+
+public class SnapshotScaleDelete extends SimpleDataset {
+  private static final Logger logger = LoggerFactory.getLogger(SnapshotScaleDelete.class);
+
+  /** Public constructor so that this class can be instantiated via reflection. */
+  public SnapshotScaleDelete() {
+    super();
+  }
+
+  private SnapshotModel snapshotModel;
+
+  private static List<BlobId> scratchFiles = new ArrayList<>();
+  private String dirObjectId;
+
+  private int NUM_SNAPSHOTS = 1;
+
+  public void setParameters(List<String> parameters) {
+    if (parameters != null && parameters.size() > 0) {
+        NUM_SNAPSHOTS = Integer.parseInt(parameters.get(0));
+    }
+    logger.debug("Number of snapshots to create and delete (default is 1): {}", NUM_SNAPSHOTS);
+  }
+
+  public void setup(List<TestUserSpecification> testUsers) throws Exception {
+    // create the profile and dataset
+    super.setup(testUsers);
+
+    // get the ApiClient for the snapshot creator, same as the dataset creator
+    ApiClient datasetCreatorClient = DataRepoUtils.getClientForTestUser(datasetCreator, server);
+    RepositoryApi repositoryApi = new RepositoryApi(datasetCreatorClient);
+
+    // dont bother to load data into the new dataset
+    // create MANY snapshots based on a single dataset
+
+/*    for (int i = 0; i < NUM_SNAPSHOTS; i++) {
+        // make the create snapshot request and wait for the job to finish
+        // the name of the snapshot will already be randomized based on the bool
+        JobModel createSnapshotJobResponse =
+            DataRepoUtils.createSnapshot(
+                repositoryApi, datasetSummaryModel, "snapshot-simple.json", true);
+
+        // save a reference to the snapshot summary model so we can delete it in cleanup()
+        // TODO ^ shouldn't I be deleting them all anyway?
+        // do I need to account for the fact that many will be deleted already?
+        // another idea would be that we create and then delete them each in the userjourney
+        SnapshotSummaryModel snapshotSummaryModel =
+            DataRepoUtils.expectJobSuccess(
+                repositoryApi, createSnapshotJobResponse, SnapshotSummaryModel.class);
+        logger.info("Successfully created snapshot: {}, index: {}",
+            snapshotSummaryModel.getName(),
+            i
+        );
+     }*/
+  }
+
+  public void userJourney(TestUserSpecification testUser) throws Exception {
+    ApiClient apiClient = DataRepoUtils.getClientForTestUser(testUser, server);
+    DataRepositoryServiceApi dataRepositoryServiceApi = new DataRepositoryServiceApi(apiClient);
+
+    for (int i = 0; i < NUM_SNAPSHOTS; i++) {
+        // make the create snapshot request and wait for the job to finish
+        // the name of the snapshot will already be randomized based on the bool
+        JobModel createSnapshotJobResponse =
+            DataRepoUtils.createSnapshot(
+                repositoryApi, datasetSummaryModel, "snapshot-simple.json", true);
+
+        // save a reference to the snapshot summary model so we can delete it in cleanup(
+        SnapshotSummaryModel snapshotSummaryModel =
+            DataRepoUtils.expectJobSuccess(
+                repositoryApi, createSnapshotJobResponse, SnapshotSummaryModel.class);
+        logger.info("Successfully created snapshot: {}, index: {}",
+            snapshotSummaryModel.getName(),
+            i
+        );
+
+        // Now delete each snapshot
+        JobModel deleteSnapshotJobResponse = repositoryApi.deleteSnapshot(snapshotSummaryModel.getId());
+        deleteSnapshotJobResponse =
+            DataRepoUtils.waitForJobToFinish(repositoryApi, deleteSnapshotJobResponse);
+        DataRepoUtils.expectJobSuccess(
+            repositoryApi, deleteSnapshotJobResponse, DeleteResponseModel.class);
+        logger.info("Successfully deleted snapshot: {}, index: {}",
+            snapshotSummaryModel.getName(),
+            i
+        );
+    }
+  }
+
+  public void cleanup(List<TestUserSpecification> testUsers) throws Exception {
+    // get the ApiClient for the dataset creator
+    ApiClient datasetCreatorClient = DataRepoUtils.getClientForTestUser(datasetCreator, server);
+    RepositoryApi repositoryApi = new RepositoryApi(datasetCreatorClient);
+
+    // make the delete request and wait for the job to finish
+    JobModel deleteSnapshotJobResponse = repositoryApi.deleteSnapshot(snapshotModel.getId());
+    deleteSnapshotJobResponse =
+        DataRepoUtils.waitForJobToFinish(repositoryApi, deleteSnapshotJobResponse);
+    DataRepoUtils.expectJobSuccess(
+        repositoryApi, deleteSnapshotJobResponse, DeleteResponseModel.class);
+    logger.info("Successfully deleted snapshot: {}", snapshotModel.getName());
+
+    // delete the profile and dataset
+    super.cleanup(testUsers);
+
+    // delete the scratch files used for ingesting tabular data and soft delete rows
+    StorageUtils.deleteFiles(
+        StorageUtils.getClientForServiceAccount(server.testRunnerServiceAccount), scratchFiles);
+  }
+}

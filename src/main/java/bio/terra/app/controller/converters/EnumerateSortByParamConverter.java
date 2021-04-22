5962c70afc4f35f4bd79e8c3d8fb7be5cffcd8ca
Trevyn Langsford <tlangs@broadinstitute.org>
2021-04-22 09:23:57 -0400

    DR-1516 Remediate SQL injection opportunity in enumerate endpoints (#895)
    * Replace strings with enums
    
    * make them case-insensitive

---
+++ src/main/java/bio/terra/app/controller/converters/EnumerateSortByParamConverter.java
@@ -0,0 +1,25 @@
+package bio.terra.app.controller.converters;
+
+import bio.terra.app.controller.exception.ValidationException;
+import bio.terra.model.EnumerateSortByParam;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Component
+public class EnumerateSortByParamConverter
+    implements Converter<String, EnumerateSortByParam> {
+
+    @Override
+    public EnumerateSortByParam convert(String source) {
+        EnumerateSortByParam result = EnumerateSortByParam.fromValue(source.toLowerCase());
+        if (result == null) {
+            String error = String.format("sort must be one of: %s.", Arrays.toString(EnumerateSortByParam.values()));
+            throw new ValidationException("Invalid enumerate parameter(s).", Collections.singletonList(error));
+        }
+        return result;
+    }
+}

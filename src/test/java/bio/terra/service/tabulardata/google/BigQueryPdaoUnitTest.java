39f47eb9429d71024c6d7bacae478c5265d49469
Shelby Holden <sholden@broadinstitute.org>
2021-05-18 10:42:44 -0400

    fix checkstyles

--- src/test/java/bio/terra/service/tabulardata/google/BigQueryPdaoUnitTest.java
+++ src/test/java/bio/terra/service/tabulardata/google/BigQueryPdaoUnitTest.java
@@ -12,7 +12,6 @@ import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 

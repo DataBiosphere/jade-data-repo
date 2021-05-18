60365eeec8bc1e9c5a4abf965a668b68a305b041
Shelby Holden <sholden@broadinstitute.org>
2021-05-18 07:48:38 -0400

    sparse outline for unit tests

---
+++ src/test/java/bio/terra/service/tabulardata/google/BigQueryPdaoUnitTest.java
@@ -0,0 +1,45 @@
+package bio.terra.service.tabulardata.google;
+
+
+import bio.terra.common.category.Unit;
+
+import org.junit.After;
+
+import org.junit.Before;
+
+import org.junit.experimental.categories.Category;
+
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Category(Unit.class)
+public class BigQueryPdaoUnitTest {
+    private static final Logger logger = LoggerFactory.getLogger(BigQueryPdaoUnitTest.class);
+
+
+    @Before
+    public void setup() throws Exception {
+    }
+
+    @After
+    public void teardown() throws Exception {
+    }
+
+
+    //TODO - Snapshot by Asset
+    //Test BigQueryPdao.mapValuesToRows()
+
+
+
+
+}

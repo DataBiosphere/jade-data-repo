dcd71019ac0738959f97af451c3592e2d21cce90
Shelby Holden <sholden@broadinstitute.org>
2021-05-20 11:13:42 -0400

    remove references in snapshot project create

--- src/main/java/bio/terra/service/snapshot/flight/create/SnapshotCreateFlight.java
+++ src/main/java/bio/terra/service/snapshot/flight/create/SnapshotCreateFlight.java
@@ -76,7 +76,7 @@ public class SnapshotCreateFlight extends Flight {
         addStep(new AuthorizeBillingProfileUseStep(profileService, snapshotReq.getProfileId(), userReq));
 
         // Get or create the project where the snapshot resources will be created
-        addStep(new CreateSnapshotGetOrCreateProjectStep(resourceService, snapshotReq, datasetService, firestoreRegion));
+        addStep(new CreateSnapshotGetOrCreateProjectStep(resourceService, firestoreRegion));
 
         // create the snapshot metadata object in postgres and lock it
         // mint a snapshot id and put it in the working map

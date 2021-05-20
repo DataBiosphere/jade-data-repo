95f18d35c3c7736056bd40bcd0d7be9969eaa8be
Shelby Holden <sholden@broadinstitute.org>
2021-05-20 11:10:59 -0400

    revert some more changes

--- src/main/java/bio/terra/service/resourcemanagement/OneDataLocationSelector.java
+++ src/main/java/bio/terra/service/resourcemanagement/OneDataLocationSelector.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Primary
-@Profile({"!terra"})
+@Profile("!terra")
 @Component
 public class OneDataLocationSelector implements DataLocationSelector {
 

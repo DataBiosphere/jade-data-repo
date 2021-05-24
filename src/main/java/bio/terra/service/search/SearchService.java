a61bc8bc281c9de96544548c5b9fece682f16bed
Florian Boulnois <fboulnoi@broadinstitute.org>
2021-05-24 09:51:03 -0400

    Add index mapping and data

--- src/main/java/bio/terra/service/search/SearchService.java
+++ src/main/java/bio/terra/service/search/SearchService.java
@@ -6,13 +6,17 @@ import bio.terra.model.SearchIndexRequest;
 import bio.terra.service.snapshot.Snapshot;
 import bio.terra.service.tabulardata.google.BigQueryPdao;
 import org.apache.http.HttpHost;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.PutMappingRequest;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -34,7 +38,7 @@ public class SearchService {
         this.client = new RestHighLevelClient(builder);
     }
 
-    private void createEmptyIndex(Snapshot snapshot) {
+    private String createEmptyIndex(Snapshot snapshot) {
         String indexName = String.format("idx-%s", snapshot.getId());
         try {
             CreateIndexRequest createIndexRequest = new CreateIndexRequest(indexName)
@@ -45,12 +49,37 @@ public class SearchService {
         } catch (IOException e) {
             throw new PdaoException("Error creating index", e);
         }
+        return indexName;
+    }
+
+    private void createIndexMapping(String indexName, List<Map<String, Object>> values) {
+        PutMappingRequest request = new PutMappingRequest(indexName);
+        Map<String, Object> properties = Map.of("values", values);
+        Map<String, Object> source = Map.of("properties", properties);
+        request.source(source);
+        try {
+            client.indices().putMapping(request, RequestOptions.DEFAULT);
+        } catch (IOException e) {
+            throw new PdaoException("Error creating index mapping", e);
+        }
+    }
+
+    private void addIndexData(String indexName, List<Map<String, Object>> values) {
+        values.forEach(v -> {
+            BulkRequest request = new BulkRequest();
+            request.add(new IndexRequest(indexName)
+                .id(v.get("uuid").toString())
+                .source(v, XContentType.JSON)
+            );
+        });
     }
 
     public SearchIndexModel indexSnapshot(Snapshot snapshot, SearchIndexRequest searchIndexRequest)
         throws InterruptedException {
-        createEmptyIndex(snapshot);
         List<Map<String, Object>> values = bigQueryPdao.getSnapshotTableData(snapshot, searchIndexRequest.getSql());
+        String indexName = createEmptyIndex(snapshot);
+        createIndexMapping(indexName, values);
+        addIndexData(indexName, values);
         return new SearchIndexModel();
     }
 }

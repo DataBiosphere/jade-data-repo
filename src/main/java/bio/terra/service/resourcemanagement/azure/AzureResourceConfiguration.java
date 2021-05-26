ccbb5c17ab6bf913ec6c1c4c819189dc65f16174
nmalfroy <nmalfroy@broadinstitute.org>
2021-05-26 13:19:52 -0400

    Add a convenience method to get Azure credentials

--- src/main/java/bio/terra/service/resourcemanagement/azure/AzureResourceConfiguration.java
+++ src/main/java/bio/terra/service/resourcemanagement/azure/AzureResourceConfiguration.java
@@ -1,5 +1,10 @@
 package bio.terra.service.resourcemanagement.azure;
 
+import com.azure.core.credential.TokenCredential;
+import com.azure.core.management.AzureEnvironment;
+import com.azure.core.management.profile.AzureProfile;
+import com.azure.identity.ClientSecretCredentialBuilder;
+import com.azure.resourcemanager.AzureResourceManager;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
@@ -21,6 +26,43 @@ public class AzureResourceConfiguration {
 
     public void setCredentials(Credentials credentials) {
         this.credentials = credentials;
+    }
+
+    /**
+     * Given a user tenant Id, return Azure credentials
+     * @param tenantId The ID of a user tenant
+     * @return A credential object that can be used to interact with Azure apis
+     */
+    public TokenCredential getAppToken(final UUID tenantId) {
+        return new ClientSecretCredentialBuilder()
+            .clientId(credentials.applicationId.toString())
+            .clientSecret(credentials.secret)
+            .tenantId(tenantId.toString())
+            .build();
+    }
+
+    /**
+     * Get Azure credentials authenticated against the home tenant.  Use this authentication method when accessing
+     * resources within a deployed managed application
+     * @return A credential object that can be used to interact with Azure apis
+     */
+    public TokenCredential getAppToken() {
+        return getAppToken(credentials.getHomeTenantId());
+    }
+
+    /**
+     * Get a resource manager client to a user's tenant.  This object can then be used to create/destroy resources
+     * @param tenantId The ID of the user's tenant
+     * @param subscriptionId The ID of the subscription that will be charged for the resources created with this client
+     * @return An authenticated {@link AzureResourceManager} client
+     */
+    public AzureResourceManager getClient(final UUID tenantId, final UUID subscriptionId) {
+        final AzureProfile profile = new AzureProfile(
+            tenantId.toString(),
+            subscriptionId.toString(),
+            AzureEnvironment.AZURE);
+        return AzureResourceManager.authenticate(getAppToken(tenantId), profile)
+            .withSubscription(subscriptionId.toString());
     }
 
     /**

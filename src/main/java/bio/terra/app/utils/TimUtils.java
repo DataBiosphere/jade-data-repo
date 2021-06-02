fcbb891101b06ec5d6a5bacbb02f21cc342e2c43
Phil Shapiro <pshapiro@broadinstitute.org>
2021-06-02 10:29:15 -0400

    Implementation and test for possible TIM encoding scheme.

---
+++ src/main/java/bio/terra/app/utils/TimUtils.java
@@ -0,0 +1,69 @@
+package bio.terra.app.utils;
+
+/**
+ * Utility methods for working with the Terra Interoperability Model (TIM)
+ */
+public final class TimUtils {
+    private TimUtils() { }
+
+    private static final String CAPITOL = "a__";
+    private static final String COLON = "c__";
+    private static final String PERIOD = "p__";
+
+    /**
+     * Encode a TIM name for ElasticSearch or BigQuery use.
+     *
+     * @param s the TIM name
+     * @return an encoded name
+     */
+    public static String encode(String s) {
+        StringBuilder result = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                result.append(CAPITOL);
+                result.append(Character.toLowerCase(c));
+            } else if (c == ':') {
+                result.append(COLON);
+            } else if (c == '.') {
+                result.append(PERIOD);
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
+    }
+
+    private static String doDecode(String s) {
+        String t = s.replaceAll(COLON, ":").replaceAll(PERIOD, ".");
+        int len = CAPITOL.length();
+        int index;
+        while ((index = t.indexOf(CAPITOL)) != -1) {
+            t = t.substring(0, index) + Character.toUpperCase(t.charAt(index + len)) + t.substring(index + len + 1);
+        }
+        return t;
+    }
+
+    /**
+     * Give a string, return true if it's likely that this was encoded and requires decoding.
+     *
+     * @param s the string
+     * @return true if it's likley to be encoded
+     */
+    private static boolean shouldDecode(String s) {
+        // Search for two code strings that all names will have at least one of.
+        return s.contains(COLON) && s.contains(CAPITOL);
+    }
+
+    /**
+     * Decode an encoded TIM name and return its original name.
+     *
+     * @param s the encoded name
+     * @return the decoded TIM name
+     */
+    public static String decode(String s) {
+        if (shouldDecode(s)) {
+            return doDecode(s);
+        }
+        return s;
+    }
+}
---
+++ src/test/java/bio/terra/app/utils/TimUtilsTest.java
@@ -0,0 +1,36 @@
+package bio.terra.app.utils;
+
+import bio.terra.common.category.Unit;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+@Category(Unit.class)
+public class TimUtilsTest {
+
+    private static final String[][] CASES =
+        {
+            {"TerraCore:Donor.TerraCore:hasAge", "a__terraa__corec__a__donorp__a__terraa__corec__hasa__age"},
+            {"TerraCore:Age.TerraCore.hasUpperBound", "a__terraa__corec__a__agep__a__terraa__corep__hasa__uppera__bound"},
+            {"TerraCore:Age.TerraCore.hasLowerBound", "a__terraa__corec__a__agep__a__terraa__corep__hasa__lowera__bound"},
+            {"TerraCore:Age.TerraCore.hasAgeUnit", "a__terraa__corec__a__agep__a__terraa__corep__hasa__agea__unit"},
+            {"TerraCore:Project.rdfs:label", "a__terraa__corec__a__projectp__rdfsc__label"},
+            {"TerraCore:BioSample.dct:identifier", "a__terraa__corec__a__bioa__samplep__dctc__identifier"}
+        };
+
+    @Test
+    public void encode() {
+        for (String[] c : CASES) {
+            assertThat(TimUtils.encode(c[0]), is(c[1]));
+        }
+    }
+
+    @Test
+    public void decode() {
+        for (String[] c : CASES) {
+            assertThat(TimUtils.decode(c[1]), is(c[0]));
+        }
+    }
+}

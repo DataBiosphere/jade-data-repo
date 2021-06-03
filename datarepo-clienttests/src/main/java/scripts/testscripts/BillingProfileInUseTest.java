e624976121a10f7008ab48eedb35ccae27bebeee
Shelby Holden <sholden@broadinstitute.org>
2021-06-03 14:14:41 -0400

    snapshot can no longer be empty

--- datarepo-clienttests/src/main/java/scripts/testscripts/BillingProfileInUseTest.java
+++ datarepo-clienttests/src/main/java/scripts/testscripts/BillingProfileInUseTest.java
@@ -3,21 +3,37 @@ package scripts.testscripts;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import bio.terra.datarepo.api.RepositoryApi;
+import bio.terra.datarepo.client.ApiClient;
 import bio.terra.datarepo.model.BillingProfileModel;
+import bio.terra.datarepo.model.BulkLoadArrayRequestModel;
+import bio.terra.datarepo.model.BulkLoadArrayResultModel;
+import bio.terra.datarepo.model.BulkLoadFileModel;
 import bio.terra.datarepo.model.DatasetSummaryModel;
 import bio.terra.datarepo.model.DeleteResponseModel;
+import bio.terra.datarepo.model.IngestRequestModel;
+import bio.terra.datarepo.model.IngestResponseModel;
+import bio.terra.datarepo.model.JobModel;
 import bio.terra.datarepo.model.SnapshotSummaryModel;
+import com.google.cloud.storage.BlobId;
+import common.utils.FileUtils;
+import common.utils.StorageUtils;
+import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import runner.config.TestUserSpecification;
 import scripts.testscripts.baseclasses.BillingProfileUsers;
+import scripts.utils.DataRepoUtils;
 import scripts.utils.tdrwrapper.DataRepoWrap;
 import scripts.utils.tdrwrapper.exception.DataRepoBadRequestClientException;
 import scripts.utils.tdrwrapper.exception.DataRepoNotFoundClientException;
 
 public class BillingProfileInUseTest extends BillingProfileUsers {
   private static final Logger logger = LoggerFactory.getLogger(BillingProfileInUseTest.class);
+  private static List<BlobId> scratchFiles = new ArrayList<>();
   private String stewardsEmail;
 
   /** Public constructor so that this class can be instantiated via reflection. */
@@ -60,6 +76,76 @@ public class BillingProfileInUseTest extends BillingProfileUsers {
       // owner2 creates a dataset and grants userUser the "custodian" role
       dataset = ownerUser2Api.createDataset(profileId, "dataset-simple.json", true);
       ownerUser2Api.addDatasetPolicyMember(dataset.getId(), "custodian", userUser.userEmail);
+
+      // load data into the new dataset
+      // note that there's a fileref in the dataset
+      // ingest a file -- TODO CannedTestData.getMeA1KBFile
+      // get the ApiClient for the snapshot creator, same as the dataset creator
+      ApiClient datasetCreatorClient = DataRepoUtils.getClientForTestUser(userUser, server);
+      RepositoryApi repositoryApi = new RepositoryApi(datasetCreatorClient);
+      URI sourceUri = new URI("gs://jade-testdata/fileloadprofiletest/1KBfile.txt");
+
+      String targetPath = "/testrunner/IngestFile/" + FileUtils.randomizeName("") + ".txt";
+
+      BulkLoadFileModel fileLoadModel =
+          new BulkLoadFileModel()
+              .sourcePath(sourceUri.toString())
+              .description("IngestFile")
+              .mimeType("text/plain")
+              .targetPath(targetPath);
+      String loadTag = FileUtils.randomizeName("lookupTest");
+      BulkLoadArrayRequestModel fileLoadModelArray =
+          new BulkLoadArrayRequestModel()
+              .profileId(profileId)
+              .loadTag(loadTag)
+              .maxFailedFileLoads(0);
+      fileLoadModelArray.addLoadArrayItem(fileLoadModel);
+
+      JobModel ingestFileJobResponse =
+          repositoryApi.bulkFileLoadArray(dataset.getId(), fileLoadModelArray);
+      ingestFileJobResponse =
+          DataRepoUtils.waitForJobToFinish(repositoryApi, ingestFileJobResponse);
+      BulkLoadArrayResultModel bulkLoadArrayResultModel =
+          DataRepoUtils.expectJobSuccess(
+              repositoryApi, ingestFileJobResponse, BulkLoadArrayResultModel.class);
+
+      String fileId = bulkLoadArrayResultModel.getLoadFileResults().get(0).getFileId();
+
+      // ingest the tabular data from the JSON file we just generated
+      // generate a JSON file with the fileref
+      String jsonLine =
+          "{\"VCF_File_Name\":\"name1\", \"Description\":\"description1\", \"VCF_File_Ref\":\""
+              + fileId
+              + "\"}\n";
+      byte[] fileRefBytes = jsonLine.getBytes(StandardCharsets.UTF_8);
+      // load a JSON file that contains the table rows to load into the test bucket
+      String jsonFileName = FileUtils.randomizeName("this-better-pass") + ".json";
+      String fileRefName = "scratch/testDRSLookup/" + jsonFileName;
+
+      String scratchFileBucketName = "jade-testdata";
+      BlobId scratchFileTabularData =
+          StorageUtils.writeBytesToFile(
+              StorageUtils.getClientForServiceAccount(server.testRunnerServiceAccount),
+              scratchFileBucketName,
+              fileRefName,
+              fileRefBytes);
+      scratchFiles.add(scratchFileTabularData); // make sure the scratch file gets cleaned up later
+      String gsPath = StorageUtils.blobIdToGSPath(scratchFileTabularData);
+
+      IngestRequestModel ingestRequest =
+          new IngestRequestModel()
+              .format(IngestRequestModel.FormatEnum.JSON)
+              .table("vcf_file")
+              .path(gsPath);
+      JobModel ingestTabularDataJobResponse =
+          repositoryApi.ingestDataset(dataset.getId(), ingestRequest);
+
+      ingestTabularDataJobResponse =
+          DataRepoUtils.waitForJobToFinish(repositoryApi, ingestTabularDataJobResponse);
+      IngestResponseModel ingestResponse =
+          DataRepoUtils.expectJobSuccess(
+              repositoryApi, ingestTabularDataJobResponse, IngestResponseModel.class);
+      logger.info("Successfully loaded data into dataset: {}", ingestResponse.getDataset());
 
       // user creates a snapshot
       snapshot =

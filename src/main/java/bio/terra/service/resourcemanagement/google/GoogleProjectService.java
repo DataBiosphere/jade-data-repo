68f9526c1b2c4c27673780a537e9965c06256e26
Shelby Holden <sholden@broadinstitute.org>
2021-06-04 16:31:21 -0400

    switch default and don't use fallback

--- src/main/java/bio/terra/service/resourcemanagement/google/GoogleProjectService.java
+++ src/main/java/bio/terra/service/resourcemanagement/google/GoogleProjectService.java
@@ -463,11 +463,11 @@ public class GoogleProjectService {
                                         final GoogleRegion firestoreRegion,
                                         final long timeout) throws IOException, InterruptedException {
         logger.info("Enabling Firestore in project {} in location {}", googleProjectId,
-            firestoreRegion.getFirestoreFallbackRegion());
+            firestoreRegion);
         // Create a request object
         Appengine.Apps.Create createRequest = appengine.apps().create(new Application()
             .setId(googleProjectId)
-            .setLocationId(firestoreRegion.getFirestoreFallbackRegion().toString())
+            .setLocationId(firestoreRegion.toString())
             .setDatabaseType(FIRESTORE_DB_TYPE)
         );
 

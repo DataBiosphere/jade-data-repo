3ae4101671135fe589699e894e0fbf63842fbade
tcjiang <tcjiang9@gmail.com>
2021-06-07 14:37:55 -0400

    temp

--- src/main/java/bio/terra/app/configuration/ElasticSearchConfiguration.java
+++ src/main/java/bio/terra/app/configuration/ElasticSearchConfiguration.java
@@ -3,7 +3,9 @@ package bio.terra.app.configuration;
 import org.apache.http.HttpHost;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,10 +14,11 @@ public class ElasticSearchConfiguration {
 
     // This Bean isn't injected directly. Its definition enables injecting RestHighLevelClient.
     @Bean
-    public RestClientBuilder restClientBuilder(
+    public RestHighLevelClient client(
             @Value("${elasticsearch.hostname}") String hostname,
             @Value("${elasticsearch.port}") int port
     ) {
-        return RestClient.builder(new HttpHost(hostname, port));
+        RestClientBuilder builder = RestClient.builder(new HttpHost(hostname, port));
+        return new RestHighLevelClient(builder);
     }
 }

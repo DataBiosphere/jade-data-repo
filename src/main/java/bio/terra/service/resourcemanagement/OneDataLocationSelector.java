33ec88f3514a267451c29b0dc8f807ad06d715ff
Michael Yessaillian <myessail@broadinstitute.org>
2021-06-08 07:56:30 -0400

    Create a new ingest bucket per dataset instead of per billing profile (#927)

--- src/main/java/bio/terra/service/resourcemanagement/OneDataLocationSelector.java
+++ src/main/java/bio/terra/service/resourcemanagement/OneDataLocationSelector.java
@@ -2,6 +2,7 @@ package bio.terra.service.resourcemanagement;
 
 import bio.terra.model.BillingProfileModel;
 import bio.terra.service.resourcemanagement.google.GoogleResourceConfiguration;
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
@@ -39,7 +40,7 @@ public class OneDataLocationSelector implements DataLocationSelector {
     }
 
     @Override
-    public String bucketForFile(String datasetName, BillingProfileModel billingProfile) {
+    public String bucketForFile(UUID datasetId, BillingProfileModel billingProfile) {
         return oneProject() + "-bucket";
     }
 }

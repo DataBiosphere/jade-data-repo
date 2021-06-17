039576855054e94300a2bc84bf1dbf8461fc7618
Saman Ehsan <se8ea@virginia.edu>
2021-06-17 12:55:05 -0400

    Respond to code review

--- src/main/java/bio/terra/service/resourcemanagement/exception/BufferServiceAPIException.java
+++ src/main/java/bio/terra/service/resourcemanagement/exception/BufferServiceAPIException.java
@@ -3,7 +3,6 @@ package bio.terra.service.resourcemanagement.exception;
 import bio.terra.buffer.client.ApiException;
 import bio.terra.common.exception.ErrorReportException;
 import java.util.Collections;
-import java.util.List;
 import org.springframework.http.HttpStatus;
 
 /** Wrapper exception for non-200 responses from calls to Buffer Service. */
@@ -17,33 +16,6 @@ public class BufferServiceAPIException extends ErrorReportException {
                 Collections.singletonList(bufferException.getResponseBody()),
                 HttpStatus.resolve(bufferException.getCode()));
         this.apiException = bufferException;
-    }
-
-    public BufferServiceAPIException(String message, ApiException apiException) {
-        super(message);
-        this.apiException = apiException;
-    }
-
-    public BufferServiceAPIException(String message, Throwable cause, ApiException apiException) {
-        super(message, cause);
-        this.apiException = apiException;
-    }
-
-    public BufferServiceAPIException(Throwable cause, ApiException apiException) {
-        super(cause);
-        this.apiException = apiException;
-    }
-
-    public BufferServiceAPIException(String message, List<String> causes, HttpStatus statusCode,
-                                     ApiException apiException) {
-        super(message, causes, statusCode);
-        this.apiException = apiException;
-    }
-
-    public BufferServiceAPIException(
-            String message, Throwable cause, List<String> causes, HttpStatus statusCode, ApiException apiException) {
-        super(message, cause, causes, statusCode);
-        this.apiException = apiException;
     }
 
     /** Get the HTTP status code of the underlying response from Buffer Service. */
--- src/main/java/bio/terra/service/resourcemanagement/exception/BufferServiceAuthorizationException.java
+++ src/main/java/bio/terra/service/resourcemanagement/exception/BufferServiceAuthorizationException.java
@@ -2,27 +2,10 @@ package bio.terra.service.resourcemanagement.exception;
 
 import bio.terra.common.exception.UnauthorizedException;
 
-import java.util.List;
-
 public class BufferServiceAuthorizationException extends UnauthorizedException {
-
-    public BufferServiceAuthorizationException(String message) {
-        super(message);
-    }
 
     public BufferServiceAuthorizationException(String message, Throwable cause) {
         super(message, cause);
     }
 
-    public BufferServiceAuthorizationException(Throwable cause) {
-        super(cause);
-    }
-
-    public BufferServiceAuthorizationException(String message, List<String> errorDetails) {
-        super(message, errorDetails);
-    }
-
-    public BufferServiceAuthorizationException(String message, Throwable cause, List<String> errorDetails) {
-        super(message, cause, errorDetails);
-    }
 }

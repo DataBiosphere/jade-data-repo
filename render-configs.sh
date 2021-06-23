f0e993e29799b8dbcfc0ab71b3dcfc994289e2ec
Saman Ehsan <se8ea@virginia.edu>
2021-06-23 15:46:13 -0400

    Use RBS tools env for tests

--- render-configs.sh
+++ render-configs.sh
@@ -8,7 +8,7 @@ docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox:latest \
     jq .data | tee /tmp/jade-dev-account.json | \
     jq -r .private_key > /tmp/jade-dev-account.pem
 
-BUFFER_CLIENT_SERVICE_ACCOUNT_VAULT_PATH=secret/dsde/terra/kernel/dev/dev/buffer/client-sa
+BUFFER_CLIENT_SERVICE_ACCOUNT_VAULT_PATH=secret/dsde/terra/kernel/integration/tools/buffer/client-sa
 BUFFER_CLIENT_SERVICE_ACCOUNT_OUTPUT_PATH=/tmp/buffer-client-sa-account.json
 
 docker run --rm -e VAULT_TOKEN=$VAULT_TOKEN broadinstitute/dsde-toolbox:latest \

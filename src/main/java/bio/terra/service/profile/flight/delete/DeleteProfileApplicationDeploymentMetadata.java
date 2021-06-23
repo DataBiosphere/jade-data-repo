fb04bc3018d08bb7a94e6bcd8372e64e0a1a0ef5
nmalfroy <nmalfroy@broadinstitute.org>
2021-06-22 21:51:45 -0400

    Add profile delete flight Azure steps

---
+++ src/main/java/bio/terra/service/profile/flight/delete/DeleteProfileApplicationDeploymentMetadata.java
@@ -0,0 +1,42 @@
+package bio.terra.service.profile.flight.delete;
+
+import bio.terra.model.BillingProfileModel;
+import bio.terra.model.CloudPlatform;
+import bio.terra.service.profile.flight.ProfileMapKeys;
+import bio.terra.service.resourcemanagement.ResourceService;
+import bio.terra.stairway.FlightContext;
+import bio.terra.stairway.FlightMap;
+import bio.terra.stairway.Step;
+import bio.terra.stairway.StepResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.UUID;
+
+public class DeleteProfileApplicationDeploymentMetadata implements Step {
+
+    private final ResourceService resourceService;
+
+    private static final Logger logger = LoggerFactory.getLogger(DeleteProfileApplicationDeploymentMetadata.class);
+
+    public DeleteProfileApplicationDeploymentMetadata(ResourceService resourceService) {
+        this.resourceService = resourceService;
+    }
+
+    @Override
+    public StepResult doStep(FlightContext context) {
+        FlightMap workingMap = context.getWorkingMap();
+        List<UUID> appIdList = workingMap.get(ProfileMapKeys.PROFILE_APPLICATION_DEPLOYMENT_ID_LIST, List.class);
+        BillingProfileModel billingProfile = workingMap.get(ProfileMapKeys.PROFILE_MODEL, BillingProfileModel.class);
+        if (billingProfile.getCloudPlatform() == CloudPlatform.AZURE) {
+            resourceService.deleteDeployedApplicationMetadata(appIdList);
+        }
+        return StepResult.getStepResultSuccess();
+    }
+
+    @Override
+    public StepResult undoStep(FlightContext context) {
+        return StepResult.getStepResultSuccess();
+    }
+}
---
+++ src/main/java/bio/terra/service/profile/flight/delete/DeleteProfileMarkUnusedApplicationDeployments.java
@@ -0,0 +1,54 @@
+package bio.terra.service.profile.flight.delete;
+
+import bio.terra.model.BillingProfileModel;
+import bio.terra.model.CloudPlatform;
+import bio.terra.service.iam.AuthenticatedUserRequest;
+import bio.terra.service.profile.ProfileService;
+import bio.terra.service.profile.flight.ProfileMapKeys;
+import bio.terra.service.resourcemanagement.ResourceService;
+import bio.terra.stairway.FlightContext;
+import bio.terra.stairway.FlightMap;
+import bio.terra.stairway.Step;
+import bio.terra.stairway.StepResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.UUID;
+
+public class DeleteProfileMarkUnusedApplicationDeployments implements Step {
+
+    private final ProfileService profileService;
+    private final ResourceService resourceService;
+    private final AuthenticatedUserRequest user;
+    private final String profileId;
+
+    private static final Logger logger = LoggerFactory.getLogger(DeleteProfileMarkUnusedApplicationDeployments.class);
+
+    public DeleteProfileMarkUnusedApplicationDeployments(ProfileService profileService,
+                                                         ResourceService resourceService,
+                                                         AuthenticatedUserRequest user,
+                                                         String profileId) {
+        this.profileService = profileService;
+        this.resourceService = resourceService;
+        this.user = user;
+        this.profileId = profileId;
+    }
+
+    @Override
+    public StepResult doStep(FlightContext context) {
+        BillingProfileModel profileModel = profileService.getProfileById(profileId, user);
+        FlightMap workingMap = context.getWorkingMap();
+        workingMap.put(ProfileMapKeys.PROFILE_MODEL, profileModel);
+        if (profileModel.getCloudPlatform() == CloudPlatform.AZURE) {
+            List<UUID> appIdList = resourceService.markUnusedApplicationDeploymentsForDelete(profileId);
+            workingMap.put(ProfileMapKeys.PROFILE_APPLICATION_DEPLOYMENT_ID_LIST, appIdList);
+        }
+        return StepResult.getStepResultSuccess();
+    }
+
+    @Override
+    public StepResult undoStep(FlightContext context) {
+        return StepResult.getStepResultSuccess();
+    }
+}

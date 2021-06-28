2e4e038584f7a199a4774cc404ed7093020dc605
Shelby Holden <sholden@broadinstitute.org>
2021-06-28 17:07:49 -0400

    wip

--- src/main/java/io/opencensus/impl/trace/TraceComponentImpl.java
+++ src/main/java/io/opencensus/impl/trace/TraceComponentImpl.java
@@ -1,8 +1,11 @@
 package io.opencensus.impl.trace;
 
+import bio.terra.app.utils.startup.StartupInitializer;
 import io.opencensus.common.Clock;
+import io.opencensus.internal.ZeroTimeClock;
 import io.opencensus.trace.Annotation;
 import io.opencensus.trace.AttributeValue;
+import io.opencensus.trace.BlankSpan;
 import io.opencensus.trace.EndSpanOptions;
 import io.opencensus.trace.Link;
 import io.opencensus.trace.Sampler;
@@ -18,12 +21,15 @@ import io.opencensus.trace.config.TraceConfig;
 import io.opencensus.trace.export.ExportComponent;
 import io.opencensus.trace.propagation.PropagationComponent;
 import org.jetbrains.annotations.Nullable;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
 public class TraceComponentImpl extends TraceComponent {
+    private static final Logger logger = LoggerFactory.getLogger(TraceComponentImpl.class);
 
     static class DRSpanBuilder extends SpanBuilder {
 
@@ -44,31 +50,45 @@ public class TraceComponentImpl extends TraceComponent {
 
         @Override
         public Span startSpan() {
-            return new Span(SpanContext.create(TraceId.generateRandomId(new Random()),
-                SpanId.generateRandomId(new Random()),
-                TraceOptions.DEFAULT), null) {
-                @Override
-                public void addAnnotation(String description, Map<String, AttributeValue> attributes) {
-
-                }
-
-                @Override
-                public void addAnnotation(Annotation annotation) {
-
-                }
-
-                @Override
-                public void addLink(Link link) {
-
-                }
-
-                @Override
-                public void end(EndSpanOptions options) {
-
-                }
-            };
+            return new DRSpan();
         }
     }
+
+    public static class DRSpan extends Span {
+
+        DRSpan() {
+            super(SpanContext.create(TraceId.generateRandomId(new Random()),
+                SpanId.generateRandomId(new Random()),
+                TraceOptions.DEFAULT), null);
+        }
+
+        @Override
+        public void addAnnotation(String description, Map<String, AttributeValue> attributes) {
+            logger.info("Tracer: {}, attributes: {}", description, attributes.toString());
+        }
+
+        @Override
+        public void addAnnotation(Annotation annotation) {
+            logger.info("addAnnotation");
+        }
+
+        @Override
+        public void addLink(Link link) {
+            logger.info("addLink");
+        }
+
+        @Override
+        public void end(EndSpanOptions options) {
+            logger.info("Tracer end");
+        }
+
+
+        @Override
+        public String toString() {
+            return "DRSpan";
+        }
+    }
+
     @Override
     public Tracer getTracer() {
         return new Tracer() {
@@ -86,12 +106,12 @@ public class TraceComponentImpl extends TraceComponent {
 
     @Override
     public PropagationComponent getPropagationComponent() {
-        return null;
+        return PropagationComponent.getNoopPropagationComponent();
     }
 
     @Override
     public Clock getClock() {
-        return null;
+        return ZeroTimeClock.getInstance();
     }
 
     @Override
@@ -101,6 +121,6 @@ public class TraceComponentImpl extends TraceComponent {
 
     @Override
     public TraceConfig getTraceConfig() {
-        return null;
+        return TraceConfig.getNoopTraceConfig();
     }
 }

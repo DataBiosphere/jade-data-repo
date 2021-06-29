102e20a07f87fbd8bc43fe7250b6fcf23c53455d
Phil Shapiro <pshapiro@broadinstitute.org>
2021-06-29 09:14:37 -0400

    moved logging span back as an inner class; other minor changes (#982)

--- src/main/java/io/opencensus/impl/trace/TraceComponentImpl.java
+++ src/main/java/io/opencensus/impl/trace/TraceComponentImpl.java
@@ -2,10 +2,16 @@ package io.opencensus.impl.trace;
 
 import io.opencensus.common.Clock;
 import io.opencensus.internal.ZeroTimeClock;
+import io.opencensus.trace.Annotation;
+import io.opencensus.trace.AttributeValue;
+import io.opencensus.trace.EndSpanOptions;
+import io.opencensus.trace.Link;
+import io.opencensus.trace.MessageEvent;
 import io.opencensus.trace.Sampler;
 import io.opencensus.trace.Span;
 import io.opencensus.trace.SpanBuilder;
 import io.opencensus.trace.SpanContext;
+import io.opencensus.trace.Status;
 import io.opencensus.trace.TraceComponent;
 import io.opencensus.trace.Tracer;
 import io.opencensus.trace.config.TraceConfig;
@@ -16,10 +22,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Map;
 
 public class TraceComponentImpl extends TraceComponent {
 
-    static class DRSpanBuilder extends SpanBuilder {
+    static class LogSpanBuilder extends SpanBuilder {
+        static final LogSpan SPAN = new LogSpan();
 
         @Override
         public SpanBuilder setSampler(Sampler sampler) {
@@ -38,7 +46,7 @@ public class TraceComponentImpl extends TraceComponent {
 
         @Override
         public Span startSpan() {
-            return new DRSpan();
+            return SPAN;
         }
     }
 
@@ -47,12 +55,12 @@ public class TraceComponentImpl extends TraceComponent {
         return new Tracer() {
             @Override
             public SpanBuilder spanBuilderWithExplicitParent(String spanName, @Nullable Span parent) {
-                return new DRSpanBuilder();
+                return new LogSpanBuilder();
             }
 
             @Override
             public SpanBuilder spanBuilderWithRemoteParent(String spanName, @Nullable SpanContext remoteParentSpanContext) {
-                return new DRSpanBuilder();
+                return new LogSpanBuilder();
             }
         };
     }
@@ -75,5 +83,48 @@ public class TraceComponentImpl extends TraceComponent {
     @Override
     public TraceConfig getTraceConfig() {
         return TraceConfig.getNoopTraceConfig();
+    }
+
+    static final class LogSpan extends Span {
+        private static final Logger logger = LoggerFactory.getLogger(LogSpan.class);
+
+        LogSpan() {
+            super(SpanContext.INVALID, null);
+        }
+
+        @Override
+        public void putAttributes(Map<String, AttributeValue> attributes) {
+            logger.info("putAttributes: {}", attributes);
+        }
+
+        @Override
+        public void addAnnotation(String description, Map<String, AttributeValue> attributes) {
+            logger.info("addAnnotation: {}, attributes: {}", description, attributes);
+        }
+
+        @Override
+        public void addAnnotation(Annotation annotation) {
+            logger.info("addAnnotation: {}", annotation);
+        }
+
+        @Override
+        public void addMessageEvent(MessageEvent messageEvent) {
+            logger.info("addMessageEvent: {}", messageEvent);
+        }
+
+        @Override
+        public void addLink(Link link) {
+            logger.info("addLink: {}", link);
+        }
+
+        @Override
+        public void setStatus(Status status) {
+            logger.info("setStatus: {}", status);
+        }
+
+        @Override
+        public void end(EndSpanOptions options) {
+            logger.info("end: {}", options);
+        }
     }
 }

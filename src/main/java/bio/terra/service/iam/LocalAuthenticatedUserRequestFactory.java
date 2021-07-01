b6c9ad6550f201329b6fc9b6865aaa30f1db52c9
Shelby Holden <sholden@broadinstitute.org>
2021-07-01 16:37:04 -0400

    hack

--- src/main/java/bio/terra/service/iam/LocalAuthenticatedUserRequestFactory.java
+++ src/main/java/bio/terra/service/iam/LocalAuthenticatedUserRequestFactory.java
@@ -1,17 +1,25 @@
 package bio.terra.service.iam;
 
 import bio.terra.app.configuration.ApplicationConfiguration;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class LocalAuthenticatedUserRequestFactory implements AuthenticatedUserRequestFactory {
     private Logger logger = LoggerFactory.getLogger(LocalAuthenticatedUserRequestFactory.class);
+    private final ObjectMapper objectMapper;
 
     // in testing scenarios and when running the server without the proxy not all the
     // header information will be available. default values will be used in these cases.
@@ -21,6 +29,7 @@ public class LocalAuthenticatedUserRequestFactory implements AuthenticatedUserRe
     @Autowired
     public LocalAuthenticatedUserRequestFactory(ApplicationConfiguration applicationConfiguration) {
         this.applicationConfiguration = applicationConfiguration;
+        this.objectMapper = new ObjectMapper();
     }
 
     // Static method to build an AuthenticatedUserRequest from data available to the controller
@@ -30,8 +39,30 @@ public class LocalAuthenticatedUserRequestFactory implements AuthenticatedUserRe
         Optional<String> token = Optional.ofNullable(req.getHeader("Authorization"))
             .map(header -> header.substring("Bearer ".length()));
 
+        if (!token.isPresent()) {
+            return new AuthenticatedUserRequest()
+                .email("")
+                .token(token)
+                .subjectId(applicationConfiguration.getUserId());
+        }
+
+        OkHttpClient httpClient = new OkHttpClient.Builder()
+            .readTimeout(5, TimeUnit.SECONDS)
+            .build();
+
+
         String email = Optional.ofNullable(req.getHeader("From"))
-            .orElse(applicationConfiguration.getUserEmail());
+            .orElseGet(() -> {
+                try {
+                    return objectMapper.readValue(httpClient.newCall(new Request.Builder()
+                        .url(String.format("https://www.googleapis.com/oauth2/v1/userinfo?access_token=%s", token.get()))
+                            .get().build())
+                            .execute().body().string(), new TypeReference<Map<String, String>>() {})
+                            .get("email");
+                } catch (IOException e) {
+                    throw new RuntimeException("Failed to reads user info", e);
+                }
+            });
 
         String userId = applicationConfiguration.getUserId();
 

3cc5810e84d2fb91a557b75fbcb1c82617ed25a3
nmalfroy <nmalfroy@broadinstitute.org>
2021-07-06 19:41:38 -0400

    Add an Azure container Pdao to get/create a container from a storage account

---
+++ src/main/java/bio/terra/service/resourcemanagement/azure/AzureContainerPdao.java
@@ -0,0 +1,52 @@
+package bio.terra.service.resourcemanagement.azure;
+
+import bio.terra.common.exception.PdaoException;
+import bio.terra.model.BillingProfileModel;
+import com.azure.storage.file.datalake.DataLakeFileSystemClient;
+import com.azure.storage.file.datalake.DataLakeServiceClient;
+import com.azure.storage.file.datalake.models.DataLakeStorageException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AzureContainerPdao {
+
+  private final AzureAuthService authService;
+
+  @Autowired
+  public AzureContainerPdao(AzureAuthService authService) {
+    this.authService = authService;
+  }
+
+  /**
+   * Get or create a container (also known as FileSystem) in an Azure storage account
+   *
+   * @param profileModel The profile that describes information needed to access the storage account
+   * @param storageAccountResource Metadata describing the storage account that contains the
+   *     container
+   * @param containerType The nature of the container
+   * @return A client connection object that can be used to create folders and files
+   */
+  public DataLakeFileSystemClient getOrCreateContainer(
+      BillingProfileModel profileModel,
+      AzureStorageAccountResource storageAccountResource,
+      AzureStorageAccountResource.ContainerType containerType) {
+    DataLakeServiceClient dataLakeClient =
+        authService.getDataLakeClient(profileModel, storageAccountResource);
+
+    try {
+      DataLakeFileSystemClient fileSystemClient =
+          dataLakeClient.getFileSystemClient(
+              storageAccountResource.determineContainer(containerType));
+      // This will 404 if the container doesn't exist
+      fileSystemClient.getProperties();
+      return fileSystemClient;
+    } catch (DataLakeStorageException e) {
+      if (e.getStatusCode() == 404) {
+        return dataLakeClient.createFileSystem(
+            storageAccountResource.determineContainer(containerType));
+      }
+      throw new PdaoException("Could not create a Storage account container");
+    }
+  }
+}
---
+++ src/test/java/bio/terra/service/resourcemanagement/azure/AzureContainerPdaoTest.java
@@ -0,0 +1,99 @@
+package bio.terra.service.resourcemanagement.azure;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import bio.terra.common.category.Unit;
+import bio.terra.common.exception.PdaoException;
+import bio.terra.model.BillingProfileModel;
+import bio.terra.service.resourcemanagement.azure.AzureStorageAccountResource.ContainerType;
+import com.azure.storage.file.datalake.DataLakeFileSystemClient;
+import com.azure.storage.file.datalake.DataLakeServiceClient;
+import com.azure.storage.file.datalake.models.DataLakeStorageException;
+import com.azure.storage.file.datalake.models.FileSystemProperties;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Category(Unit.class)
+public class AzureContainerPdaoTest {
+
+  @Mock private AzureAuthService authService;
+  @Mock private DataLakeServiceClient dataLakeServiceClient;
+  private AzureStorageAccountResource storageAccountResource;
+  private BillingProfileModel billingProfile;
+  private AzureContainerPdao dao;
+
+  @Before
+  public void setUp() throws Exception {
+    when(authService.getDataLakeClient(any(), any())).thenReturn(dataLakeServiceClient);
+
+    billingProfile = new BillingProfileModel();
+    storageAccountResource =
+        new AzureStorageAccountResource()
+            .name("mystorageaccount")
+            .metadataContainer("md")
+            .dataContainer("d");
+
+    dao = new AzureContainerPdao(authService);
+  }
+
+  @Test
+  public void testGetContainer() {
+    DataLakeFileSystemClient dataLakeFileSystemClient = mock(DataLakeFileSystemClient.class);
+    FileSystemProperties properties = mock(FileSystemProperties.class);
+    when(properties.getETag()).thenReturn("TAG");
+    when(dataLakeFileSystemClient.getProperties()).thenReturn(properties);
+    when(dataLakeServiceClient.getFileSystemClient("d")).thenReturn(dataLakeFileSystemClient);
+
+    DataLakeFileSystemClient returnedClient =
+        dao.getOrCreateContainer(billingProfile, storageAccountResource, ContainerType.DATA);
+
+    assertThat(returnedClient.getProperties().getETag(), equalTo("TAG"));
+    verify(dataLakeServiceClient, times(0)).createFileSystem(any());
+  }
+
+  @Test
+  public void testCreateContainer() {
+    DataLakeFileSystemClient dataLakeFileSystemClient = mock(DataLakeFileSystemClient.class);
+    DataLakeStorageException exception = mock(DataLakeStorageException.class);
+    when(exception.getStatusCode()).thenReturn(404);
+    when(dataLakeFileSystemClient.getProperties()).thenThrow(exception);
+    when(dataLakeServiceClient.getFileSystemClient("d")).thenReturn(dataLakeFileSystemClient);
+
+    dao.getOrCreateContainer(billingProfile, storageAccountResource, ContainerType.DATA);
+
+    verify(dataLakeServiceClient, times(1)).createFileSystem(eq("d"));
+  }
+
+  @Test
+  public void testGetOrCreateContainerNoPermissions() {
+    DataLakeFileSystemClient dataLakeFileSystemClient = mock(DataLakeFileSystemClient.class);
+    DataLakeStorageException exception = mock(DataLakeStorageException.class);
+    when(exception.getStatusCode()).thenReturn(403);
+    when(dataLakeFileSystemClient.getProperties()).thenThrow(exception);
+    when(dataLakeServiceClient.getFileSystemClient("md")).thenReturn(dataLakeFileSystemClient);
+
+    assertThrows(
+        PdaoException.class,
+        () ->
+            dao.getOrCreateContainer(
+                billingProfile, storageAccountResource, ContainerType.METADATA),
+        "Invalid failure causes real error");
+  }
+}

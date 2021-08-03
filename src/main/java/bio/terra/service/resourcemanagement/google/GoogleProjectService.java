552bca496f59a4419fe6c40ad5533b0d3c75e9e5
Shelby Holden <sholden@broadinstitute.org>
2021-08-03 12:21:48 -0400

    spotbugs

--- src/main/java/bio/terra/service/resourcemanagement/google/GoogleProjectService.java
+++ src/main/java/bio/terra/service/resourcemanagement/google/GoogleProjectService.java
@@ -291,7 +291,7 @@ public class GoogleProjectService {
 
   /**
    * Google requires labels to consist of only lowercase, alphanumeric, "_", and "-" characters.
-   * This value can be at most 63 characters long. 
+   * This value can be at most 63 characters long.
    *
    * @param string String to clean
    * @return The cleaned String

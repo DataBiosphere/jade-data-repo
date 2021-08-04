dad19233501bf5def149a51754b3b0bb8a887fb7
Nick Malfroy-Camine <nmalfroy@broadinstitute.org>
2021-08-04 09:14:34 -0400

    DR-2005 Make it possible to create Azure snapshots (#1022)
    * Don't fail if firestore object has not crc32c
    
    Since Azure files don't have this
    
    * Don't set file ACLs on buckets when creating an Azure snapshot
    
    * Add a method in the FSFile object to infer the cloud type
    
    * Add logic to return Azure files with Drs object lookup
    
    * Do some basic URI sanitation
    
    * PR Feedback

---
+++ src/test/java/bio/terra/service/filedata/google/firestore/FSFileTest.java
@@ -0,0 +1,36 @@
+package bio.terra.service.filedata.google.firestore;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import bio.terra.common.category.Unit;
+import bio.terra.model.CloudPlatform;
+import bio.terra.service.filedata.FSFile;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+@Category(Unit.class)
+public class FSFileTest {
+
+  @Test
+  public void testCloudPlatformDetection() {
+    assertThat(
+        new FSFile().gspath("gs://mybucket/test.txt").getCloudPlatform(),
+        equalTo(CloudPlatform.GCP));
+    assertThat(
+        new FSFile()
+            .gspath("gs://mybucket/dsid/fid/file with space and #hash%percent+plus.txt")
+            .getCloudPlatform(),
+        equalTo(CloudPlatform.GCP));
+    assertThat(
+        new FSFile().gspath("https://myacct.blob.core.windows.net/fs/test.txt").getCloudPlatform(),
+        equalTo(CloudPlatform.AZURE));
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> new FSFile().gspath("ftp://notsupported.txt").getCloudPlatform());
+    assertThrows(
+        IllegalArgumentException.class,
+        () -> new FSFile().gspath("https://myhost.com/notsupported.txt").getCloudPlatform());
+  }
+}

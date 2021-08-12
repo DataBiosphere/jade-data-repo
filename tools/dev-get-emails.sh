a629dac1444426f24a6e737f828172d2351c5192
Florian Boulnois <fboulnoi@broadinstitute.org>
2021-08-12 19:12:06 -0400

    Switch back to original account

--- tools/dev-get-emails.sh
+++ tools/dev-get-emails.sh
@@ -3,12 +3,12 @@
 
 set -eu
 
-read -rp "Enter your Broad email: " BROAD_EMAIL
 read -rp "Enter your dev email: " DEV_EMAIL
 
+ORIG_EMAIL=$(gcloud config get-value account 2>/dev/null)
 gcloud auth login "${DEV_EMAIL}" 2>/dev/null
 TOKEN=$(gcloud auth print-access-token)
-gcloud auth login "${BROAD_EMAIL}" 2>/dev/null
+gcloud auth login "${ORIG_EMAIL}" 2>/dev/null
 
 dataset_list() {
     curl -s -X GET "https://jade.datarepo-dev.broadinstitute.org/api/repository/v1/datasets?direction=desc&limit=10000&offset=0&sort=created_date" -H "accept: application/json" -H "authorization: Bearer ${TOKEN}" | jq -r '.items[] | "\(.id) \(.name)"'

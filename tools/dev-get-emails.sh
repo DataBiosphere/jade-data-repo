9bc2f272776fd0e8d4abb3ef58c6e9c755ce57f0
Florian Boulnois <fboulnoi@broadinstitute.org>
2021-08-13 11:10:27 -0400

    Split on multiple lines

--- tools/dev-get-emails.sh
+++ tools/dev-get-emails.sh
@@ -11,11 +11,16 @@ TOKEN=$(gcloud auth print-access-token)
 gcloud auth login "${ORIG_EMAIL}" 2>/dev/null
 
 dataset_list() {
-    curl -s -X GET "https://jade.datarepo-dev.broadinstitute.org/api/repository/v1/datasets?direction=desc&limit=10000&offset=0&sort=created_date" -H "accept: application/json" -H "authorization: Bearer ${TOKEN}" | jq -r '.items[] | "\(.id) \(.name)"'
+    curl -s -X GET "https://jade.datarepo-dev.broadinstitute.org/api/repository/v1/datasets?direction=desc&limit=10000&offset=0&sort=created_date" \
+        -H "accept: application/json" \
+        -H "authorization: Bearer ${TOKEN}" \
+            | jq -r '.items[] | "\(.id) \(.name)"'
 }
 
 dataset_policy() {
-    curl -s -X GET "https://sam.dsde-dev.broadinstitute.org/api/resources/v2/dataset/${1}/policies" -H "accept: application/json" -H "authorization: Bearer ${TOKEN}"
+    curl -s -X GET "https://sam.dsde-dev.broadinstitute.org/api/resources/v2/dataset/${1}/policies" \
+        -H "accept: application/json" \
+        -H "authorization: Bearer ${TOKEN}"
 }
 
 dataset_emails() {
@@ -24,7 +29,8 @@ dataset_emails() {
     for ROW in $(dataset_list) ; do
         DATASET_ID=$(echo "${ROW}" | cut -d' ' -f1)
         DATASET_NAME=$(echo "${ROW}" | cut -d' ' -f2)
-        DATASET_EMAILS=$(dataset_policy "${DATASET_ID}" | jq --arg NAME "${DATASET_NAME}" '.[] | select(.policyName == "steward") | .policy | {memberEmails} | . + {name: $NAME}')
+        DATASET_EMAILS=$(dataset_policy "${DATASET_ID}" \
+            | jq --arg NAME "${DATASET_NAME}" '.[] | select(.policyName == "steward") | .policy | {memberEmails} | . + {name: $NAME}')
         ALL_EMAILS+=("${DATASET_EMAILS}")
     done
     echo "${ALL_EMAILS[@]}" | jq -s '.' > emails.json

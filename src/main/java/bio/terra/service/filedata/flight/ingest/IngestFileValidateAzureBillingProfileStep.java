939c3f66375704279425f8897f00bf96f896c9e1
Saman Ehsan <se8ea@virginia.edu>
2021-08-26 08:24:17 -0500

    Fix spotless error

--- src/main/java/bio/terra/service/filedata/flight/ingest/IngestFileValidateAzureBillingProfileStep.java
+++ src/main/java/bio/terra/service/filedata/flight/ingest/IngestFileValidateAzureBillingProfileStep.java
@@ -1,17 +1,14 @@
 package bio.terra.service.filedata.flight.ingest;
 
-import bio.terra.common.CloudPlatformWrapper;
-import bio.terra.model.CloudPlatform;
 import bio.terra.service.dataset.Dataset;
 import bio.terra.service.dataset.exception.IngestFailureException;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.Step;
 import bio.terra.stairway.StepResult;
 import bio.terra.stairway.StepStatus;
+import java.util.UUID;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.UUID;
 
 public class IngestFileValidateAzureBillingProfileStep implements Step {
 
@@ -20,8 +17,7 @@ public class IngestFileValidateAzureBillingProfileStep implements Step {
   private final UUID profileId;
   private final Dataset dataset;
 
-  public IngestFileValidateAzureBillingProfileStep(UUID profileId,
-                                                   Dataset dataset) {
+  public IngestFileValidateAzureBillingProfileStep(UUID profileId, Dataset dataset) {
     this.profileId = profileId;
     this.dataset = dataset;
   }
@@ -32,15 +28,15 @@ public class IngestFileValidateAzureBillingProfileStep implements Step {
     if (!defaultBillingProfileId.equals(profileId)) {
       logger.warn(
           "The billing profile in the ingest request ({}) does not match the default billing profile ({})",
-          profileId, defaultBillingProfileId);
+          profileId,
+          defaultBillingProfileId);
       IngestFailureException ex =
           new IngestFailureException(
               String.format(
                   "Can't ingest files using a different billing profile %s than the Azure dataset %s.",
-                      profileId, defaultBillingProfileId));
+                  profileId, defaultBillingProfileId));
       return new StepResult(StepStatus.STEP_RESULT_FAILURE_FATAL, ex);
     }
-
 
     return StepResult.getStepResultSuccess();
   }

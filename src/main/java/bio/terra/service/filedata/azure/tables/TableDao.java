d1dca7de31119bd01387235e4f89b8bf8780636b
Saman Ehsan <se8ea@virginia.edu>
2021-08-31 10:10:06 -0400

    Remove unused code in azure tabledao

--- src/main/java/bio/terra/service/filedata/azure/tables/TableDao.java
+++ src/main/java/bio/terra/service/filedata/azure/tables/TableDao.java
@@ -1,6 +1,5 @@
 package bio.terra.service.filedata.azure.tables;
 
-import bio.terra.app.logging.PerformanceLogger;
 import bio.terra.model.BillingProfileModel;
 import bio.terra.service.configuration.ConfigEnum;
 import bio.terra.service.configuration.ConfigurationService;
@@ -9,7 +8,6 @@ import bio.terra.service.filedata.*;
 import bio.terra.service.filedata.exception.FileNotFoundException;
 import bio.terra.service.filedata.google.firestore.FireStoreDirectoryEntry;
 import bio.terra.service.filedata.google.firestore.FireStoreFile;
-import bio.terra.service.filedata.google.firestore.FireStoreUtils;
 import bio.terra.service.filedata.google.firestore.InterruptibleConsumer;
 import bio.terra.service.profile.ProfileDao;
 import bio.terra.service.resourcemanagement.azure.AzureAuthService;
@@ -26,7 +24,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 // Operations on a file often need to touch file and directory collections that is,
-// the FireStoreFileDao and the FireStoreDirectoryDao.
+// the Azure TableFileDao and the TableDirectoryDao.
 // The data to make an FSDir or FSFile is now spread between the file collection and the
 // directory collection, so a lookup needs to visit two places to generate a complete FSItem.
 // This class coordinates operations between the daos.
@@ -48,9 +46,7 @@ public class TableDao {
   private final ProfileDao profileDao;
   private final AzureAuthService azureAuthService;
   private final FileMetadataUtils fileMetadataUtils;
-  private final FireStoreUtils fireStoreUtils;
   private final ConfigurationService configurationService;
-  private final PerformanceLogger performanceLogger;
 
   @Autowired
   public TableDao(
@@ -59,17 +55,13 @@ public class TableDao {
       ProfileDao profileDao,
       AzureAuthService azureAuthService,
       FileMetadataUtils fileMetadataUtils,
-      FireStoreUtils fireStoreUtils,
-      ConfigurationService configurationService,
-      PerformanceLogger performanceLogger) {
+      ConfigurationService configurationService) {
     this.directoryDao = directoryDao;
     this.fileDao = fileDao;
     this.profileDao = profileDao;
     this.azureAuthService = azureAuthService;
     this.fileMetadataUtils = fileMetadataUtils;
-    this.fireStoreUtils = fireStoreUtils;
     this.configurationService = configurationService;
-    this.performanceLogger = performanceLogger;
   }
 
   public void createDirectoryEntry(
@@ -124,15 +116,6 @@ public class TableDao {
     logger.info("deleting direcotry entries");
     directoryDao.deleteDirectoryEntriesFromCollection(tableServiceClient);
   }
-
-  //      public FireStoreDirectoryEntry lookupDirectoryEntry(Dataset dataset, String fileId)
-  //              throws InterruptedException {
-  //          Firestore firestore =
-  //
-  //   FireStoreProject.get(dataset.getProjectResource().getGoogleProjectId()).getFirestore();
-  //          String datasetId = dataset.getId().toString();
-  //          return directoryDao.retrieveById(firestore, datasetId, fileId);
-  //      }
 
   public FireStoreDirectoryEntry lookupDirectoryEntryByPath(
       Dataset dataset,
@@ -217,87 +200,23 @@ public class TableDao {
         fileId);
   }
 
-  //    /**
-  //     * Retrieve a batch of FSFile by id
-  //     *
-  //     * @param container - dataset or snapshot containing file's directory entry
-  //     * @param fileIds - list of ids of file identifiers - directory identifiers will throw
-  //     * @return list of FSItem of retrieved files; throws on not found
-  //     */
-  //      public List<FSFile> batchRetrieveById(
-  //              FSContainerInterface container, List<String> fileIds, int enumerateDepth)
-  //              throws InterruptedException {
-  //
-  //          Firestore firestore =
-  // FireStoreProject.get(container.getProjectResource().getGoogleProjectId()).getFirestore();
-  //          String containerId = container.getId().toString();
-  //
-  //          List<FireStoreDirectoryEntry> directoryEntries =
-  //                  directoryDao.batchRetrieveById(firestore, containerId, fileIds);
-  //
-  //          // TODO: When we have more than one dataset in a snapshot then we will have to
-  //          //  split entries by underlying dataset. For now we know that they all come from one
-  // dataset.
-  //          List<FireStoreFile> files =
-  //                  fileDao.batchRetrieveFileMetadata(firestore, containerId, directoryEntries);
-  //
-  //          List<FSFile> resultList = new ArrayList<>();
-  //          if (directoryEntries.size() != files.size()) {
-  //              throw new FileSystemExecutionException("List sizes should be identical");
-  //          }
-  //
-  //          for (int i = 0; i < files.size(); i++) {
-  //              FireStoreFile file = files.get(i);
-  //              FireStoreDirectoryEntry entry = directoryEntries.get(i);
-  //
-  //              FSFile fsFile =
-  //                      new FSFile()
-  //                              .fileId(UUID.fromString(entry.getFileId()))
-  //                              .collectionId(UUID.fromString(entry.getDatasetId()))
-  //                              .datasetId(UUID.fromString(entry.getDatasetId()))
-  //                              .createdDate(Instant.parse(file.getFileCreatedDate()))
-  //                              .path(fileMetadataUtils.getFullPath(entry.getPath(),
-  // entry.getName()))
-  //                              .checksumCrc32c(file.getChecksumCrc32c())
-  //                              .checksumMd5(file.getChecksumMd5())
-  //                              .size(file.getSize())
-  //                              .description(file.getDescription())
-  //                              .gspath(file.getGspath())
-  //                              .mimeType(file.getMimeType())
-  //                              .bucketResourceId(file.getBucketResourceId())
-  //                              .loadTag(file.getLoadTag());
-  //
-  //              resultList.add(fsFile);
-  //          }
-  //
-  //          return resultList;
-  //      }
+  //   -- private methods --
 
-  //      public List<String> validateRefIds(Dataset dataset, List<String> refIdArray)
-  //              throws InterruptedException {
-  //          Firestore firestore =
-  // FireStoreProject.get(dataset.getProjectResource().getGoogleProjectId()).getFirestore();
-  //          String datasetId = dataset.getId().toString();
-  //          return directoryDao.validateRefIds(firestore, datasetId, refIdArray);
-  //      }
-
-  // -- private methods --
-  //
-  //      /**
-  //       * Retrieves an FSItem object
-  //       *
-  //       * @param tableServiceClient The client for the dataset or snapshot containing the
-  //       *                           virtual file system
-  //       * @param datasetTableServiceClient The client for the dataset (which contains the file
-  //       *     object metadata)
-  //       * @param collectionId The ID of the collection in the fsItemFirestore connection that
-  //       *                     contains the virtual file system objects
-  //       * @param enumerateDepth how far to enumerate the directory structure
-  //       * @param fireStoreDirectoryEntry The object to enumerate entries within
-  //       * @param context provides either the file id or the file path, for use in error messages.
-  //       * @return An {@link FSItem} representation of the passed in fireStoreDirectoryEntry with
-  // nested FSItems
-  //       */
+  /**
+   * Retrieves an FSItem object
+   *
+   * @param tableServiceClient The client for the dataset or snapshot containing the virtual file
+   *     system
+   * @param datasetTableServiceClient The client for the dataset (which contains the file object
+   *     metadata)
+   * @param collectionId The ID of the collection in the fsItemFirestore connection that contains
+   *     the virtual file system objects
+   * @param enumerateDepth how far to enumerate the directory structure
+   * @param fireStoreDirectoryEntry The object to enumerate entries within
+   * @param context provides either the file id or the file path, for use in error messages.
+   * @return An {@link FSItem} representation of the passed in fireStoreDirectoryEntry with nested
+   *     FSItems
+   */
   private FSItem retrieveWorker(
       TableServiceClient tableServiceClient,
       TableServiceClient datasetTableServiceClient,
@@ -327,22 +246,21 @@ public class TableDao {
         enumerateDepth,
         fireStoreDirectoryEntry);
   }
-  //
-  //      /**
-  //       * Create an FSItem object
-  //       *
-  //       * @param tableServiceClient The client for the dataset or snapshot containing the
-  //       *                           virtual file system
-  //       * @param datasetTableServiceClient The client for the dataset (which contains the file
-  //       *     object metadata)
-  //       * @param collectionId The ID of the collection in the fsItemFirestore connection that
-  // contains
-  //       *     the virtual file system objects
-  //       * @param level how far to enumerate the directory structure
-  //       * @param fireStoreDirectoryEntry The object to enumerate entries within
-  //       * @return An {@link FSItem} representation of the passed in fireStoreDirectoryEntry with
-  // nested FSItems
-  //       */
+
+  /**
+   * Create an FSItem object
+   *
+   * @param tableServiceClient The client for the dataset or snapshot containing the virtual file
+   *     system
+   * @param datasetTableServiceClient The client for the dataset (which contains the file object
+   *     metadata)
+   * @param collectionId The ID of the collection in the fsItemFirestore connection that contains
+   *     the virtual file system objects
+   * @param level how far to enumerate the directory structure
+   * @param fireStoreDirectoryEntry The object to enumerate entries within
+   * @return An {@link FSItem} representation of the passed in fireStoreDirectoryEntry with nested
+   *     FSItems
+   */
   private FSItem makeFSDir(
       TableServiceClient tableServiceClient,
       TableServiceClient datasetTableServiceClient,
@@ -430,127 +348,7 @@ public class TableDao {
 
     return fsFile;
   }
-  //
-  //    // Recursively compute the size and checksums of a directory
-  //    FireStoreDirectoryEntry computeDirectory(
-  //            Firestore snapshotFirestore,
-  //            Firestore datasetFirestore,
-  //            String snapshotId,
-  //            FireStoreDirectoryEntry dirEntry,
-  //            List<FireStoreDirectoryEntry> updateBatch)
-  //            throws InterruptedException {
-  //
-  //        String fullPath = fireStoreUtils.getFullPath(dirEntry.getPath(), dirEntry.getName());
-  //        List<FireStoreDirectoryEntry> enumDir =
-  //                directoryDao.enumerateDirectory(snapshotFirestore, snapshotId, fullPath);
-  //
-  //        List<FireStoreDirectoryEntry> enumComputed = new ArrayList<>();
-  //
-  //        // Recurse to compute results from underlying directories
-  //        try (Stream<FireStoreDirectoryEntry> stream = enumDir.stream()) {
-  //            enumComputed.addAll(
-  //                    stream
-  //                            .filter(f -> !f.getIsFileRef())
-  //                            .map(
-  //                                    f -> {
-  //                                        try {
-  //                                            return computeDirectory(
-  //                                                    snapshotFirestore, datasetFirestore,
-  // snapshotId, f, updateBatch);
-  //                                        } catch (InterruptedException e) {
-  //                                            throw new DirectoryMetadataComputeException(
-  //                                                    "Error computing directory metadata", e);
-  //                                        }
-  //                                    })
-  //                            .collect(Collectors.toList()));
-  //        }
-  //
-  //        // Collect metadata for file objects in the directory
-  //        try (Stream<FireStoreDirectoryEntry> stream = enumDir.stream()) {
-  //            // Group FireStoreDirectoryEntry objects by dataset Id to process one dataset at a
-  // time
-  //            final Map<String, List<FireStoreDirectoryEntry>> fileRefsByDatasetId =
-  //                    stream
-  //                            .filter(FireStoreDirectoryEntry::getIsFileRef)
-  //
-  // .collect(Collectors.groupingBy(FireStoreDirectoryEntry::getDatasetId));
-  //
-  //            for (Map.Entry<String, List<FireStoreDirectoryEntry>> entry :
-  //                    fileRefsByDatasetId.entrySet()) {
-  //                // Retrieve the file metadata from Firestore
-  //                final List<FireStoreFile> fireStoreFiles =
-  //                        fileDao.batchRetrieveFileMetadata(datasetFirestore, entry.getKey(),
-  // entry.getValue());
-  //
-  //                final AtomicInteger index = new AtomicInteger(0);
-  //                enumComputed.addAll(
-  //                        CollectionUtils.collect(
-  //                                entry.getValue(),
-  //                                dirItem -> {
-  //                                    final FireStoreFile file =
-  // fireStoreFiles.get(index.getAndIncrement());
-  //                                    if (file == null) {
-  //                                        throw new FileNotFoundException("File metadata was
-  // missing");
-  //                                    }
-  //                                    return dirItem
-  //                                            .size(file.getSize())
-  //                                            .checksumMd5(file.getChecksumMd5())
-  //                                            .checksumCrc32c(file.getChecksumCrc32c());
-  //                                }));
-  //            }
-  //        }
-  //
-  //        // Collect the ingredients for computing this directory's checksums and size
-  //        List<String> md5Collection = new ArrayList<>();
-  //        List<String> crc32cCollection = new ArrayList<>();
-  //        long totalSize = 0L;
-  //
-  //        for (FireStoreDirectoryEntry dirItem : enumComputed) {
-  //            totalSize = totalSize + dirItem.getSize();
-  //            if (!StringUtils.isEmpty(dirItem.getChecksumCrc32c())) {
-  //                crc32cCollection.add(dirItem.getChecksumCrc32c().toLowerCase());
-  //            }
-  //            if (!StringUtils.isEmpty(dirItem.getChecksumMd5())) {
-  //                md5Collection.add(dirItem.getChecksumMd5().toLowerCase());
-  //            }
-  //        }
-  //
-  //        // Compute checksums
-  //        // The spec is not 100% clear on the algorithm. I made specific choices on
-  //        // how to implement it:
-  //        // - set hex strings to lowercase before processing so we get consistent sort
-  //        //   order and digest results.
-  //        // - do not make leading zeros converting crc32 long to hex and it is returned
-  //        //   in lowercase. (Matches the semantics of toHexString).
-  //        Collections.sort(md5Collection);
-  //        String md5Concat = StringUtils.join(md5Collection, StringUtils.EMPTY);
-  //        String md5Checksum = fireStoreUtils.computeMd5(md5Concat);
-  //
-  //        Collections.sort(crc32cCollection);
-  //        String crc32cConcat = StringUtils.join(crc32cCollection, StringUtils.EMPTY);
-  //        String crc32cChecksum = fireStoreUtils.computeCrc32c(crc32cConcat);
-  //
-  //        // Update the directory in place
-  //        dirEntry.checksumCrc32c(crc32cChecksum).checksumMd5(md5Checksum).size(totalSize);
-  //        updateEntry(snapshotFirestore, snapshotId, dirEntry, updateBatch);
-  //
-  //        return dirEntry;
-  //    }
-  //
-  //    private void updateEntry(
-  //            Firestore firestore,
-  //            String snapshotId,
-  //            FireStoreDirectoryEntry entry,
-  //            List<FireStoreDirectoryEntry> updateBatch)
-  //            throws InterruptedException {
-  //
-  //        updateBatch.add(entry);
-  //        int batchSize = configurationService.getParameterValue(FIRESTORE_SNAPSHOT_BATCH_SIZE);
-  //        if (updateBatch.size() >= batchSize) {
-  //            logger.info("Snapshot compute updating batch of {} directory entries", batchSize);
-  //            directoryDao.batchStoreDirectoryEntry(firestore, snapshotId, updateBatch);
-  //            updateBatch.clear();
-  //        }
-  //    }
+
+  // TODO: Implement computeDirectory to recursively compute the size and checksums of a directory
+
 }

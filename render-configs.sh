b9c9e73715ae3c6391fceba1a927c541992ca4d0
Florian Boulnois <fboulnoi@broadinstitute.org>
2021-09-01 17:26:51 -0400

    Fix field to read from vault

--- render-configs.sh
+++ render-configs.sh
@@ -7,8 +7,8 @@ vault read -format=json secret/dsde/datarepo/dev/sa-key.json \
     | jq .data | tee /tmp/jade-dev-account.json \
     | jq -r .private_key > /tmp/jade-dev-account.pem
 
-vault read -format=json secret/dsde/datarepo/dev/helm-azure \
-    | jq -r .private_key > /tmp/jade-dev-azure.key
+vault read -field=applicationsecret secret/dsde/datarepo/dev/helm-azure \
+    > /tmp/jade-dev-azure.key
 
 BUFFER_CLIENT_SERVICE_ACCOUNT_VAULT_PATH=secret/dsde/terra/kernel/integration/tools/buffer/client-sa
 BUFFER_CLIENT_SERVICE_ACCOUNT_OUTPUT_PATH=/tmp/buffer-client-sa-account.json

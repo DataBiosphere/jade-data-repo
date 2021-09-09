0a025ead61ee87ba9b9a94883c2c0494fe146492
Shelby Holden <sholden@broadinstitute.org>
2021-09-09 10:45:20 -0400

    wip

--- src/main/java/bio/terra/service/filedata/azure/blobstore/AzureBufferedReader.java
+++ src/main/java/bio/terra/service/filedata/azure/blobstore/AzureBufferedReader.java
@@ -3,9 +3,16 @@ package bio.terra.service.filedata.azure.blobstore;
 import bio.terra.service.dataset.flight.ingest.IngestUtils;
 import bio.terra.service.filedata.google.gcs.GcsPdao;
 import com.azure.storage.blob.BlobUrlParts;
+import com.azure.storage.blob.models.BlobProperties;
+import com.azure.storage.blob.models.BlobRequestConditions;
+import com.azure.storage.blob.specialized.BlobAsyncClientBase;
+import com.azure.storage.blob.specialized.BlobClientBase;
+import com.azure.storage.blob.specialized.BlobInputStream;
+import com.azure.storage.blob.specialized.SpecializedBlobClientBuilder;
 import com.google.cloud.storage.Storage;
 import org.springframework.beans.factory.annotation.Autowired;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.nio.channels.Channels;
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
@@ -15,14 +22,15 @@ public class AzureBufferedReader extends BufferedReader {
 
   @Autowired AzureBlobStorePdao azureBlobStorePdao;
 
-    public AzureBufferedReader(Storage storage, UUID tenantId, String blobStoreUrl) {
+    public AzureBufferedReader(Storage storage, UUID tenantId, String blobStoreUrl) throws IOException {
       IngestUtils.validateBlobAzureBlobFileURL(blobStoreUrl);
       BlobUrlParts ingestRequestSignUrlBlob =
          azureBlobStorePdao.getOrSignUrlForSourceFactory(blobStoreUrl, tenantId);
+//      BlobAsyncClientBase asyncClientBase = new BlobAsyncClientBase();
+      SpecializedBlobClientBuilder builder = new SpecializedBlobClientBuilder();
+      BlobClientBase clientBase = builder.buildPageBlobClient();
+      BlobInputStream stream = clientBase.openInputStream();
       // TODO - figure out how to get from URL to buffered reader
-//      super(
-//          Channels.newReader(
-//              GcsPdao.getBlobFromGsPath(storage, blobStoreUrl, projectId).reader(),
-//              StandardCharsets.UTF_8.name()));
+      super(stream);
     }
   }

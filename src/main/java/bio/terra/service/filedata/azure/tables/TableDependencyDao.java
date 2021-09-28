6d9b7dae7ce9697f17a600a6860b819bb83f9125
Saman Ehsan <sehsan@broadinstitute.org>
2021-09-28 16:19:11 -0400

    [DR-2081] Add ability to update Azure dataset dependencies table (#1069)
    * Store snapshot dependency metadata in Azure
    
    * Add delete azure snapshot file dependencies method
    
    * Remove unused firestore dependency method
    
    * Spotless fixes
    
    * Spotless fixes
    
    * batch lookup by file
    
    * Batch add/update snapshot file dependencies
    
    * Cleanup filter
    
    * Simplify FireStoreDependency toTableEntity
    
    * Remove unused code
    
    * Spotless fixes
    
    * Add azure dependency dao connected test
    
    * Upsert snapshot file dependencies
    
    * Fix unit test
    
    Co-authored-by: Shelby Holden <sholden@broadinstitute.org>

---
+++ src/main/java/bio/terra/service/filedata/azure/tables/TableDependencyDao.java
@@ -0,0 +1,99 @@
+package bio.terra.service.filedata.azure.tables;
+
+import bio.terra.service.filedata.google.firestore.FireStoreDependency;
+import com.azure.core.http.rest.PagedIterable;
+import com.azure.data.tables.TableClient;
+import com.azure.data.tables.TableServiceClient;
+import com.azure.data.tables.models.ListEntitiesOptions;
+import com.azure.data.tables.models.TableEntity;
+import com.azure.data.tables.models.TableTransactionAction;
+import com.azure.data.tables.models.TableTransactionActionType;
+import com.google.common.annotations.VisibleForTesting;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import org.apache.commons.collections4.ListUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TableDependencyDao {
+  private final Logger logger = LoggerFactory.getLogger(TableDependencyDao.class);
+  private static final String DEPENDENCY_TABLE_NAME_SUFFIX = "dependencies";
+  private static final int MAX_FILTER_CLAUSES = 15;
+
+  @Autowired
+  public TableDependencyDao() {}
+
+  @VisibleForTesting
+  public String getDatasetDependencyTableName(UUID datasetId) {
+    return "datarepo" + datasetId.toString().replaceAll("-", "") + DEPENDENCY_TABLE_NAME_SUFFIX;
+  }
+
+  public void storeSnapshotFileDependencies(
+      TableServiceClient tableServiceClient, UUID datasetId, UUID snapshotId, List<String> refIds) {
+
+    // We construct the snapshot file system without using transactions. We can get away with that,
+    // because no one can access this snapshot during its creation.
+    String dependencyTableName = getDatasetDependencyTableName(datasetId);
+    tableServiceClient.createTableIfNotExists(dependencyTableName);
+    TableClient tableClient = tableServiceClient.getTableClient(dependencyTableName);
+    // The partition size is one less than the MAX_FILTER_CLAUSES to account for the snapshotId
+    // filter
+    ListUtils.partition(refIds, MAX_FILTER_CLAUSES - 1)
+        .forEach(
+            refIdChunk -> {
+              String filter =
+                  refIdChunk.stream()
+                          // maybe wrap or cause in parenthesis
+                          .map(
+                              refId ->
+                                  String.format(
+                                      "%s eq '%s'", FireStoreDependency.FILE_ID_FIELD_NAME, refId))
+                          .collect(Collectors.joining(" or "))
+                      + String.format(
+                          " and %s eq '%s'",
+                          FireStoreDependency.SNAPSHOT_ID_FIELD_NAME, snapshotId);
+              List<TableEntity> entities =
+                  TableServiceClientUtils.filterTable(
+                      tableServiceClient, dependencyTableName, filter);
+              List<String> existing =
+                  entities.stream()
+                      .map(e -> e.getProperty(FireStoreDependency.FILE_ID_FIELD_NAME).toString())
+                      .collect(Collectors.toList());
+              // Create any entities that do not already exist
+              refIdChunk.stream()
+                  .filter(id -> !existing.contains(id))
+                  .forEach(refId -> createDependencyEntity(tableClient, snapshotId, refId));
+            });
+  }
+
+  private void createDependencyEntity(TableClient tableClient, UUID snapshotId, String refId) {
+    FireStoreDependency fireStoreDependency =
+        new FireStoreDependency().snapshotId(snapshotId.toString()).fileId(refId).refCount(1L);
+    TableEntity fireStoreDependencyEntity = FireStoreDependency.toTableEntity(fireStoreDependency);
+    tableClient.upsertEntity(fireStoreDependencyEntity);
+  }
+
+  public void deleteSnapshotFileDependencies(
+      TableServiceClient tableServiceClient, UUID datasetId, UUID snapshotId) {
+    String dependencyTableName = getDatasetDependencyTableName(datasetId);
+    if (TableServiceClientUtils.tableHasEntries(tableServiceClient, dependencyTableName)) {
+      TableClient tableClient = tableServiceClient.getTableClient(dependencyTableName);
+      ListEntitiesOptions options =
+          new ListEntitiesOptions().setFilter(String.format("snapshotId eq '%s'", snapshotId));
+      PagedIterable<TableEntity> entities = tableClient.listEntities(options, null, null);
+      var batchEntities =
+          entities.stream()
+              .map(entity -> new TableTransactionAction(TableTransactionActionType.DELETE, entity))
+              .collect(Collectors.toList());
+      logger.info(
+          "Deleting snapshot {} file dependencies from {}", snapshotId, dependencyTableName);
+      tableClient.submitTransaction(batchEntities);
+    } else {
+      logger.warn("No snapshot file dependencies found to be deleted from dataset");
+    }
+  }
+}
---
+++ src/test/java/bio/terra/service/filedata/azure/tables/TableDependencyConnectedTest.java
@@ -0,0 +1,93 @@
+package bio.terra.service.filedata.azure.tables;
+
+import static org.junit.Assert.*;
+
+import bio.terra.app.configuration.ConnectedTestConfiguration;
+import bio.terra.common.AzureUtils;
+import bio.terra.common.category.Connected;
+import bio.terra.service.filedata.google.firestore.FireStoreDependency;
+import com.azure.core.credential.AzureNamedKeyCredential;
+import com.azure.data.tables.TableClient;
+import com.azure.data.tables.TableServiceClient;
+import com.azure.data.tables.TableServiceClientBuilder;
+import com.azure.data.tables.models.TableEntity;
+import com.azure.data.tables.models.TableServiceException;
+import java.util.List;
+import java.util.UUID;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles({"google", "connectedtest"})
+@Category(Connected.class)
+public class TableDependencyConnectedTest {
+
+  @Autowired private ConnectedTestConfiguration connectedTestConfiguration;
+  @Autowired private TableDependencyDao dependencyDao;
+  @Autowired AzureUtils azureUtils;
+  private TableServiceClient tableServiceClient;
+
+  private static final UUID DATASET_ID = UUID.randomUUID();
+  private static final UUID SNAPSHOT_ID = UUID.randomUUID();
+  private static final UUID SNAPSHOT_ID2 = UUID.randomUUID();
+  private static final String FILE_ID = UUID.randomUUID().toString();
+  private static final List<String> REF_IDS = List.of(FILE_ID);
+
+  @Before
+  public void setUp() {
+    tableServiceClient =
+        new TableServiceClientBuilder()
+            .credential(
+                new AzureNamedKeyCredential(
+                    connectedTestConfiguration.getSourceStorageAccountName(),
+                    azureUtils.getSourceStorageAccountPrimarySharedKey()))
+            .endpoint(
+                "https://"
+                    + connectedTestConfiguration.getSourceStorageAccountName()
+                    + ".table.core.windows.net")
+            .buildClient();
+  }
+
+  @Test
+  public void testCreateDeleteDependencyEntries() {
+    String tableName = dependencyDao.getDatasetDependencyTableName(DATASET_ID);
+    TableClient tableClient = tableServiceClient.getTableClient(tableName);
+
+    // Add snapshot file dependency
+    dependencyDao.storeSnapshotFileDependencies(
+        tableServiceClient, DATASET_ID, SNAPSHOT_ID, REF_IDS);
+    TableEntity createdEntity = tableClient.getEntity(SNAPSHOT_ID.toString(), FILE_ID);
+    assertEntityCorrect(createdEntity, SNAPSHOT_ID, FILE_ID, 1L);
+
+    // Add same file in a different snapshot
+    dependencyDao.storeSnapshotFileDependencies(
+        tableServiceClient, DATASET_ID, SNAPSHOT_ID2, REF_IDS);
+
+    // Deleting the SNAPSHOT_ID dependencies does not affect other snapshot records
+    dependencyDao.deleteSnapshotFileDependencies(tableServiceClient, DATASET_ID, SNAPSHOT_ID);
+    assertThrows(
+        TableServiceException.class, () -> tableClient.getEntity(SNAPSHOT_ID.toString(), FILE_ID));
+    assertNotNull(tableClient.getEntity(SNAPSHOT_ID2.toString(), FILE_ID));
+
+    dependencyDao.deleteSnapshotFileDependencies(tableServiceClient, DATASET_ID, SNAPSHOT_ID2);
+    assertThrows(
+        TableServiceException.class, () -> tableClient.getEntity(SNAPSHOT_ID2.toString(), FILE_ID));
+  }
+
+  private void assertEntityCorrect(
+      TableEntity entity, UUID snapshotId, String fileId, Long refCount) {
+    assertEquals(
+        entity.getProperty(FireStoreDependency.SNAPSHOT_ID_FIELD_NAME), snapshotId.toString());
+    assertEquals(entity.getProperty(FireStoreDependency.FILE_ID_FIELD_NAME), fileId);
+    assertEquals(entity.getProperty(FireStoreDependency.REF_COUNT_FIELD_NAME), refCount);
+  }
+}

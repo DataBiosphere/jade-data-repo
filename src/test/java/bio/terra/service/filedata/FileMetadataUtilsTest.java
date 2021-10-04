4997e41e5b293cce69ab0991a2073dd19e716455
Shelby Holden <sholden@broadinstitute.org>
2021-10-04 12:17:37 -0400

    Add comments and another check

--- src/test/java/bio/terra/service/filedata/FileMetadataUtilsTest.java
+++ src/test/java/bio/terra/service/filedata/FileMetadataUtilsTest.java
@@ -64,10 +64,11 @@ public class FileMetadataUtilsTest {
   @Test
   public void findNewDirectoryPathsTestCache() {
     List<FireStoreDirectoryEntry> testEntries = initTestEntries(5);
-    // LRU has enough space for all entries
+    // LRU does NOT have enough space for all entries
     LRUMap<String, Boolean> pathMap = new LRUMap<>(2);
 
     Set<String> newPaths = FileMetadataUtils.findNewDirectoryPaths(testEntries, pathMap);
+    // "newPaths" returns all of the new entries, but the pathMap only contains last 2 entries
     assertThat("Should be 6 new entries", newPaths.size(), equalTo(6));
     assertThat(
         "Should contain 6 unique directory paths",
@@ -88,9 +89,12 @@ public class FileMetadataUtilsTest {
         pathMap,
         hasKey("/_dr_/test"));
 
-    // Make same call - with one added path. All but the common path was kicked out the cache, so
-    // we'll see
-    // 6 unique entries
+    // Make same call - with one added path. All but the common path was kicked out the cache
+    // b/c the cache size is so small, so we'll see 6 unique entries.
+    // "/_dr_/test" still in cache
+    // "/_dr_/test/path-4" was kicked out of cache, so when we check that entry, we don't know that
+    // that it's an existing entry and thus returned as a new entries to cache
+    // "/_dr_/test/diffPath" added to cache b/c last file encountered in list
     testEntries.add(new FireStoreDirectoryEntry().path("/test/diffPath").name("file-0.json"));
     assertThat("Should be 6 new entries", newPaths.size(), equalTo(6));
     Set<String> updatedPaths = FileMetadataUtils.findNewDirectoryPaths(testEntries, pathMap);
@@ -115,21 +119,37 @@ public class FileMetadataUtilsTest {
   }
 
   @Test
-  public void testShuffledNewPaths() {
+  public void testUniqueNewPaths() {
     List<FireStoreDirectoryEntry> testEntries = initTestEntries(5);
-    Collections.shuffle(testEntries);
-    // LRU has enough space for all entries
+
+    // Checking for unique new paths requires two constraints on the findNewDirectoryPaths method
+    // Would this happen in real life?
+    // It's a reasonable scenario that all files with the same
+    // base path may not be loaded right next to each other in a file ingest
+    // 1 - Cache size smaller than the number of unique paths
     LRUMap<String, Boolean> pathMap = new LRUMap<>(2);
+    // 2 - Shuffling entries so that files with same base path are not sequential
+    // When they're sequential, the first instance will add them to the cache
+    // And then automatically won't get added as a new path
+    Collections.shuffle(testEntries);
 
     Set<String> newPaths = FileMetadataUtils.findNewDirectoryPaths(testEntries, pathMap);
-    // We can't test for specific items in cache since they're shuffled, but we can check for
-    // the right number of new, unique paths
     assertThat("Should be 6 new entries", newPaths.size(), equalTo(6));
+    assertThat(
+        "Should contain 6 unique directory paths",
+        newPaths,
+        containsInAnyOrder(
+            "/_dr_/test/path-0",
+            "/_dr_/test",
+            "/_dr_/test/path-1",
+            "/_dr_/test/path-2",
+            "/_dr_/test/path-3",
+            "/_dr_/test/path-4"));
   }
 
   private List<FireStoreDirectoryEntry> initTestEntries(int numDirectories) {
     List<FireStoreDirectoryEntry> testEntries = new ArrayList<>();
-    // Add two files per different directory path
+    // Add 4 files per different directory path
     for (int i = 0; i < numDirectories; i++) {
       String path = String.format("/test/path-%s", i);
       testEntries.add(new FireStoreDirectoryEntry().path(path).name("file-0.json"));

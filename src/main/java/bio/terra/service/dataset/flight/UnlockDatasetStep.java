72b20d568257c0bb4230e7b69ea40eb090d861fd
Shelby Holden <sholden@broadinstitute.org>
2021-11-03 16:33:53 -0400

    consolidate datasetId null check

--- src/main/java/bio/terra/service/dataset/flight/UnlockDatasetStep.java
+++ src/main/java/bio/terra/service/dataset/flight/UnlockDatasetStep.java
@@ -2,12 +2,12 @@ package bio.terra.service.dataset.flight;
 
 import bio.terra.common.exception.RetryQueryException;
 import bio.terra.service.dataset.DatasetDao;
-import bio.terra.service.dataset.exception.DatasetLockException;
 import bio.terra.service.dataset.flight.ingest.OptionalStep;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.FlightMap;
 import bio.terra.stairway.StepResult;
 import bio.terra.stairway.StepStatus;
+import java.util.Objects;
 import java.util.UUID;
 import java.util.function.Predicate;
 import org.slf4j.Logger;
@@ -49,21 +49,14 @@ public class UnlockDatasetStep extends OptionalStep {
 
   @Override
   public StepResult doOptionalStep(FlightContext context) {
-    if (datasetId == null) {
-      FlightMap map = context.getWorkingMap();
-
+    boolean rowUpdated;
+    FlightMap map = context.getWorkingMap();
+    try {
       // In the create case, we won't have the dataset id at step creation. We'll expect it to be in
       // the working map.
-      datasetId = map.get(DatasetWorkingMapKeys.DATASET_ID, UUID.class);
-      if (datasetId == null) {
-        return new StepResult(
-            StepStatus.STEP_RESULT_FAILURE_FATAL,
-            new DatasetLockException("Expected dataset id in working map."));
-      }
-    }
-
-    boolean rowUpdated;
-    try {
+      datasetId =
+          Objects.requireNonNullElse(
+              datasetId, map.get(DatasetWorkingMapKeys.DATASET_ID, UUID.class));
       if (sharedLock) {
         rowUpdated = datasetDao.unlockShared(datasetId, context.getFlightId());
       } else {

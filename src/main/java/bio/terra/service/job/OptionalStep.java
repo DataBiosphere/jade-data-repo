333eb73caf49bfad9211da21672e64b3052029dd
Phil Shapiro <pshapiro@broadinstitute.org>
2021-11-10 15:01:38 -0500

    - add logging to OptionalStep to log the wrapped class when running `do` or `undo` operations
    - fix logging for the skip case to log the wrapped class
    
    - remove logging from `IngestUtils.isCombinedFileIngest()` since it will be logged whenever it's called from `OptionalStep`
    
    - delete empty file left behind by accident

--- src/main/java/bio/terra/service/job/OptionalStep.java
+++ src/main/java/bio/terra/service/job/OptionalStep.java
@@ -23,18 +23,20 @@ public abstract class OptionalStep implements Step {
   public StepResult doStep(FlightContext flightContext)
       throws InterruptedException, RetryException {
     if (isEnabled(flightContext)) {
+      logger.info("Running {} step based on flight context state", step.getClass().getName());
       return step.doStep(flightContext);
     }
-    logger.info("Skipping {} step based on flight context state", getClass().getName());
+    logger.info("Skipping {} step based on flight context state", step.getClass().getName());
     return StepResult.getStepResultSuccess();
   }
 
   @Override
   public StepResult undoStep(FlightContext flightContext) throws InterruptedException {
     if (isEnabled(flightContext)) {
+      logger.info("Running {} undo step based on flight context state", step.getClass().getName());
       return step.undoStep(flightContext);
     }
-    logger.info("Skipping {} undo step based on flight context state", getClass().getName());
+    logger.info("Skipping {} undo step based on flight context state", step.getClass().getName());
     return StepResult.getStepResultSuccess();
   }
 }

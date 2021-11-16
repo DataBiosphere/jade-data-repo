cc427769f628d4e235aa9ed6d5757bcccf2aac4f
Shelby Holden <sholden@broadinstitute.org>
2021-11-16 16:33:10 -0500

    outline steps

---
+++ src/main/java/bio/terra/service/upgrade/flight/BackfillRowMetadataTablesStep.java
@@ -0,0 +1,35 @@
+package bio.terra.service.upgrade.flight;
+
+import bio.terra.model.UpgradeModel;
+import bio.terra.stairway.FlightContext;
+import bio.terra.stairway.Step;
+import bio.terra.stairway.StepResult;
+import bio.terra.stairway.exception.RetryException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BackfillRowMetadataTablesStep implements Step {
+  private static final Logger logger = LoggerFactory.getLogger(BackfillRowMetadataTablesStep.class);
+  private UpgradeModel request;
+  BackfillRowMetadataTablesStep(UpgradeModel request) {
+    this.request = request;
+  }
+
+  @Override
+  public StepResult doStep(FlightContext context) throws InterruptedException, RetryException {
+    logger.info("HELLO FROM BackfillRowMetadataTablesStep");
+    // enumerate datasets
+    // for each dataset:
+      // connect to big query for dataset's data project
+      // for each table:
+            // retrieve the row metadata table name from the database
+            // check if row metadata table already exists in big query
+            // if doesn't exist, create row metadata table
+    return StepResult.getStepResultSuccess();
+  }
+
+  @Override
+  public StepResult undoStep(FlightContext context) throws InterruptedException {
+    return StepResult.getStepResultSuccess();
+  }
+}
---
+++ src/main/java/bio/terra/service/upgrade/flight/RowMetadataUpdateFlight.java
@@ -0,0 +1,22 @@
+package bio.terra.service.upgrade.flight;
+
+import bio.terra.model.UpgradeModel;
+import bio.terra.service.iam.AuthenticatedUserRequest;
+import bio.terra.service.job.JobMapKeys;
+import bio.terra.stairway.Flight;
+import bio.terra.stairway.FlightMap;
+import org.springframework.context.ApplicationContext;
+
+public class RowMetadataUpdateFlight extends Flight {
+  public RowMetadataUpdateFlight(FlightMap inputParameters, Object applicationContext) {
+    super(inputParameters, applicationContext);
+
+    ApplicationContext appContext = (ApplicationContext) applicationContext;
+    //ProfileService profileService = (ProfileService) appContext.getBean("profileService");
+
+    UpgradeModel request =
+        inputParameters.get(JobMapKeys.REQUEST.getKeyName(), UpgradeModel.class);
+
+    addStep(new BackfillRowMetadataTablesStep(request));
+  }
+}

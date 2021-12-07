72a8674193b3d179f06feb837957817e39dfd59a
nmalfroy <nmalfroy@broadinstitute.org>
2021-12-06 23:16:25 -0500

    Make sure that unit and connected tests also use the LocalAuthenticatedUserRequestFactory

--- src/main/java/bio/terra/service/iam/LocalAuthenticatedUserRequestFactory.java
+++ src/main/java/bio/terra/service/iam/LocalAuthenticatedUserRequestFactory.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Primary
 @Component
-@Profile({"local"})
+@Profile({"local", "connected-test", "unit-test"})
 public class LocalAuthenticatedUserRequestFactory implements AuthenticatedUserRequestFactory {
   private Logger logger = LoggerFactory.getLogger(LocalAuthenticatedUserRequestFactory.class);
 

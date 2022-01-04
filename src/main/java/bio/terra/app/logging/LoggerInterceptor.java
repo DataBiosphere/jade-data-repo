34b18973212b7fe8f281b85fa9244136f948c619
Nick Malfroy-Camine <nmalfroy@broadinstitute.org>
2022-01-04 09:21:02 -0500

    DR-2330 DR-2333 Fix logging unauthed requests and add timing info (#1178)
    * Make sure that loginterceptor doesn't fail on unauthenticated requests
    
    * Track how long requests take
    
    * Update src/main/java/bio/terra/app/logging/LoggerInterceptor.java
    
    Co-authored-by: Phil Shapiro <pshapiro@broadinstitute.org>
    
    Co-authored-by: Phil Shapiro <pshapiro@broadinstitute.org>

--- src/main/java/bio/terra/app/logging/LoggerInterceptor.java
+++ src/main/java/bio/terra/app/logging/LoggerInterceptor.java
@@ -1,9 +1,11 @@
 package bio.terra.app.logging;
 
+import bio.terra.common.exception.UnauthorizedException;
 import bio.terra.common.iam.AuthenticatedUserRequest;
 import bio.terra.common.iam.AuthenticatedUserRequestFactory;
 import com.google.gson.Gson;
 import java.util.Map;
+import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
@@ -14,7 +16,17 @@ import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 public class LoggerInterceptor implements HandlerInterceptor {
-  private static Logger logger = LoggerFactory.getLogger(LoggerInterceptor.class);
+  private static final Logger logger = LoggerFactory.getLogger(LoggerInterceptor.class);
+  // Constants for requests coming in that aren't authenticated
+  private static final String UNAUTHED_USER_ID = "N/A";
+  private static final String UNAUTHED_EMAIL = "N/A";
+  private static final String UNAUTHED_INSTITUTE = "N/A";
+  // Don't log requests for URLs that end with any of   the following paths
+  private static final Set<String> LOG_EXCLUDE_LIST = Set.of("/status");
+
+  private static final String REQUEST_START_ATTRIBUTE = "x-request-start";
+  private static final long NOT_FOUND_DURATION = -1;
+
   private final AuthenticatedUserRequestFactory authenticatedUserRequestFactory;
 
   @Autowired
@@ -23,31 +35,56 @@ public class LoggerInterceptor implements HandlerInterceptor {
   }
 
   @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+      throws Exception {
+    request.setAttribute(REQUEST_START_ATTRIBUTE, System.currentTimeMillis());
+    return true;
+  }
+
+  @Override
   public void afterCompletion(
       HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
       throws Exception {
-    AuthenticatedUserRequest userReq = authenticatedUserRequestFactory.from(request);
-    String userId = userReq.getSubjectId();
-    String userEmail = userReq.getEmail();
-    String institute = userEmail != null ? userEmail.substring(userEmail.indexOf("@") + 1) : null;
+
+    String userId;
+    String userEmail;
+    String institute;
+    try {
+      AuthenticatedUserRequest userReq = authenticatedUserRequestFactory.from(request);
+      userId = userReq.getSubjectId();
+      userEmail = userReq.getEmail();
+      institute = userEmail != null ? userEmail.substring(userEmail.indexOf("@") + 1) : null;
+    } catch (UnauthorizedException e) {
+      userId = UNAUTHED_USER_ID;
+      userEmail = UNAUTHED_EMAIL;
+      institute = UNAUTHED_INSTITUTE;
+    }
+
     String url = request.getRequestURL().toString();
     String method = request.getMethod();
     Map<String, String[]> paramMap = request.getParameterMap();
     Gson gson = new Gson();
     String paramString = gson.toJson(paramMap);
     String responseStatus = Integer.toString(response.getStatus());
-
+    long requestDuration;
+    Long requestStartTime = (Long) request.getAttribute(REQUEST_START_ATTRIBUTE);
+    if (requestStartTime != null) {
+      requestDuration = System.currentTimeMillis() - requestStartTime;
+    } else {
+      requestDuration = NOT_FOUND_DURATION;
+    }
     // skip logging the status endpoint
-    if (!url.endsWith("/status")) {
+    if (LOG_EXCLUDE_LIST.stream().noneMatch(url::endsWith)) {
       logger.info(
-          "userId: {}, email: {}, institute: {}, url: {}, method: {}, params: {}, status: {}",
+          "userId: {}, email: {}, institute: {}, url: {}, method: {}, params: {}, status: {}, duration: {}",
           userId,
           userEmail,
           institute,
           url,
           method,
           paramString,
-          responseStatus);
+          responseStatus,
+          requestDuration);
     }
 
     if (ex != null) {

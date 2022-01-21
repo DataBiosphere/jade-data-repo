8889b326dfc686e2d0d4246d223058a8ba4c534b
tlangs <tlangs@broadinstitute.org>
2022-01-21 12:32:44 -0500

    retain original error messages, fill in the blanks

--- src/main/java/bio/terra/service/job/StairwayExceptionFieldsFactory.java
+++ src/main/java/bio/terra/service/job/StairwayExceptionFieldsFactory.java
@@ -17,29 +17,34 @@ public class StairwayExceptionFieldsFactory {
   public static StairwayExceptionFields fieldsFromException(Exception ex) {
     Optional<String> stepName = getStepNameFromStairwayException(ex);
 
+    Optional<String> fieldExceptionClassName = Optional.empty();
+    if (ErrorReportException.class.isAssignableFrom(ex.getClass())) {
+      fieldExceptionClassName = Optional.of(ex.getClass().getName());
+    }
+
     if (stepName.isEmpty()) {
       return new StairwayExceptionFields()
-          .setClassName(JobExecutionException.class.getName())
+          .setClassName(fieldExceptionClassName.orElse(JobExecutionException.class.getName()))
           .setDataRepoException(true)
           .setMessage(getJobExceptionMessage(ex))
           .setErrorDetails(getJobExceptionDetails(ex));
     }
 
     return new StairwayExceptionFields()
-        .setClassName(StepExecutionException.class.getName())
+        .setClassName(fieldExceptionClassName.orElse(StepExecutionException.class.getName()))
         .setDataRepoException(true)
         .setMessage(getStepExceptionMessage(stepName.get(), ex))
-        .setErrorDetails(getStepExceptionDetails(stepName.get(), ex));
+        .setErrorDetails(getStepExceptionDetails(ex));
   }
 
   private static Optional<String> getStepNameFromStairwayException(Exception ex) {
-    Class<? extends Step> failedStep = null;
+    String failedStepName = null;
     for (StackTraceElement ste : ex.getStackTrace()) {
       try {
         Class<?> cls = Class.forName(ste.getClassName());
         if (Step.class.isAssignableFrom(cls)) {
           // Java 17 will make this casting unnecessary
-          failedStep = (Class<? extends Step>) cls;
+          failedStepName = cls.getSimpleName();
           break;
         }
       } catch (ClassNotFoundException e) {
@@ -47,50 +52,52 @@ public class StairwayExceptionFieldsFactory {
             "All steps in a stacktrace should be able to be found", e);
       }
     }
-    return Optional.ofNullable(failedStep.getSimpleName());
+    return Optional.ofNullable(failedStepName);
   }
 
   private static String getJobExceptionMessage(Exception exception) {
+    if (StringUtils.isNotBlank(exception.getMessage())) {
+      return exception.getMessage();
+    }
     return String.format(
         "Encountered a %s while running the job", exception.getClass().getSimpleName());
   }
 
   private static List<String> getJobExceptionDetails(Exception exception) {
-    List<String> details = new ArrayList<>();
-    details.add("The job failed, but not while running a step");
-    if (StringUtils.isNotBlank(exception.getMessage())) {
-      details.add("The job reported the following message: " + exception.getMessage());
-    }
     if (ErrorReportException.class.isAssignableFrom(exception.getClass())) {
       ErrorReportException errorReportException = (ErrorReportException) exception;
-      details.addAll(errorReportException.getCauses());
+      return errorReportException.getCauses();
+    } else {
+      List<String> details = new ArrayList<>();
+      details.add("The job failed, but not while running a step");
+      details.add(CONTACT_TEAM_MESSAGE);
+      return details;
     }
-    return details;
   }
 
   private static String getStepExceptionMessage(String stepName, Exception exception) {
+    if (StringUtils.isNotBlank(exception.getMessage())) {
+      return exception.getMessage();
+    }
     return String.format(
-        "Encountered %s while running %s. Check error details for more information and possible fixes",
-        exception.getClass().getSimpleName(), stepName);
+        "Encountered %s while running %s.", exception.getClass().getSimpleName(), stepName);
   }
 
-  private static List<String> getStepExceptionDetails(String stepName, Exception exception) {
-    List<String> details = new ArrayList<>();
-    if (StringUtils.isNotBlank(exception.getMessage())) {
-      details.add("The step reported the following message: " + exception.getMessage());
-    }
-    if ("IamUnauthorizedException".equals(exception.getClass().getSimpleName())) {
-      details.add(
-          "If this is a long-running job, the user token may have timed out. "
-              + "Consider breaking the job into smaller jobs if possible.");
-      details.add(
-          "There may be a problem with bucket access. "
-              + "Has the user (or PET) account been granted access to all necessary files?");
-    }
+  private static List<String> getStepExceptionDetails(Exception exception) {
     if (ErrorReportException.class.isAssignableFrom(exception.getClass())) {
       ErrorReportException errorReportException = (ErrorReportException) exception;
-      details.addAll(errorReportException.getCauses());
+      return errorReportException.getCauses();
+    } else {
+      List<String> details = new ArrayList<>();
+      if ("IamUnauthorizedException".equals(exception.getClass().getSimpleName())) {
+        details.add(
+            "If this is a long-running job, the user token may have timed out. "
+                + "Consider breaking the job into smaller jobs if possible.");
+        details.add(
+            "There may be a problem with bucket access. "
+                + "Has the user (or PET) account been granted access to all necessary files?");
+      }
+      return details;
     }
-    return details;
   }
 }

f11305d32cc8cf2a134525914432c1a1dbc50078
TC <tjiang@broadinstitute.org>
2022-02-08 11:37:08 -0500

    DR-2353 add run reason and skip reason (#1203)
    * add run reason and skip reason
    
    * pr comment and spotlessApply

--- src/main/java/bio/terra/service/dataset/flight/ingest/CombinedFileIngestOptionalStep.java
+++ src/main/java/bio/terra/service/dataset/flight/ingest/CombinedFileIngestOptionalStep.java
@@ -3,6 +3,7 @@ package bio.terra.service.dataset.flight.ingest;
 import bio.terra.service.job.OptionalStep;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.Step;
+import java.util.Objects;
 
 public class CombinedFileIngestOptionalStep extends OptionalStep {
   public CombinedFileIngestOptionalStep(Step step) {
@@ -15,7 +16,15 @@ public class CombinedFileIngestOptionalStep extends OptionalStep {
   }
 
   @Override
-  public String getReason() {
+  public String getSkipReason() {
     return "there are no files to ingest";
+  }
+
+  @Override
+  public String getRunReason(FlightContext context) {
+    long numFiles =
+        Objects.requireNonNullElse(
+            context.getWorkingMap().get(IngestMapKeys.NUM_BULK_LOAD_FILE_MODELS, Long.class), 0L);
+    return String.format("%s bulk load file(s) found to ingest", numFiles);
   }
 }

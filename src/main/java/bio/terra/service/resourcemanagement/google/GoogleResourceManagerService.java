97c3d5d2f4ac36ce0431124bcfb10aa59563a50f
Shelby Holden <sholden@broadinstitute.org>
2022-02-16 08:52:08 -0500

    code review: switch to var

--- src/main/java/bio/terra/service/resourcemanagement/google/GoogleResourceManagerService.java
+++ src/main/java/bio/terra/service/resourcemanagement/google/GoogleResourceManagerService.java
@@ -88,9 +88,8 @@ public class GoogleResourceManagerService {
 
   public void deleteProject(String googleProjectId) {
     try {
-      CloudResourceManager resourceManager = cloudResourceManager();
-      CloudResourceManager.Projects.Delete request =
-          resourceManager.projects().delete(googleProjectId);
+      var resourceManager = cloudResourceManager();
+      var request = resourceManager.projects().delete(googleProjectId);
       // the response will be empty if the request is successful in the delete
       request.execute();
     } catch (IOException | GeneralSecurityException e) {

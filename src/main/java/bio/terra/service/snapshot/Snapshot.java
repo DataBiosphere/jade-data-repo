3466ed40d3a969c7e1678f53b35972a0531996d6
TC Jiang <tjiang@broadinstitute.org>
2022-02-15 19:15:28 -0500

    address pr comments

--- src/main/java/bio/terra/service/snapshot/Snapshot.java
+++ src/main/java/bio/terra/service/snapshot/Snapshot.java
@@ -6,6 +6,7 @@ import bio.terra.common.Column;
 import bio.terra.common.LogPrintable;
 import bio.terra.common.Relationship;
 import bio.terra.model.SnapshotRequestContentsModel;
+import bio.terra.service.dataset.Dataset;
 import bio.terra.service.filedata.FSContainerInterface;
 import bio.terra.service.filedata.google.firestore.FireStoreProject;
 import bio.terra.service.resourcemanagement.azure.AzureStorageAccountResource;
@@ -104,6 +105,13 @@ public class Snapshot implements FSContainerInterface, LogPrintable {
     return snapshotSources.get(0);
   }
 
+  public Dataset getSourceDataset() {
+    if (snapshotSources.isEmpty()) {
+      throw new CorruptMetadataException("Snapshot sources should never be empty!");
+    }
+    return snapshotSources.get(0).getDataset();
+  }
+
   public Optional<SnapshotTable> getTableById(UUID id) {
     for (SnapshotTable tryTable : getTables()) {
       if (tryTable.getId().equals(id)) {
@@ -171,17 +179,16 @@ public class Snapshot implements FSContainerInterface, LogPrintable {
 
   @Override
   public FireStoreProject firestoreConnection() {
-    String datasetProjectId =
-        getFirstSnapshotSource().getDataset().getProjectResource().getGoogleProjectId();
+    String datasetProjectId = getSourceDataset().getProjectResource().getGoogleProjectId();
     return FireStoreProject.get(datasetProjectId);
   }
 
   public AzureRegion getStorageAccountRegion() {
-    return getFirstSnapshotSource().getDataset().getStorageAccountRegion();
+    return getSourceDataset().getStorageAccountRegion();
   }
 
   public boolean isSecureMonitoringEnabled() {
-    return getFirstSnapshotSource().getDataset().isSecureMonitoringEnabled();
+    return getSourceDataset().isSecureMonitoringEnabled();
   }
 
   public SnapshotRequestContentsModel getCreationInformation() {
--- src/main/java/bio/terra/service/snapshot/flight/export/SnapshotExportDumpFirestoreStep.java
+++ src/main/java/bio/terra/service/snapshot/flight/export/SnapshotExportDumpFirestoreStep.java
@@ -1,5 +1,7 @@
 package bio.terra.service.snapshot.flight.export;
 
+import bio.terra.service.dataset.Dataset;
+import bio.terra.service.filedata.google.firestore.FireStoreDao;
 import bio.terra.service.filedata.google.gcs.GcsChannelWriter;
 import bio.terra.service.resourcemanagement.google.GoogleBucketResource;
 import bio.terra.service.snapshot.Snapshot;
@@ -12,28 +14,28 @@ import bio.terra.stairway.StepStatus;
 import bio.terra.stairway.exception.RetryException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.CollectionReference;
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.FirestoreOptions;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class SnapshotExportDumpFirestoreStep implements Step {
   private final SnapshotService snapshotService;
+  private final FireStoreDao fireStoreDao;
   private final UUID snapshotId;
   private final ObjectMapper objectMapper;
 
   public SnapshotExportDumpFirestoreStep(
-      SnapshotService snapshotService, UUID snapshotId, ObjectMapper objectMapper) {
+      SnapshotService snapshotService,
+      FireStoreDao fireStoreDao,
+      UUID snapshotId,
+      ObjectMapper objectMapper) {
     this.snapshotService = snapshotService;
+    this.fireStoreDao = fireStoreDao;
     this.snapshotId = snapshotId;
     this.objectMapper = objectMapper;
   }
@@ -41,54 +43,46 @@ public class SnapshotExportDumpFirestoreStep implements Step {
   @Override
   public StepResult doStep(FlightContext context) throws InterruptedException, RetryException {
     Snapshot snapshot = snapshotService.retrieve(snapshotId);
-    String firestoreProject =
-        snapshot.getSnapshotSources().get(0).getDataset().getProjectResource().getGoogleProjectId();
+    String fileName = getFileName(context);
+    context.getWorkingMap().put(SnapshotWorkingMapKeys.SNAPSHOT_EXPORT_GSPATHS_FILENAME, fileName);
 
-    try {
-      FirestoreOptions firestoreOptions =
-          FirestoreOptions.getDefaultInstance().toBuilder()
-              .setProjectId(firestoreProject)
-              .setCredentials(GoogleCredentials.getApplicationDefault())
-              .build();
-      String sourceDatasetId = snapshot.getSnapshotSources().get(0).getDataset().getId().toString();
-      String collectionName = String.format("%s-files", sourceDatasetId);
-      final Firestore db = firestoreOptions.getService();
-      final CollectionReference collection = db.collection(collectionName);
-      QuerySnapshot queryResult = collection.get().get();
-      GcsChannelWriter writer = makeWriterForDumpFile(context);
-
+    try (GcsChannelWriter writer = makeWriterForDumpFile(context, fileName)) {
+      Dataset sourceDataset = snapshot.getSourceDataset();
+      String collectionName = String.format("%s-files", sourceDataset.getId());
+      QuerySnapshot queryResult =
+          fireStoreDao.retrieveCollectionByName(
+              sourceDataset.getProjectResource().getGoogleProjectId(), collectionName);
+      // note, creates an empty file for empty query result
       for (QueryDocumentSnapshot d : queryResult.getDocuments()) {
-        Map<String, Object> dumpData = new HashMap<>();
-        dumpData.put("fileId", d.getData().get("fileId"));
-        dumpData.put("gspath", d.getData().get("gspath"));
+        // convert to snake_case here to match column names in BQ
+        Map<String, Object> dumpData =
+            Map.of(
+                "file_id", d.getData().get("fileId"),
+                "gs_path", d.getData().get("gspath"));
         try {
           writer.write(objectMapper.writeValueAsString(dumpData));
           writer.write("\n");
         } catch (JsonProcessingException e) {
           return new StepResult(StepStatus.STEP_RESULT_FAILURE_FATAL, e);
-        } catch (IOException e) {
-          return new StepResult(StepStatus.STEP_RESULT_FAILURE_FATAL, e);
         }
       }
-      writer.close();
     } catch (IOException | ExecutionException e) {
       return new StepResult(StepStatus.STEP_RESULT_FAILURE_RETRY, e);
     }
     return StepResult.getStepResultSuccess();
   }
 
-  private GcsChannelWriter makeWriterForDumpFile(FlightContext context) {
+  private GcsChannelWriter makeWriterForDumpFile(FlightContext context, String fileName) {
     GoogleBucketResource exportBucket =
         context
             .getWorkingMap()
             .get(SnapshotWorkingMapKeys.SNAPSHOT_EXPORT_BUCKET, GoogleBucketResource.class);
-    String targetPath = getFileName(context);
     Storage storage =
         StorageOptions.newBuilder()
             .setProjectId(exportBucket.getProjectResource().getGoogleProjectId())
             .build()
             .getService();
-    return new GcsChannelWriter(storage, exportBucket.getName(), targetPath);
+    return new GcsChannelWriter(storage, exportBucket.getName(), fileName);
   }
 
   private String getFileName(FlightContext context) {

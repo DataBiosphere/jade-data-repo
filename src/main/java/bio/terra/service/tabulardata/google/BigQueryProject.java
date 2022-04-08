2d425c6834d6933f3e7d387d88829c6ae0885386
Shelby Holden <sholden@broadinstitute.org>
2022-04-08 16:54:28 -0400

    DR-2526 - Address BQ Read Timeout w/ Extended timeout + Additional Retry Case (#1255)
    * Set same transport options on BigQuery instance
    
    * Add another retry case
    
    * spotless
    
    * Just wire up gcsConfiguration
    
    * Revert "Just wire up gcsConfiguration"
    
    This reverts commit 0acbcae3bed2d073aa727fc4638b6e97b764a1bf.
    
    * move timeout to constant
    
    * [doesn't work] - asking about approach
    
    * Revert "[doesn't work] - asking about approach"
    
    This reverts commit d9484a8072f4351ea83e826f94b7952be03b86bd.

--- src/main/java/bio/terra/service/tabulardata/google/BigQueryProject.java
+++ src/main/java/bio/terra/service/tabulardata/google/BigQueryProject.java
@@ -22,6 +22,9 @@ import com.google.cloud.bigquery.TableDefinition;
 import com.google.cloud.bigquery.TableId;
 import com.google.cloud.bigquery.TableInfo;
 import com.google.cloud.bigquery.TableResult;
+import com.google.cloud.http.HttpTransportOptions;
+import com.google.cloud.storage.StorageOptions;
+import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -37,11 +40,23 @@ public final class BigQueryProject {
       new ConcurrentHashMap<>();
   private final String projectId;
   private final BigQuery bigQuery;
+  private final int TIMEOUT_SECONDS = 40;
 
   private BigQueryProject(String projectId) {
     logger.info("Retrieving Bigquery project for project id: {}", projectId);
     this.projectId = projectId;
-    bigQuery = BigQueryOptions.newBuilder().setProjectId(projectId).build().getService();
+    HttpTransportOptions transportOptions = StorageOptions.getDefaultHttpTransportOptions();
+    transportOptions =
+        transportOptions.toBuilder()
+            .setConnectTimeout(TIMEOUT_SECONDS * 1000)
+            .setReadTimeout(TIMEOUT_SECONDS * 1000)
+            .build();
+    bigQuery =
+        BigQueryOptions.newBuilder()
+            .setTransportOptions(transportOptions)
+            .setProjectId(projectId)
+            .build()
+            .getService();
   }
 
   public static BigQueryProject get(String projectId) {
@@ -157,11 +172,14 @@ public final class BigQueryProject {
         });
   }
 
-  private void bigQueryAclUpdateShouldRetry(RuntimeException ex) {
+  private void bigQueryAclUpdateShouldRetry(BigQueryException ex) {
     String message = ex.getMessage();
     if (message.startsWith("IAM setPolicy") && message.endsWith("does not exist.")) {
       throw new AclUtils.AclRetryException(
           "Policy does not exist. Retrying to wait for propagation", ex, "propagation");
+    }
+    if (message.startsWith("Read timed out") || ex.getCause() instanceof SocketTimeoutException) {
+      throw new AclUtils.AclRetryException("Timeout.", ex, "Timeout");
     }
     throw ex;
   }

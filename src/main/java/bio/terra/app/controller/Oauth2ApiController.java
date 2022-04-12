752efb383605cfe5564d3600c3b672e7ca9614b7
nmalfroy <nmalfroy@broadinstitute.org>
2022-04-12 10:56:14 -0400

    Add config and initialization for new OIDC config

---
+++ src/main/java/bio/terra/app/controller/Oauth2ApiController.java
@@ -0,0 +1,123 @@
+package bio.terra.app.controller;
+
+import bio.terra.app.configuration.OpenIDConnectConfiguration;
+import bio.terra.common.exception.BadRequestException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.api.client.util.Charsets;
+import com.google.common.annotations.VisibleForTesting;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.servlet.http.HttpServletRequest;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.message.BasicNameValuePair;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class Oauth2ApiController {
+  static final String AUTHORIZE_ENDPOINT = "/oauth2/authorize";
+  static final String TOKEN_REFRESH_ENDPOINT = "/oauth2/token";
+  private static final String SCOPE_PARAM = "scope";
+  private static final String CLIENT_SECRET_PARAM = "client_secret";
+  private final RestTemplate restTemplate;
+  private final OpenIDConnectConfiguration openIDConnectConfiguration;
+
+  @Autowired
+  public Oauth2ApiController(OpenIDConnectConfiguration openIDConnectConfiguration) {
+    this.openIDConnectConfiguration = openIDConnectConfiguration;
+    restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory());
+  }
+
+  @RequestMapping(value = AUTHORIZE_ENDPOINT)
+  public String oauthAuthorize(HttpServletRequest request) {
+    return "redirect:"
+        + openIDConnectConfiguration.getAuthorizationEndpoint()
+        + "?"
+        + decorateAuthRedirectQueryParameters(request.getQueryString());
+  }
+
+  // Modify the query parameters that were passed to be compatible with configured Oauth flow
+  private String decorateAuthRedirectQueryParameters(String initialParams) {
+    List<NameValuePair> parameters = URLEncodedUtils.parse(initialParams, Charsets.UTF_8);
+
+    parameters.addAll(
+        URLEncodedUtils.parse(openIDConnectConfiguration.getExtraAuthParams(), Charsets.UTF_8));
+
+    parameters =
+        parameters.stream()
+            .map(
+                p -> {
+                  if (openIDConnectConfiguration.isAddClientIdToScope()
+                      && p.getName().equalsIgnoreCase(SCOPE_PARAM)) {
+                    return new BasicNameValuePair(
+                        p.getName(), p.getValue() + " " + openIDConnectConfiguration.getClientId());
+                  } else {
+                    return p;
+                  }
+                })
+            .collect(Collectors.toList());
+
+    return URLEncodedUtils.format(parameters, Charsets.UTF_8);
+  }
+
+  @RequestMapping(value = TOKEN_REFRESH_ENDPOINT, method = RequestMethod.POST)
+  public ResponseEntity<JsonNode> oauthTokenPost(HttpServletRequest request) {
+    String queryString =
+        StringUtils.isEmpty(request.getQueryString()) ? "" : "?" + request.getQueryString();
+    String actualEndpoint = openIDConnectConfiguration.getTokenEndpoint() + queryString;
+
+    String requestBody;
+    try (BufferedReader reader = request.getReader()) {
+      requestBody = addClientSecret(IOUtils.toString(reader));
+    } catch (IOException e) {
+      throw new BadRequestException("Could not process refresh request", e);
+    }
+
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+    return getRestTemplate()
+        .exchange(
+            actualEndpoint,
+            HttpMethod.POST,
+            new HttpEntity<>(requestBody, headers),
+            JsonNode.class);
+  }
+
+  // Modify the form url encoded body and add the client secret if it was not specified
+  private String addClientSecret(String requestBody) {
+    List<NameValuePair> parameters = URLEncodedUtils.parse(requestBody, Charsets.UTF_8);
+
+    if (!StringUtils.isEmpty(openIDConnectConfiguration.getClientSecret())
+        && parameters.stream()
+            .noneMatch(
+                p ->
+                    p.getName().equalsIgnoreCase(CLIENT_SECRET_PARAM)
+                        && !StringUtils.isEmpty(p.getValue()))) {
+      parameters.add(
+          new BasicNameValuePair(
+              CLIENT_SECRET_PARAM, openIDConnectConfiguration.getClientSecret()));
+    }
+
+    return URLEncodedUtils.format(parameters, Charsets.UTF_8);
+  }
+
+  @VisibleForTesting
+  RestTemplate getRestTemplate() {
+    return restTemplate;
+  }
+}
---
+++ src/test/java/bio/terra/app/controller/Oauth2ApiControllerTest.java
@@ -0,0 +1,134 @@
+package bio.terra.app.controller;
+
+import static bio.terra.app.controller.Oauth2ApiController.AUTHORIZE_ENDPOINT;
+import static bio.terra.app.controller.Oauth2ApiController.TOKEN_REFRESH_ENDPOINT;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import bio.terra.app.configuration.OpenIDConnectConfiguration;
+import bio.terra.common.category.Unit;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties = {"datarepo.testWithEmbeddedDatabase=false"})
+@AutoConfigureMockMvc
+@ActiveProfiles({"google", "unittest"})
+@Category(Unit.class)
+public class Oauth2ApiControllerTest {
+
+  @SpyBean private OpenIDConnectConfiguration openIDConnectConfiguration;
+  @SpyBean private Oauth2ApiController controller;
+  @Autowired private ObjectMapper objectMapper;
+  @Autowired private MockMvc mvc;
+
+  @Captor private ArgumentCaptor<HttpEntity<String>> postCaptor;
+
+  @Before
+  public void setUp() throws Exception {
+    openIDConnectConfiguration.setAddClientIdToScope(false);
+    openIDConnectConfiguration.setExtraAuthParams(null);
+  }
+
+  @Test
+  public void testForwardingLogic() throws Exception {
+    openIDConnectConfiguration.setAddClientIdToScope(false);
+    mvc.perform(get(AUTHORIZE_ENDPOINT + "?id=client_idwith\"fun'characters&scope=foo bar"))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(
+            redirectedUrl(
+                openIDConnectConfiguration.getAuthorizationEndpoint()
+                    + "?id=client_idwith%22fun%27characters&scope=foo+bar"));
+  }
+
+  @Test
+  public void testForwardingLogicWithClientIdInjected() throws Exception {
+    openIDConnectConfiguration.setClientId("my_id");
+    openIDConnectConfiguration.setAddClientIdToScope(true);
+    mvc.perform(get(AUTHORIZE_ENDPOINT + "?id=client_id&scope=foo bar"))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(
+            redirectedUrl(
+                openIDConnectConfiguration.getAuthorizationEndpoint()
+                    + "?id=client_id&scope=foo+bar+my_id"));
+  }
+
+  @Test
+  public void testForwardingLogicWithClientIdInjectedAndNewParameters() throws Exception {
+    openIDConnectConfiguration.setClientId("my_id");
+    openIDConnectConfiguration.setAddClientIdToScope(true);
+    openIDConnectConfiguration.setExtraAuthParams("foo=1&bar='2'");
+    mvc.perform(get(AUTHORIZE_ENDPOINT + "?id=client_id&scope=foo bar"))
+        .andExpect(status().is3xxRedirection())
+        .andExpect(
+            redirectedUrl(
+                openIDConnectConfiguration.getAuthorizationEndpoint()
+                    + "?id=client_id&scope=foo+bar+my_id&foo=1&bar=%272%27"));
+  }
+
+  @Test
+  public void testProxyTokenLogic() throws Exception {
+    testProxyTokenLogic(null);
+  }
+
+  @Test
+  public void testProxyTokenLogicWithSecret() throws Exception {
+    testProxyTokenLogic("supersecret");
+  }
+
+  private void testProxyTokenLogic(String clientSecret) throws Exception {
+    RestTemplate restTemplate = mock(RestTemplate.class);
+    String tokenEndpoint = "http://foo.com/token";
+    String requestBody = "access_token=foo";
+    JsonNode returnNode = objectMapper.readValue("{\"access_token\": \"tkn\"}", JsonNode.class);
+    when(openIDConnectConfiguration.getTokenEndpoint()).thenReturn(tokenEndpoint);
+    openIDConnectConfiguration.setClientSecret(clientSecret);
+    when(restTemplate.exchange(
+            anyString(), eq(HttpMethod.POST), any(HttpEntity.class), eq(JsonNode.class)))
+        .thenReturn(ResponseEntity.ok().body(returnNode));
+    when(controller.getRestTemplate()).thenReturn(restTemplate);
+
+    mvc.perform(post(TOKEN_REFRESH_ENDPOINT).content(requestBody))
+        .andExpect(status().is2xxSuccessful())
+        .andExpect(jsonPath("$.access_token").value("tkn"));
+
+    verify(restTemplate, times(1))
+        .exchange(eq(tokenEndpoint), eq(HttpMethod.POST), postCaptor.capture(), eq(JsonNode.class));
+
+    String expectedBody =
+        clientSecret == null ? requestBody : requestBody + "&" + "client_secret=" + clientSecret;
+    assertThat(
+        "proxy call body is passed through",
+        postCaptor.getValue().getBody(),
+        equalTo(expectedBody));
+  }
+}

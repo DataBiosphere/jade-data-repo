36760f6bc036a6fd99b91aae243fd90408f0e880
Saman Ehsan <se8ea@virginia.edu>
2022-05-11 14:54:55 -0400

    Fix SAM call

--- datarepo-clienttests/src/main/java/scripts/utils/SAMUtils.java
+++ datarepo-clienttests/src/main/java/scripts/utils/SAMUtils.java
@@ -84,7 +84,7 @@ public class SAMUtils {
   public static boolean isDataRepoSteward(ApiClient apiClient, String datarepoResourceId)
       throws ApiException {
     ResourcesApi resourcesApi = new ResourcesApi(apiClient);
-    return resourcesApi.resourceActionsV2("datarepo", datarepoResourceId, "create_dataset");
+    return resourcesApi.resourcePermissionV2("datarepo", datarepoResourceId, "create_dataset");
   }
 
   /**

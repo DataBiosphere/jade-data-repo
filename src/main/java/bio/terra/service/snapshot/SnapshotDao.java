646001e481ae417b30d0f7592ee8553c3c6ab6c0
Saman Ehsan <se8ea@virginia.edu>
2022-06-15 10:59:31 -0700

    Allow patching snapshot properties

--- src/main/java/bio/terra/service/snapshot/SnapshotDao.java
+++ src/main/java/bio/terra/service/snapshot/SnapshotDao.java
@@ -690,12 +690,14 @@ public class SnapshotDao {
   @Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.SERIALIZABLE)
   public boolean patch(UUID id, SnapshotPatchRequestModel patchRequest) {
     String sql =
-        "UPDATE snapshot SET consent_code = COALESCE(:consent_code, consent_code) WHERE id = :id";
+        "UPDATE snapshot SET consent_code = COALESCE(:consent_code, consent_code), " +
+            "properties = COALESCE(cast(:properties as jsonb), properties) WHERE id = :id";
 
     MapSqlParameterSource params =
         new MapSqlParameterSource()
             .addValue("consent_code", patchRequest.getConsentCode())
-            .addValue("id", id);
+            .addValue("id", id)
+            .addValue("properties", DaoUtils.propertiesToString(objectMapper, patchRequest.getProperties()));
 
     int rowsAffected = jdbcTemplate.update(sql, params);
     boolean patchSucceeded = (rowsAffected == 1);

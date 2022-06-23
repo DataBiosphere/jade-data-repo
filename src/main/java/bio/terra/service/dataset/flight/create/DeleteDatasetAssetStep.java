9bbe7f5cf77d35fd06620d0f363c5d91833fe895
Saman Ehsan <se8ea@virginia.edu>
2022-06-23 16:33:19 -0400

    Check for snapshots in asset delete

--- src/main/java/bio/terra/service/dataset/flight/create/DeleteDatasetAssetStep.java
+++ src/main/java/bio/terra/service/dataset/flight/create/DeleteDatasetAssetStep.java
@@ -1,12 +1,15 @@
 package bio.terra.service.dataset.flight.create;
 
+import bio.terra.app.controller.exception.ValidationException;
 import bio.terra.service.dataset.AssetDao;
 import bio.terra.service.job.JobMapKeys;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.FlightMap;
 import bio.terra.stairway.Step;
 import bio.terra.stairway.StepResult;
+import java.util.List;
 import java.util.UUID;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
 
 public class DeleteDatasetAssetStep implements Step {
@@ -17,17 +20,17 @@ public class DeleteDatasetAssetStep implements Step {
     this.assetDao = assetDao;
   }
 
-  private UUID getAssetId(FlightContext context) {
-    // get asset Id
-    UUID assetId =
-        UUID.fromString(
-            context.getInputParameters().get(JobMapKeys.ASSET_ID.getKeyName(), String.class));
-    return assetId;
-  }
-
   @Override
   public StepResult doStep(FlightContext context) {
-    assetDao.delete(getAssetId(context));
+    FlightMap inputs = context.getInputParameters();
+    UUID datasetId = UUID.fromString(inputs.get(JobMapKeys.DATASET_ID.getKeyName(), String.class));
+    UUID assetId = UUID.fromString(inputs.get(JobMapKeys.ASSET_ID.getKeyName(), String.class));
+    List<UUID> snapshotIds = assetDao.retrieveSnapshotsForAsset(datasetId, assetId);
+    if (snapshotIds.size() != 0) {
+      throw new ValidationException(
+          "The asset is being used by snapshots: " + StringUtils.join(snapshotIds, ","));
+    }
+    assetDao.delete(assetId);
     FlightMap map = context.getWorkingMap();
     map.put(JobMapKeys.STATUS_CODE.getKeyName(), HttpStatus.NO_CONTENT);
     return StepResult.getStepResultSuccess();

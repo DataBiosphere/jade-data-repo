524eb9406aa3e98a74ba3eb3381202e9c17bc7a9
Saman Ehsan <se8ea@virginia.edu>
2022-07-08 14:15:34 -0400

    Validate against existing schema

--- src/main/java/bio/terra/common/ValidationUtils.java
+++ src/main/java/bio/terra/common/ValidationUtils.java
@@ -1,13 +1,21 @@
 package bio.terra.common;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.UUID;
 import java.util.function.Function;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
+import bio.terra.model.ColumnModel;
+import bio.terra.model.RelationshipModel;
+import bio.terra.model.RelationshipTermModel;
+import bio.terra.model.TableDataType;
+import bio.terra.model.TableModel;
 import org.apache.commons.lang3.StringUtils;
 
 public final class ValidationUtils {
@@ -71,4 +79,52 @@ public final class ValidationUtils {
 
     return value;
   }
+
+  public static Map<String, String> validateRelationshipTerm(RelationshipTermModel term, List<TableModel> tables) {
+    String tableName = term.getTable();
+    String columnName = term.getColumn();
+    Map<String, String> termErrors = new HashMap<>();
+    Optional<TableModel> table = tables.stream().filter(t -> t.getName().equals(tableName)).findFirst();
+    if (table.isEmpty()) {
+      termErrors.put("InvalidRelationshipTermTable",
+          String.format("Invalid table %s", tableName));
+    } else {
+      Optional<ColumnModel> columnModel = table.get().getColumns().stream()
+          .filter(c -> c.getName().equals(columnName)).findFirst();
+      if (columnModel.isEmpty()) {
+        termErrors.put("InvalidRelationshipTermTableColumn",
+            String.format("invalid table %s.%s", tableName, columnName));
+      } else {
+        if (!isInvalidPrimaryOrForeignKeyType(columnModel.get())) {
+          termErrors.put("OptionalForeignKeyColumn",
+              String.format("Foreign key column %s cannot be marked as not required",
+                  columnName));
+        }
+      }
+    }
+    return termErrors;
+  }
+
+  public static ArrayList<Map<String, String>> getRelationshipValidationErrors(
+      RelationshipModel relationship,
+      List<TableModel> tables) {
+    ArrayList<Map<String, String>> errors = new ArrayList<>();
+      RelationshipTermModel fromTerm = relationship.getFrom();
+      if (fromTerm != null) {
+        errors.add(validateRelationshipTerm(fromTerm, tables));
+      }
+
+      RelationshipTermModel toTerm = relationship.getTo();
+      if (toTerm != null) {
+        errors.add(validateRelationshipTerm(toTerm, tables));
+      }
+    return errors;
+  }
+
+  public static boolean isInvalidPrimaryOrForeignKeyType(ColumnModel columnModel) {
+    Set<TableDataType> invalidTypes = Set.of(TableDataType.DIRREF, TableDataType.FILEREF);
+    return columnModel.getDatatype() != null && invalidTypes.contains(columnModel.getDatatype());
+  }
+
+
 }

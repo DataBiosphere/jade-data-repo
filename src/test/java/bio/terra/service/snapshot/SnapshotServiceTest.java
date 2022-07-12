87d7dcbca0f62966bd6aaa14f40d2d7c42dec446
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2022-07-12 12:32:45 -0400

    [DR-2675] ECM exception should not block snapshot enumeration (#1321)
    * [DR-2675] ECM exception should not block snapshot enumeration
    
    * Manually update api version to faciliate hotfix
    
    * Instead of staging/prod hotfix, alpha hotfix and early promotion
    
    To be safe, undoing our manual semantic version hotfix bump: version changes will be handled automatically on PR merge to develop.

--- src/test/java/bio/terra/service/snapshot/SnapshotServiceTest.java
+++ src/test/java/bio/terra/service/snapshot/SnapshotServiceTest.java
@@ -9,6 +9,8 @@ import static org.hamcrest.Matchers.hasEntry;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import bio.terra.app.model.GoogleCloudResource;
@@ -32,6 +34,7 @@ import bio.terra.model.StorageResourceModel;
 import bio.terra.model.TableDataType;
 import bio.terra.model.TableModel;
 import bio.terra.service.auth.iam.IamAction;
+import bio.terra.service.auth.iam.IamResourceType;
 import bio.terra.service.auth.iam.IamRole;
 import bio.terra.service.auth.iam.IamService;
 import bio.terra.service.auth.ras.EcmService;
@@ -59,9 +62,11 @@ import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.HttpClientErrorException;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {SnapshotService.class, MetadataDataAccessUtils.class})
@@ -360,6 +365,22 @@ public class SnapshotServiceTest {
         "Patch with consent code update requires passport identifier update permissions",
         service.patchSnapshotIamActions(new SnapshotPatchRequestModel().consentCode("c99")),
         containsInAnyOrder(IamAction.UPDATE_SNAPSHOT, IamAction.UPDATE_PASSPORT_IDENTIFIER));
+  }
+
+  @Test
+  public void listAuthorizedSnapshots() throws Exception {
+    Map<UUID, Set<IamRole>> samAuthorizedSnapshots =
+        Map.of(UUID.randomUUID(), Set.of(IamRole.STEWARD));
+    when(ecmService.getRasDbgapPermissions(TEST_USER))
+        .thenThrow(new HttpClientErrorException(HttpStatus.I_AM_A_TEAPOT));
+    when(iamService.listAuthorizedResources(TEST_USER, IamResourceType.DATASNAPSHOT))
+        .thenReturn(samAuthorizedSnapshots);
+
+    assertThat(
+        "ECM exception should not block snapshot enumeration",
+        service.listAuthorizedSnapshots(TEST_USER),
+        equalTo(samAuthorizedSnapshots));
+    verify(ecmService, times(1)).getRasDbgapPermissions(TEST_USER);
   }
 
   @Test

2b4e7a67d54a3cabad40d42fc0bb1eabbb980b03
Elliot Otchet <otchet@broadinstitute.org>
2022-10-26 20:42:36 -0400

    Memorialize github comment in code.

--- src/main/java/bio/terra/service/common/CommonFlightUtils.java
+++ src/main/java/bio/terra/service/common/CommonFlightUtils.java
@@ -22,6 +22,16 @@ public class CommonFlightUtils {
     return null;
   }
 
+  /**
+   * Obtains flight information of Interest that might be useful in a journal entry.
+   * Patterned after an allowList because the flightContext input parameters can contain
+   * some sensitive information that we don't want included in a journal entry.
+   * The pattern of using the request payload to hold arbitrary objects that can be returned
+   * as JSON led to the inclusion of a hint in the key name so that our front end can detect
+   * and unwrap this value.
+   * @param flightContext
+   * @return LinkedHashMap<String, String>
+   */
   public static Map<String, String> getFlightInformationOfInterest(
       @NotNull FlightContext flightContext) {
     Map<String, String> results = new LinkedHashMap<>();

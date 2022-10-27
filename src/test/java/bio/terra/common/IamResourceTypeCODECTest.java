610be875b96ac5869402246b003cdb13e5db673f
Elliot Otchet <otchet@broadinstitute.org>
2022-10-26 20:31:41 -0400

    Test codec in both directions.

--- src/test/java/bio/terra/common/IamResourceTypeCODECTest.java
+++ src/test/java/bio/terra/common/IamResourceTypeCODECTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 import bio.terra.common.category.Unit;
+import bio.terra.model.IamResourceTypeEnum;
 import bio.terra.service.auth.iam.IamResourceType;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
@@ -20,6 +21,14 @@ public class IamResourceTypeCODECTest {
           "CODEC encodes and decodes",
           IamResourceTypeCODEC.toIamResourceType(
               IamResourceTypeCODEC.toIamResourceTypeEnum(resource).toString()),
+          equalTo(resource));
+    }
+
+    for (IamResourceTypeEnum resource : IamResourceTypeEnum.values()) {
+      assertThat(
+          "CODEC encodes and decodes",
+          IamResourceTypeCODEC.toIamResourceTypeEnum(
+              IamResourceTypeCODEC.toIamResourceType(resource.toString())),
           equalTo(resource));
     }
   }

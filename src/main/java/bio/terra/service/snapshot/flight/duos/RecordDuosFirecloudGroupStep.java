14a4a9e12fc5672580668b72aed6d0eb17c199a2
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2022-11-02 13:22:55 -0400

    Move related line of code below its associated comment

--- src/main/java/bio/terra/service/snapshot/flight/duos/RecordDuosFirecloudGroupStep.java
+++ src/main/java/bio/terra/service/snapshot/flight/duos/RecordDuosFirecloudGroupStep.java
@@ -21,10 +21,10 @@ public class RecordDuosFirecloudGroupStep implements Step {
   @Override
   public StepResult doStep(FlightContext context) throws InterruptedException, RetryException {
     UUID duosFirecloudGroupId = duosDao.insertFirecloudGroup(getFirecloudGroup(context));
-    DuosFirecloudGroupModel retrieved = duosDao.retrieveFirecloudGroup(duosFirecloudGroupId);
 
     // When inserting a new record, DuosDao and the DB generate additional metadata
     // (ex. creation time) so we'll retrieve the record and rewrite it in the working map.
+    DuosFirecloudGroupModel retrieved = duosDao.retrieveFirecloudGroup(duosFirecloudGroupId);
     context.getWorkingMap().put(SnapshotDuosMapKeys.FIRECLOUD_GROUP, retrieved);
 
     return StepResult.getStepResultSuccess();

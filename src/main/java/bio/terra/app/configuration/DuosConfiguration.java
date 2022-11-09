e35a883471106bd8c3c7924f33a33213fcf10c5b
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2022-11-09 13:53:14 -0500

    @EnableConfigurationProperties is redundant

--- src/main/java/bio/terra/app/configuration/DuosConfiguration.java
+++ src/main/java/bio/terra/app/configuration/DuosConfiguration.java
@@ -1,11 +1,9 @@
 package bio.terra.app.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@EnableConfigurationProperties
 @ConfigurationProperties(prefix = "duos")
 public class DuosConfiguration {
 

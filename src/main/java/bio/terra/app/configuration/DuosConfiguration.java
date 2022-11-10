0af77d4b39f1f18acc8fc473e88b288cb7535029
Phil Shapiro <pshapiro@broadinstitute.org>
2022-11-10 13:23:46 -0500

    seems like ConstructorBinding is required

--- src/main/java/bio/terra/app/configuration/DuosConfiguration.java
+++ src/main/java/bio/terra/app/configuration/DuosConfiguration.java
@@ -1,6 +1,8 @@
 package bio.terra.app.configuration;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
 
+@ConstructorBinding
 @ConfigurationProperties(prefix = "duos")
 public record DuosConfiguration(String basePath) {}

9877cf9704355d5ad16117160d737332c4d98cfe
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2022-11-16 12:02:57 -0500

    Add DuosClient.getApprovedUsers which fetches approved users for a DUOS dataset
    - Within user fetch, leverage GoogleCredentialsService to run as TDR SA
    - Break out DUOS exception handling into common method - used for both dataset and authorized user fetch
    - Add DuosClient unit tests

--- src/main/java/bio/terra/service/duos/DuosClient.java
+++ src/main/java/bio/terra/service/duos/DuosClient.java
@@ -1,9 +1,12 @@
 package bio.terra.service.duos;
 
 import bio.terra.app.configuration.DuosConfiguration;
+import bio.terra.common.exception.ErrorReportException;
 import bio.terra.common.iam.AuthenticatedUserRequest;
+import bio.terra.service.auth.oauth2.GoogleCredentialsService;
 import bio.terra.service.duos.exception.DuosDatasetBadRequestException;
 import bio.terra.service.duos.exception.DuosDatasetNotFoundException;
+import bio.terra.service.duos.exception.DuosInternalServerErrorException;
 import com.google.common.annotations.VisibleForTesting;
 import java.util.List;
 import org.springframework.http.HttpEntity;
@@ -13,19 +16,26 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class DuosClient {
 
+  @VisibleForTesting static final List<String> DUOS_SCOPES = List.of("email", "profile");
+
   private final DuosConfiguration duosConfiguration;
   private final RestTemplate restTemplate;
+  private final GoogleCredentialsService googleCredentialsService;
 
-  public DuosClient(DuosConfiguration duosConfiguration, RestTemplate restTemplate) {
+  public DuosClient(
+      DuosConfiguration duosConfiguration,
+      RestTemplate restTemplate,
+      GoogleCredentialsService googleCredentialsService) {
     this.duosConfiguration = duosConfiguration;
     this.restTemplate = restTemplate;
     restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+    this.googleCredentialsService = googleCredentialsService.scopes(DUOS_SCOPES);
   }
 
   /**
@@ -42,12 +52,15 @@ public class DuosClient {
     return String.format("%s/status", duosConfiguration.getBasePath());
   }
 
+  /**
+   * @return status of DUOS and its subsystems
+   */
   public SystemStatus status() {
     HttpHeaders unauthedHeaders = getHttpHeaders();
-    return restTemplate
-        .exchange(
-            getStatusUrl(), HttpMethod.GET, new HttpEntity<>(unauthedHeaders), SystemStatus.class)
-        .getBody();
+    ResponseEntity<SystemStatus> statusCall =
+        restTemplate.exchange(
+            getStatusUrl(), HttpMethod.GET, new HttpEntity<>(unauthedHeaders), SystemStatus.class);
+    return statusCall.getBody();
   }
 
   @VisibleForTesting
@@ -59,8 +72,6 @@ public class DuosClient {
    * @param duosId DUOS dataset ID
    * @param userRequest authenticated user
    * @return the DuosDataset associated with duosId
-   * @throws DuosDatasetBadRequestException if identifier is malformed
-   * @throws DuosDatasetNotFoundException if no DUOS dataset found
    */
   public DuosDataset getDataset(String duosId, AuthenticatedUserRequest userRequest) {
     HttpHeaders authedHeaders = getHttpHeaders();
@@ -73,14 +84,56 @@ public class DuosClient {
               new HttpEntity<>(authedHeaders),
               DuosDataset.class);
       return datasetCall.getBody();
-    } catch (HttpClientErrorException ex) {
-      switch (ex.getStatusCode()) {
-        case BAD_REQUEST -> throw new DuosDatasetBadRequestException(
-            "DUOS dataset identifier %s is malformed".formatted(duosId), ex);
-        case NOT_FOUND -> throw new DuosDatasetNotFoundException(
-            "Could not find DUOS dataset for identifier %s".formatted(duosId), ex);
-        default -> throw ex;
-      }
+    } catch (HttpStatusCodeException ex) {
+      throw convertDuosExToDataRepoEx(ex, duosId);
+    }
+  }
+
+  @VisibleForTesting
+  String getApprovedUsersUrl(String duosId) {
+    return getDatasetUrl(duosId) + "/approved/users";
+  }
+
+  /**
+   * This call to DUOS is performed as the TDR SA rather than a supplied authenticated user.
+   *
+   * @param duosId DUOS dataset ID
+   * @return all users who have access to the dataset via an accepted DAR
+   */
+  public DuosDatasetApprovedUsers getApprovedUsers(String duosId) {
+    HttpHeaders saHeaders = getHttpHeaders();
+    saHeaders.setBearerAuth(googleCredentialsService.getApplicationDefaultAccessToken());
+    try {
+      ResponseEntity<DuosDatasetApprovedUsers> approvedUsersCall =
+          restTemplate.exchange(
+              getApprovedUsersUrl(duosId),
+              HttpMethod.GET,
+              new HttpEntity<>(saHeaders),
+              DuosDatasetApprovedUsers.class);
+      return approvedUsersCall.getBody();
+    } catch (HttpStatusCodeException ex) {
+      throw convertDuosExToDataRepoEx(ex, duosId);
+    }
+  }
+
+  @VisibleForTesting
+  static ErrorReportException convertDuosExToDataRepoEx(
+      HttpStatusCodeException duosEx, String duosId) {
+    switch (duosEx.getStatusCode()) {
+      case BAD_REQUEST:
+        {
+          return new DuosDatasetBadRequestException(
+              "DUOS dataset identifier %s is malformed".formatted(duosId), duosEx);
+        }
+      case NOT_FOUND:
+        {
+          return new DuosDatasetNotFoundException(
+              "Could not find DUOS dataset for identifier %s".formatted(duosId), duosEx);
+        }
+      default:
+        {
+          return new DuosInternalServerErrorException("Unexpected error from DUOS", duosEx);
+        }
     }
   }
 }
--- src/test/java/bio/terra/service/duos/DuosClientTest.java
+++ src/test/java/bio/terra/service/duos/DuosClientTest.java
@@ -2,6 +2,7 @@ package bio.terra.service.duos;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertThrows;
 import static org.mockito.ArgumentMatchers.argThat;
 import static org.mockito.ArgumentMatchers.eq;
@@ -11,10 +12,14 @@ import bio.terra.app.configuration.DuosConfiguration;
 import bio.terra.common.category.Unit;
 import bio.terra.common.fixtures.AuthenticationFixtures;
 import bio.terra.common.iam.AuthenticatedUserRequest;
+import bio.terra.service.auth.oauth2.GoogleCredentialsService;
 import bio.terra.service.duos.exception.DuosDatasetBadRequestException;
 import bio.terra.service.duos.exception.DuosDatasetNotFoundException;
+import bio.terra.service.duos.exception.DuosInternalServerErrorException;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
 import org.apache.http.HttpHeaders;
 import org.junit.Before;
 import org.junit.Test;
@@ -38,20 +43,26 @@ public class DuosClientTest {
 
   @Mock private DuosConfiguration duosConfiguration;
   @Mock private RestTemplate restTemplate;
+  @Mock private GoogleCredentialsService googleCredentialsService;
 
   private static final String BASE_PATH = "https://consent.dsde-dev.broadinstitute.org";
   private static final String DUOS_ID = "DUOS-123456";
   private static final Integer DUOS_DATASET_ID = 1;
   private static final AuthenticatedUserRequest TEST_USER =
       AuthenticationFixtures.randomUserRequest();
+  private static final String TDR_SA_TOKEN = UUID.randomUUID().toString();
 
   private DuosClient duosClient;
 
   @Before
   public void setup() {
-    duosClient = new DuosClient(duosConfiguration, restTemplate);
+    when(googleCredentialsService.scopes(DuosClient.DUOS_SCOPES))
+        .thenReturn(googleCredentialsService);
+    when(googleCredentialsService.getApplicationDefaultAccessToken()).thenReturn(TDR_SA_TOKEN);
 
     when(duosConfiguration.getBasePath()).thenReturn(BASE_PATH);
+
+    duosClient = new DuosClient(duosConfiguration, restTemplate, googleCredentialsService);
   }
 
   @Test
@@ -137,14 +148,85 @@ public class DuosClientTest {
         equalTo(expectedEx));
   }
 
+  @Test
+  public void testGetApprovedUsers() {
+    var expectedBody =
+        new DuosDatasetApprovedUsers(
+            List.of("a", "b", "c").stream().map(DuosDatasetApprovedUser::new).toList());
+    when(restTemplate.exchange(
+            eq(duosClient.getApprovedUsersUrl(DUOS_ID)),
+            eq(HttpMethod.GET),
+            argThat(this::hasTdrSaToken),
+            eq(DuosDatasetApprovedUsers.class)))
+        .thenReturn(ResponseEntity.ok(expectedBody));
+
+    assertThat(
+        "Authorized users are returned",
+        duosClient.getApprovedUsers(DUOS_ID),
+        equalTo(expectedBody));
+  }
+
+  @Test
+  public void testGetApprovedUsersThrows() {
+    var expectedEx = new HttpClientErrorException(HttpStatus.NOT_FOUND);
+    when(restTemplate.exchange(
+            eq(duosClient.getApprovedUsersUrl(DUOS_ID)),
+            eq(HttpMethod.GET),
+            argThat(this::hasTdrSaToken),
+            eq(DuosDatasetApprovedUsers.class)))
+        .thenThrow(expectedEx);
+
+    DuosDatasetNotFoundException thrown =
+        assertThrows(
+            DuosDatasetNotFoundException.class, () -> duosClient.getApprovedUsers(DUOS_ID));
+    assertThat(
+        "Exception thrown when DUOS dataset not found for the supplied ID",
+        thrown.getCause(),
+        equalTo(expectedEx));
+  }
+
+  @Test
+  public void testConvertDuosExToDataRepoEx() {
+    var badRequestEx = new HttpClientErrorException(HttpStatus.BAD_REQUEST);
+    assertThat(
+        DuosClient.convertDuosExToDataRepoEx(badRequestEx, DUOS_ID),
+        instanceOf(DuosDatasetBadRequestException.class));
+
+    var notFoundEx = new HttpClientErrorException(HttpStatus.NOT_FOUND);
+    assertThat(
+        DuosClient.convertDuosExToDataRepoEx(notFoundEx, DUOS_ID),
+        instanceOf(DuosDatasetNotFoundException.class));
+
+    var unexpectedEx = new HttpClientErrorException(HttpStatus.I_AM_A_TEAPOT);
+    assertThat(
+        DuosClient.convertDuosExToDataRepoEx(unexpectedEx, DUOS_ID),
+        instanceOf(DuosInternalServerErrorException.class));
+
+    var internalServerErrorEx = new HttpClientErrorException(HttpStatus.INTERNAL_SERVER_ERROR);
+    assertThat(
+        DuosClient.convertDuosExToDataRepoEx(internalServerErrorEx, DUOS_ID),
+        instanceOf(DuosInternalServerErrorException.class));
+  }
+
+  // Utilities for examining HttpEntity parameters
+  private List<String> getAuthorizations(HttpEntity httpEntity) {
+    return Optional.ofNullable(httpEntity.getHeaders().get(HttpHeaders.AUTHORIZATION))
+        .orElse(List.of());
+  }
+
   private boolean isUnauthenticated(HttpEntity httpEntity) {
-    List<String> authorizations = httpEntity.getHeaders().get(HttpHeaders.AUTHORIZATION);
-    return authorizations == null || authorizations.isEmpty();
+    return getAuthorizations(httpEntity).isEmpty();
   }
 
   private boolean hasUserToken(HttpEntity httpEntity) {
     String bearerHeader = "Bearer %s".formatted(TEST_USER.getToken());
-    List<String> authorizations = httpEntity.getHeaders().get(HttpHeaders.AUTHORIZATION);
+    List<String> authorizations = getAuthorizations(httpEntity);
+    return authorizations.size() == 1 && authorizations.get(0).equals(bearerHeader);
+  }
+
+  private boolean hasTdrSaToken(HttpEntity httpEntity) {
+    String bearerHeader = "Bearer %s".formatted(TDR_SA_TOKEN);
+    List<String> authorizations = getAuthorizations(httpEntity);
     return authorizations.size() == 1 && authorizations.get(0).equals(bearerHeader);
   }
 }

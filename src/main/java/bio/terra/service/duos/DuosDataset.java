eb762021505e053231c2c687bc347c882cf863dd
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2022-11-16 12:03:58 -0500

    Removed unneeded @VisibleForTesting annotation in standalone DuosDataset record
    This record definition was previously embedded within DuosClient

--- src/main/java/bio/terra/service/duos/DuosDataset.java
+++ src/main/java/bio/terra/service/duos/DuosDataset.java
@@ -1,8 +1,6 @@
 package bio.terra.service.duos;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.google.common.annotations.VisibleForTesting;
 
-@VisibleForTesting
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DuosDataset(Integer dataSetId) {}

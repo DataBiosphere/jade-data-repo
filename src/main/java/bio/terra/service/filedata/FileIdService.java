329faab336c3b322b2cc689641ea1404f10dcf5d
nmalfroy <nmalfroy@broadinstitute.org>
2023-02-03 14:48:54 -0500

    Make sure file id calculation works with null md5

--- src/main/java/bio/terra/service/filedata/FileIdService.java
+++ src/main/java/bio/terra/service/filedata/FileIdService.java
@@ -3,6 +3,7 @@ package bio.terra.service.filedata;
 import bio.terra.service.dataset.Dataset;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +12,11 @@ public class FileIdService {
   private static final String HASH_SEPARATOR = "#";
 
   public UUID calculateFileId(Dataset dataset, FSItem fsItem) {
-    if (dataset.hasPredictableFileIds()) {
+    return calculateFileId(dataset.hasPredictableFileIds(), fsItem);
+  }
+
+  public UUID calculateFileId(boolean hasPredictableFileIds, FSItem fsItem) {
+    if (hasPredictableFileIds) {
       return UUID.nameUUIDFromBytes(createHashableContent(fsItem).getBytes(StandardCharsets.UTF_8));
     } else {
       return UUID.randomUUID();
@@ -21,6 +26,9 @@ public class FileIdService {
   private String createHashableContent(FSItem fsItem) {
     return String.join(
         HASH_SEPARATOR,
-        List.of(fsItem.getPath(), fsItem.getChecksumMd5(), String.valueOf(fsItem.getSize())));
+        List.of(
+            fsItem.getPath(),
+            Objects.requireNonNullElse(fsItem.getChecksumMd5(), ""),
+            String.valueOf(fsItem.getSize())));
   }
 }

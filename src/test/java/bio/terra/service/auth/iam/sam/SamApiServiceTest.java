bcfb439037b7ffd0c1c2ace559f4529e2662609d
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2023-02-16 16:53:14 -0500

    [DR-2931] SamIam API construction moved to new SamApiService
    This was done with the aim of making unit testing easier.
    SamIamTest no longer has to spy the SamIam instance: we instead mock SamApiService and its responses.
    Added unit tests to SamApiService to verify that our Sam ApiClient read timeouts are being set as we expect.
    
    While here, I realized that RawlsServiceTest was involving SamIam unnecessarily.
    It should only be testing RawlsServiceTest functionality.
    I pared down this test substantially and added in RawlsService.getWorkspaceLink helper method to make testing easier.
    WorkspaceResponse was refactored to no longer set a workspace link when serializing to a WorkspacePolicyModel:
    it makes more sense to set the workspace link once we have the WorkspacePolicyModel in RawlsService.resolvePolicyEmails.

---
+++ src/test/java/bio/terra/service/auth/iam/sam/SamApiServiceTest.java
@@ -0,0 +1,67 @@
+package bio.terra.service.auth.iam.sam;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
+
+import bio.terra.app.configuration.SamConfiguration;
+import bio.terra.common.category.Unit;
+import bio.terra.service.configuration.ConfigEnum;
+import bio.terra.service.configuration.ConfigurationService;
+import java.util.Map;
+import org.broadinstitute.dsde.workbench.client.sam.ApiClient;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles({"google", "unittest"})
+@Category(Unit.class)
+@RunWith(MockitoJUnitRunner.StrictStubs.class)
+public class SamApiServiceTest {
+
+  @Mock private SamConfiguration samConfig;
+  @Mock private ConfigurationService configurationService;
+  private SamApiService samApiService;
+
+  private static final String TOKEN = "some-access-token";
+  private static final int OPERATION_TIMEOUT_SECONDS = 123;
+  private static final int OPERATION_TIMEOUT_MILLIS = OPERATION_TIMEOUT_SECONDS * 1000;
+
+  @Before
+  public void setUp() throws Exception {
+    samApiService = new SamApiService(samConfig, configurationService);
+
+    when(configurationService.getParameterValue(ConfigEnum.SAM_OPERATION_TIMEOUT_SECONDS))
+        .thenReturn(OPERATION_TIMEOUT_SECONDS);
+  }
+
+  @Test
+  public void testSamApiClientReadTimeouts() {
+    // We only have one unauthorized Sam API -- StatusApi -- and this is unlikely to change.
+    assertThat(
+        "StatusApi keeps default read timeout",
+        samApiService.statusApi().getApiClient().getReadTimeout(),
+        equalTo(new ApiClient().getReadTimeout()));
+
+    var authorizedApiClients =
+        Map.of(
+            "ResourcesApi", samApiService.resourcesApi(TOKEN).getApiClient(),
+            "GoogleApi", samApiService.googleApi(TOKEN).getApiClient(),
+            "UsersApi", samApiService.usersApi(TOKEN).getApiClient(),
+            "TermsOfServiceApi", samApiService.termsOfServiceApi(TOKEN).getApiClient(),
+            "GroupApi", samApiService.groupApi(TOKEN).getApiClient());
+    authorizedApiClients
+        .keySet()
+        .forEach(
+            name -> {
+              assertThat(
+                  name + " has expected read timeout",
+                  authorizedApiClients.get(name).getReadTimeout(),
+                  equalTo(OPERATION_TIMEOUT_MILLIS));
+            });
+  }
+}

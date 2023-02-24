d2ccea6aa8a5a4010b63d4fd2be838fc13072955
nmalfroy <nmalfroy@broadinstitute.org>
2023-02-24 10:00:49 -0500

    Truncate the job response if the number of files is too big (1000)

--- src/main/java/bio/terra/service/filedata/flight/ingest/IngestBulkBulkModeResponseStep.java
+++ src/main/java/bio/terra/service/filedata/flight/ingest/IngestBulkBulkModeResponseStep.java
@@ -7,12 +7,14 @@ import bio.terra.service.job.JobMapKeys;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.FlightMap;
 import bio.terra.stairway.StepResult;
+import org.apache.commons.collections4.CollectionUtils;
 
 // It expects the following working map data:
 // - BULK_LOAD_RESULT - a BulkLoadArrayResultModel object
 //
 public class IngestBulkBulkModeResponseStep extends DefaultUndoStep {
 
+  private static final int MAX_FILE_RESULTS = 1000;
   private final boolean isArrayMode;
 
   public IngestBulkBulkModeResponseStep(boolean isArrayMode) {
@@ -28,6 +30,12 @@ public class IngestBulkBulkModeResponseStep extends DefaultUndoStep {
     if (isArrayMode) {
       workingMap.put(JobMapKeys.RESPONSE.getKeyName(), result);
     } else {
+      // Truncate file results if needed.  Note: not doing this for arrays since that API takes in
+      // the values so presumably can return them back.  This is explicitly for the case where the
+      // file data is loaded from a cloud file
+      if (CollectionUtils.size(result.getLoadFileResults()) > MAX_FILE_RESULTS) {
+        result.loadFileResults(result.getLoadFileResults().subList(0, MAX_FILE_RESULTS));
+      }
       workingMap.put(JobMapKeys.RESPONSE.getKeyName(), result.getLoadSummary());
     }
 

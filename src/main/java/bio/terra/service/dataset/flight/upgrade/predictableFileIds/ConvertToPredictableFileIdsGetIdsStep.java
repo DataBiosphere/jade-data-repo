109d2ff36782691aa2932ec08c0042b70f8cc926
Nick Malfroy-Camine <nmalfroy@broadinstitute.org>
2023-03-31 18:47:16 -0400

    DR-2935 Add upgrade method to convert random file ids to predicable file ids (#1432)
    * Move firestore querying method to be reused and add new method to scan (bulk) delete
    
    * Add option to filter files to only files when retrieving fileid from firestore
    
    (specifically, ignoring directories)
    
    * Add methods to update the predictable ids flag
    
    * Add BigQueryPDao methods to handle file id updates
    
    * Add the flight that will upgrade a single dataset
    
    * Register upgrade and add support for passing in flight inputs
    
    * PR Feedback cleanup
    
    - Test code cleanup
    - Convert sql template creation to set parameters in a fluent style in BigQueryPDao's
    - Convert a bunch of steps to use DefaultUndoStep
    - Variable renames
    
    * Properly handle rolling back pg metadata if the flight fails

---
+++ src/main/java/bio/terra/service/dataset/flight/upgrade/predictableFileIds/ConvertToPredictableFileIdsGetIdsStep.java
@@ -0,0 +1,75 @@
+package bio.terra.service.dataset.flight.upgrade.predictableFileIds;
+
+import bio.terra.service.dataset.Dataset;
+import bio.terra.service.dataset.DatasetService;
+import bio.terra.service.filedata.FSFile;
+import bio.terra.service.filedata.FileIdService;
+import bio.terra.service.filedata.google.firestore.FireStoreDao;
+import bio.terra.service.filedata.google.firestore.FireStoreUtils;
+import bio.terra.service.job.DefaultUndoStep;
+import bio.terra.stairway.FlightContext;
+import bio.terra.stairway.StepResult;
+import bio.terra.stairway.exception.RetryException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Objects;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import org.apache.commons.collections4.ListUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ConvertToPredictableFileIdsGetIdsStep extends DefaultUndoStep {
+  private static final Logger logger =
+      LoggerFactory.getLogger(ConvertToPredictableFileIdsGetIdsStep.class);
+
+  private final UUID datasetId;
+  private final DatasetService datasetService;
+  private final FireStoreDao fileDao;
+  private final FileIdService fileIdService;
+
+  public ConvertToPredictableFileIdsGetIdsStep(
+      UUID datasetId,
+      DatasetService datasetService,
+      FireStoreDao fileDao,
+      FileIdService fileIdService) {
+    this.datasetId = datasetId;
+    this.datasetService = datasetService;
+    this.fileDao = fileDao;
+    this.fileIdService = fileIdService;
+  }
+
+  @Override
+  public StepResult doStep(FlightContext context) throws InterruptedException, RetryException {
+    Dataset dataset = datasetService.retrieve(datasetId);
+
+    // Retrieve existing file ids
+    List<String> existingFileIds = fileDao.retrieveAllFileIds(dataset, false);
+    List<FSFile> fsFiles = new ArrayList<>(existingFileIds.size());
+    for (var fileIdBatch :
+        ListUtils.partition(existingFileIds, FireStoreUtils.MAX_FIRESTORE_BATCH_SIZE)) {
+      fsFiles.addAll(fileDao.batchRetrieveById(dataset, fileIdBatch, 0));
+    }
+
+    // Calculate new file ids (and store as strings to avoid serialization issues)
+    Map<String, String> oldToNewMappings =
+        fsFiles.stream()
+            .map(
+                f ->
+                    Map.entry(
+                        f.getFileId().toString(),
+                        fileIdService.calculateFileId(true, f).toString()))
+            .filter(e -> !Objects.equals(e.getKey(), e.getValue()))
+            .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+
+    context.getWorkingMap().put(ConvertFileIdUtils.FILE_ID_MAPPINGS_FIELD, oldToNewMappings);
+    context
+        .getWorkingMap()
+        .put(
+            ConvertFileIdUtils.DATASET_USES_PREDICTABLE_IDS_AT_START,
+            dataset.hasPredictableFileIds());
+    return StepResult.getStepResultSuccess();
+  }
+}
--- src/test/java/bio/terra/service/dataset/DatasetServiceUnitTest.java
+++ src/test/java/bio/terra/service/dataset/DatasetServiceUnitTest.java
@@ -7,12 +7,15 @@ import static org.hamcrest.Matchers.hasEntry;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import bio.terra.app.usermetrics.UserLoggingMetrics;
 import bio.terra.common.MetadataEnumeration;
 import bio.terra.common.category.Unit;
 import bio.terra.model.DatasetPatchRequestModel;
+import bio.terra.model.DatasetSummaryModel;
 import bio.terra.service.auth.iam.IamAction;
 import bio.terra.service.auth.iam.IamRole;
 import bio.terra.service.auth.iam.IamService;
@@ -109,5 +112,16 @@ public class DatasetServiceUnitTest {
         datasetService.patchDatasetIamActions(
             new DatasetPatchRequestModel().description("an updated description")),
         containsInAnyOrder(IamAction.MANAGE_SCHEMA));
+  }
+
+  @Test
+  public void updatePredictableIdsFlag() {
+    UUID datasetId = UUID.randomUUID();
+    DatasetSummary summary = mock(DatasetSummary.class);
+    when(summary.toModel()).thenReturn(new DatasetSummaryModel().id(datasetId));
+    when(datasetDao.retrieveSummaryById(datasetId)).thenReturn(summary);
+    datasetService.setPredictableFileIds(datasetId, true);
+    verify(datasetDao).setPredictableFileId(datasetId, true);
+    verify(datasetDao).retrieveSummaryById(datasetId);
   }
 }

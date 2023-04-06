fade266450b146f26f6dd80f3ea9b2bdac883c9b
Nick Malfroy-Camine <nmalfroy@broadinstitute.org>
2023-04-06 14:07:29 -0400

    DR-2998 Chunk inserts into BQ during file id migration (#1438)
    * Add utility method to chunk java maps
    
    * Insert file mappings into BQ in chunks of 1000

--- src/main/java/bio/terra/service/dataset/flight/upgrade/predictableFileIds/ConvertToPredictableFileIdsBqStageDataStep.java
+++ src/main/java/bio/terra/service/dataset/flight/upgrade/predictableFileIds/ConvertToPredictableFileIdsBqStageDataStep.java
@@ -1,5 +1,6 @@
 package bio.terra.service.dataset.flight.upgrade.predictableFileIds;
 
+import bio.terra.common.MapUtils;
 import bio.terra.service.dataset.Dataset;
 import bio.terra.service.dataset.DatasetService;
 import bio.terra.service.job.DefaultUndoStep;
@@ -7,6 +8,7 @@ import bio.terra.service.tabulardata.google.bigquery.BigQueryDatasetPdao;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.StepResult;
 import bio.terra.stairway.exception.RetryException;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import org.slf4j.Logger;
@@ -15,6 +17,8 @@ import org.slf4j.LoggerFactory;
 public class ConvertToPredictableFileIdsBqStageDataStep extends DefaultUndoStep {
   private static final Logger logger =
       LoggerFactory.getLogger(ConvertToPredictableFileIdsBqStageDataStep.class);
+
+  private static final int INSERT_CHUNK_SIZE = 1000;
 
   private final UUID datasetId;
   private final DatasetService datasetService;
@@ -40,7 +44,15 @@ public class ConvertToPredictableFileIdsBqStageDataStep extends DefaultUndoStep 
       return StepResult.getStepResultSuccess();
     }
     // Load file ids into the table
-    bigQueryDatasetPdao.fileIdMappingToStagingTable(dataset, oldToNewMappings);
+    // Chunk the writes to avoid overwhelming BigQuery
+    List<Map<UUID, UUID>> chunkedOldToNewMappings =
+        MapUtils.partitionMap(oldToNewMappings, INSERT_CHUNK_SIZE);
+
+    int i = 0;
+    for (Map<UUID, UUID> chunk : chunkedOldToNewMappings) {
+      logger.info("inserting chunk {} of {}", ++i, chunkedOldToNewMappings.size());
+      bigQueryDatasetPdao.fileIdMappingToStagingTable(dataset, chunk);
+    }
 
     return StepResult.getStepResultSuccess();
   }

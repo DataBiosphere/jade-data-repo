c82b72b97ca0fc236898748b5577f12b074841fa
Shelby Holden <sholden@broadinstitute.org>
2023-05-18 15:24:34 -0400

    Make parse error more specific; Update test b/c failing in new way due to parsing

--- src/main/java/bio/terra/service/common/QueryUtils.java
+++ src/main/java/bio/terra/service/common/QueryUtils.java
@@ -1,6 +1,8 @@
 package bio.terra.service.common;
 
 import bio.terra.grammar.Query;
+import bio.terra.grammar.exception.InvalidFilterException;
+import bio.terra.grammar.exception.InvalidQueryException;
 import org.apache.commons.lang3.StringUtils;
 
 public class QueryUtils {
@@ -10,7 +12,11 @@ public class QueryUtils {
         (filter.matches("(?i)\\(?where\\s+.*\\)?")) ? filter : "WHERE (" + filter + ")";
     // Parse a Sql skeleton with the filter
     // Note: We could specifically parse the where statement, but it doesn't catch as many errors
-    Query.parse("select * from schema.table " + filterWithWhere);
+    try {
+      Query.parse("select * from schema.table " + filterWithWhere);
+    } catch (InvalidQueryException ex) {
+      throw new InvalidFilterException("Unable to parse user provided filter: " + filter);
+    }
     return filterWithWhere;
   }
 }

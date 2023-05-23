cf2f7d29ba09247b414afc4d8d862b54b21ee7a2
Saman Ehsan <se8ea@virginia.edu>
2023-05-23 18:17:52 -0400

    Consolidate snapshot policy create/delete steps

--- src/main/java/bio/terra/service/snapshot/flight/create/CreateSnapshotPolicyStep.java
+++ src/main/java/bio/terra/service/snapshot/flight/create/CreateSnapshotPolicyStep.java
@@ -1,54 +1,42 @@
 package bio.terra.service.snapshot.flight.create;
 
-import bio.terra.policy.model.TpsPolicyInput;
-import bio.terra.policy.model.TpsPolicyInputs;
 import bio.terra.service.policy.PolicyService;
-import bio.terra.service.policy.exception.PolicyConflictException;
+import bio.terra.service.snapshot.flight.SnapshotPolicyStep;
 import bio.terra.service.snapshot.flight.SnapshotWorkingMapKeys;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.FlightMap;
-import bio.terra.stairway.Step;
 import bio.terra.stairway.StepResult;
 import bio.terra.stairway.exception.RetryException;
 import java.util.UUID;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class CreateSnapshotPolicyStep implements Step {
-  private static final Logger logger = LoggerFactory.getLogger(CreateSnapshotPolicyStep.class);
+public class CreateSnapshotPolicyStep extends SnapshotPolicyStep {
 
-  private final PolicyService policyService;
   private final boolean enableSecureMonitoring;
 
   public CreateSnapshotPolicyStep(PolicyService policyService, boolean enableSecureMonitoring) {
-    this.policyService = policyService;
+    super(policyService);
     this.enableSecureMonitoring = enableSecureMonitoring;
+  }
+
+  @Override
+  public boolean isSecureMonitoringEnabled(FlightContext flightContext) {
+    return enableSecureMonitoring;
+  }
+
+  @Override
+  public UUID getSnapshotId(FlightContext flightContext) {
+    FlightMap flightMap = flightContext.getWorkingMap();
+    return flightMap.get(SnapshotWorkingMapKeys.SNAPSHOT_ID, UUID.class);
   }
 
   @Override
   public StepResult doStep(FlightContext flightContext)
       throws InterruptedException, RetryException {
-    if (enableSecureMonitoring) {
-      FlightMap flightMap = flightContext.getWorkingMap();
-      UUID snapshotId = flightMap.get(SnapshotWorkingMapKeys.SNAPSHOT_ID, UUID.class);
-      TpsPolicyInput protectedDataPolicy = PolicyService.getProtectedDataPolicyInput();
-      TpsPolicyInputs policyInputs = new TpsPolicyInputs().addInputsItem(protectedDataPolicy);
-      try {
-        policyService.createSnapshotPao(snapshotId, policyInputs);
-      } catch (PolicyConflictException ex) {
-        logger.warn("Policy access object already exists for snapshot {}", snapshotId);
-      }
-    }
-    return StepResult.getStepResultSuccess();
+    return createPaoStep(flightContext);
   }
 
   @Override
   public StepResult undoStep(FlightContext flightContext) throws InterruptedException {
-    if (enableSecureMonitoring) {
-      FlightMap flightMap = flightContext.getWorkingMap();
-      UUID snapshotId = flightMap.get(SnapshotWorkingMapKeys.SNAPSHOT_ID, UUID.class);
-      policyService.deletePaoIfExists(snapshotId);
-    }
-    return StepResult.getStepResultSuccess();
+    return deletePaoStep(flightContext);
   }
 }
--- src/main/java/bio/terra/service/snapshot/flight/delete/DeleteSnapshotPolicyStep.java
+++ src/main/java/bio/terra/service/snapshot/flight/delete/DeleteSnapshotPolicyStep.java
@@ -1,61 +1,49 @@
 package bio.terra.service.snapshot.flight.delete;
 
-import bio.terra.policy.model.TpsPolicyInput;
-import bio.terra.policy.model.TpsPolicyInputs;
 import bio.terra.service.dataset.Dataset;
 import bio.terra.service.dataset.DatasetService;
 import bio.terra.service.dataset.flight.DatasetWorkingMapKeys;
 import bio.terra.service.policy.PolicyService;
-import bio.terra.service.policy.exception.PolicyConflictException;
+import bio.terra.service.snapshot.flight.SnapshotPolicyStep;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.FlightMap;
-import bio.terra.stairway.Step;
 import bio.terra.stairway.StepResult;
 import bio.terra.stairway.exception.RetryException;
 import java.util.UUID;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-public class DeleteSnapshotPolicyStep implements Step {
-  private static final Logger logger = LoggerFactory.getLogger(DeleteSnapshotPolicyStep.class);
+public class DeleteSnapshotPolicyStep extends SnapshotPolicyStep {
   private final DatasetService datasetService;
-  private final PolicyService policyService;
   private final UUID snapshotId;
 
   public DeleteSnapshotPolicyStep(
       DatasetService datasetService, PolicyService policyService, UUID snapshotId) {
+    super(policyService);
     this.datasetService = datasetService;
-    this.policyService = policyService;
     this.snapshotId = snapshotId;
+  }
+
+  @Override
+  public boolean isSecureMonitoringEnabled(FlightContext context) throws InterruptedException {
+    FlightMap map = context.getWorkingMap();
+    UUID datasetId = map.get(DatasetWorkingMapKeys.DATASET_ID, UUID.class);
+    Dataset dataset = datasetService.retrieve(datasetId);
+    return dataset.isSecureMonitoringEnabled();
+  }
+
+  @Override
+  public UUID getSnapshotId(FlightContext flightContext) {
+    return snapshotId;
   }
 
   @Override
   public StepResult doStep(FlightContext flightContext)
       throws InterruptedException, RetryException {
-    FlightMap map = flightContext.getWorkingMap();
-    UUID datasetId = map.get(DatasetWorkingMapKeys.DATASET_ID, UUID.class);
-    Dataset dataset = datasetService.retrieve(datasetId);
-    if (dataset.isSecureMonitoringEnabled()) {
-      policyService.deletePaoIfExists(snapshotId);
-    }
-    return StepResult.getStepResultSuccess();
+    return deletePaoStep(flightContext);
   }
 
   @Override
   public StepResult undoStep(FlightContext flightContext)
       throws InterruptedException, RetryException {
-    FlightMap map = flightContext.getWorkingMap();
-    UUID datasetId = map.get(DatasetWorkingMapKeys.DATASET_ID, UUID.class);
-    Dataset dataset = datasetService.retrieve(datasetId);
-    if (dataset.isSecureMonitoringEnabled()) {
-      TpsPolicyInput protectedDataPolicy = PolicyService.getProtectedDataPolicyInput();
-      TpsPolicyInputs policyInputs = new TpsPolicyInputs().addInputsItem(protectedDataPolicy);
-      try {
-        policyService.createSnapshotPao(snapshotId, policyInputs);
-      } catch (PolicyConflictException ex) {
-        logger.warn("Policy access object already exists for snapshot {}", snapshotId);
-      }
-    }
-    return StepResult.getStepResultSuccess();
+    return createPaoStep(flightContext);
   }
 }

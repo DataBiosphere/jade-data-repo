df08dc978605a4f8a36418e13887b56a1ffebe54
Saman Ehsan <se8ea@virginia.edu>
2023-05-30 13:30:02 -0400

    Add retries to CreateDatasetRegisterIngestServiceAccountStep

--- src/main/java/bio/terra/service/dataset/flight/create/CreateDatasetRegisterIngestServiceAccountStep.java
+++ src/main/java/bio/terra/service/dataset/flight/create/CreateDatasetRegisterIngestServiceAccountStep.java
@@ -2,10 +2,12 @@ package bio.terra.service.dataset.flight.create;
 
 import bio.terra.service.auth.iam.IamService;
 import bio.terra.service.dataset.flight.DatasetWorkingMapKeys;
+import bio.terra.service.resourcemanagement.exception.GoogleResourceException;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.FlightMap;
 import bio.terra.stairway.Step;
 import bio.terra.stairway.StepResult;
+import bio.terra.stairway.StepStatus;
 
 /** The step is only meant to be invoked for GCP backed datasets. */
 public class CreateDatasetRegisterIngestServiceAccountStep implements Step {
@@ -22,7 +24,15 @@ public class CreateDatasetRegisterIngestServiceAccountStep implements Step {
     String datasetServiceAccount =
         workingMap.get(DatasetWorkingMapKeys.SERVICE_ACCOUNT_EMAIL, String.class);
 
-    iamService.registerUser(datasetServiceAccount);
+    try {
+      iamService.registerUser(datasetServiceAccount);
+    } catch (GoogleResourceException e) {
+      if (e.getCause().getMessage().contains("Could not generate Google access token")) {
+        return new StepResult(StepStatus.STEP_RESULT_FAILURE_RETRY, e);
+      } else {
+        return new StepResult(StepStatus.STEP_RESULT_FAILURE_FATAL, e);
+      }
+    }
 
     return StepResult.getStepResultSuccess();
   }

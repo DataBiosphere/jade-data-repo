46c7e3c6837a63d68c27a2431f2863f9caffb72c
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2023-06-06 18:01:16 -0400

    [DR-3059] Refactoring some tests to JUnit5 with test slicing (#1466)
    * ConfigTest and StatusTest removed in favor of UnauthenticatedApiControllerTest
    
    Those previous controller unit tests now have their functionality tested as part of a JUnit5 controller test with test slicing (only opting in that controller's bean).  This makes for faster test runtimes and no inadvertent BardClient calls.  As a trade-off, we do have to annotate explicitly annotate the inputs to UnauthenticatedApiController with @MockBean, otherwise the true beans will get pulled in.
    Existing StatusTest tests made more sense as a true unit test, so I added StatusServiceTest (also in JUnit5).
    Drive-bys: got to remove a fault in StatusService that existed only for testing, and removed unneeded parameters to UnauthenticatedApiController.
    
    * Drive-by: I think Oauth2ApiControllerTest was missing profile specification
    
    This ensures that we're loading our profiles for unit tests.
    
    * Preference for alphabetized annotations
    
    * Refactor SnapshotValidationTest to JUnit5 with test slicing
    
    * Convert JournalServiceTest to JUnit5, split into new JournalDaoTest
    
    The existing JournalServiceTest behaved as a connected test with its interactions with JournalDao.
    Now, we test these components separately: JournalDaoTest has an embedded database and tests the DAO code, JournalService test tests the service code.
    
    * Refactor UserMetricsInterceptorTest to JUnit5 with test slicing
    
    * Refactor CloudRegionTest, CloudResourceTest to JUnit5 with test slicing
    
    * Forgot to include new AuthenticationFixtures method
    
    * Convert DatasetBucketDaoTest to JUnit5
    
    * We shouldn't need to configure Bard for all unit tests
    
    * Go through modified tests to apply new learnings
    
    - Where it's not too much trouble, favor Hamcrest Matchers over JUnit Assert.  Otherwise, when migrating to JUnit5 we should also move to the new JUnit Jupiter Assertions package, whose methods reorder their input parameters.
    - Convert CloudRegionTest, CloudResourceTest into pure unit tests -- no mocks, no Spring annotations.  We're now creating a new ObjectMapper rather than autowiring one.  Open to discussion on whether the additional Spring test overhead is necessary for confidence in these tests.
    - Trying to figure out the appropriate annotations for different test categories (controller unit with MVC, DAO unit with embedded DB, unit with autowiring and mocking, unit with mocking only).  I'm getting closer but not there yet, will raise in design discussion.
    
    * Revert conversion of EmbeddedDatabaseTests to JUnit 5
    
    Possibilities:
    - JUnit 5 versions are legitimately running 4x slower than JUnit 4 versions
    - JUnit 4 versions are not reporting runtime in the way I'd expect in Gradle results / scans (is initialization excluded?)
    - JUnit 4 versions are not behaving as we'd expect w/r/t embedded databases (possibly not performing the initialization we expect)
    
    I've elected to move forward only with those 4->5 refactors which are known to improve performance. I don't want to risk driving up unit test runtime while we don't understand how to configure JUnit 5 embedded database tests in a performant, reliable way.
    
    * Drive-by: JobServiceTest was using default Stairway polling interval of 10 seconds
    
    This test is still misclassified as a unit test, and would be more appropriately classified as a connected test or refactored.
    But altering the Stairway polling to eliminate its sleep in between checking that test dummy flights complete dramatically drives down test suite runtime: individual tests that previously took over a minute to run excluding initialization now take under a second.
    
    * Fully remove LIVENESS_FAULT including in client tests
    
    The fault was originally added to help test Stairway flight recovery on k8s pod restart.
    Since then, we've upgraded Stairway and removed our custom initialization code.

--- src/test/java/bio/terra/app/usermetrics/UserMetricsInterceptorTest.java
+++ src/test/java/bio/terra/app/usermetrics/UserMetricsInterceptorTest.java
@@ -4,145 +4,183 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import bio.terra.app.configuration.ApplicationConfiguration;
 import bio.terra.app.configuration.UserMetricsConfiguration;
-import bio.terra.common.category.Unit;
 import bio.terra.common.exception.UnauthorizedException;
+import bio.terra.common.fixtures.AuthenticationFixtures;
 import bio.terra.common.iam.AuthenticatedUserRequest;
 import bio.terra.common.iam.AuthenticatedUserRequestFactory;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.experimental.categories.Category;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ContextConfiguration;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(properties = {"datarepo.testWithEmbeddedDatabase=false"})
-@AutoConfigureMockMvc
 @ActiveProfiles({"google", "unittest"})
-@Category(Unit.class)
+@ContextConfiguration(classes = UserLoggingMetrics.class)
+@SpringBootTest
+@Tag("bio.terra.common.category.Unit")
 public class UserMetricsInterceptorTest {
-  @SpyBean private AuthenticatedUserRequestFactory authenticatedUserRequestFactory;
+  @Autowired private UserLoggingMetrics eventProperties;
   @MockBean private BardClient bardClient;
-  @Autowired private UserLoggingMetrics loggingMetrics;
-  @Autowired private UserMetricsConfiguration metricsConfig;
+  @MockBean private AuthenticatedUserRequestFactory authenticatedUserRequestFactory;
+  @MockBean private ApplicationConfiguration applicationConfiguration;
+  @MockBean private UserMetricsConfiguration metricsConfig;
+
+  private ExecutorService metricsPerformanceThreadpool;
+  private UserMetricsInterceptor userMetricsInterceptor;
+
   @Captor private ArgumentCaptor<AuthenticatedUserRequest> authCaptor;
-  @SpyBean private UserMetricsInterceptor metricsInterceptor;
   @Mock HttpServletRequest request;
   @Mock HttpServletResponse response;
 
-  @Before
-  public void setUp() throws Exception {
-    metricsConfig.setIgnorePaths(List.of());
-    when(request.getMethod()).thenReturn("post");
-    when(request.getRequestURI()).thenReturn("/foo/bar");
+  private static final String APP_ID = "testapp";
+  private static final String BARD_BASE_PATH = "https://bard.com";
+  private static final String TOKEN = "footoken";
+  private static final String DNS_NAME = "some.dnsname.org";
+  private static final String METHOD = "post";
+  private static final String REQUEST_URI = "/foo/bar";
+  private static final Integer NUM_EXECUTOR_THREADS = 3;
+
+  private static final AuthenticatedUserRequest TEST_USER =
+      AuthenticationFixtures.userRequest(TOKEN);
+
+  @BeforeEach
+  void setUp() {
+    when(metricsConfig.getIgnorePaths()).thenReturn(List.of());
+    when(metricsConfig.getAppId()).thenReturn(APP_ID);
+    when(metricsConfig.getBardBasePath()).thenReturn(BARD_BASE_PATH);
+
+    when(applicationConfiguration.getDnsName()).thenReturn(DNS_NAME);
+
+    when(request.getMethod()).thenReturn(METHOD.toLowerCase());
+    when(request.getRequestURI()).thenReturn(REQUEST_URI);
+
+    metricsPerformanceThreadpool = Executors.newFixedThreadPool(NUM_EXECUTOR_THREADS);
+
+    userMetricsInterceptor =
+        new UserMetricsInterceptor(
+            bardClient,
+            authenticatedUserRequestFactory,
+            applicationConfiguration,
+            metricsConfig,
+            eventProperties,
+            metricsPerformanceThreadpool);
   }
 
-  @Test
-  public void testSendEvent() throws Exception {
-    mockRequestAuth(request);
-
-    runAndWait(request, response);
-
-    verify(bardClient, times(1))
-        .logEvent(
-            authCaptor.capture(),
-            eq(
-                new BardEvent(
-                    UserMetricsInterceptor.API_EVENT_NAME,
-                    Map.of(
-                        BardEventProperties.METHOD_FIELD_NAME, "POST",
-                        BardEventProperties.PATH_FIELD_NAME, "/foo/bar"),
-                    "testapp",
-                    "some.dnsname.org")));
-
-    assertThat("token is correct", authCaptor.getValue().getToken(), equalTo("footoken"));
-  }
-
-  @Test
-  public void testSendEventWithBillingProfileId() throws Exception {
-    String billingProfileId = UUID.randomUUID().toString();
-    loggingMetrics.set(BardEventProperties.BILLING_PROFILE_ID_FIELD_NAME, billingProfileId);
-
-    mockRequestAuth(request);
-
-    runAndWait(request, response);
-
-    verify(bardClient, times(1))
-        .logEvent(
-            authCaptor.capture(),
-            eq(
-                new BardEvent(
-                    UserMetricsInterceptor.API_EVENT_NAME,
-                    Map.of(
-                        BardEventProperties.METHOD_FIELD_NAME, "POST",
-                        BardEventProperties.PATH_FIELD_NAME, "/foo/bar",
-                        BardEventProperties.BILLING_PROFILE_ID_FIELD_NAME, billingProfileId),
-                    "testapp",
-                    "some.dnsname.org")));
-
-    assertThat("token is correct", authCaptor.getValue().getToken(), equalTo("footoken"));
-  }
-
-  @Test
-  public void testSendEventNotFiredWithNoToken() throws Exception {
-    doThrow(new UnauthorizedException("Building AuthenticatedUserRequest failed"))
-        .when(authenticatedUserRequestFactory)
-        .from(any());
-
-    runAndWait(request, response);
-
-    verify(bardClient, times(0)).logEvent(any(), any());
-  }
-
-  @Test
-  public void testSendEventNotFiredWithIgnoredUrl() throws Exception {
-    metricsConfig.setIgnorePaths(List.of("/foo/bar"));
-    mockRequestAuth(request);
-
-    runAndWait(request, response);
-
-    verify(bardClient, times(0)).logEvent(any(), any());
-  }
-
-  @Test
-  public void testSendEventNotFiredWithIgnoredWildcardUrl() throws Exception {
-    metricsConfig.setIgnorePaths(List.of("/foo/*"));
-    mockRequestAuth(request);
-
-    runAndWait(request, response);
-
-    verify(bardClient, times(0)).logEvent(any(), any());
+  @AfterEach
+  void tearDown() {
+    if (metricsPerformanceThreadpool != null) {
+      metricsPerformanceThreadpool.shutdownNow();
+    }
   }
 
   private void mockRequestAuth(HttpServletRequest request) {
-    when(request.getHeader("Authorization")).thenReturn("Bearer footoken");
-    when(request.getHeader("From")).thenReturn("me@me.me");
+    when(authenticatedUserRequestFactory.from(request)).thenReturn(TEST_USER);
   }
 
-  private void runAndWait(HttpServletRequest request, HttpServletResponse response)
-      throws Exception {
-    metricsInterceptor.afterCompletion(request, response, new Object(), null);
+  @Test
+  void testSendEvent() throws Exception {
+    mockRequestAuth(request);
+
+    runAndWait();
+
+    verify(bardClient)
+        .logEvent(
+            authCaptor.capture(),
+            eq(
+                new BardEvent(
+                    UserMetricsInterceptor.API_EVENT_NAME,
+                    Map.of(
+                        BardEventProperties.METHOD_FIELD_NAME,
+                        METHOD.toUpperCase(),
+                        BardEventProperties.PATH_FIELD_NAME,
+                        REQUEST_URI),
+                    APP_ID,
+                    DNS_NAME)));
+
+    assertThat("token is correct", authCaptor.getValue().getToken(), equalTo(TOKEN));
+  }
+
+  @Test
+  void testSendEventWithBillingProfileId() throws Exception {
+    String billingProfileId = UUID.randomUUID().toString();
+    eventProperties.set(BardEventProperties.BILLING_PROFILE_ID_FIELD_NAME, billingProfileId);
+
+    mockRequestAuth(request);
+
+    runAndWait();
+
+    verify(bardClient)
+        .logEvent(
+            authCaptor.capture(),
+            eq(
+                new BardEvent(
+                    UserMetricsInterceptor.API_EVENT_NAME,
+                    Map.of(
+                        BardEventProperties.METHOD_FIELD_NAME, METHOD.toUpperCase(),
+                        BardEventProperties.PATH_FIELD_NAME, REQUEST_URI,
+                        BardEventProperties.BILLING_PROFILE_ID_FIELD_NAME, billingProfileId),
+                    APP_ID,
+                    DNS_NAME)));
+
+    assertThat("token is correct", authCaptor.getValue().getToken(), equalTo(TOKEN));
+  }
+
+  @Test
+  void testSendEventNotFiredWithNoBardBasePath() throws Exception {
+    when(metricsConfig.getBardBasePath()).thenReturn(null);
+
+    runAndWait();
+
+    verify(bardClient, never()).logEvent(any(), any());
+  }
+
+  @Test
+  void testSendEventNotFiredWithNoToken() throws Exception {
+    when(authenticatedUserRequestFactory.from(any()))
+        .thenThrow(new UnauthorizedException("Building AuthenticatedUserRequest failed"));
+
+    runAndWait();
+
+    verify(bardClient, never()).logEvent(any(), any());
+  }
+
+  @ParameterizedTest
+  @ValueSource(strings = {REQUEST_URI, "/foo/*"})
+  void testSendEventNotFiredWithIgnoredUrl(String ignorePath) throws Exception {
+    when(metricsConfig.getIgnorePaths()).thenReturn(List.of(ignorePath));
+    mockRequestAuth(request);
+
+    runAndWait();
+
+    verify(bardClient, never()).logEvent(any(), any());
+  }
+
+  private void runAndWait() throws Exception {
+    userMetricsInterceptor.afterCompletion(request, response, new Object(), null);
     // This waits for the threadpool to wrap up so that we can examine the results
-    metricsConfig.metricsPerformanceThreadpool().awaitTermination(100, TimeUnit.MILLISECONDS);
+    metricsPerformanceThreadpool.awaitTermination(100, TimeUnit.MILLISECONDS);
   }
 }

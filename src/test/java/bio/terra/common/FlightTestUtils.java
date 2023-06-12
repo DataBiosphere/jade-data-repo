f31b7b651330dac03bb7dda6084c7c9acf23d309
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2023-06-12 16:46:19 -0400

    Add unit tests to verify resource locking in snapshot creation flight

--- src/test/java/bio/terra/common/FlightTestUtils.java
+++ src/test/java/bio/terra/common/FlightTestUtils.java
@@ -8,4 +8,11 @@ public class FlightTestUtils {
   public static List<String> getStepNames(Flight flight) {
     return flight.getSteps().stream().map(step -> step.getClass().getSimpleName()).toList();
   }
+
+  public static <T> List<T> getStepsWithClass(Flight flight, Class<T> clazz) {
+    return flight.getSteps().stream()
+        .filter(step -> clazz.isInstance(step))
+        .map(step -> clazz.cast(step))
+        .toList();
+  }
 }

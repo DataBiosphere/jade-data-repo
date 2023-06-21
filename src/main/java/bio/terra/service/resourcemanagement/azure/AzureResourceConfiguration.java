7208f5a9023628dd4057525d59e5c4fb4a3ef8ac
nmalfroy <nmalfroy@broadinstitute.org>
2023-06-21 16:50:13 -0400

    Add methods for getting new clients for managing logging resources in Azure

--- src/main/java/bio/terra/service/resourcemanagement/azure/AzureResourceConfiguration.java
+++ src/main/java/bio/terra/service/resourcemanagement/azure/AzureResourceConfiguration.java
@@ -5,6 +5,8 @@ import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.identity.ClientSecretCredentialBuilder;
 import com.azure.resourcemanager.AzureResourceManager;
+import com.azure.resourcemanager.loganalytics.LogAnalyticsManager;
+import com.azure.resourcemanager.securityinsights.SecurityInsightsManager;
 import java.util.UUID;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -99,6 +101,41 @@ public class AzureResourceConfiguration {
         new AzureProfile(tenantId.toString(), subscriptionId.toString(), AzureEnvironment.AZURE);
     return AzureResourceManager.authenticate(getAppToken(tenantId), profile)
         .withSubscription(subscriptionId.toString());
+  }
+
+  /**
+   * Get a log analytics resource manager client to a user's tenant. This object can then be used to
+   * create/destroy resources Note: this is a separate method from the one above because the log
+   * analytics client is not GA yet so does not return a generic AzureResourceManager object
+   *
+   * @param tenantId The ID of the user's tenant
+   * @param subscriptionId The ID of the subscription that will be charged for the resources created
+   *     with this client
+   * @return An authenticated {@link LogAnalyticsManager} client
+   */
+  public LogAnalyticsManager getLogAnalyticsManagerClient(
+      final UUID tenantId, final UUID subscriptionId) {
+    final AzureProfile profile =
+        new AzureProfile(tenantId.toString(), subscriptionId.toString(), AzureEnvironment.AZURE);
+    return LogAnalyticsManager.authenticate(getAppToken(), profile);
+  }
+
+  /**
+   * Get a security insights (e.g. Sentinel) resource manager client to a user's tenant. This object
+   * can then be used to create/destroy resources Note: this is a separate method from the one above
+   * because the security insights client is not GA yet so does not return a generic
+   * AzureResourceManager object
+   *
+   * @param tenantId The ID of the user's tenant
+   * @param subscriptionId The ID of the subscription that will be charged for the resources created
+   *     with this client
+   * @return An authenticated {@link LogAnalyticsManager} client
+   */
+  public SecurityInsightsManager getSecurityInsightsManagerClient(
+      final UUID tenantId, final UUID subscriptionId) {
+    final AzureProfile profile =
+        new AzureProfile(tenantId.toString(), subscriptionId.toString(), AzureEnvironment.AZURE);
+    return SecurityInsightsManager.authenticate(getAppToken(), profile);
   }
 
   /**

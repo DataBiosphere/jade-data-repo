7086708cf8285a8d678d34dc360b587056e4e43f
Saman Ehsan <se8ea@virginia.edu>
2023-08-02 12:48:00 -0400

    Convert BigQueryConfiguration to record

--- src/main/java/bio/terra/service/filedata/google/bq/BigQueryConfiguration.java
+++ src/main/java/bio/terra/service/filedata/google/bq/BigQueryConfiguration.java
@@ -2,35 +2,24 @@ package bio.terra.service.filedata.google.bq;
 
 import java.util.Optional;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.context.properties.ConstructorBinding;
 import org.springframework.context.annotation.Profile;
 
 /** Configuration for interacting with BigQuery. */
-@Configuration
+@ConstructorBinding
 @Profile("google")
 @ConfigurationProperties(prefix = "datarepo.bq")
-public class BigQueryConfiguration {
+public record BigQueryConfiguration(Integer rateLimitRetries, Integer rateLimitRetryWaitMs) {
 
   private static final int DEFAULT_MAX_NUM_UPDATE_LIMIT_INDUCED_RETRIES = 3;
   private static final int DEFAULT_RETRY_WAIT_MS = 500;
-
-  private Integer rateLimitRetries;
-  private Integer rateLimitRetryWaitMs;
 
   public int getRateLimitRetries() {
     return Optional.ofNullable(rateLimitRetries)
         .orElse(DEFAULT_MAX_NUM_UPDATE_LIMIT_INDUCED_RETRIES);
   }
 
-  public void setRateLimitRetries(Integer rateLimitRetries) {
-    this.rateLimitRetries = rateLimitRetries;
-  }
-
   public int getRateLimitRetryWaitMs() {
     return Optional.ofNullable(rateLimitRetryWaitMs).orElse(DEFAULT_RETRY_WAIT_MS);
-  }
-
-  public void setRateLimitRetryWaitMs(Integer rateLimitRetryWaitMs) {
-    this.rateLimitRetryWaitMs = rateLimitRetryWaitMs;
   }
 }

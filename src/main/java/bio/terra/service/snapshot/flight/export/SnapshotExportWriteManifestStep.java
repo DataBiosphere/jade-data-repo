380b94efc0bebd3bca8a8f205b4b682f3378d722
nmalfroy <nmalfroy@broadinstitute.org>
2023-09-28 14:15:02 -0400

    Add flight support for optionally signing URLs for GCP snapshot exports

--- src/main/java/bio/terra/service/snapshot/flight/export/SnapshotExportWriteManifestStep.java
+++ src/main/java/bio/terra/service/snapshot/flight/export/SnapshotExportWriteManifestStep.java
@@ -1,5 +1,7 @@
 package bio.terra.service.snapshot.flight.export;
 
+import static bio.terra.service.filedata.google.gcs.GcsConstants.REQUESTED_BY_QUERY_PARAM;
+
 import bio.terra.common.FlightUtils;
 import bio.terra.common.iam.AuthenticatedUserRequest;
 import bio.terra.model.SnapshotExportResponseModel;
@@ -10,6 +12,7 @@ import bio.terra.model.SnapshotExportResponseModelFormatParquetLocationTables;
 import bio.terra.model.SnapshotModel;
 import bio.terra.service.common.gcs.GcsUriUtils;
 import bio.terra.service.filedata.google.gcs.GcsPdao;
+import bio.terra.service.filedata.google.gcs.GcsProjectFactory;
 import bio.terra.service.job.DefaultUndoStep;
 import bio.terra.service.job.JobMapKeys;
 import bio.terra.service.resourcemanagement.google.GoogleBucketResource;
@@ -22,34 +25,46 @@ import bio.terra.stairway.StepStatus;
 import bio.terra.stairway.exception.RetryException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collectors;
+import java.util.concurrent.TimeUnit;
 
 public class SnapshotExportWriteManifestStep extends DefaultUndoStep {
+
+  private static final Duration URL_TTL = Duration.ofMinutes(60);
 
   private final UUID snapshotId;
   private final SnapshotService snapshotService;
   private final GcsPdao gcsPdao;
+  private final GcsProjectFactory gcsProjectFactory;
   private final ObjectMapper objectMapper;
   private final AuthenticatedUserRequest userReq;
   private final boolean validatePrimaryKeyUniqueness;
+  private final boolean signUrls;
 
   public SnapshotExportWriteManifestStep(
       UUID snapshotId,
       SnapshotService snapshotService,
       GcsPdao gcsPdao,
+      GcsProjectFactory gcsProjectFactory,
       ObjectMapper objectMapper,
       AuthenticatedUserRequest userReq,
-      boolean validatePrimaryKeyUniqueness) {
+      boolean validatePrimaryKeyUniqueness,
+      boolean signUrls) {
     this.snapshotId = snapshotId;
     this.snapshotService = snapshotService;
     this.gcsPdao = gcsPdao;
+    this.gcsProjectFactory = gcsProjectFactory;
     this.objectMapper = objectMapper;
     this.userReq = userReq;
     this.validatePrimaryKeyUniqueness = validatePrimaryKeyUniqueness;
+    this.signUrls = signUrls;
   }
 
   @Override
@@ -65,17 +80,24 @@ public class SnapshotExportWriteManifestStep extends DefaultUndoStep {
         GcsUriUtils.getGsPathFromComponents(
             exportBucket.getName(), String.format("%s/manifest.json", context.getFlightId()));
 
+    SnapshotModel snapshot = snapshotService.retrieveSnapshotModel(snapshotId, userReq);
+    Storage storage = gcsProjectFactory.getStorage(snapshot.getDataProject(), true);
     List<SnapshotExportResponseModelFormatParquetLocationTables> tables =
         paths.entrySet().stream()
             .map(
                 entry ->
                     new SnapshotExportResponseModelFormatParquetLocationTables()
                         .name(entry.getKey())
-                        .paths(entry.getValue()))
-            .collect(Collectors.toList());
+                        .paths(
+                            signUrls
+                                ? entry.getValue().stream()
+                                    .map(path -> signGsUrl(storage, path))
+                                    .toList()
+                                : entry.getValue()))
+            .toList();
 
-    SnapshotModel snapshot = snapshotService.retrieveSnapshotModel(snapshotId, userReq);
-
+    String effectiveExportManifestFile =
+        signUrls ? signGsUrl(storage, exportManifestPath) : exportManifestPath;
     SnapshotExportResponseModel responseModel =
         new SnapshotExportResponseModel()
             .snapshot(snapshot)
@@ -83,7 +105,7 @@ public class SnapshotExportWriteManifestStep extends DefaultUndoStep {
                 new SnapshotExportResponseModelFormat()
                     .parquet(
                         new SnapshotExportResponseModelFormatParquet()
-                            .manifest(exportManifestPath)
+                            .manifest(effectiveExportManifestFile)
                             .location(
                                 new SnapshotExportResponseModelFormatParquetLocation()
                                     .tables(tables))));
@@ -106,5 +128,19 @@ public class SnapshotExportWriteManifestStep extends DefaultUndoStep {
     context.getWorkingMap().put(JobMapKeys.RESPONSE.getKeyName(), responseModel);
 
     return StepResult.getStepResultSuccess();
+  }
+
+  private String signGsUrl(Storage storage, String gsPath) {
+    BlobId locator = GcsUriUtils.parseBlobUri(gsPath);
+    BlobInfo blobInfo = BlobInfo.newBuilder(locator).build();
+    Map<String, String> queryParams = Map.of(REQUESTED_BY_QUERY_PARAM, userReq.getEmail());
+    return storage
+        .signUrl(
+            blobInfo,
+            URL_TTL.toMinutes(),
+            TimeUnit.MINUTES,
+            Storage.SignUrlOption.withQueryParams(queryParams),
+            Storage.SignUrlOption.withV4Signature())
+        .toString();
   }
 }

ba9ede7312064edd59e38db6baa042ed460a7d8c
Bria Morgan <bmorgan@broadinstitute.org>
2023-10-16 15:07:14 -0400

    test update and gradle task

--- src/test/java/bio/terra/pact/provider/DataRepoProviderTest.java
+++ src/test/java/bio/terra/pact/provider/DataRepoProviderTest.java
@@ -1,5 +1,8 @@
 package bio.terra.pact.provider;
 
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+
 import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
@@ -11,16 +14,12 @@ import bio.terra.common.iam.AuthenticatedUserRequestFactory;
 import bio.terra.service.auth.iam.exception.IamForbiddenException;
 import bio.terra.service.snapshot.SnapshotService;
 import bio.terra.service.snapshot.exception.SnapshotNotFoundException;
+import java.util.UUID;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.util.UUID;
-
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.when;
 
 @Provider("tdr-provider")
 @PactBroker
@@ -28,8 +27,7 @@ import static org.mockito.Mockito.when;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class DataRepoProviderTest {
 
-  @MockBean
-  SnapshotService snapshotService;
+  @MockBean SnapshotService snapshotService;
 
   @MockBean AuthenticatedUserRequestFactory authenticatedUserRequestFactory;
   private static final AuthenticatedUserRequest TEST_USER =
@@ -43,12 +41,15 @@ public class DataRepoProviderTest {
 
   @State("snapshot does not exist")
   void getNonexistentSnapshot() {
-    when(snapshotService.retrieveSnapshotModel(UUID.fromString("12345678-abc9-012d-3456-e7fab89cd01e"), TEST_USER)).thenThrow(new SnapshotNotFoundException("Snapshot not found"));
+    when(snapshotService.retrieveSnapshotModel(
+            UUID.fromString("12345678-abc9-012d-3456-e7fab89cd01e"), TEST_USER))
+        .thenThrow(new SnapshotNotFoundException("Snapshot not found"));
   }
 
   @State("user does not have access to snapshot")
   void noAccessToSnapshot() {
-    doThrow(new IamForbiddenException("User does not have required action")).when(snapshotService).verifySnapshotReadable(UUID.fromString("12345678-abc9-012d-3456-e7fab89cd01e"), TEST_USER);
+    doThrow(new IamForbiddenException("User does not have required action"))
+        .when(snapshotService)
+        .verifySnapshotReadable(UUID.fromString("12345678-abc9-012d-3456-e7fab89cd01e"), TEST_USER);
   }
-
 }

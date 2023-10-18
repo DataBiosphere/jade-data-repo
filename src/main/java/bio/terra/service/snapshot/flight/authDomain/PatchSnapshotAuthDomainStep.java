f76d45ea05febe0ba840d13e58d621b43b5a5ebe
Saman Ehsan <sehsan@broadinstitute.org>
2023-10-18 15:39:23 -0400

    Only allow users to set auth domain once

--- src/main/java/bio/terra/service/snapshot/flight/authDomain/PatchSnapshotAuthDomainStep.java
+++ src/main/java/bio/terra/service/snapshot/flight/authDomain/PatchSnapshotAuthDomainStep.java
@@ -4,8 +4,10 @@ import bio.terra.common.iam.AuthenticatedUserRequest;
 import bio.terra.service.auth.iam.IamResourceType;
 import bio.terra.service.auth.iam.IamService;
 import bio.terra.service.job.DefaultUndoStep;
+import bio.terra.service.snapshot.exception.SnapshotAuthDomainExistsException;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.StepResult;
+import bio.terra.stairway.StepStatus;
 import bio.terra.stairway.exception.RetryException;
 import java.util.List;
 import java.util.UUID;
@@ -32,6 +34,15 @@ public class PatchSnapshotAuthDomainStep extends DefaultUndoStep {
 
   @Override
   public StepResult doStep(FlightContext context) throws InterruptedException, RetryException {
+    List<String> existingAuthDomain =
+        iamService.retrieveAuthDomain(userRequest, IamResourceType.DATASNAPSHOT, snapshotId);
+        if (!existingAuthDomain.isEmpty()) {
+          return new StepResult(
+              StepStatus.STEP_RESULT_FAILURE_FATAL,
+              new SnapshotAuthDomainExistsException(
+                  "Snapshot " + snapshotId + " already has an auth domain set: " +
+                      existingAuthDomain));
+        }
     iamService.patchAuthDomain(userRequest, IamResourceType.DATASNAPSHOT, snapshotId, userGroups);
     return StepResult.getStepResultSuccess();
   }

3059d384b05ba151f0d14dc1b8519d8df241c12c
Bria Morgan <bmorgan@broadinstitute.org>
2023-10-19 09:42:34 -0400

    some updates

--- src/test/java/bio/terra/pact/provider/DataRepoProviderTest.java
+++ src/test/java/bio/terra/pact/provider/DataRepoProviderTest.java
@@ -1,5 +1,7 @@
 package bio.terra.pact.provider;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.when;
 
@@ -11,12 +13,14 @@ import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider;
 import bio.terra.common.fixtures.AuthenticationFixtures;
 import bio.terra.common.iam.AuthenticatedUserRequest;
 import bio.terra.common.iam.AuthenticatedUserRequestFactory;
+import bio.terra.model.SnapshotModel;
 import bio.terra.service.auth.iam.exception.IamForbiddenException;
 import bio.terra.service.snapshot.SnapshotService;
 import bio.terra.service.snapshot.exception.SnapshotNotFoundException;
 import java.util.UUID;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -24,6 +28,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @Provider("tdr-provider")
 @PactBroker
 @ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class DataRepoProviderTest {
 
@@ -39,10 +44,10 @@ public class DataRepoProviderTest {
     context.verifyInteraction();
   }
 
-  @State("snapshot does not exist")
+  @State("snapshot doesn't exist")
   void getNonexistentSnapshot() {
     when(snapshotService.retrieveSnapshotModel(
-            UUID.fromString("12345678-abc9-012d-3456-e7fab89cd01e"), TEST_USER))
+            eq(UUID.fromString("12345678-abc9-012d-3456-e7fab89cd01e")), any(), any()))
         .thenThrow(new SnapshotNotFoundException("Snapshot not found"));
   }
 
@@ -51,5 +56,13 @@ public class DataRepoProviderTest {
     doThrow(new IamForbiddenException("User does not have required action"))
         .when(snapshotService)
         .verifySnapshotReadable(UUID.fromString("12345678-abc9-012d-3456-e7fab89cd01e"), TEST_USER);
+  }
+
+  @State("user has access to snapshot")
+  void successfulSnapshot() {
+    UUID id = UUID.fromString("12345678-abc9-012d-3456-e7fab89cd01e");
+    when(snapshotService.retrieveSnapshotModel(any(), any(), any()))
+        .thenReturn(
+            new SnapshotModel().id(id).name("snapshot name").description("SNAPSHOT_DESCRIPTION"));
   }
 }

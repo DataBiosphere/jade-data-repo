04f8b09b7ccf4fa214718fec2e793cd92b065795
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2023-10-25 16:13:26 -0400

    Instruct Pact context to use MockMvc as its target

--- src/test/java/bio/terra/pact/provider/DataRepoProviderTest.java
+++ src/test/java/bio/terra/pact/provider/DataRepoProviderTest.java
@@ -9,6 +9,7 @@ import au.com.dius.pact.provider.junit5.PactVerificationContext;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
+import au.com.dius.pact.provider.spring.junit5.MockMvcTestTarget;
 import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider;
 import bio.terra.app.controller.SnapshotsApiController;
 import bio.terra.common.category.Pact;
@@ -24,6 +25,7 @@ import bio.terra.service.snapshot.SnapshotRequestValidator;
 import bio.terra.service.snapshot.SnapshotService;
 import bio.terra.service.snapshot.exception.SnapshotNotFoundException;
 import java.util.UUID;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -52,6 +54,11 @@ public class DataRepoProviderTest {
   @MockBean private AssetModelValidator assetModelValidator;
 
   private static final UUID SNAPSHOT_ID = UUID.fromString("12345678-abc9-012d-3456-e7fab89cd01e");
+
+  @BeforeEach
+  void before(PactVerificationContext context) {
+    context.setTarget(new MockMvcTestTarget(mvc));
+  }
 
   @TestTemplate
   @ExtendWith(PactVerificationSpringProvider.class)

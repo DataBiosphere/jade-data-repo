0f47268334d0a9bc0768ef87768cc174a039897c
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2023-10-25 12:54:32 -0400

    Set up Pact test with Spring boot test slicing
    This follows existing convention in SnapshotsApiControllerTest:
    by only loading in the required SnapshotsApiController into the context and mocking all of its inputs,
    I want to see what's actually required when setting up the application context before introducing additional env variables.
    While here, also broke out the reused UUID into a variable.

--- src/test/java/bio/terra/pact/provider/DataRepoProviderTest.java
+++ src/test/java/bio/terra/pact/provider/DataRepoProviderTest.java
@@ -10,36 +10,48 @@ import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
 import au.com.dius.pact.provider.spring.junit5.PactVerificationSpringProvider;
+import bio.terra.app.controller.SnapshotsApiController;
 import bio.terra.common.category.Pact;
-import bio.terra.common.fixtures.AuthenticationFixtures;
-import bio.terra.common.iam.AuthenticatedUserRequest;
 import bio.terra.common.iam.AuthenticatedUserRequestFactory;
 import bio.terra.model.SnapshotModel;
+import bio.terra.service.auth.iam.IamService;
 import bio.terra.service.auth.iam.exception.IamForbiddenException;
+import bio.terra.service.dataset.AssetModelValidator;
+import bio.terra.service.dataset.IngestRequestValidator;
+import bio.terra.service.filedata.FileService;
+import bio.terra.service.job.JobService;
+import bio.terra.service.snapshot.SnapshotRequestValidator;
 import bio.terra.service.snapshot.SnapshotService;
 import bio.terra.service.snapshot.exception.SnapshotNotFoundException;
 import java.util.UUID;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 
 @Tag(Pact.TAG)
 @Provider("tdr")
 @PactBroker
-@ExtendWith(SpringExtension.class)
-@AutoConfigureMockMvc
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ContextConfiguration(classes = SnapshotsApiController.class)
+@WebMvcTest
 public class DataRepoProviderTest {
 
-  @MockBean SnapshotService snapshotService;
+  @Autowired private MockMvc mvc;
 
-  @MockBean AuthenticatedUserRequestFactory authenticatedUserRequestFactory;
-  private static final AuthenticatedUserRequest TEST_USER =
-      AuthenticationFixtures.randomUserRequest();
+  @MockBean private JobService jobService;
+  @MockBean private SnapshotRequestValidator snapshotRequestValidator;
+  @MockBean private SnapshotService snapshotService;
+  @MockBean private IamService iamService;
+  @MockBean private IngestRequestValidator ingestRequestValidator;
+  @MockBean private FileService fileService;
+  @MockBean private AuthenticatedUserRequestFactory authenticatedUserRequestFactory;
+  @MockBean private AssetModelValidator assetModelValidator;
+
+  private static final UUID SNAPSHOT_ID = UUID.fromString("12345678-abc9-012d-3456-e7fab89cd01e");
 
   @TestTemplate
   @ExtendWith(PactVerificationSpringProvider.class)
@@ -49,25 +61,26 @@ public class DataRepoProviderTest {
 
   @State("snapshot doesn't exist")
   void getNonexistentSnapshot() {
-    when(snapshotService.retrieveSnapshotModel(
-            eq(UUID.fromString("12345678-abc9-012d-3456-e7fab89cd01e")), any(), any()))
+    when(snapshotService.retrieveSnapshotModel(eq(SNAPSHOT_ID), any(), any()))
         .thenThrow(
             new SnapshotNotFoundException(
-                "Snapshot not found - id: 12345678-abc9-012d-3456-e7fab89cd01e"));
+                String.format("Snapshot not found - id: %s", SNAPSHOT_ID)));
   }
 
   @State("user does not have access to snapshot")
   void noAccessToSnapshot() {
     doThrow(new IamForbiddenException("User does not have required action"))
         .when(snapshotService)
-        .verifySnapshotReadable(any(), any());
+        .verifySnapshotReadable(eq(SNAPSHOT_ID), any());
   }
 
   @State("user has access to snapshot")
   void successfulSnapshot() {
-    UUID id = UUID.fromString("12345678-abc9-012d-3456-e7fab89cd01e");
-    when(snapshotService.retrieveSnapshotModel(any(), any(), any()))
+    when(snapshotService.retrieveSnapshotModel(eq(SNAPSHOT_ID), any(), any()))
         .thenReturn(
-            new SnapshotModel().id(id).name("snapshot name").description("SNAPSHOT_DESCRIPTION"));
+            new SnapshotModel()
+                .id(SNAPSHOT_ID)
+                .name("snapshot name")
+                .description("SNAPSHOT_DESCRIPTION"));
   }
 }

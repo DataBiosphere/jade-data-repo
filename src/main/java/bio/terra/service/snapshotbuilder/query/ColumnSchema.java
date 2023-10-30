2270245453ce888cd12716c89647763afd15d32f
Phil Shapiro <pshapiro@broadinstitute.org>
2023-10-30 14:29:33 -0400

    remove unused code related to query response

--- src/main/java/bio/terra/service/snapshotbuilder/query/ColumnSchema.java
+++
@@ -1,4 +0,0 @@
-package bio.terra.service.snapshotbuilder.query;
-
-/** The schema for a column in a {@link RowResult} describing the data in a column. */
-public record ColumnSchema(String columnName, CellValue.SQLDataType sqlDataType) {}

3f5f6ef2867be9cc5a5931b1c72d6fbb799d0e4d
Sky Rubenstein <blrubenstein@wpi.edu>
2023-10-30 14:48:52 -0400

    WIP getConcepts endpoint

--- src/main/java/bio/terra/service/snapshotbuilder/SnapshotBuilderService.java
+++ src/main/java/bio/terra/service/snapshotbuilder/SnapshotBuilderService.java
@@ -1,6 +1,9 @@
 package bio.terra.service.snapshotbuilder;
 
+import bio.terra.model.SnapshotBuilderConcept;
+import bio.terra.model.SnapshotBuilderGetConceptsResponse;
 import bio.terra.model.SnapshotBuilderSettings;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.stereotype.Component;
 
@@ -19,5 +22,18 @@ public class SnapshotBuilderService {
   public SnapshotBuilderSettings updateSnapshotBuilderSettings(
       UUID id, SnapshotBuilderSettings settings) {
     return snapshotBuilderSettingsDao.upsertSnapshotBuilderSettingsByDataset(id, settings);
+  }
+
+  public SnapshotBuilderGetConceptsResponse getConceptsFromParent(Integer parentId) {
+    // TODO: Build real query - this should get the name and ID from the concept table, the count
+    // from the occurrence table, and the existence of children from the concept_ancestor table.
+    return new SnapshotBuilderGetConceptsResponse()
+        .result(
+            List.of(
+                new SnapshotBuilderConcept()
+                    .count(100)
+                    .name("Stub concept")
+                    .hasChildren(true)
+                    .id(parentId + 1)));
   }
 }

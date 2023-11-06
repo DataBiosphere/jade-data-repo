06b416b17909be576d0cb494b140102bec8a31b6
Shelby Holden <sholden@broadinstitute.org>
2023-11-05 22:57:13 -0500

    Update lock check in DatasetDao

--- src/main/java/bio/terra/service/dataset/DatasetDao.java
+++ src/main/java/bio/terra/service/dataset/DatasetDao.java
@@ -324,15 +324,12 @@ public class DatasetDao implements TaggableResourceDao {
       }
 
       numRowsUpdated = jdbcTemplate.update(sql, params);
-      // TODO - fix this check
-      if (numRowsUpdated == 0 && lockType.lockAttempted()) {
-        // this method checks if the dataset exists
-        // if it does not exist, then the method throws a DatasetNotFoundException
-        // we don't need the result (dataset summary) here, just the existence check,
-        // so ignore the return value.
-        retrieveSummaryById(datasetId);
 
-        throw new DatasetLockException("Failed to lock the dataset", lockType.getErrorDetails());
+      if (numRowsUpdated == 0 && lockType.lockAttempted()) {
+        String exclusiveLock = retrieveSummaryById(datasetId).getResourceLocks().getExclusive();
+        if (exclusiveLock == null) {
+          throw new DatasetLockException("Failed to lock the dataset", lockType.getErrorDetails());
+        }
       }
     } catch (DatasetNotFoundException notFound) {
       logger.error(

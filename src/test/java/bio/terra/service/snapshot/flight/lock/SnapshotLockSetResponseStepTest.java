a16d6c96026af386c55be39ec77d2782bf284997
Shelby Holden <sholden@broadinstitute.org>
2023-11-29 12:05:06 -0500

    Add SnapshotLockResponseStepTest

---
+++ src/test/java/bio/terra/service/snapshot/flight/lock/SnapshotLockSetResponseStepTest.java
@@ -0,0 +1,57 @@
+package bio.terra.service.snapshot.flight.lock;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import bio.terra.common.category.Unit;
+import bio.terra.model.ResourceLocks;
+import bio.terra.model.SnapshotSummaryModel;
+import bio.terra.service.job.JobMapKeys;
+import bio.terra.service.snapshot.SnapshotService;
+import bio.terra.stairway.FlightContext;
+import bio.terra.stairway.FlightMap;
+import java.util.UUID;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+
+@ExtendWith(MockitoExtension.class)
+@Tag(Unit.TAG)
+class SnapshotLockSetResponseStepTest {
+  private static final UUID SNAPSHOT_ID = UUID.randomUUID();
+  private static final String FLIGHT_ID = "flight-id";
+  private SnapshotLockSetResponseStep step;
+  @Mock private SnapshotService snapshotService;
+  @Mock private FlightContext flightContext;
+
+  @Test
+  void doStep() {
+    // Setup
+    step = new SnapshotLockSetResponseStep(snapshotService, SNAPSHOT_ID);
+    // when(flightContext.getFlightId()).thenReturn(FLIGHT_ID);
+    when(flightContext.getWorkingMap()).thenReturn(new FlightMap());
+    var lockName = "lock123";
+    var locks = new ResourceLocks().exclusive(lockName);
+    var snapshotSummaryModel = new SnapshotSummaryModel().resourceLocks(locks);
+    when(snapshotService.retrieveSnapshotSummary(SNAPSHOT_ID)).thenReturn(snapshotSummaryModel);
+
+    // Perform Step
+    step.doStep(flightContext);
+
+    // Confirm Reponse is correctly set
+    FlightMap workingMap = flightContext.getWorkingMap();
+    assertThat(
+        "Response is the ResourceLocks object",
+        workingMap.get(JobMapKeys.RESPONSE.getKeyName(), ResourceLocks.class),
+        equalTo(locks));
+    assertThat(
+        "Response Status is HttpStatus.OK",
+        workingMap.get(JobMapKeys.STATUS_CODE.getKeyName(), HttpStatus.class),
+        equalTo(HttpStatus.OK));
+  }
+}

853872778ee995501a5150ddfad39b11a473bb4d
Shelby Holden <sholden@broadinstitute.org>
2023-12-01 08:24:56 -0500

    Default direction if not set

--- src/main/java/bio/terra/common/SqlSortDirection.java
+++ src/main/java/bio/terra/common/SqlSortDirection.java
@@ -8,10 +8,17 @@ public enum SqlSortDirection {
   DESC;
 
   public static SqlSortDirection from(SqlSortDirectionAscDefault direction) {
+    // Since we don't require the sort direction field, it can still be null
+    if (direction == null) {
+      return ASC;
+    }
     return direction == SqlSortDirectionAscDefault.ASC ? ASC : DESC;
   }
 
   public static SqlSortDirection from(SqlSortDirectionDescDefault direction) {
+    if (direction == null) {
+      return DESC;
+    }
     return direction == SqlSortDirectionDescDefault.ASC ? ASC : DESC;
   }
 }

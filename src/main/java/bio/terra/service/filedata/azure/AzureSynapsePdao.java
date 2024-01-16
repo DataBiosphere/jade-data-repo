4575ccc7594783d40e7d3bfef2813fea36fb5080
Nick Malfroy-Camine <nmalfroy@broadinstitute.org>
2024-01-16 09:30:27 -0500

    [DR-3409] Handle large string, text and array fields in Azure ingest (#1568)
    * Drive by fix to make tests pass locally
    
    This was causing issues with bean resolution when default google credentials are not a service account (e.g. when you are logged in with a Google user account).
    
    * Make our use of Synapse handle text that is larger than 8000 bytes
    
    In two cases:
    - When there are straight up more than 8K bytes in a text field
    - When the serialized JSON is more than 8K bytes
    
    Note: this makes it so that the default column handling on ingest is to explicitly
    cast the JSON values. The requiresJSONCast option on columns is repurposed to indicate
    loading the json value using OPENJSON
    
    * Unfortunately, varchar(max) doesn't appear to work with CSV files
    
    * Make sure that the special json loading happens for string fields too
    
    * PR Feedback:
    
    - add a test that ingest fails on larger csv fields
    - use @Nullable annotation for service account email parameter
    - update API doc for csv ingests

--- src/main/java/bio/terra/service/filedata/azure/AzureSynapsePdao.java
+++ src/main/java/bio/terra/service/filedata/azure/AzureSynapsePdao.java
@@ -154,9 +154,9 @@ public class AzureSynapsePdao {
                     <if(c.isFileType)>
                        <if(c.arrayOf)>
                          <if(isGlobalFileIds)>
-                           (SELECT '[' + STRING_AGG('"drs://<drsLocator>v2_' + [file_id] + '"', ',') + ']' FROM OPENJSON([<c.name>]) WITH ([file_id] VARCHAR(8000) '$') WHERE [<c.name>] != '') AS [<c.name>]
+                           (SELECT '[' + STRING_AGG('"drs://<drsLocator>v2_' + [file_id] + '"', ',') + ']' FROM OPENJSON([<c.name>]) WITH ([file_id] VARCHAR(max) '$') WHERE [<c.name>] != '') AS [<c.name>]
                          <else>
-                           (SELECT '[' + STRING_AGG('"drs://<drsLocator>v1_<snapshotId>_' + [file_id] + '"', ',') + ']' FROM OPENJSON([<c.name>]) WITH ([file_id] VARCHAR(8000) '$') WHERE [<c.name>] != '') AS [<c.name>]
+                           (SELECT '[' + STRING_AGG('"drs://<drsLocator>v1_<snapshotId>_' + [file_id] + '"', ',') + ']' FROM OPENJSON([<c.name>]) WITH ([file_id] VARCHAR(max) '$') WHERE [<c.name>] != '') AS [<c.name>]
                          <endif>
                        <else>
                          <if(isGlobalFileIds)>
@@ -177,7 +177,7 @@ public class AzureSynapsePdao {
                  BULK '<ingestFileName>',
                  DATA_SOURCE = '<ingestFileDataSourceName>',
                  FORMAT = 'parquet') WITH (
-                              <columns:{c|[<c.name>] <if(c.requiresTypeCast)>varchar(8000)<else><c.synapseDataType><endif>
+                              <columns:{c|[<c.name>] <if(c.requiresTypeCast)>varchar(max)<else><c.synapseDataType><endif>
                               <if(c.requiresCollate)> COLLATE <collation><endif>
                               }; separator=", ">
                              ) AS rows
@@ -271,9 +271,9 @@ public class AzureSynapsePdao {
           <else>
           newid() as datarepo_row_id,
           <columns:{c|
-          <if(c.requiresJSONCast)>cast(JSON_VALUE(doc, '$.<c.name>') as <c.synapseDataType>) [<c.name>]
-          <elseif(c.arrayOf)>cast(JSON_QUERY(doc, '$.<c.name>') as VARCHAR(8000)) [<c.name>]
-          <else>JSON_VALUE(doc, '$.<c.name>') [<c.name>]
+          <if(c.arrayOf)>cast(JSON_QUERY(doc, '$.<c.name>') as VARCHAR(max)) [<c.name>]
+          <elseif(c.requiresJSONCast)>(SELECT field FROM OPENJSON(doc) WITH (field <c.synapseDataType> '$.<c.name>')) [<c.name>]
+          <else>cast(JSON_VALUE(doc, '$.<c.name>') as <c.synapseDataType>) [<c.name>]
           <endif>
           }; separator=", ">
           <endif>
@@ -293,7 +293,7 @@ public class AzureSynapsePdao {
           <endif>
               ) WITH (
                 <if(isCSV)>
-          <columns:{c|[<c.name>] <c.synapseDataType>
+          <columns:{c|[<c.name>] <c.synapseDataTypeForCsv>
           <if(c.requiresCollate)> COLLATE <collation><endif>
           }; separator=", ">
           <else>doc nvarchar(max)
@@ -320,7 +320,7 @@ public class AzureSynapsePdao {
       SELECT DISTINCT [Element] AS [<refCol>]
           FROM [<tableName>]
           /* Note, refIds can be either UUIDs or drs ids, which is why we are extracting Element as a large value */
-          CROSS APPLY OPENJSON([<refCol>]) WITH (Element VARCHAR(8000) '$') AS ARRAY_VALUES
+          CROSS APPLY OPENJSON([<refCol>]) WITH (Element VARCHAR(max) '$') AS ARRAY_VALUES
           WHERE [<refCol>] IS NOT NULL
           AND [Element] IS NOT NULL;""";
 
@@ -350,7 +350,7 @@ public class AzureSynapsePdao {
                   FROM OPENROWSET(BULK '<parquetFileLocation>',
                                 DATA_SOURCE = '<datasource>',
                                 FORMAT='PARQUET') WITH (
-                                  <columns:{c|[<c.name>] <if(c.requiresTypeCast)>varchar(8000)<else><c.synapseDataType><endif>
+                                  <columns:{c|[<c.name>] <if(c.requiresTypeCast)>varchar(max)<else><c.synapseDataType><endif>
                                   <if(c.requiresCollate)> COLLATE <collation><endif>
                                   }; separator=", ">
                                  ) AS rows

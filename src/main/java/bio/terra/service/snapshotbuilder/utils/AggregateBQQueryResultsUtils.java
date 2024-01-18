343ffcfb65e610858a39a651d303355e4105d67e
Shelby Holden <sholden@broadinstitute.org>
2024-01-18 11:54:48 -0500

    stub out the hasChildren and count fields

--- src/main/java/bio/terra/service/snapshotbuilder/utils/AggregateBQQueryResultsUtils.java
+++ src/main/java/bio/terra/service/snapshotbuilder/utils/AggregateBQQueryResultsUtils.java
@@ -6,13 +6,16 @@ import java.util.List;
 import java.util.stream.StreamSupport;
 
 public class AggregateBQQueryResultsUtils {
+  // TODO - pull real values for hasChildren and count
   public static List<SnapshotBuilderConcept> aggregateConceptResults(TableResult result) {
     return StreamSupport.stream(result.iterateAll().spliterator(), false)
         .map(
             row ->
                 new SnapshotBuilderConcept()
                     .id((int) (row.get("concept_id").getLongValue()))
-                    .name(row.get("concept_name").getStringValue()))
+                    .name(row.get("concept_name").getStringValue())
+                    .hasChildren(true)
+                    .count(1))
         .toList();
   }
 }

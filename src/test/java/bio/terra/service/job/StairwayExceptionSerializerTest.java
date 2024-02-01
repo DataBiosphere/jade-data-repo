3b572f2f51076951e0eddcdc4ad4976e457b0116
Shelby Holden <sholden@broadinstitute.org>
2024-02-01 09:39:08 -0500

    Update to test both serialize and deserialize operations

--- src/test/java/bio/terra/service/job/StairwayExceptionSerializerTest.java
+++ src/test/java/bio/terra/service/job/StairwayExceptionSerializerTest.java
@@ -4,12 +4,14 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 import bio.terra.app.configuration.ApplicationConfiguration;
+import bio.terra.buffer.client.ApiException;
 import bio.terra.common.category.Unit;
 import bio.terra.service.resourcemanagement.exception.BufferServiceAPIException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 
 @ActiveProfiles({"google", "unittest"})
@@ -18,33 +20,45 @@ class StairwayExceptionSerializerTest {
 
   private StairwayExceptionSerializer stairwayExceptionSerializer;
 
+  private String serializedException;
+  private Exception originalException;
+
   @BeforeEach
   void setUp() {
     ObjectMapper objectMapper = new ApplicationConfiguration().objectMapper();
     stairwayExceptionSerializer = new StairwayExceptionSerializer(objectMapper);
+
+    originalException =
+        new BufferServiceAPIException(
+            new ApiException(
+                "No projects are available in the datarepo pool",
+                HttpStatus.NOT_FOUND.value(),
+                null,
+                null));
+    // Fake providing step info in stack trace
+    var fakeStackTrace =
+        new StackTraceElement(
+            "bio.terra.common.GetResourceBufferProjectStep",
+            "handoutResource",
+            "BufferService.java",
+            83);
+    originalException.setStackTrace(new StackTraceElement[] {fakeStackTrace});
+
+    serializedException = stairwayExceptionSerializer.serialize(originalException);
+  }
+
+  @Test
+  void serializeTest() {
+    assertThat(
+        serializedException,
+        equalTo(
+            """
+                {"className":"bio.terra.service.resourcemanagement.exception.BufferServiceAPIException","message":"Error from Buffer Service","errorDetails":[null],"dataRepoException":true}"""));
   }
 
   @Test
   void deserializeDataRepoException() {
-    var serializedException =
-        """
-        {"className":"bio.terra.service.resourcemanagement.exception.BufferServiceAPIException","message":"Error from Buffer Service",
-        "errorDetails":["The step failed for an unknown reason.","Please contact the TDR team for help."],"dataRepoException":true}
-        """;
     var deserializedException = stairwayExceptionSerializer.deserialize(serializedException);
     assertThat(deserializedException.getMessage(), equalTo("Error from Buffer Service"));
-  }
-
-  @Test
-  void deserializeErrorCode() {
-    var serializedException =
-        """
-        {"className":"bio.terra.service.resourcemanagement.exception.BufferServiceAPIException","message":"Error from Buffer Service",
-        "errorDetails":["The step failed for an unknown reason.","Please contact the TDR team for help."],"dataRepoException":true,"apiErrorReportException":true,"errorCode":"429"}
-        """;
-    BufferServiceAPIException deserializedException =
-        (BufferServiceAPIException) stairwayExceptionSerializer.deserialize(serializedException);
-    assertThat(deserializedException.getMessage(), equalTo("Error from Buffer Service"));
-    assertThat(deserializedException.getApiExceptionStatus(), equalTo(429));
   }
 }

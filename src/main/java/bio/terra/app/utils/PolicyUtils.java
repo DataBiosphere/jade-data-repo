2df2a252be99edea27c0d077125853975e3dbe10
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2024-02-29 21:45:37 -0500

    [WOR-1551] TDR cannot deserialize Rawls workspace responses (#1609)

--- src/main/java/bio/terra/app/utils/PolicyUtils.java
+++ src/main/java/bio/terra/app/utils/PolicyUtils.java
@@ -4,22 +4,20 @@ import bio.terra.model.PolicyModel;
 import bio.terra.model.ResourcePolicyModel;
 import bio.terra.model.SamPolicyModel;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class PolicyUtils {
 
   public static List<PolicyModel> samToTdrPolicyModels(List<SamPolicyModel> samPolicyModels) {
     return samPolicyModels.stream()
         .map(spm -> new PolicyModel().name(spm.getName()).members(spm.getMembers()))
-        .collect(Collectors.toList());
+        .toList();
   }
 
   public static List<PolicyModel> resourcePolicyToPolicyModel(
       List<ResourcePolicyModel> resourcePolicyModels) {
     return resourcePolicyModels.stream()
         .map(
-            rpm ->
-                new PolicyModel().name(rpm.getPolicyName()).members(List.of(rpm.getPolicyEmail())))
-        .collect(Collectors.toList());
+            rpm -> new PolicyModel().name(rpm.getPolicyName()).addMembersItem(rpm.getPolicyEmail()))
+        .toList();
   }
 }

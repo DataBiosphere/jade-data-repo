98903401d6e37f32ee865433699f5725eeb8e601
Evan Suslovich <esuslovi@broadinstitute.org>
2024-03-04 10:18:18 -0500

    spotless check

--- src/main/java/bio/terra/service/snapshotbuilder/utils/AggregateBQQueryResultsUtils.java
+++ src/main/java/bio/terra/service/snapshotbuilder/utils/AggregateBQQueryResultsUtils.java
@@ -2,7 +2,6 @@ package bio.terra.service.snapshotbuilder.utils;
 
 import bio.terra.model.SnapshotBuilderConcept;
 import com.google.cloud.bigquery.TableResult;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.stream.StreamSupport;
 
@@ -10,17 +9,19 @@ public class AggregateBQQueryResultsUtils {
   // TODO - pull real values for hasChildren and count
   public static List<SnapshotBuilderConcept> aggregateConceptResults(TableResult result) {
     return StreamSupport.stream(result.iterateAll().spliterator(), false)
-        .map(row -> {
-          int count;
-            count = ((int) row.get("count").getLongValue());
-            return new SnapshotBuilderConcept()
-              .id((int) (row.get("concept_id").getLongValue()))
-              .name(row.get("concept_name").getStringValue())
-              .hasChildren(true)
-              .count(count);
-        })
+        .map(
+            row -> {
+              int count;
+              count = ((int) row.get("count").getLongValue());
+              return new SnapshotBuilderConcept()
+                  .id((int) (row.get("concept_id").getLongValue()))
+                  .name(row.get("concept_name").getStringValue())
+                  .hasChildren(true)
+                  .count(count);
+            })
         .toList();
   }
+
   public static List<Integer> rollupCountsMapper(TableResult result) {
     return StreamSupport.stream(result.iterateAll().spliterator(), false)
         .map(row -> (int) row.get(0).getLongValue())

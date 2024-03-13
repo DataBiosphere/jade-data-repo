65c2c2992a647866d65440dba148f853f712a4c0
Shelby Holden <sholden@broadinstitute.org>
2024-03-13 10:27:16 -0400

    Update render configs to take an env argument and set azure env variables

--- render-configs.sh
+++ render-configs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# This script pulls the needed secrets from vault and sets the values as environment variables
+# If you want these values to show up in intellij test/run profiles, you need to restart intellij after running this script
+
+# If you want to run Azure Integration tests locally, you need to point to the "integration" environment
+ENV=${1:-dev}
 LOCAL_TOKEN=$(cat ~/.vault-token)
 VAULT_TOKEN=${1:-$LOCAL_TOKEN}
 
@@ -7,26 +12,34 @@ vault read -format=json secret/dsde/datarepo/dev/sa-key.json \
     | jq .data | tee /tmp/jade-dev-account.json \
     | jq -r .private_key > /tmp/jade-dev-account.pem
 
-vault read -field=tenant-id secret/dsde/datarepo/dev/azure-application-secrets \
-    > /tmp/jade-dev-tenant-id.key
+vault read -field=tenant-id secret/dsde/datarepo/${ENV}/azure-application-secrets \
+    > /tmp/jade-${ENV}-tenant-id.key
 
-vault read -field=client-id secret/dsde/datarepo/dev/azure-application-secrets \
-    > /tmp/jade-dev-client-id.key
+vault read -field=client-id secret/dsde/datarepo/${ENV}/azure-application-secrets \
+    > /tmp/jade-${ENV}-client-id.key
 
-vault read -field=client-secret secret/dsde/datarepo/dev/azure-application-secrets \
-    > /tmp/jade-dev-azure.key
+vault read -field=client-secret secret/dsde/datarepo/${ENV}/azure-application-secrets \
+    > /tmp/jade-${ENV}-azure.key
 
-vault read -field=synapse-sql-admin-user secret/dsde/datarepo/dev/azure-application-secrets \
-    > /tmp/jade-dev-synapse-admin-user.key
+vault read -field=synapse-sql-admin-user secret/dsde/datarepo/${ENV}/azure-application-secrets \
+    > /tmp/jade-${ENV}-synapse-admin-user.key
 
-vault read -field=synapse-sql-admin-password secret/dsde/datarepo/dev/azure-application-secrets \
-    > /tmp/jade-dev-synapse-admin-password.key
+vault read -field=synapse-sql-admin-password secret/dsde/datarepo/${ENV}/azure-application-secrets \
+    > /tmp/jade-${ENV}-synapse-admin-password.key
 
-vault read -field=synapse-encryption-key secret/dsde/datarepo/dev/azure-application-secrets \
-    > /tmp/jade-dev-synapse-encryption-key.key
+vault read -field=synapse-encryption-key secret/dsde/datarepo/${ENV}/azure-application-secrets \
+    > /tmp/jade-${ENV}-synapse-encryption-key.key
 
 BUFFER_CLIENT_SERVICE_ACCOUNT_VAULT_PATH=secret/dsde/terra/kernel/integration/tools/buffer/client-sa
 BUFFER_CLIENT_SERVICE_ACCOUNT_OUTPUT_PATH=/tmp/buffer-client-sa-account.json
 
 vault read -field=key ${BUFFER_CLIENT_SERVICE_ACCOUNT_VAULT_PATH} \
     | base64 -d > ${BUFFER_CLIENT_SERVICE_ACCOUNT_OUTPUT_PATH}
+
+export GOOGLE_APPLICATION_CREDENTIALS=/tmp/jade-dev-account.json
+export AZURE_CREDENTIALS_APPLICATIONID=$(cat /tmp/jade-${ENV}-client-id.key)
+export AZURE_CREDENTIALS_SECRET=$(cat /tmp/jade-${ENV}-azure.key)
+export AZURE_SYNAPSE_SQLADMINUSER=$(cat /tmp/jade-${ENV}-synapse-admin-user.key)
+export AZURE_SYNAPSE_SQLADMINPASSWORD=$(cat /tmp/jade-${ENV}-synapse-admin-password.key)
+export AZURE_CREDENTIALS_HOMETENANTID=$(cat /tmp/jade-${ENV}-tenant-id.key)
+export AZURE_SYNAPSE_ENCRYPTIONKEY=$(cat /tmp/jade-${ENV}-synapse-encryption-key.key)

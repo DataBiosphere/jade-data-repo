9234aa9f958c159d3846e3330a743f3491e5ead0
Evan Suslovich <esuslovi@broadinstitute.org>
2024-03-28 14:14:45 -0400

    add static method for always true method

--- src/main/java/bio/terra/service/snapshotbuilder/query/FilterVariable.java
+++ src/main/java/bio/terra/service/snapshotbuilder/query/FilterVariable.java
@@ -1,3 +1,7 @@
 package bio.terra.service.snapshotbuilder.query;
 
-public interface FilterVariable extends SqlExpression {}
+public interface FilterVariable extends SqlExpression {
+  static FilterVariable alwaysTrueFilter() {
+    return platform -> "1=1";
+  }
+}

db27ab1e9c3c318282c99e01635c956947c2e0e2
Phil Shapiro <pshapiro@broadinstitute.org>
2024-03-29 09:37:31 -0400

    add tests for new APIs, update tests to use newer APIs in a few places

--- src/test/java/bio/terra/service/snapshotbuilder/query/filtervariable/BinaryFilterVariableTest.java
+++ src/test/java/bio/terra/service/snapshotbuilder/query/filtervariable/BinaryFilterVariableTest.java
@@ -3,34 +3,47 @@ package bio.terra.service.snapshotbuilder.query.filtervariable;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-import bio.terra.common.CloudPlatformWrapper;
 import bio.terra.common.category.Unit;
-import bio.terra.model.CloudPlatform;
-import bio.terra.service.snapshotbuilder.query.FieldPointer;
 import bio.terra.service.snapshotbuilder.query.FieldVariable;
 import bio.terra.service.snapshotbuilder.query.Literal;
 import bio.terra.service.snapshotbuilder.query.QueryTestUtils;
 import bio.terra.service.snapshotbuilder.query.TableVariable;
 import java.util.List;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.EnumSource;
+import org.junit.jupiter.api.Test;
 
 @Tag(Unit.TAG)
 class BinaryFilterVariableTest {
 
-  @ParameterizedTest
-  @EnumSource(CloudPlatform.class)
-  void renderSQL(CloudPlatform platform) {
-    var binaryOperator = BinaryFilterVariable.BinaryOperator.EQUALS;
-    var literal = new Literal("foo");
+  private static FieldVariable createField() {
     TableVariable tableVariable = TableVariable.forPrimary(QueryTestUtils.fromTableName("table"));
     TableVariable.generateAliases(List.of(tableVariable));
-    var filterVariable =
+    return tableVariable.makeFieldVariable("column");
+  }
+
+  @Test
+  void renderSQL() {
+    var filter =
         new BinaryFilterVariable(
-            new FieldVariable(new FieldPointer(null, "column"), tableVariable),
-            binaryOperator,
-            literal);
-    assertThat(filterVariable.renderSQL(CloudPlatformWrapper.of(platform)), is("t.column = 'foo'"));
+            createField(), BinaryFilterVariable.BinaryOperator.LESS_THAN, new Literal(1234));
+    assertThat(filter.renderSQL(null), is("t.column < 1234"));
+  }
+
+  @Test
+  void equals() {
+    var filter = BinaryFilterVariable.equals(createField(), new Literal("foo"));
+    assertThat(filter.renderSQL(null), is("t.column = 'foo'"));
+  }
+
+  @Test
+  void notEquals() {
+    var filter = BinaryFilterVariable.notEquals(createField(), new Literal("foo"));
+    assertThat(filter.renderSQL(null), is("t.column != 'foo'"));
+  }
+
+  @Test
+  void notNull() {
+    var filter = BinaryFilterVariable.notNull(createField());
+    assertThat(filter.renderSQL(null), is("t.column IS NOT NULL"));
   }
 }

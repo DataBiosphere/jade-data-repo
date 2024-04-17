2e071b111abdbcf2a76f6f4c437136f4a4f9d7a7
Evan Suslovich <esuslovi@broadinstitute.org>
2024-04-17 11:22:17 -0400

    the code was deleted for some reason, adding backg

---
+++ src/main/java/bio/terra/service/snapshotbuilder/utils/constants/ConceptConstants.java
@@ -0,0 +1,10 @@
+package bio.terra.service.snapshotbuilder.utils.constants;
+
+public final class ConceptConstants {
+  public static final String CONCEPT = "concept";
+  public static final String DOMAIN_ID = "domain_id";
+  public static final String CONCEPT_NAME = "concept_name";
+  public static final String CONCEPT_ID = "concept_id";
+  public static final String STANDARD_CONCEPT = "standard_concept";
+  public static final String CONCEPT_CODE = "concept_code";
+}
---
+++ src/main/java/bio/terra/service/snapshotbuilder/utils/constants/ConceptRelationshipConstants.java
@@ -0,0 +1,8 @@
+package bio.terra.service.snapshotbuilder.utils.constants;
+
+public final class ConceptRelationshipConstants {
+  public static final String CONCEPT_RELATIONSHIP = "concept_relationship";
+  public static final String CONCEPT_ID_1 = "concept_id_1";
+  public static final String CONCEPT_ID_2 = "concept_id_2";
+  public static final String RELATIONSHIP_ID = "relationship_id";
+}

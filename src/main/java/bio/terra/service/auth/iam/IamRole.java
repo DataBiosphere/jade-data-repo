b9e1c15892ffb4b364353318fbcfc7d3d33ff30f
Sky Rubenstein <blrubenstein@wpi.edu>
2024-04-18 09:45:10 -0400

    [DC-830] Add new actions and roles (#1651)
    * Add new actions and roles
    
    * Reorder actions

--- src/main/java/bio/terra/service/auth/iam/IamRole.java
+++ src/main/java/bio/terra/service/auth/iam/IamRole.java
@@ -8,10 +8,13 @@ public enum IamRole {
   ADMIN,
   STEWARD,
   CUSTODIAN,
+  AGGREGATE_DATA_READER,
   READER,
   DISCOVERER,
+  SNAPSHOT_BUILDER_MANAGER,
   OWNER,
   USER,
+  APPROVER,
   SNAPSHOT_CREATOR,
   /** A role used by Firecloud Managed Groups */
   MEMBER;

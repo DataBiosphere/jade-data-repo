c1388d2371d3e9f86741c18c930c310b4418b4a5
Phil Shapiro <pshapiro@broadinstitute.org>
2024-04-19 11:28:15 -0400

    add comment

--- src/main/java/bio/terra/service/snapshotbuilder/query/TrueLiteral.java
+++ src/main/java/bio/terra/service/snapshotbuilder/query/TrueLiteral.java
@@ -4,6 +4,8 @@ public class TrueLiteral implements SqlExpression {
 
   @Override
   public String renderSQL(SqlRenderContext context) {
+    // In T-SQL, TRUE is not a keyword, so we need to use 1 instead. The JDBC API converts
+    // the 1 to a boolean TRUE when the query result is processed.
     return context.getPlatform().choose("true", "1");
   }
 }

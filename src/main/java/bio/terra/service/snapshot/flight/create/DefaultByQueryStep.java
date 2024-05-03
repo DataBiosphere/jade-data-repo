e8b1fabab8eb9bc7000d2ec96ea5043a56f5446c
rjohanek <rjohanek@broadinstitute.org>
2024-05-03 15:56:02 -0400

    get original request everytime (no converted request)

--- src/main/java/bio/terra/service/snapshot/flight/create/DefaultByQueryStep.java
+++ src/main/java/bio/terra/service/snapshot/flight/create/DefaultByQueryStep.java
@@ -10,14 +10,8 @@ import bio.terra.stairway.Step;
 public abstract class DefaultByQueryStep implements Step {
 
   SnapshotRequestModel getByQueryRequestModel(FlightContext context) {
-    FlightMap workingMap = context.getWorkingMap();
-    if (workingMap.containsKey(SnapshotWorkingMapKeys.BY_QUERY_SNAPSHOT_REQUEST_MODEL)) {
-      return workingMap.get(
-          SnapshotWorkingMapKeys.BY_QUERY_SNAPSHOT_REQUEST_MODEL, SnapshotRequestModel.class);
-    } else {
-      return context
+    return context
           .getInputParameters()
           .get(JobMapKeys.REQUEST.getKeyName(), SnapshotRequestModel.class);
-    }
   }
 }

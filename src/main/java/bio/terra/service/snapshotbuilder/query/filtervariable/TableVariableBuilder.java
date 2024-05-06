7066e2112d7ec8f5e358aa20882a191438804c98
Evan Suslovich <esuslovi@broadinstitute.org>
2024-05-06 00:07:23 -0400

    created a TableVariableBuilder function which acts as a builder

--- src/main/java/bio/terra/service/snapshotbuilder/query/filtervariable/TableVariableBuilder.java
+++ src/main/java/bio/terra/service/snapshotbuilder/query/filtervariable/TableVariableBuilder.java
@@ -1,2 +1,37 @@
-package bio.terra.service.snapshotbuilder.query.filtervariable;public class TableVariableBuilder {
+package bio.terra.service.snapshotbuilder.query.filtervariable;
+
+import bio.terra.service.snapshotbuilder.query.FieldVariable;
+
+public class TableVariableBuilder {
+  private String joinField;
+  private FieldVariable joinFieldOnParent;
+  private boolean isLeftJoin;
+
+  public TableVariableBuilder leftJoin(String joinField) {
+    this.isLeftJoin = true;
+    return this;
+  }
+
+  public TableVariableBuilder join(String joinField) {
+    this.isLeftJoin = false;
+    this.joinField = joinField;
+    return this;
+  }
+
+  public TableVariableBuilder on(FieldVariable joinFieldOnParent) {
+    this.joinFieldOnParent = joinFieldOnParent;
+    return this;
+  }
+
+  public String getJoinField() {
+    return joinField;
+  }
+
+  public FieldVariable getJoinFieldOnParent() {
+    return joinFieldOnParent;
+  }
+
+  public boolean isLeftJoin() {
+    return isLeftJoin;
+  }
 }

1edd2d009aaa6c4d7bc12febcc74e7fea87b4aea
Evan Suslovich <esuslovi@broadinstitute.org>
2024-05-23 15:12:25 -0400

    adding ConceptAncestor argument in leftJoinOnDescendantConceptto remove the need of instantiating another ConceptAncestor on each time this is called, add another getFieldVariable that creates alias counts

--- src/main/java/bio/terra/service/snapshotbuilder/query/tables/DomainOccurrence.java
+++ src/main/java/bio/terra/service/snapshotbuilder/query/tables/DomainOccurrence.java
@@ -1,25 +1,18 @@
 package bio.terra.service.snapshotbuilder.query.tables;
 
+import static bio.terra.service.snapshotbuilder.query.tables.Person.PERSON_ID;
+
 import bio.terra.model.SnapshotBuilderDomainOption;
-import bio.terra.service.snapshotbuilder.query.FieldPointer;
 import bio.terra.service.snapshotbuilder.query.FieldVariable;
 import bio.terra.service.snapshotbuilder.query.TablePointer;
 import bio.terra.service.snapshotbuilder.query.TableVariable;
-import bio.terra.service.snapshotbuilder.utils.constants.ConditionOccurrence;
-
-import static bio.terra.service.snapshotbuilder.query.tables.Person.PERSON_ID;
 
 public class DomainOccurrence extends Table {
 
   private SnapshotBuilderDomainOption domainOption;
-  private TablePointer tablePointer;
-
-  private TableVariable tableVariable;
 
   private DomainOccurrence(TablePointer tablePointer) {
     super(TableVariable.forPrimary(tablePointer));
-    this.tablePointer = tablePointer;
-    this.tableVariable = TableVariable.forPrimary(tablePointer);
   }
 
   public DomainOccurrence(SnapshotBuilderDomainOption domainOption) {
@@ -28,8 +21,11 @@ public class DomainOccurrence extends Table {
   }
 
   private DomainOccurrence(
-      TablePointer tablePointer, String joinField, FieldVariable joinFieldOnParent) {
-    super(TableVariable.forJoined(tablePointer, joinField, joinFieldOnParent));
+      TablePointer tablePointer,
+      String joinField,
+      FieldVariable joinFieldOnParent,
+      boolean leftJoin) {
+    super(TableVariable.forJoined(tablePointer, joinField, joinFieldOnParent, leftJoin));
   }
 
   public static DomainOccurrence asPrimary(TablePointer tablePointer) {
@@ -37,36 +33,23 @@ public class DomainOccurrence extends Table {
   }
 
   public static DomainOccurrence leftJoinOnDescendantConcept(
-      SnapshotBuilderDomainOption domainOption) {
+      SnapshotBuilderDomainOption domainOption, ConceptAncestor conceptAncestor) {
     return new DomainOccurrence(
         TablePointer.fromTableName(domainOption.getTableName()),
         domainOption.getColumnName(),
-        ConceptAncestor.asPrimary().descendant_concept_id());
-  }
-
-  public static DomainOccurrence joinOnPersonId(String tableName, FieldVariable joinFieldOnParent) {
-    return new DomainOccurrence(
-        TablePointer.fromTableName(tableName), PERSON_ID, joinFieldOnParent);
+        conceptAncestor.descendant_concept_id(),
+        true);
   }
 
   public FieldVariable getJoinColumn() {
     return getFieldVariable(this.domainOption.getColumnName());
   }
 
-  public FieldVariable getConditionConceptId() {
-    return getFieldVariable(ConditionOccurrence.CONDITION_CONCEPT_ID);
-  }
-
   public FieldVariable getPerson() {
     return getFieldVariable(PERSON_ID);
   }
 
-  public FieldVariable getCountPerson() {
-    return getFieldVariable(PERSON_ID);
-  }
-
   public FieldVariable countPersonId() {
-    return new FieldVariable(
-        new FieldPointer(tablePointer, PERSON_ID, "COUNT"), tableVariable, "count", true);
+    return getFieldVariable(PERSON_ID, "COUNT", "count", true);
   }
 }

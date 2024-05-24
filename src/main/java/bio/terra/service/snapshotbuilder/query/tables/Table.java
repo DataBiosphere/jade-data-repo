d2623b4d87e47f1ce4767d6d71d360eac1f1426e
Evan Suslovich <esuslovi@broadinstitute.org>
2024-05-24 15:32:19 -0400

    Table implements SqlExpression

--- src/main/java/bio/terra/service/snapshotbuilder/query/tables/Table.java
+++ src/main/java/bio/terra/service/snapshotbuilder/query/tables/Table.java
@@ -1,12 +1,13 @@
 package bio.terra.service.snapshotbuilder.query.tables;
 
 import bio.terra.service.snapshotbuilder.query.FieldVariable;
+import bio.terra.service.snapshotbuilder.query.SqlExpression;
 import bio.terra.service.snapshotbuilder.query.SqlRenderContext;
 import bio.terra.service.snapshotbuilder.query.TableVariable;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Table {
+public class Table implements SqlExpression {
   private final Map<String, FieldVariable> fields = new HashMap<>();
   private final TableVariable tableVariable;
 
@@ -22,6 +23,7 @@ public class Table {
     return this.tableVariable.isPrimary();
   }
 
+  @Override
   public String renderSQL(SqlRenderContext context) {
     return this.tableVariable.renderSQL(context);
   }

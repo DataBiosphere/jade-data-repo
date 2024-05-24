ab2bd3400feb63d67e9064c2a13bd7b7699702f3
Evan Suslovich <esuslovi@broadinstitute.org>
2024-05-24 13:08:56 -0400

    add private constructor

--- src/main/java/bio/terra/service/snapshotbuilder/utils/constants/ConditionOccurrence.java
+++ src/main/java/bio/terra/service/snapshotbuilder/utils/constants/ConditionOccurrence.java
@@ -2,6 +2,8 @@ package bio.terra.service.snapshotbuilder.utils.constants;
 
 public class ConditionOccurrence {
 
+  private ConditionOccurrence() {}
+
   public static final String TABLE_NAME = "condition_occurrence";
   public static final String CONDITION_CONCEPT_ID = "condition_concept_id";
   public static final String PERSON_ID = "person_id";

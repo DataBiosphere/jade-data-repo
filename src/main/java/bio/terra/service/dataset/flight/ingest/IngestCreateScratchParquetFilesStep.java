f4a52195a2e5db9c1ad5c74d9bdf76f86169a760
Florian Boulnois <fboulnoi@broadinstitute.org>
2024-06-28 14:03:02 -0400

    refactor: rename scratchParquetFile to parquetFilePath

--- src/main/java/bio/terra/service/dataset/flight/ingest/IngestCreateScratchParquetFilesStep.java
+++ src/main/java/bio/terra/service/dataset/flight/ingest/IngestCreateScratchParquetFilesStep.java
@@ -82,9 +82,8 @@ public class IngestCreateScratchParquetFilesStep implements Step {
     AzureStorageAccountResource storageAccountResource =
         workingMap.get(
             CommonMapKeys.DATASET_STORAGE_ACCOUNT_RESOURCE, AzureStorageAccountResource.class);
-    String scratchParquetFile = workingMap.get(IngestMapKeys.PARQUET_FILE_PATH, String.class);
-    azureBlobStorePdao.deleteScratchParquet(
-        scratchParquetFile, storageAccountResource, userRequest);
+    String parquetFilePath = workingMap.get(IngestMapKeys.PARQUET_FILE_PATH, String.class);
+    azureBlobStorePdao.deleteScratchParquet(parquetFilePath, storageAccountResource, userRequest);
 
     return StepResult.getStepResultSuccess();
   }

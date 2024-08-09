d578e7bada5105acaeda9145cfa719c4f961f2cd
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2024-08-09 15:01:15 -0400

    [DCJ-511] azureTableThreadpool blocks when saturated (#1750)
    * [DCJ-511] Introduce BlockingRejectedExecutionHandler
    
    This can be supplied to ThreadPoolExecutors to define behavior when a task can't be accepted.
    
    If the denial is due to saturation of the threadpool and queue, this handler blocks when waiting for a spot in the queue.
    
    The present default behavior is that the task is rejected via a thrown RejectedExecutionException.
    
    * azureTableThreadpool blocks when saturated
    
    This is dictated by property azure.threading.blockWhenSaturated.
    
    * Add unit tests for BlockingRejectedExecutionHandler
    
    * Driveby - store azureTableThreadpool name in static variable
    
    This way, we can easily see in our IDE all the places where this Spring Bean is being pulled into another component.
    
    We no longer have to redefine the same string throughout code and tests.
    
    * Remove Stringified Runnable from error messages
    
    They are unlikely to provide useful information to developers or users.
    
    From @pshapiro4broad PR feedback --
    
    >Does it make sense to use Runnable.toString() here? By default this is the same as Object.toString() (which would print the object's type and memory address), but maybe the Runnables used in TDR will print something more useful.

--- src/test/java/bio/terra/app/configuration/AzureResourceConfigurationTest.java
+++ src/test/java/bio/terra/app/configuration/AzureResourceConfigurationTest.java
@@ -4,11 +4,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.instanceOf;
 
+import bio.terra.common.BlockingRejectedExecutionHandler;
 import bio.terra.common.category.Unit;
 import bio.terra.service.resourcemanagement.azure.AzureResourceConfiguration;
 import bio.terra.service.resourcemanagement.azure.AzureResourceConfiguration.Threading;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
 import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
@@ -17,9 +21,10 @@ public class AzureResourceConfigurationTest {
   private static final int NUM_TABLE_THREADS = 3;
   private static final int MAX_QUEUE_SIZE = 5;
 
-  @Test
-  void azureTableThreadPool() {
-    Threading threading = new Threading(NUM_TABLE_THREADS, MAX_QUEUE_SIZE);
+  @ParameterizedTest
+  @ValueSource(booleans = {true, false})
+  void azureTableThreadPool(boolean blockWhenSaturated) {
+    Threading threading = new Threading(NUM_TABLE_THREADS, MAX_QUEUE_SIZE, blockWhenSaturated);
     AzureResourceConfiguration config =
         new AzureResourceConfiguration(null, null, 0, 0, null, null, threading);
     AsyncTaskExecutor genericExecutor = config.azureTableThreadpool();
@@ -29,5 +34,12 @@ public class AzureResourceConfigurationTest {
     assertThat(azureTableThreadPool.getMaxPoolSize(), equalTo(NUM_TABLE_THREADS));
     assertThat(azureTableThreadPool.getKeepAliveSeconds(), equalTo(0));
     assertThat(azureTableThreadPool.getQueueCapacity(), equalTo(MAX_QUEUE_SIZE));
+    RejectedExecutionHandler rejectedExecutionHandler =
+        azureTableThreadPool.getThreadPoolExecutor().getRejectedExecutionHandler();
+    if (blockWhenSaturated) {
+      assertThat(rejectedExecutionHandler, instanceOf(BlockingRejectedExecutionHandler.class));
+    } else {
+      assertThat(rejectedExecutionHandler, instanceOf(ThreadPoolExecutor.AbortPolicy.class));
+    }
   }
 }

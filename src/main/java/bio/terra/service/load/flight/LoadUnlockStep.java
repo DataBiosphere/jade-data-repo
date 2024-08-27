7a68e925bb88408a7d76dcb942d49675bad9c9d7
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2024-08-27 15:40:52 -0400

    LoadUnlockStep accepts user-supplied lockName
    The default behavior when unspecified is to unlock the current flight's ID (when clearing a lock taken out earlier in the flight).
    In the case of manual dataset unlocking, we want to clear a lock taken out by a past flight.
    Manual dataset unlocking will not have the load tag to unlock in its flight context, so unlocks any load tag associated with the dataset ID and lock name.

--- src/main/java/bio/terra/service/load/flight/LoadUnlockStep.java
+++ src/main/java/bio/terra/service/load/flight/LoadUnlockStep.java
@@ -1,33 +1,72 @@
 package bio.terra.service.load.flight;
 
+import bio.terra.service.dataset.flight.ingest.IngestUtils;
 import bio.terra.service.job.DefaultUndoStep;
 import bio.terra.service.job.JobMapKeys;
 import bio.terra.service.load.LoadLockKey;
 import bio.terra.service.load.LoadService;
+import bio.terra.service.load.exception.LoadLockFailureException;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.StepResult;
+import java.util.Optional;
 
 public class LoadUnlockStep extends DefaultUndoStep {
   private final LoadService loadService;
+  private final String userSuppliedLockName;
 
   /**
-   * This step is meant to be shared by dataset and filesystem flights for unlocking the load tag.
+   * This step is meant to be shared by dataset and filesystem flights for removing {@code
+   * lockName}'s lock held on the load tag in the targeted dataset.
    *
    * <p>It expects the following to be available in the flight context:
    *
    * <ul>
-   *   <li>{@link LoadMapKeys#LOAD_TAG} in the input parameters or working map
+   *   <li>(Optional) {@link LoadMapKeys#LOAD_TAG} in the input parameters or working map (if
+   *       unavailable, any lock associated with the dataset and {@code lockName} will be cleared)
+   *   <li>{@link JobMapKeys#DATASET_ID} in the input parameters
+   * </ul>
+   *
+   * @param lockName the name of the lock to remove (if unspecified, defaults to this flight's ID)
+   */
+  public LoadUnlockStep(LoadService loadService, String lockName) {
+    this.loadService = loadService;
+    this.userSuppliedLockName = lockName;
+  }
+
+  /**
+   * This step is meant to be shared by dataset and filesystem flights for removing this flight's
+   * lock held on the load tag in the targeted dataset.
+   *
+   * <p>It expects the following to be available in the flight context:
+   *
+   * <ul>
+   *   <li>(Optional) {@link LoadMapKeys#LOAD_TAG} in the input parameters or working map (if
+   *       unavailable, any lock associated with the dataset and this flight's ID will be cleared)
    *   <li>{@link JobMapKeys#DATASET_ID} in the input parameters
    * </ul>
    */
   public LoadUnlockStep(LoadService loadService) {
-    this.loadService = loadService;
+    this(loadService, null);
   }
 
   @Override
   public StepResult doStep(FlightContext context) {
-    LoadLockKey loadLockKey = loadService.getLoadLockKey(context);
-    loadService.unlockLoad(loadLockKey, context.getFlightId());
+    String lockName = getLockName(context);
+    try {
+      LoadLockKey loadLockKey = loadService.getLoadLockKey(context);
+      loadService.unlockLoad(loadLockKey, lockName);
+    } catch (LoadLockFailureException ex) {
+      // The flight context may not have a load tag.  We'll clear any lock associated with the
+      // flight's dataset and lock name in that case.
+      loadService.unlockLoad(IngestUtils.getDatasetId(context), lockName);
+    }
     return StepResult.getStepResultSuccess();
+  }
+
+  /**
+   * @return the name of the lock to remove (if unspecified, defaults to this flight's ID)
+   */
+  private String getLockName(FlightContext context) {
+    return Optional.ofNullable(userSuppliedLockName).orElse(context.getFlightId());
   }
 }
--- src/test/java/bio/terra/service/load/flight/LoadUnlockStepTest.java
+++ src/test/java/bio/terra/service/load/flight/LoadUnlockStepTest.java
@@ -2,16 +2,19 @@ package bio.terra.service.load.flight;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import bio.terra.common.category.Unit;
+import bio.terra.service.job.JobMapKeys;
 import bio.terra.service.load.LoadLockKey;
 import bio.terra.service.load.LoadService;
+import bio.terra.service.load.exception.LoadLockFailureException;
 import bio.terra.stairway.FlightContext;
+import bio.terra.stairway.FlightMap;
 import bio.terra.stairway.StepResult;
 import java.util.UUID;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -26,20 +29,53 @@ class LoadUnlockStepTest {
   private LoadUnlockStep step;
 
   private static final String FLIGHT_ID = "flightId";
-  private static final LoadLockKey LOAD_LOCK_KEY = new LoadLockKey("loadTag", UUID.randomUUID());
+  private static final String LOCK_NAME = "suppliedLockName";
+  private static final UUID DATASET_ID = UUID.randomUUID();
+  private static final LoadLockKey LOAD_LOCK_KEY = new LoadLockKey("loadTag", DATASET_ID);
 
-  @BeforeEach
-  void setup() {
+  private void mockFlightId() {
     when(flightContext.getFlightId()).thenReturn(FLIGHT_ID);
-    when(loadService.getLoadLockKey(flightContext)).thenReturn(LOAD_LOCK_KEY);
+  }
 
-    step = new LoadUnlockStep(loadService);
+  private void mockDatasetId() {
+    FlightMap inputParameters = new FlightMap();
+    inputParameters.put(JobMapKeys.DATASET_ID.getKeyName(), DATASET_ID);
+    when(flightContext.getInputParameters()).thenReturn(inputParameters);
   }
 
   @Test
-  void doStep() {
+  void doStep_flightId() {
+    mockFlightId();
+    when(loadService.getLoadLockKey(flightContext)).thenReturn(LOAD_LOCK_KEY);
+    step = new LoadUnlockStep(loadService);
+
     assertThat(step.doStep(flightContext), equalTo(StepResult.getStepResultSuccess()));
 
+    // When a lockName isn't supplied to step construction, the active flight ID is passed to our
+    // unlock load call.
     verify(loadService).unlockLoad(LOAD_LOCK_KEY, FLIGHT_ID);
+  }
+
+  @Test
+  void doStep_lockName() {
+    when(loadService.getLoadLockKey(flightContext)).thenReturn(LOAD_LOCK_KEY);
+    step = new LoadUnlockStep(loadService, LOCK_NAME);
+
+    assertThat(step.doStep(flightContext), equalTo(StepResult.getStepResultSuccess()));
+
+    // When a lockName is supplied to step construction, it's passed to our unlock load call.
+    verify(loadService).unlockLoad(LOAD_LOCK_KEY, LOCK_NAME);
+  }
+
+  @Test
+  void doStep_unlockLoadByDataset() {
+    mockDatasetId();
+    doThrow(LoadLockFailureException.class).when(loadService).getLoadLockKey(flightContext);
+    step = new LoadUnlockStep(loadService, LOCK_NAME);
+
+    assertThat(step.doStep(flightContext), equalTo(StepResult.getStepResultSuccess()));
+
+    // When a loadTag to unlock isn't available in the flight context, we unlock by dataset ID only.
+    verify(loadService).unlockLoad(DATASET_ID, LOCK_NAME);
   }
 }

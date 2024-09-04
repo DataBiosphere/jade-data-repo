6805d55a23dfdc51f90c05f50fda916b4c3cf563
Olivia Kotsopoulos <okotsopo@broadinstitute.org>
2024-09-04 11:07:36 -0400

    [DCJ-650] Expand ResourceLockConflict error messages
    Emitted when an attempt to manually unlock a resource (dataset, snapshot) fails because the user-supplied lock to remove doesn't exist on the resource.

--- src/test/java/bio/terra/service/snapshot/flight/unlock/UnlockSnapshotCheckLockNameStepTest.java
+++ src/test/java/bio/terra/service/snapshot/flight/unlock/UnlockSnapshotCheckLockNameStepTest.java
@@ -1,21 +1,29 @@
 package bio.terra.service.snapshot.flight.unlock;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.mockito.Mockito.when;
 
 import bio.terra.common.category.Unit;
 import bio.terra.model.ResourceLocks;
 import bio.terra.model.SnapshotSummaryModel;
+import bio.terra.service.common.ResourceLockConflict;
+import bio.terra.service.dataset.flight.DatasetWorkingMapKeys;
 import bio.terra.service.snapshot.SnapshotService;
 import bio.terra.stairway.FlightContext;
 import bio.terra.stairway.FlightMap;
+import bio.terra.stairway.StepResult;
 import bio.terra.stairway.StepStatus;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -27,53 +35,66 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @Tag(Unit.TAG)
 class UnlockSnapshotCheckLockNameStepTest {
   private static final UUID SNAPSHOT_ID = UUID.randomUUID();
+  private static final String LOCK_NAME = "lock-name";
+  private static final String OTHER_LOCK_NAME = "other-lock-name";
+
+  private FlightMap workingMap;
   private UnlockSnapshotCheckLockNameStep step;
+  private SnapshotSummaryModel snapshotSummaryModel;
   @Mock private FlightContext flightContext;
   @Mock private SnapshotService snapshotService;
 
-  @ParameterizedTest
-  @MethodSource
-  void doStep(
-      SnapshotSummaryModel snapshotSummaryModel,
-      String requestedLockName,
-      StepStatus expectedStatus,
-      String exceptionMessage)
-      throws InterruptedException {
-    // Setup
-    step = new UnlockSnapshotCheckLockNameStep(snapshotService, SNAPSHOT_ID, requestedLockName);
-    when(snapshotService.retrieveSnapshotSummary(SNAPSHOT_ID)).thenReturn(snapshotSummaryModel);
-    FlightMap workingMap = new FlightMap();
+  @BeforeEach
+  void beforeEach() {
+    workingMap = new FlightMap();
     when(flightContext.getWorkingMap()).thenReturn(workingMap);
 
-    // Perform Step
-    var stepResult = step.doStep(flightContext);
+    step = new UnlockSnapshotCheckLockNameStep(snapshotService, SNAPSHOT_ID, LOCK_NAME);
 
-    // Confirm response is correctly returned
-    assertThat(stepResult.getStepStatus(), equalTo(expectedStatus));
-    if (exceptionMessage != null) {
-      assertThat(
-          "Expected exception",
-          stepResult.getException().get().getMessage(),
-          containsString(exceptionMessage));
-    }
+    snapshotSummaryModel = new SnapshotSummaryModel();
+    when(snapshotService.retrieveSnapshotSummary(SNAPSHOT_ID)).thenReturn(snapshotSummaryModel);
   }
 
-  private static Stream<Arguments> doStep() {
+  @Test
+  void doStep_success() throws InterruptedException {
+    snapshotSummaryModel.resourceLocks(new ResourceLocks().exclusive(LOCK_NAME));
+
+    assertThat(step.doStep(flightContext), equalTo(StepResult.getStepResultSuccess()));
+    assertThat(workingMap.get(DatasetWorkingMapKeys.IS_SHARED_LOCK, Boolean.class), equalTo(false));
+  }
+
+  private static Stream<Arguments> doStep_failure() {
+    String noLocksMessage = "Datasnapshot %s is not locked.".formatted(SNAPSHOT_ID);
+
+    String noMatchingLockMessage =
+        """
+            Datasnapshot %s has no lock named '%s'.
+            Do you mean to remove one of these existing locks instead?
+            """
+            .formatted(SNAPSHOT_ID, LOCK_NAME);
+
     return Stream.of(
+        arguments(new ResourceLocks(), new ResourceLockConflict(noLocksMessage)),
         arguments(
-            new SnapshotSummaryModel().resourceLocks(new ResourceLocks()),
-            "lockName",
-            StepStatus.STEP_RESULT_FAILURE_FATAL,
-            "Resource is not locked."),
-        arguments(
-            new SnapshotSummaryModel().resourceLocks(new ResourceLocks().exclusive("LockName")),
-            "LockName",
-            StepStatus.STEP_RESULT_SUCCESS,
-            null),
-        arguments(
-            new SnapshotSummaryModel().resourceLocks(new ResourceLocks().exclusive("LockName")),
-            "OtherLockName",
-            StepStatus.STEP_RESULT_FAILURE_FATAL,
-            "Resource not locked by OtherLockName. It is locked by flight(s) LockName."));
+            new ResourceLocks().exclusive(OTHER_LOCK_NAME),
+            new ResourceLockConflict(noMatchingLockMessage, List.of(OTHER_LOCK_NAME))));
+  }
+
+  @ParameterizedTest
+  @MethodSource
+  void doStep_failure(ResourceLocks existingResourceLocks, ResourceLockConflict expectedException)
+      throws InterruptedException {
+    snapshotSummaryModel.resourceLocks(existingResourceLocks);
+
+    StepResult stepResult = step.doStep(flightContext);
+    assertThat(stepResult.getStepStatus(), equalTo(StepStatus.STEP_RESULT_FAILURE_FATAL));
+
+    Optional<Exception> maybeException = stepResult.getException();
+    assertTrue(maybeException.isPresent());
+    Exception exception = maybeException.get();
+    assertThat(exception, instanceOf(ResourceLockConflict.class));
+    ResourceLockConflict resourceLockConflict = (ResourceLockConflict) exception;
+    assertThat(resourceLockConflict.getMessage(), equalTo(expectedException.getMessage()));
+    assertThat(resourceLockConflict.getCauses(), equalTo(expectedException.getCauses()));
   }
 }

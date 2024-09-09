38b11bf531a774ae9d88a7b7d0b599d1b7c08631
Sky Rubenstein <blrubenstein@wpi.edu>
2024-09-09 14:55:16 -0400

    Shellcheck

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # set up logging utilities
-source $(dirname $0)/utils.sh
+source "$(dirname "$0")"/utils.sh
 
 # shared variables across all scripts
-ROOT_DIR="$(cd "$(dirname "$0")/.."; pwd)"
+ROOT_DIR="$(cd "$(dirname "$0")"/.. || exit; pwd)"
 SCRIPTS_DIR=${SCRIPTS_DIR:-${ROOT_DIR}/scripts}
 TMP_DIR=${TMP_DIR:-${ROOT_DIR}/build/tmp}
 

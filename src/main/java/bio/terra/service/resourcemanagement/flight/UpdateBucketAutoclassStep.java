97881df8bce0cad5c43731103021ee6699ac5039
Florian Boulnois <fboulnoi@broadinstitute.org>
2024-09-11 11:40:25 -0400

    feat: add step to set autoclass settings on a bucket

---
+++ src/main/java/bio/terra/service/resourcemanagement/flight/UpdateBucketAutoclassStep.java
@@ -0,0 +1,43 @@
+package bio.terra.service.resourcemanagement.flight;
+
+import bio.terra.service.filedata.flight.FileMapKeys;
+import bio.terra.service.resourcemanagement.google.GoogleBucketResource;
+import bio.terra.service.resourcemanagement.google.GoogleBucketService;
+import bio.terra.stairway.FlightContext;
+import bio.terra.stairway.FlightMap;
+import bio.terra.stairway.Step;
+import bio.terra.stairway.StepResult;
+
+public class UpdateBucketAutoclassStep implements Step {
+
+  private final GoogleBucketService googleBucketService;
+
+  public UpdateBucketAutoclassStep(GoogleBucketService googleBucketService) {
+    this.googleBucketService = googleBucketService;
+  }
+
+  @Override
+  public StepResult doStep(FlightContext context) throws InterruptedException {
+    FlightMap workingMap = context.getWorkingMap();
+    GoogleBucketResource bucketResource =
+        workingMap.get(FileMapKeys.BUCKET_INFO, GoogleBucketResource.class);
+
+    googleBucketService.setBucketAutoclassToArchive(bucketResource);
+
+    return StepResult.getStepResultSuccess();
+  }
+
+  @Override
+  public StepResult undoStep(FlightContext context) throws InterruptedException {
+    FlightMap workingMap = context.getWorkingMap();
+    GoogleBucketResource bucketResource =
+        workingMap.get(FileMapKeys.BUCKET_INFO, GoogleBucketResource.class);
+
+    googleBucketService.setBucketAutoclass(
+        bucketResource,
+        bucketResource.getAutoclassEnabled(),
+        bucketResource.getTerminalStorageClass());
+
+    return StepResult.getStepResultSuccess();
+  }
+}

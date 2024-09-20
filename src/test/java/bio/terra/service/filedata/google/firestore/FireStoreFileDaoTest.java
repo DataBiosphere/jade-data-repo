855d4da8752d8864b62d9a985cb3254a09c6e50e
Shelby Holden <sholden@broadinstitute.org>
2024-09-20 11:48:32 -0400

    Instead use beforeAll and afterAll in order to break down into smaller bite-sized tests

--- src/test/java/bio/terra/service/filedata/google/firestore/FireStoreFileDaoTest.java
+++ src/test/java/bio/terra/service/filedata/google/firestore/FireStoreFileDaoTest.java
@@ -24,10 +24,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.IntStream;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -39,6 +42,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @ActiveProfiles({"google", "connectedtest"})
 @Tag(Connected.TAG)
 @EmbeddedDatabaseTest
@@ -57,7 +61,7 @@ class FireStoreFileDaoTest {
   private List<FireStoreFile> files;
   private List<String> objects;
 
-  @BeforeEach
+  @BeforeAll
   public void setup() throws Exception {
     connectedOperations.stubOutSamCalls(samService);
 
@@ -68,6 +72,19 @@ class FireStoreFileDaoTest {
     datasetId = summaryModel.getId().toString();
 
     firestore = TestFirestoreProvider.getFirestore(summaryModel.getDataProject());
+  }
+
+  @BeforeEach
+  public void seedDirectoryEntries() throws InterruptedException {
+    // Make sure there aren't any lingering entries from other tests - directory is empty
+    fileDao.deleteFilesFromDataset(firestore, datasetId, fsf -> {});
+    assertThat(
+        "Cleanup from previous test successful",
+        fileDao.enumerateAllWithEmptyField(firestore, datasetId, "fileId"),
+        hasSize(0));
+    directoryEntries = null;
+    files = null;
+    objects = null;
 
     // seed firestore with a collection of directory and file entries
     directoryEntries =
@@ -86,9 +103,92 @@ class FireStoreFileDaoTest {
   }
 
   @AfterEach
+  public void cleanupAfterEachTest() throws Exception {
+    if (datasetId != null) {
+      fileDao.deleteFilesFromDataset(firestore, datasetId, fsf -> {});
+    }
+  }
+
+  @AfterAll
   public void cleanup() throws Exception {
-    // delete remaining files from firestore, or else database delete will fail
-    // and confirm delete all works as expected
+    connectedOperations.stubOutSamCalls(samService);
+    connectedOperations.teardown();
+  }
+
+  @Test
+  void testRetrieveFileMetadata() throws Exception {
+    for (String obj : objects) {
+      assertThat(
+          "All files were added to firestore and that the correct file size is returned",
+          fileDao.retrieveFileMetadata(firestore, datasetId, obj).getSize(),
+          equalTo(FILE_SIZE));
+    }
+
+    // batch retrieve
+    List<FireStoreFile> retrievedFiles =
+        fileDao.batchRetrieveFileMetadata(firestore, datasetId, directoryEntries);
+    assertEquals(files, retrievedFiles);
+  }
+
+  @Test
+  void testRetrieveNonExistentFileMetadata() throws Exception {
+    FireStoreDirectoryEntry directoryEntry =
+        new FireStoreDirectoryEntry().fileId(UUID.randomUUID().toString());
+    assertThrows(
+        FileSystemCorruptException.class,
+        () -> fileDao.batchRetrieveFileMetadata(firestore, datasetId, List.of(directoryEntry)));
+  }
+
+  @Test
+  void testUpdateFileMetadata() throws Exception {
+    // update file size field
+    var file0 = files.get(0).size(CHANGED_FILE_SIZE);
+    fileDao.upsertFileMetadata(firestore, datasetId, file0);
+    assertThat(
+        "Returned updated file size for file0",
+        fileDao.retrieveFileMetadata(firestore, datasetId, file0.getFileId()).getSize(),
+        equalTo(CHANGED_FILE_SIZE));
+  }
+
+  @Test
+  void testEnumerateAllWithEmptyField() throws Exception {
+    var file1 = files.get(1).checksumMd5("md5");
+    fileDao.upsertFileMetadata(firestore, datasetId, file1);
+    List<FireStoreFile> filesWithNullMd5Field =
+        fileDao.enumerateAllWithEmptyField(firestore, datasetId, "checksumMd5");
+    assertThat(
+        "All but one file has empty field", filesWithNullMd5Field, hasSize(files.size() - 1));
+  }
+
+  @Test
+  void testConflictingLoadTags() throws Exception {
+    var file2 = files.get(2); // By default it should have loadTag = "loadTag"
+    fileDao.upsertFileMetadata(firestore, datasetId, file2);
+    Throwable cause =
+        assertThrows(
+                "upsert fails with load tag conflict (and try the list uploader)",
+                FileSystemExecutionException.class,
+                () ->
+                    fileDao.upsertFileMetadata(firestore, datasetId, List.of(file2.loadTag("lt2"))))
+            .getCause();
+    assertThat(
+        "Correct cause triggered the error",
+        cause.getCause(),
+        isA(FileAlreadyExistsException.class));
+  }
+
+  @Test
+  void testDeleteSingleFileMetadata() throws Exception {
+    var file0 = files.get(0);
+    assertThat("File existed", fileDao.deleteFileMetadata(firestore, datasetId, file0.getFileId()));
+    assertThat(
+        "File entry was deleted",
+        fileDao.retrieveFileMetadata(firestore, datasetId, file0.getFileId()),
+        is(nullValue()));
+  }
+
+  @Test
+  void testDeleteAllFileMetadataEntries() throws InterruptedException {
     List<String> deleteIds = new ArrayList<>();
     fileDao.deleteFilesFromDataset(
         firestore,
@@ -106,73 +206,6 @@ class FireStoreFileDaoTest {
       FireStoreFile existCheck = fileDao.retrieveFileMetadata(firestore, datasetId, fileId);
       assertThat("File entry was deleted", existCheck, is(nullValue()));
     }
-
-    connectedOperations.teardown();
-  }
-
-  @Test
-  void createDeleteFileTest() throws Exception {
-    // ====== test retrieving file metadata firestore entries ======
-    for (String obj : objects) {
-      assertThat(
-          "All files were added to firestore and that the correct file size is returned",
-          fileDao.retrieveFileMetadata(firestore, datasetId, obj).getSize(),
-          equalTo(FILE_SIZE));
-    }
-
-    // batch retrieve
-    List<FireStoreFile> retrievedFiles =
-        fileDao.batchRetrieveFileMetadata(firestore, datasetId, directoryEntries);
-    assertEquals(files, retrievedFiles);
-
-    // can't retrieve non-existent file
-    FireStoreDirectoryEntry directoryEntry =
-        new FireStoreDirectoryEntry().fileId(UUID.randomUUID().toString());
-    assertThrows(
-        FileSystemCorruptException.class,
-        () -> fileDao.batchRetrieveFileMetadata(firestore, datasetId, List.of(directoryEntry)));
-
-    // ====== test updating file metadata firestore entries ======
-    // update file size field
-    var file0 = files.get(0).size(CHANGED_FILE_SIZE);
-    fileDao.upsertFileMetadata(firestore, datasetId, file0);
-    assertThat(
-        "Returned updated file size for file0",
-        fileDao.retrieveFileMetadata(firestore, datasetId, file0.getFileId()).getSize(),
-        equalTo(CHANGED_FILE_SIZE));
-
-    // test enumerateAllWithEmptyField
-    var file1 = files.get(1).checksumMd5("md5");
-    fileDao.upsertFileMetadata(firestore, datasetId, file1);
-    List<FireStoreFile> filesWithNullMd5Field =
-        fileDao.enumerateAllWithEmptyField(firestore, datasetId, "checksumMd5");
-    assertThat(
-        "All but one file has empty field", filesWithNullMd5Field, hasSize(files.size() - 1));
-
-    // conflicting load tags
-    var file2 = files.get(2); // By default it should have loadTag = "loadTag"
-    fileDao.upsertFileMetadata(firestore, datasetId, file2);
-    Throwable cause =
-        assertThrows(
-                "upsert fails with load tag conflict (and try the list uploader)",
-                FileSystemExecutionException.class,
-                () ->
-                    fileDao.upsertFileMetadata(firestore, datasetId, List.of(file2.loadTag("lt2"))))
-            .getCause();
-    assertThat(
-        "Correct cause triggered the error",
-        cause.getCause(),
-        isA(FileAlreadyExistsException.class));
-
-    // ====== test deleting file metadata firestore entries ======
-    // delete single file
-    assertThat("File existed", fileDao.deleteFileMetadata(firestore, datasetId, file0.getFileId()));
-    assertThat(
-        "File entry was deleted",
-        fileDao.retrieveFileMetadata(firestore, datasetId, file0.getFileId()),
-        is(nullValue()));
-    // remove first file from list so that we can test batch delete in the cleanup method
-    objects = objects.stream().filter(obj -> !obj.equals(file0.getFileId())).toList();
   }
 
   private FireStoreFile makeFile(String fileId) {

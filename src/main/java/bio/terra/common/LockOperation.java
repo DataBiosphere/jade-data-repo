f0c6da1beecb81ecb671e09d9e4f9c9d49db6112
rjohanek <rjohanek@broadinstitute.org>
2024-10-10 09:51:49 -0400

    update error messaging for locking snapshots/datasets to include the lock name

--- src/main/java/bio/terra/common/LockOperation.java
+++ src/main/java/bio/terra/common/LockOperation.java
@@ -1,5 +1,6 @@
 package bio.terra.common;
 
+import bio.terra.model.ResourceLocks;
 import java.util.List;
 
 /** Actions which could be taken to modify the locks held on a resource (dataset, snapshot). */
@@ -27,13 +28,36 @@ public enum LockOperation {
    * @return additional guidance for users investigating a failed lock operation, usually as part of
    *     a failed job / flight.
    */
-  public List<String> getErrorDetails() {
+  public List<String> getErrorDetails(ResourceLocks conflictingLocks) {
     var template =
         "A failure to obtain %s on a resource likely means that it's already %s by another process.";
     if (description != null && conflict != null) {
+      if (conflictingLocks != null && !isEmpty(conflictingLocks)) {
+        template =
+            template
+                + " Conflicting lock(s): %s. You can remove a lock by using the unlock API endpoint with the ID of the resource (Snapshot or Dataset), the name of the lock, and forceUnlock set to false.";
+        if (this == LOCK_EXCLUSIVE) {
+          // include any locks that exist both exclusive and shared (but only one will be populated)
+          if (isExclusive(conflictingLocks)) {
+            return List.of(
+                template.formatted(description, conflict, conflictingLocks.getExclusive()));
+          } else if (areShared(conflictingLocks)) {
+            String locks = String.join(", ", conflictingLocks.getShared());
+            return List.of(template.formatted(description, conflict, locks));
+          }
+        } else if (this == LOCK_SHARED) {
+          // include only exclusive locks if the lock operation attempted was shared
+          return List.of(
+              template.formatted(description, conflict, conflictingLocks.getExclusive()));
+        }
+      }
       return List.of(template.formatted(description, conflict));
     }
     return List.of();
+  }
+
+  public List<String> getErrorDetails() {
+    return getErrorDetails(null);
   }
 
   /**
@@ -42,5 +66,17 @@ public enum LockOperation {
    */
   public boolean lockAttempted() {
     return isLock;
+  }
+
+  private boolean isEmpty(ResourceLocks locks) {
+    return !isExclusive(locks) && !areShared(locks);
+  }
+
+  private boolean isExclusive(ResourceLocks locks) {
+    return locks.getExclusive() != null && !locks.getExclusive().isEmpty();
+  }
+
+  private boolean areShared(ResourceLocks locks) {
+    return locks.getShared() != null && !locks.getShared().isEmpty();
   }
 }

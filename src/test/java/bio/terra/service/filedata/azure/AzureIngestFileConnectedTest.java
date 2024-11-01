079afaeafca33c15070cb1fa87e3ce494d3fcea1
Shelby Holden <sholden@broadinstitute.org>
2024-11-01 07:53:35 -0400

    DT-956: Re-enable Azure tests (#1846)
    Revert "[DCJ-826] Temporarily disable Azure tests (#1839)"
    
    This reverts commit ae0ee4737fd5517a8ceaf8e4dc0b9f86c20a011d.

--- src/test/java/bio/terra/service/filedata/azure/AzureIngestFileConnectedTest.java
+++ src/test/java/bio/terra/service/filedata/azure/AzureIngestFileConnectedTest.java
@@ -46,7 +46,6 @@ import java.util.stream.Stream;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
@@ -66,7 +65,6 @@ import org.springframework.test.context.junit4.SpringRunner;
 @ActiveProfiles({"google", "connectedtest"})
 @Category(Connected.class)
 @EmbeddedDatabaseTest
-@Ignore("DCJ-826: Temporarily disabled due to missing Azure resources")
 public class AzureIngestFileConnectedTest {
   private static final Logger logger = LoggerFactory.getLogger(AzureIngestFileConnectedTest.class);
 

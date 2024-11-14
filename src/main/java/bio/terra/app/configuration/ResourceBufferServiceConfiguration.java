138620db124fb84d8b5f7adc8b235d01782b91ab
rjohanek <rjohanek@broadinstitute.org>
2024-11-14 11:44:53 -0500

    rm references to rbs sa credentials

--- src/main/java/bio/terra/app/configuration/ResourceBufferServiceConfiguration.java
+++ src/main/java/bio/terra/app/configuration/ResourceBufferServiceConfiguration.java
@@ -2,8 +2,6 @@ package bio.terra.app.configuration;
 
 import com.google.auth.oauth2.AccessToken;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.auth.oauth2.ServiceAccountCredentials;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -16,14 +14,10 @@ public record ResourceBufferServiceConfiguration(
   // I think we'd want to re-use our app scopes.
   private static final List<String> BUFFER_SCOPES = List.of("openid", "email", "profile");
 
-  // TODO - not sure if this is actually how we want to do this, just copying wsm's implementation
-  // for now
   public String getAccessToken() throws IOException {
-    try (FileInputStream fileInputStream = new FileInputStream(clientCredentialFilePath)) {
-      GoogleCredentials credentials =
-          ServiceAccountCredentials.fromStream(fileInputStream).createScoped(BUFFER_SCOPES);
-      AccessToken token = credentials.refreshAccessToken();
-      return token.getTokenValue();
-    }
+    GoogleCredentials credentials =
+        GoogleCredentials.getApplicationDefault().createScoped(BUFFER_SCOPES);
+    AccessToken token = credentials.refreshAccessToken();
+    return token.getTokenValue();
   }
 }

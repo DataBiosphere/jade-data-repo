834fb98a982b3e01ef9afbda11b65c48940a9bc8
Shelby Holden <sholden@broadinstitute.org>
2024-11-21 13:41:44 -0500

    Couple of tests and then placeholders for (I think) every case we need to cover
    Basically, any error case that can be reached in datasetRequestValidator.validateTable() needs to be able to hand both a create dataset request or an update dataset schema request.
    
    The tests pass if you switch from "schema" to "changes"

--- src/test/java/bio/terra/service/dataset/DatasetSchemaUpdateValidatorTest.java
+++ src/test/java/bio/terra/service/dataset/DatasetSchemaUpdateValidatorTest.java
@@ -26,6 +26,7 @@ import bio.terra.service.snapshotbuilder.SnapshotBuilderService;
 import java.util.List;
 import java.util.UUID;
 import org.apache.commons.lang3.StringUtils;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -43,6 +44,7 @@ import org.springframework.test.web.servlet.MvcResult;
 @ContextConfiguration(
     classes = {
       DatasetSchemaUpdateValidator.class,
+      DatasetRequestValidator.class,
       DatasetsApiController.class,
       ApiValidationExceptionHandler.class
     })
@@ -50,34 +52,46 @@ import org.springframework.test.web.servlet.MvcResult;
 @Tag(Unit.TAG)
 class DatasetSchemaUpdateValidatorTest {
 
-  @Autowired private MockMvc mvc;
+  @Autowired
+  private MockMvc mvc;
 
-  @MockBean private JobService jobService;
-  @MockBean private DatasetService datasetService;
-  @MockBean private IamService iamService;
-  @MockBean private FileService fileService;
-  @MockBean private AuthenticatedUserRequestFactory authenticatedUserRequestFactory;
-  @MockBean private SnapshotBuilderService snapshotBuilderService;
-  @MockBean private IngestRequestValidator ingestRequestValidator;
-  @MockBean private AssetModelValidator assetModelValidator;
-  @MockBean private DataDeletionRequestValidator dataDeletionRequestValidator;
-  @MockBean private DatasetRequestValidator datasetRequestValidator;
+  @MockBean
+  private JobService jobService;
+  @MockBean
+  private DatasetService datasetService;
+  @MockBean
+  private IamService iamService;
+  @MockBean
+  private FileService fileService;
+  @MockBean
+  private AuthenticatedUserRequestFactory authenticatedUserRequestFactory;
+  @MockBean
+  private SnapshotBuilderService snapshotBuilderService;
+  @MockBean
+  private IngestRequestValidator ingestRequestValidator;
+  @MockBean
+  private AssetModelValidator assetModelValidator;
+  @MockBean
+  private DataDeletionRequestValidator dataDeletionRequestValidator;
 
   @BeforeEach
   void setup() throws Exception {
     when(ingestRequestValidator.supports(any())).thenReturn(true);
-    when(datasetRequestValidator.supports(any())).thenReturn(true);
     when(assetModelValidator.supports(any())).thenReturn(true);
     when(dataDeletionRequestValidator.supports(any())).thenReturn(true);
   }
 
   private ErrorModel expectBadDatasetUpdateRequest(DatasetSchemaUpdateModel datasetRequest)
       throws Exception {
+    return expectBadDatasetUpdateRequest(TestUtils.mapToJson(datasetRequest));
+  }
+
+  private ErrorModel expectBadDatasetUpdateRequest(String datasetRequest) throws Exception {
     MvcResult result =
         mvc.perform(
                 post("/api/repository/v1/datasets/{id}/updateSchema", UUID.randomUUID())
                     .contentType(MediaType.APPLICATION_JSON)
-                    .content(TestUtils.mapToJson(datasetRequest)))
+                    .content(datasetRequest))
             .andExpect(status().is4xxClientError())
             .andReturn();
 
@@ -154,21 +168,94 @@ class DatasetSchemaUpdateValidatorTest {
   @Test
   void testNoColumns() throws Exception {
     String newTableName = "new_table";
-    String newTableColumnName = "new_table_column";
     DatasetSchemaUpdateModel updateModel =
         new DatasetSchemaUpdateModel()
-            .description("column addition tests")
+            .description("No columns with new table")
+            .changes(
+                new DatasetSchemaUpdateModelChanges()
+                    .addTables(List.of(DatasetFixtures.tableModel(newTableName, List.of()))));
+    ErrorModel errorModel = expectBadDatasetUpdateRequest(updateModel);
+    assertThat(
+        "Required column with new table throws error",
+        errorModel.getErrorDetail().get(0),
+        containsString("IncompleteSchemaDefinition"));
+  }
+
+  @Test
+  void testInvalidDatatypeColumns() throws Exception {
+    // Load bad data type from JSON b/c couldn't set it in the model
+    String json = TestUtils.loadJson("update-schema-bad-data-type.json");
+    ErrorModel errorModel = expectBadDatasetUpdateRequest(json);
+    String responseBody = String.join(", ", errorModel.getErrorDetail());
+    assertThat(
+        "Invalid DataTypes are logged and returned",
+        responseBody,
+        Matchers.containsString(
+            "invalid datatype in table column(s): bad_column, "
+                + "DataTypes must be lowercase, valid DataTypes are [string, boolean, bytes, date, datetime, dirref, fileref, "
+                + "float, float64, integer, int64, numeric, record, text, time, timestamp]"));
+  }
+
+  @Test
+  void testDuplicateColumns() throws Exception {
+    String newTableName = "new_table";
+    DatasetSchemaUpdateModel updateModel =
+        new DatasetSchemaUpdateModel()
+            .description("No columns with new table")
             .changes(
                 new DatasetSchemaUpdateModelChanges()
                     .addTables(
                         List.of(
-                            DatasetFixtures.tableModel(newTableName, List.of(newTableColumnName)),
                             DatasetFixtures.tableModel(
-                                newTableName, List.of(newTableColumnName)))));
+                                newTableName, List.of("column1", "column1")))));
     ErrorModel errorModel = expectBadDatasetUpdateRequest(updateModel);
     assertThat(
-        "Required column throws error",
+        "Required column with new table throws error",
         errorModel.getErrorDetail().get(0),
-        containsString("DuplicateTableNames"));
+        containsString("DuplicateColumnNames"));
+  }
+
+  @Test
+  void testMissingPrimaryKeyColumn() {
+
+  }
+
+  @Test
+  void testOptionalPrimaryKeyColumn() {
+
+  }
+
+  @Test
+  void testInvalidColumnMode() {
+
+  }
+
+  @Test
+  void testMissingDatePartitionOptions() {
+
+  }
+
+  @Test
+  void testInvalidDatePartitionColumnType() {
+
+  }
+
+  @Test
+  void testInvalidDatePartitionColumnName() {
+
+  }
+
+  @Test
+  void testMissingIntPartitionOptions() {
+  }
+
+  @Test
+  void testMissingIntPartitionColumnName() {
+
+  }
+
+  @Test
+  void testInvalidIntPartitionColumnType() {
+
   }
 }

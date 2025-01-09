a5acff4cee9fb778aa6d998ac0b4b1918f5d69ff
rjohanek <rjohanek@broadinstitute.org>
2025-01-09 13:38:25 -0500

    update documentation, rm pgcrypto extension

--- scripts/init-db/postgres-init.sql
+++ scripts/init-db/postgres-init.sql
@@ -6,5 +6,3 @@ ALTER DATABASE datarepo OWNER to drmanager;
 CREATE DATABASE stairway;
 GRANT ALL PRIVILEGES ON DATABASE stairway to drmanager;
 ALTER DATABASE stairway OWNER to drmanager;
-\c datarepo
-CREATE EXTENSION IF NOT EXISTS pgcrypto;

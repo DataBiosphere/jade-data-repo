2cf8a03add3c252c032ae2a0fc40f93356824815
Phil Shapiro <pshapiro@broadinstitute.org>
2025-01-09 12:10:54 -0500

    migrate profile controller test to use WebMvcTest

--- src/test/java/bio/terra/service/profile/ProfileAPIControllerTest.java
+++ src/test/java/bio/terra/service/profile/ProfileAPIControllerTest.java
@@ -7,7 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
@@ -17,6 +16,7 @@ import static org.mockito.Mockito.when;
 
 import bio.terra.app.configuration.ApplicationConfiguration;
 import bio.terra.common.category.Unit;
+import bio.terra.common.fixtures.AuthenticationFixtures;
 import bio.terra.common.iam.AuthenticatedUserRequest;
 import bio.terra.common.iam.AuthenticatedUserRequestFactory;
 import bio.terra.model.BillingProfileModel;
@@ -34,88 +34,80 @@ import bio.terra.service.auth.iam.PolicyMemberValidator;
 import bio.terra.service.auth.iam.exception.IamForbiddenException;
 import bio.terra.service.job.JobService;
 import bio.terra.service.profile.exception.ProfileNotFoundException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.http.HttpServletRequest;
 import java.util.UUID;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
-@ExtendWith(MockitoExtension.class)
+@ActiveProfiles({"google", "unittest"})
+@ContextConfiguration(
+    classes = {
+      ProfileApiController.class,
+      ProfileRequestValidator.class,
+      ProfileUpdateRequestValidator.class,
+      PolicyMemberValidator.class
+    })
 @Tag(Unit.TAG)
+@WebMvcTest
 class ProfileAPIControllerTest {
+  @MockBean private ProfileService profileService;
+  @MockBean private AuthenticatedUserRequestFactory authenticatedUserRequestFactory;
+  @MockBean private JobService jobService;
 
-  @Mock private ObjectMapper objectMapper;
-  @Mock private HttpServletRequest request;
-  @Mock private ProfileService profileService;
-  @Mock private ProfileRequestValidator billingProfileRequestValidator;
-  @Mock private ProfileUpdateRequestValidator profileUpdateRequestValidator;
-  @Mock private PolicyMemberValidator policyMemberValidator;
-  @Mock private AuthenticatedUserRequestFactory authenticatedUserRequestFactory;
-  @Mock private JobService jobService;
+  @MockBean private IamService iamService;
+  @MockBean private ApplicationConfiguration applicationConfiguration;
 
-  @Mock private IamService iamService;
-  @Mock private ApplicationConfiguration applicationConfiguration;
-
-  private ProfileApiController apiController;
-  private AuthenticatedUserRequest user;
+  @Autowired ProfileApiController apiController;
+  private static final AuthenticatedUserRequest TEST_USER =
+      AuthenticationFixtures.randomUserRequest();
 
   @BeforeEach
   void setup() {
-    apiController =
-        new ProfileApiController(
-            objectMapper,
-            request,
-            profileService,
-            billingProfileRequestValidator,
-            profileUpdateRequestValidator,
-            policyMemberValidator,
-            jobService,
-            authenticatedUserRequestFactory,
-            iamService,
-            applicationConfiguration);
-    user =
-        AuthenticatedUserRequest.builder()
-            .setSubjectId("DatasetUnit")
-            .setEmail("dataset@unit.com")
-            .setToken("token")
-            .build();
+    when(authenticatedUserRequestFactory.from(any())).thenReturn(TEST_USER);
   }
 
   @Test
   void testCreateProfile() {
-    when(authenticatedUserRequestFactory.from(request)).thenReturn(user);
-    var billingProfileRequestModel = new BillingProfileRequestModel();
+    var billingProfileRequestModel =
+        new BillingProfileRequestModel().profileName("profileName").biller("biller");
     String jobId = "jobId";
-    when(profileService.createProfile(billingProfileRequestModel, user)).thenReturn("jobId");
+    when(profileService.createProfile(billingProfileRequestModel, TEST_USER)).thenReturn(jobId);
 
     var jobModel = new JobModel();
     jobModel.setJobStatus(JobStatusEnum.RUNNING);
-    when(jobService.retrieveJob(jobId, user)).thenReturn(jobModel);
+    when(jobService.retrieveJob(jobId, TEST_USER)).thenReturn(jobModel);
 
     ResponseEntity<JobModel> entity = apiController.createProfile(billingProfileRequestModel);
     assertThat("Correct job model is returned from request", entity.getBody(), is(jobModel));
   }
 
+  private static BillingProfileUpdateModel createUpdateModel() {
+    return new BillingProfileUpdateModel()
+        .id(UUID.randomUUID())
+        .billingAccountId("id")
+        .description("description");
+  }
+
   @Test
   void testUpdateProfile() {
-    when(authenticatedUserRequestFactory.from(request)).thenReturn(user);
-    var billingProfileUpdateModel = new BillingProfileUpdateModel().id(UUID.randomUUID());
+    var billingProfileUpdateModel = createUpdateModel();
     String jobId = "jobId";
-    when(profileService.updateProfile(billingProfileUpdateModel, user)).thenReturn(jobId);
+    when(profileService.updateProfile(billingProfileUpdateModel, TEST_USER)).thenReturn(jobId);
 
     var jobModel = new JobModel();
     jobModel.setJobStatus(JobStatusEnum.RUNNING);
-    when(jobService.retrieveJob(jobId, user)).thenReturn(jobModel);
+    when(jobService.retrieveJob(jobId, TEST_USER)).thenReturn(jobModel);
 
     ResponseEntity<JobModel> entity = apiController.updateProfile(billingProfileUpdateModel);
     assertThat("Correct job model is returned from request", entity.getBody(), is(jobModel));
@@ -123,27 +115,27 @@ class ProfileAPIControllerTest {
 
   @Test
   void testUpdateProfileNotFound() {
-    UUID profileId = UUID.randomUUID();
-    doThrow(ProfileNotFoundException.class).when(profileService).getProfileByIdNoCheck(profileId);
-    var billingProfileUpdateModel = new BillingProfileUpdateModel().id(profileId);
+    var billingProfileUpdateModel = createUpdateModel();
+    doThrow(ProfileNotFoundException.class)
+        .when(profileService)
+        .getProfileByIdNoCheck(billingProfileUpdateModel.getId());
     assertThrows(
         ProfileNotFoundException.class,
         () -> apiController.updateProfile(billingProfileUpdateModel));
     verifyNoInteractions(iamService);
-    verify(profileService, never()).updateProfile(billingProfileUpdateModel, user);
+    verify(profileService, never()).updateProfile(billingProfileUpdateModel, TEST_USER);
   }
 
   @Test
   void testUpdateProfileForbidden() {
-    when(authenticatedUserRequestFactory.from(request)).thenReturn(user);
-    UUID profileId = UUID.randomUUID();
+    var billingProfileUpdateModel = createUpdateModel();
+    UUID profileId = billingProfileUpdateModel.getId();
     when(profileService.getProfileByIdNoCheck(profileId))
         .thenReturn(new BillingProfileModel().id(profileId));
     mockProfileForbidden(profileId, IamAction.UPDATE_BILLING_ACCOUNT);
-    var billingProfileUpdateModel = new BillingProfileUpdateModel().id(profileId);
     assertThrows(
         IamForbiddenException.class, () -> apiController.updateProfile(billingProfileUpdateModel));
-    verify(profileService, never()).updateProfile(billingProfileUpdateModel, user);
+    verify(profileService, never()).updateProfile(billingProfileUpdateModel, TEST_USER);
   }
 
   @ParameterizedTest
@@ -152,10 +144,9 @@ class ProfileAPIControllerTest {
       boolean deleteCloudResources,
       int expectedAdminAuthNumberOfInvocations,
       int expectedSpendProfileAuthNumberOfInvocations) {
-    when(authenticatedUserRequestFactory.from(any())).thenReturn(user);
-    UUID deleteId = UUID.fromString("aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee");
+    UUID deleteId = UUID.randomUUID();
     String jobId = "jobId";
-    when(profileService.deleteProfile(deleteId, deleteCloudResources, user)).thenReturn(jobId);
+    when(profileService.deleteProfile(deleteId, deleteCloudResources, TEST_USER)).thenReturn(jobId);
     var applicationId = "broad-jade-dev";
     if (deleteCloudResources) {
       when(applicationConfiguration.getResourceId()).thenReturn(applicationId);
@@ -163,17 +154,16 @@ class ProfileAPIControllerTest {
 
     var jobModel = new JobModel();
     jobModel.setJobStatus(JobStatusEnum.RUNNING);
-    when(jobService.retrieveJob(jobId, user)).thenReturn(jobModel);
+    when(jobService.retrieveJob(jobId, TEST_USER)).thenReturn(jobModel);
 
     ResponseEntity<JobModel> entity = apiController.deleteProfile(deleteId, deleteCloudResources);
     // Only check for admin auth if deleteCloudResources is true
     verify(iamService, times(expectedAdminAuthNumberOfInvocations))
-        .verifyAuthorization(
-            eq(user), eq(IamResourceType.DATAREPO), eq(applicationId), eq(IamAction.DELETE));
+        .verifyAuthorization(TEST_USER, IamResourceType.DATAREPO, applicationId, IamAction.DELETE);
     // Only check if user has access on the spend profile if we're not doing the admin check
     verify(iamService, times(expectedSpendProfileAuthNumberOfInvocations))
         .verifyAuthorization(
-            user, IamResourceType.SPEND_PROFILE, deleteId.toString(), IamAction.DELETE);
+            TEST_USER, IamResourceType.SPEND_PROFILE, deleteId.toString(), IamAction.DELETE);
     assertThat("Correct job model is returned from delete request", entity.getBody(), is(jobModel));
   }
 
@@ -188,29 +178,26 @@ class ProfileAPIControllerTest {
     assertThrows(
         ProfileNotFoundException.class, () -> apiController.deleteProfile(profileId, false));
     verifyNoInteractions(iamService);
-    verify(profileService, never()).deleteProfile(profileId, false, user);
+    verify(profileService, never()).deleteProfile(profileId, false, TEST_USER);
   }
 
   @Test
   void testDeleteProfileForbidden() {
-    when(authenticatedUserRequestFactory.from(request)).thenReturn(user);
     UUID profileId = UUID.randomUUID();
     when(profileService.getProfileByIdNoCheck(profileId))
         .thenReturn(new BillingProfileModel().id(profileId));
     mockProfileForbidden(profileId, IamAction.DELETE);
     assertThrows(IamForbiddenException.class, () -> apiController.deleteProfile(profileId, false));
-    verify(profileService, never()).deleteProfile(profileId, false, user);
+    verify(profileService, never()).deleteProfile(profileId, false, TEST_USER);
   }
 
   @Test
   void testAddProfilePolicyMember() {
-    when(authenticatedUserRequestFactory.from(any())).thenReturn(user);
-
     UUID id = UUID.fromString("aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee");
     String policyName = "policyName";
-    var policyMemberRequest = new PolicyMemberRequest();
+    var policyMemberRequest = new PolicyMemberRequest().email("email");
     var policyModel = new PolicyModel();
-    when(profileService.addProfilePolicyMember(id, policyName, policyMemberRequest, user))
+    when(profileService.addProfilePolicyMember(id, policyName, policyMemberRequest, TEST_USER))
         .thenReturn(policyModel);
 
     ResponseEntity<PolicyResponse> response =
@@ -223,6 +210,7 @@ class ProfileAPIControllerTest {
   private void mockProfileForbidden(UUID profileId, IamAction action) {
     doThrow(IamForbiddenException.class)
         .when(iamService)
-        .verifyAuthorization(user, IamResourceType.SPEND_PROFILE, profileId.toString(), action);
+        .verifyAuthorization(
+            TEST_USER, IamResourceType.SPEND_PROFILE, profileId.toString(), action);
   }
 }

65fddbf85589a7b5460577444cde8fc1c3d2eb1f
Phil Shapiro <pshapiro@broadinstitute.org>
2025-01-16 15:27:52 -0500

    fix issue where exception couldn't be deserialized

--- src/main/java/bio/terra/service/resourcemanagement/exception/BigQueryAclExhaustionException.java
+++ src/main/java/bio/terra/service/resourcemanagement/exception/BigQueryAclExhaustionException.java
@@ -1,6 +1,10 @@
 package bio.terra.service.resourcemanagement.exception;
 
 public class BigQueryAclExhaustionException extends GoogleResourceException {
+  public BigQueryAclExhaustionException(String message) {
+    super(message);
+  }
+
   public BigQueryAclExhaustionException(String message, Throwable cause) {
     super(message, cause);
   }

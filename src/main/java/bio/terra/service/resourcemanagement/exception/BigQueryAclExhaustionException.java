7060bd639122d7a2231d6c5e1a1363b4ed2a6b14
Phil Shapiro <pshapiro@broadinstitute.org>
2025-01-23 12:00:41 -0500

    revert test changes

--- src/main/java/bio/terra/service/resourcemanagement/exception/BigQueryAclExhaustionException.java
+++ src/main/java/bio/terra/service/resourcemanagement/exception/BigQueryAclExhaustionException.java
@@ -1,6 +1,8 @@
 package bio.terra.service.resourcemanagement.exception;
 
-public class BigQueryAclExhaustionException extends GoogleResourceException {
+import bio.terra.common.exception.BadRequestException;
+
+public class BigQueryAclExhaustionException extends BadRequestException {
   // This constructor is required so this can be deserialized using StairwayExceptionSerializer.
   public BigQueryAclExhaustionException(String message) {
     super(message);

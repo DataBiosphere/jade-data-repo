508327005041eb72837844a929d54b10194a7f2b
rjohanek <rjohanek@broadinstitute.org>
2025-02-28 14:53:23 -0500

    update flight test to check constructor of step

--- src/test/java/bio/terra/service/dataset/flight/inheritsteward/EnableInheritStewardFlightTest.java
+++ src/test/java/bio/terra/service/dataset/flight/inheritsteward/EnableInheritStewardFlightTest.java
@@ -3,12 +3,15 @@ package bio.terra.service.dataset.flight.inheritsteward;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.mockConstruction;
+import static org.mockito.Mockito.when;
 
 import bio.terra.common.FlightTestUtils;
 import bio.terra.common.category.Unit;
 import bio.terra.service.auth.iam.IamAction;
 import bio.terra.service.auth.iam.IamResourceType;
+import bio.terra.service.dataset.DatasetDao;
 import bio.terra.service.job.JobMapKeys;
 import bio.terra.stairway.FlightMap;
 import java.util.UUID;
@@ -25,6 +28,7 @@ import org.springframework.context.ApplicationContext;
 class EnableInheritStewardFlightTest {
 
   @Mock private ApplicationContext context;
+  @Mock private DatasetDao datasetDao;
   private FlightMap inputParameters;
   private static final UUID DATASET_ID = UUID.randomUUID();
 
@@ -34,22 +38,18 @@ class EnableInheritStewardFlightTest {
     inputParameters.put(JobMapKeys.IAM_RESOURCE_TYPE.getKeyName(), IamResourceType.DATASET);
     inputParameters.put(JobMapKeys.IAM_RESOURCE_ID.getKeyName(), DATASET_ID);
     inputParameters.put(JobMapKeys.IAM_ACTION.getKeyName(), IamAction.SET_INHERIT_STEWARD);
-  }
-
-  @Test
-  void testCorrectStepsEnableInheritStewardFlight() {
-    var flight = new EnableInheritStewardFlight(inputParameters, context);
-    var steps = FlightTestUtils.getStepNames(flight);
-    assertThat(steps, contains("InheritStewardSetFlagStep"));
+    when(context.getBean(DatasetDao.class)).thenReturn(datasetDao);
   }
 
   @Test
   void testParametersForSetFlagStep() {
+    DatasetDao datasetDao = mock();
+    when(context.getBean(DatasetDao.class)).thenReturn(datasetDao);
     try (var mockedStep =
         mockConstruction(
             InheritStewardSetFlagStep.class,
             (mock, context) -> {
-              new EnableInheritStewardFlight(inputParameters, context);
+              assertThat((DatasetDao) context.arguments().get(0), equalTo(datasetDao));
               assertThat(
                   "The correct datasetId is passed to the step",
                   (UUID) context.arguments().get(1),
@@ -58,6 +58,10 @@ class EnableInheritStewardFlightTest {
                   "The correct boolean flag is passed to the step",
                   (boolean) context.arguments().get(2),
                   equalTo(true));
-            })) {}
+            })) {
+      var flight = new EnableInheritStewardFlight(inputParameters, context);
+      var steps = FlightTestUtils.getStepNames(flight);
+      assertThat(steps, contains("InheritStewardSetFlagStep"));
+    }
   }
 }

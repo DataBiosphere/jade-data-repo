771ab86ee78a9d9ecaf4bfc4d725ee8fabab0cfe
Phil Shapiro <pshapiro@broadinstitute.org>
2025-03-03 12:01:06 -0500

    update swagger version in WebConfig; add comment about updating the swagger to build.gradle

--- src/main/java/bio/terra/app/configuration/WebConfig.java
+++ src/main/java/bio/terra/app/configuration/WebConfig.java
@@ -36,6 +36,6 @@ public class WebConfig implements WebMvcConfigurer {
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry
         .addResourceHandler("/webjars/swagger-ui-dist/**")
-        .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui-dist/5.19.0/");
+        .addResourceLocations("classpath:/META-INF/resources/webjars/swagger-ui-dist/5.20.0/");
   }
 }

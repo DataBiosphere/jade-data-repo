b98100d7a277dcd2de07220e31dea9609ad107cd
Florian Boulnois <fboulnoi@broadinstitute.org>
2025-04-01 09:26:10 -0400

    fix: update swagger in WebConfig to 5.20.2

--- src/main/java/bio/terra/app/configuration/WebConfig.java
+++ src/main/java/bio/terra/app/configuration/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig implements WebMvcConfigurer {
   @Autowired private LoggerInterceptor loggerInterceptor;
   @Autowired private UserMetricsInterceptor metricsInterceptor;
 
-  public static final String SWAGGER_UI_VERSION = "5.20.1";
+  public static final String SWAGGER_UI_VERSION = "5.20.2";
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {

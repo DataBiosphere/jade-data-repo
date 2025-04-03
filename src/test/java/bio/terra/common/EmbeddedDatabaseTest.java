e1589dd113ce061462acb6676e45627ddbddfe44
Florian Boulnois <fboulnoi@broadinstitute.org>
2025-04-03 14:39:02 -0400

    feat: allow colima to be used for db tests

--- src/test/java/bio/terra/common/EmbeddedDatabaseTest.java
+++ src/test/java/bio/terra/common/EmbeddedDatabaseTest.java
@@ -1,6 +1,7 @@
 package bio.terra.common;
 
 import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+import io.zonky.test.db.provider.postgres.PostgreSQLContainerCustomizer;
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Inherited;
@@ -8,6 +9,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.testcontainers.containers.wait.strategy.Wait;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
@@ -18,4 +22,12 @@ import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabas
     provider = AutoConfigureEmbeddedDatabase.DatabaseProvider.DOCKER,
     beanName = "embeddedDataSource")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public @interface EmbeddedDatabaseTest {}
+public @interface EmbeddedDatabaseTest {
+  @Configuration
+  class PostgreSQLContainerCustomizerConfiguration {
+    @Bean
+    public PostgreSQLContainerCustomizer postgresContainerCustomizer() {
+      return container -> container.waitingFor(Wait.forListeningPort());
+    }
+  }
+}

66a8d18faacdbc720bce83c5c4bcc349e6db55de
Shelby Holden <sholden@broadinstitute.org>
2026-05-15 14:36:36 -0400

    clean up check

--- src/main/java/bio/terra/service/filedata/DrsService.java
+++ src/main/java/bio/terra/service/filedata/DrsService.java
@@ -658,9 +658,7 @@ public class DrsService {
       // If a userProject is explicitly passed in, then use that to sign the url.
       // Note: the expectation is that this is a Terra hosted bucket
       if (!StringUtils.isEmpty(userProject)) {
-        if (authUser != null
-            && authUser.getToken() != null
-            && StringUtils.isNotEmpty(authUser.getToken())) {
+        if (authUser != null && StringUtils.isNotEmpty(authUser.getToken())) {
           logger.info(
               "Signing URL via SAM for snapshot {} with userProject '{}' and token (starts with {} has length {})",
               cachedSnapshot.id,

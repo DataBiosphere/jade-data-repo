9acc21938b28f7d1812916848e8c7b4366f997e3
Shelby Holden <sholden@broadinstitute.org>
2026-05-15 14:54:04 -0400

    switch to 16

--- src/main/java/bio/terra/service/filedata/DrsService.java
+++ src/main/java/bio/terra/service/filedata/DrsService.java
@@ -663,7 +663,7 @@ public class DrsService {
               "Signing URL via SAM for snapshot {} with userProject '{}' and token (starts with {} has length {})",
               cachedSnapshot.id,
               userProject,
-              authUser.getToken().subSequence(0, Math.min(15, authUser.getToken().length())),
+              authUser.getToken().subSequence(0, Math.min(16, authUser.getToken().length())),
               authUser.getToken().length());
           return new DRSAccessURL()
               .url(samService.signUrlForBlob(authUser, userProject, gsPath, URL_TTL));

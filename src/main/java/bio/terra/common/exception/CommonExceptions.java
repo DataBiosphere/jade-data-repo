dbb2da34830052d17862535cdb45500b68df932c
Adam Nichols <aednichols@gmail.com>
2026-09-21 09:51:09 -0400

    `AZURE_NOT_SUPPORTED` exception

--- src/main/java/bio/terra/common/exception/CommonExceptions.java
+++ src/main/java/bio/terra/common/exception/CommonExceptions.java
@@ -6,4 +6,7 @@ public class CommonExceptions {
   public static final FeatureNotImplementedException TRANSACTIONS_NOT_IMPLEMENTED_IN_AZURE =
       new FeatureNotImplementedException(
           "Transaction support is not yet implemented for Azure backed datasets");
+
+  public static final FeatureNotImplementedException AZURE_NOT_SUPPORTED =
+      new FeatureNotImplementedException("Azure is no longer supported by TDR");
 }

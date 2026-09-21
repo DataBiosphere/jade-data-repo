2038af2e3295f71154b02753455af41f699f8510
Adam Nichols <aednichols@gmail.com>
2026-09-21 14:43:02 -0400

    Happy to `Supply` the right exception

--- src/main/java/bio/terra/common/exception/CommonExceptions.java
+++ src/main/java/bio/terra/common/exception/CommonExceptions.java
@@ -1,5 +1,7 @@
 package bio.terra.common.exception;
 
+import java.util.function.Supplier;
+
 /** Common exceptions that tend to be thrown in different areas */
 public class CommonExceptions {
 
@@ -9,4 +11,14 @@ public class CommonExceptions {
 
   public static final FeatureNotImplementedException AZURE_NOT_SUPPORTED =
       new FeatureNotImplementedException("Azure is no longer supported by TDR");
+
+  /**
+   * Supplier for the Azure branch of a {@link bio.terra.common.CloudPlatformWrapper#choose} fork:
+   * always throws {@link #AZURE_NOT_SUPPORTED}.
+   */
+  public static <T> Supplier<T> azureNotSupported() {
+    return () -> {
+      throw AZURE_NOT_SUPPORTED;
+    };
+  }
 }
